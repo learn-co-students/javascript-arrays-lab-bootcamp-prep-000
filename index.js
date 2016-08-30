b7a5a904c4bc5a29b34051c6e02fa85e46690b4a
Anthony Zeitlin <zeitlin1@gmail.com>
2016-08-30 00:15:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten() {
+  appendKitten = [...kittens, "Broom"]
+  return appendKitten
+}
+
+function prependKitten() {
+  prependKitten = ["Arnold",...kittens]
+  return prependKitten
+}
+
+
+function removeLastKitten() {
+  removeLastKitten = kittens.slice(0, kittens.length - 1)
+  return removeLastKitten
+}
+
+function removeFirstKitten() {
+  removeFirstKitten = kittens.slice(1)
+  return removeFirstKitten
+}
