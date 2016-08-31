0126adef1d87efa7e69cb244a146d08c95044e26
Colin McKean <ezikill@hotmail.com>
2016-08-31 15:43:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+}
+function appendKitten(name){
+var ourKittens = [...kittens,name];
+return ourKittens;
+}
+
+function prependKitten(name){
+  var ourKittens = [name,...kittens];
+  return ourKittens;
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1);
+}
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
