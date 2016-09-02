209bce07b0f903052259827e11855d79a0a79edd
Rachel Salois <rachel.salois@gmail.com>
2016-09-02 00:33:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  var temp = [...kittens, name]
+  return temp
+}
+
+function prependKitten(name){
+  var temp = [name, ...kittens]
+  return temp
+}
+
+function removeLastKitten(){
+  var temp = kittens.slice(0, kittens.length-1)
+  return temp
+}
+
+function removeFirstKitten(){
+  var temp = kittens.slice(1)
+return temp
+}
