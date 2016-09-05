c3e0f22f24a554742b19c7b869fe6d93f7526143
erika1066 <hastings.erika@gmail.com>
2016-09-05 17:58:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+
+// var window.kittens = ['Milo', 'Otis', 'Garfield'];
+
+var destructivelyAppendKitten = (array) => {
+  // var kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens.push('Ralph');
+  return array;
+}
+
+var destructivelyPrependKitten = (array) => {
+  kittens.unshift('Bob');
+  return array;
+}
+
+var destructivelyRemoveLastKitten = (array) => {
+  kittens.pop();
+  return array;
+}
+
+var destructivelyRemoveFirstKitten = (array) => {
+  kittens = kittens.slice(1);
+  return array;
+}
+
+var appendKitten = (array) => {
+  array = [...kittens, 'Broom'];
+  return array;
+}
+
+var prependKitten = (array) => {
+  array = ['Arnold', ...kittens];
+  return array;
+}
+
+var removeLastKitten = (array) => {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  array = [kittens.pop()];
+  return kittens;
+}
+
+var removeFirstKitten = (array) => {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  array = [kittens.shift()];
+  return kittens;
+}
