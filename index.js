523246dfe1f9a91e952b0ed43c50616b044a91ad
Ivan Collado <collado.ip@gmail.com>
2016-09-06 05:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+const kittens = []
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var tempArray = kittens;
+  return [...tempArray, name];
+}
+
+function prependKitten(name) {
+  var tempArray = kittens;
+  return [name, ...tempArray];
+}
+
+function removeLastKitten() {
+  var tempArray = kittens.slice(0, kittens.length - 1);
+  return tempArray;
+}
+
+function removeFirstKitten() {
+  var tempArray = kittens.slice(1);
+  return tempArray;
+}
