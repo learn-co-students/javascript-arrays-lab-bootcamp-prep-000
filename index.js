3f9fea6c54af740ec447d50843ada6878fea8952
Darren Paddock <darrenlpaddock@gmail.com>
2016-09-07 16:26:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = [...kittens, name];
+  return kittens;
+}
+
+function prependKitten(name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+function removeLastKitten() {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = kittens.slice(0, kittens.length - 1)
+  return kittens;
+}
+
+function removeFirstKitten() {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = kittens.slice(1);
+  return kittens;
+}
