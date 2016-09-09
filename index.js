82a063f38394ea05406182c1f49c72f8c3be89b0
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-09-09 12:24:26 +0000

    Done.

--- index.js
+++ index.js
@@ -34,3 +34,12 @@ var kittens = ["Milo", "Otis", "Garfield"]
   function prependKitten(element) {
   return [element, ...kittens]
 }
+var kittens = ["Milo", "Otis", "Garfield"]
+function removeFirstKitten() {
+  return kittens.slice(1)
+  }
+
+  var kittens = ["Milo", "Otis", "Garfield"]
+  function removeLastKitten() {
+    return kittens.slice(0, kittens.length - 1)
+    }
