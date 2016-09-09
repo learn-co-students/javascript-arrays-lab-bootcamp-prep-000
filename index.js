2020702d5de1e2767bc0c040dd3da6a9e7f57a12
Gopinath Ramasamy <gopinath.ramasamy@outlook.com>
2016-09-09 04:19:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+const kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+//kittens.pop();
+kittens.push(name);
+return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  //kittens.pop();
+   kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+ kittens.pop();
+ return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+ return kittens;
+}
+function prependKitten(name){
+  var kitten = [name, ...kittens];
+  return kitten;
+}
+function appendKitten(name){
+  var kitten = [...kittens, name];
+  return kitten;
+}
+function removeLastKitten(){
+ var kitten = kittens.slice(0, kittens.length-1);
+ //kitten.pop();
+ return kitten;
+}
+function removeFirstKitten(){
+  //var kitten = kittens;
+var kitten = kittens.slice(1);
+return kitten;
+}
