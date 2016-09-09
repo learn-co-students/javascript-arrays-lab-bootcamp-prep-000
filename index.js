87aa28c83ed10053c2584fa7c74eaaf5ceaf5be3
jagneesh malhotra <j.g.malhotra@gmail.com>
2016-09-09 23:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function kittens (){
+kittens = ["Milo", "Otis", "Garfield"];
+kittens
+}
+
+function destructivelyAppendKitten(name) {
+kittens.push(`${name}`);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(`${name}`);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var name = [...kittens, `${name}`]
+  return name
+}
+
+function prependKitten(name) {
+  var name = [`${name}`, ...kittens]
+  return name
+}
+
+function removeLastKitten() {
+ var name = kittens.slice(0, kittens.length-1);
+  return name;
+}
+
+function removeFirstKitten() {
+  var name = kittens.slice(1);
+  return name 
+}
