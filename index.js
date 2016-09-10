7dcee4e4b5161c0d05513034a871afe8fe760b7b
Laamia Islam <laamia.islam@gmail.com>
2016-09-10 21:54:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+
+}
+
+function prependKitten(name) {
+
+}
+
+function removeLastKitten() {
+
+}
+
+function removeFirstKitten() {
+
+}
