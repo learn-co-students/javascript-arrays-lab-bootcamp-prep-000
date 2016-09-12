d332b81ab741aaa87a23e1a91a7c50a90b477ee4
Christi Reid <christi@limerentfox.com>
2016-09-12 02:31:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 const app = "I don't do much."
+
+// create kittens array
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+// add kitten to end of original array
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+// add kitten to beginning of original array
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+// remove last kitten from original array
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+// remove first kitten from original array
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+// add kitten to end of new array
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+// prepends to new array
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+// remove last kitten without changing original array
+function removeLastKitten() {
+  var newKittens = kittens.slice(-1)
+  return newKittens
+}
+
+// remove last kitten without changing original array
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length-1)
+  return newKittens
+}
+
+// remove first kitten without changing original array
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
