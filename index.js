f0adc6ae92e3790db804f29cb75b2ec152a3201f
Rebecca Broughton <beckybroughton@me.com>
2016-09-13 19:32:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+
+var kittens = ["milo", "otis", "garfield"]
+
+function destructivelyAppendKitten (kitten)
+{
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name)
+{
+  var newKittens = [...kittens, name]
+  return newKittens;
+}
+
+function prependKitten(name)
+{
+  var newKittens = [name, ...kittens]
+  return newKittens;
+}
+
+function removeLastKitten()
+{
+  var newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens;
+}
+
+function removeFirstKitten()
+{
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
