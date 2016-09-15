b3c6ef8d4a3486c081c80cd74f0ba43b98a4fd4e
John Donnelly <raged48@yahoo.com>
2016-09-15 07:58:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+function kittens(){
+  var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+];}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, -1);
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
