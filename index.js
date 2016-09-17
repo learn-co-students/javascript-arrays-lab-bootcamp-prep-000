703f7c5509aafe0b6f97073b2b78fc695471363c
Jon Hjerting <jon@hjerting.com>
2016-09-17 21:41:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = function(name) {
+    kittens.push(name);
+    return kittens;
+}
+
+var destructivelyPrependKitten = function(name) {
+    kittens.unshift(name);
+    return kittens;
+}
+
+var destructivelyRemoveLastKitten = function() {
+    kittens.pop();
+    return kittens;
+}
+
+var destructivelyRemoveFirstKitten = function() {
+    kittens.shift();
+    return kittens;
+}
+
+var appendKitten = function(name) {
+    return [...kittens, name];
+}
+
+var prependKitten = function(name) {
+    return [name, ...kittens];
+}
+
+var removeLastKitten = function() {
+    return kittens.slice(0, -1);
+}
+
+var removeFirstKitten = function() {
+    return kittens.slice(1);
+}
