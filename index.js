ec6ead281702ae609f7393b1d6df1e3d741bed7d
Noorulain Khaliq <nkhaliq86@gmail.com>
2016-09-20 04:28:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name);
+  return kittens;
+}
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+var destructivelyRemoveLastKitten = function() {
+  kittens.pop();
+  return kittens;
+}
+var destructivelyRemoveFirstKitten = function() {
+  kittens.shift();
+  return kittens;
+}
+var appendKitten = function(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+var prependKitten = function(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+var removeLastKitten = function() {
+  var newArray  = kittens;
+  return newArray.slice(0,-1);
+}
+var removeFirstKitten = function() {
+  var newArray = kittens;
+  return newArray.slice(1);
+
+}
