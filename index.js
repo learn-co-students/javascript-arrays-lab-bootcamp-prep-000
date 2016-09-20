f713621e882f2c63a5ef78b72a39b2f1a14a936f
Byron Injeeli <byroninjeeli@gmail.com>
2016-09-20 20:32:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+	var nArray = [];
+	var i = 0;
+	while (i < kittens.length){
+		nArray.push(kittens[i]);
+		i += 1;
+	}
+
+	nArray.push(name);
+	return nArray;
+}
+
+function prependKitten(name){
+  var moreKittens = [name,...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten(){
+	var nArray = []
+	var i = 0
+	while (i < kittens.length){
+		nArray.push(kittens[i])
+		i += 1
+	}
+
+	nArray.pop();
+	return nArray;
+}
+
+function removeFirstKitten(){
+  var nArray = [];
+  var i = 0;
+  while (i < kittens.length){
+    nArray.push(kittens[i]);
+    i += 1;
+  }
+  nArray.shift();
+  return nArray;
+}
