5bc2f1e529f17ab9518cf16269800c1ea1c6ce16
Stan Aretinskiy <sr575sr@gmail.com>
2016-09-21 22:52:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function appendKitten(name) {
+  return [...kittens ,name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
