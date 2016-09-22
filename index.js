53b541c949001f5da63547c59f011f1ee5ebe4a5
Robert King <robert.k25@gmail.com>
2016-09-22 20:51:19 +0000

    Done.

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(-1);
+  return kittens.slice(0,-1);
 }
 
 function removeFirstKitten() {
-  return kittens.slice(0);
+  return kittens.slice(1);
 }
