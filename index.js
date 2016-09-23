a66d5d9bb7e8b508cd0c69be58239e6d8a6b8c51
Michael Chan <michael.chan212@gmail.com>
2016-09-23 02:16:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = [];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var akittens = [...kittens, name]
+  return akittens
+}
+
+function prependKitten(name) {
+  var pkittens = [name, ...kittens]
+  return pkittens
+}
+
+function removeLastKitten() {
+  var lastKitten = kittens.slice(0, 2)
+  return lastKitten
+}
+
+function removeFirstKitten() {
+  var firstKitten = kittens.slice(1)
+  return firstKitten
+}
