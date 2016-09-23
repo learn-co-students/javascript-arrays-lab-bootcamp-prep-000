5f9a072359e3736d30de371eb056a9ccf30736c9
Gabriela Pinto <gajspp@gmail.com>
2016-09-23 00:44:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name)
+}
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name)
+}
+var destructivelyRemoveLastKitten = function() {
+  kittens.pop()
+}
+var destructivelyRemoveFirstKitten = function() {
+  kittens.shift()
+}
+var appendKitten = function(name) {
+  return [...kittens, name]
+}
+var prependKitten = function(name) {
+  return [name, ...kittens]
+}
+var removeFirstKitten = function() {
+  return kittens.slice(1)
+}
+var removeLastKitten = function() {
+  return kittens.slice(0, kittens.length - 1)
+}
