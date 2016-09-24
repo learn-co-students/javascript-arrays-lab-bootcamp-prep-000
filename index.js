c52ee31320c5a042317d1c5f0fac2ce9048d736a
Luke O'Connor <luke.p.oconnor@gmail.com>
2016-09-24 05:03:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(newKitten) {
+  kittens.push(newKitten);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyPrependKitten(newKitten) {
+  kittens.unshift(newKitten);
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(newKitten) {
+  var newArray = [...kittens, newKitten]
+  return newArray;
+}
+
+function prependKitten(newKitten) {
+  var newArray = [newKitten, ...kittens]
+  return newArray;
+}
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0,2)
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(-2)
+  return newArray;
+}
