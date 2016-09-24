5d0781e098f5a1654b8e0765c039afe144713a73
timothy martel <t.m.martel@gmail.com>
2016-09-24 20:59:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1);
+}
