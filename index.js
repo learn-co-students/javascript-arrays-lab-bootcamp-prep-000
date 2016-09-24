a092a9abb987dd06f42818db6c3379da56455da1
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2016-09-24 23:35:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeFirstKitten(){
+  kittens.unshift;
+  return kittens;
+}
+
+function destructivelyAppendKitten(name){
+  kittens = [...kittens, name]
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens = [name, ...kittens]
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+    kittens.pop();
+    return kittens
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1);
+}
