239f21d8d7719a8074c805bb86b514f783e9afa3
Katie Baumez <kt995@hotmail.com>
2016-09-28 13:27:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-const app = "I don't do much."
+/*const app = "I don't do much.";*/
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var newArray = [];
+  return newArray=[...kittens, name];
+}
+
+function prependKitten(name){
+  var newArray2 = [];
+  return newArray2=[name, ...kittens];
+}
+
+function removeLastKitten(){
+  var newArray3 = [];
+  return newArray3 = [...kittens.slice(0, kittens.length-1)]
+}
+
+function removeFirstKitten(){
+  var newArray4 = [];
+  return newArray4 = [...kittens.slice(1, kittens.length)]
+}
+/*destructivelyAppendKitten("Smokey");
+
+var destructivelyPrependKitten = function(name){
+  kittens.unshift(name);
+};*/
+
+//some other comment
