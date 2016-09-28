5c1f71f43cb6ceead77e2ec246ff742e8cec5626
Evgen Pushnoy <pushnoyj@gmail.com>
2016-09-28 03:09:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  window.kittens.push("Ralph")
+  return window.kittens
+}
+
+function destructivelyPrependKitten(name) {
+  window.kittens.unshift("Bob");
+  return window.kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  window.kittens.pop()
+  return window.kittens
+}
+function destructivelyRemoveFirstKitten(){
+  window.kittens.shift()
+  return window.kittens
+}
+function appendKitten(name){
+  var kittens = [...window.kittens, name]
+  return kittens
+}
+
+function prependKitten(name){
+  var kittens = [name, ...window.kittens]
+  return kittens
+}
+function removeLastKitten(){
+  var kittens = window.kittens.slice(0, window.kittens.length - 1)
+  return kittens
+}
+function removeFirstKitten(){
+  var kittens = window.kittens.slice(1)
+  return kittens
+}
