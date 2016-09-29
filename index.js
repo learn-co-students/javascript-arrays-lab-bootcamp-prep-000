44acd4fde61e432fdb5344c15087e139661bfb1b
pressstart101 <walterscott404@yandex.com>
2016-09-29 18:39:13 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name){
+kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
+
+
+
+
+
+/*
+push() pushes elements onto the ends of arrays
+pop() pops them off; 
+unshift() adds elements to the beginnings of arrays, 
+andshift() pulls them off.
+*/
