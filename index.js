6becd43d0022e45a3ceed48d8dc7a6a973d7512e
Chelsey Blackmon <chelseyab@gmail.com>
2016-09-30 17:11:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+kittens.push(name);
+return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+kittens.unshift(name);
+return kittens;
+}
+function destructivelyRemoveLastKitten() {
+return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+return kittens.shift();
+}
+
+function appendKitten(name) {
+const moreKittens = [...kittens, name];
+return moreKittens;
+}
+
+function prependKitten(name) {
+const preKittens = [name, ...kittens]
+return preKittens;
+}
+
+function removeLastKitten() {
+const lessKittens = kittens.slice(0, kittens.length - 1);
+return lessKittens;
+}
+
+function removeFirstKitten() {
+const evenLessKittens = kittens.slice(1);
+return evenLessKittens;
+}
