4223aee95715b5faa6ac7c7d9938793f30ad4820
Alla Gorik <evstafevna87@gmail.com>
2016-10-04 01:11:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name)
+{kittens.push(name);
+return kittens;}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function removeLastKitten(name) {
+  var AAA = kittens.slice(0, kittens.length -1);
+  return AAA;
+}
+
+function removeFirstKitten(name) {
+  var AAA = kittens.slice(1);
+  return AAA;
+}
+
+function appendKitten(name) {
+  var A = [...kittens, name];
+  return A;
+}
+
+function prependKitten(name) {
+  var AA = [name, ...kittens];
+  return AA;
+}
