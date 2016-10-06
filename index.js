ac8881f80b7d90aea4f35ce66cb86e905298711b
Matthew Yack <mail2matthew@gmail.com>
2016-10-06 15:40:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
