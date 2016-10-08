c68067734b00d35f0297cc2a2d0583a07fecf757
Jason Ioannides <jason.ioannides@gmail.com>
2016-10-08 00:16:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+
+var kittens = [
+               "Milo",
+               "Otis",
+               "Garfield"
+               ];
+
+function destructivelyAppendKitten(name) {
+
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+
+  return kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+
+  return newArray;
+}
+
+function prependKitten(name) {
+
+  return [name, ...kittens];
+}
+
+
+function removeLastKitten(name) {
+  var solution = kittens.slice(0, 2);
+
+  return solution;
+}
+
+function removeFirstKitten(name) {
+  var solution = kittens.slice(1);
+
+  return solution;
+}
