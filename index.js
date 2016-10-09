6514f181e1113a553cf1be23a7af76dce3ea01ef
Aaron Garza <robertaarongarza@gmail.com>
2016-10-09 01:48:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var array = kittens.slice();
+  array.push(name);
+  return array;
+}
+
+function prependKitten(name) {
+  var array = kittens.slice();
+  array.unshift(name);
+  return array;
+}
+
+function removeLastKitten() {
+  var array = kittens.slice();
+  array.pop();
+  return array;
+}
+
+function removeFirstKitten() {
+  var array = kittens.slice();
+  array.shift();
+  return array
+}
