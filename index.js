3e7da80baf7175562ff9a2ece85615d83d7ed19f
Rachel Whaley <rachelwhaley24@gmail.com>
2016-10-12 03:50:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kit2 = [...kittens, name]
+  return kit2
+}
+
+function prependKitten(name) {
+  var kit2 = [name, ...kittens]
+  return kit2
+}
+
+function removeLastKitten() {
+  var kit2 = kittens.slice(0,kittens.length-1)
+  return kit2
+}
+
+function removeFirstKitten() {
+  var kit2 = kittens.slice(1, kittens.length)
+  return kit2
+}
