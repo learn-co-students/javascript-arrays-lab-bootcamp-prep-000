e24f4272e1607742465ec134adc7433ffe95b0e9
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-14 06:21:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+var kittens
+var destructivelyAppendKitten=(k)=>kittens.push(k)
+var destructivelyPrependKitten=(k)=>kittens.unshift(k)
+var destructivelyRemoveLastKitten=()=>kittens.pop()
+var destructivelyRemoveFirstKitten=()=>kittens.shift()
+var appendKitten=(k)=>[...kittens,k]
+var prependKitten=(k)=>[k,...kittens]
+var removeLastKitten=()=>kittens.slice(0,kittens.length-1)
+var removeFirstKitten=()=>kittens.slice(1)
