12ab8b7c8059e0d17fffcefdbdd43105133c94ca
Islam Zulfigarov <azerinator@gmail.com>
2016-10-19 13:28:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function appendKitten(name){
+var updated_kitten=[...kittens, name];
+return updated_kitten;
+}
+
+function prependKitten(name){
+  var updated_kitten=[name, ...kittens]
+  return updated_kitten;
+}
+
+function removeLastKitten() {
+var updated_kitten=kittens.slice(0, kittens.length-1);
+return updated_kitten;
+}
+
+function removeFirstKitten() {
+  var updated_kitten=kittens.slice(1);
+  return updated_kitten;
+}
