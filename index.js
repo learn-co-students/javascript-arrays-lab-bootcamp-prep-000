d63f8588285c3be77a50201f0e23ba6c044687c8
Brett Brading <bbbrading@gmail.com>
2016-10-19 14:00:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+    return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+    return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+    return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+    return kittens
+}
+
+function appendKitten(name){
+  const morekittens = [ ...kittens, name]
+    return morekittens
+}
+
+function prependKitten(name){
+  const onemorekitten = [name, ...kittens]
+    return onemorekitten
+}
+
+function removeLastKitten(){
+  const byelastkitten = kittens.slice(0, kittens.length - 1)
+    return byelastkitten
+}
+
+function removeFirstKitten(){
+  const byefirstkitten = kittens.slice(1)
+    return byefirstkitten
+}
