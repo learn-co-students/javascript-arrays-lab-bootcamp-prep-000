11fa3fd03b64d3e8c9d9060d870e7e1dbd8ce533
Christopher Gupta <gupta.christopher@gmail.com>
2016-10-21 15:14:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens = kittens.slice(0, kittens.length - 1)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens = kittens.slice(1)
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
