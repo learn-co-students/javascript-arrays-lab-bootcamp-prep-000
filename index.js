3022474accf36386173e4a41dd2ffe7fc45cb216
Vanessa Jimenez <vjimenez127@gmail.com>
2016-10-23 22:19:45 +0000

    Done.

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(1);
-  return kittens; 
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
 }
