2d12d1029acdffaebfc11e8968f4fc9a4d8eb48a
Xuebing Wang <xbwang93@gmail.com>
2016-10-24 20:14:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kitten){
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(kitten){
+  return kittens.pop(kitten)
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+  return kittens.shift(kitten)
+}
+
+function appendKitten(kitten){
+  return [...kittens, kitten]
+}
+
+function prependKitten(kitten){
+  return [kitten, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0 , kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1 , kittens.length)
+}
