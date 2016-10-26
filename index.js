906f42e6a5a41bb3ca3e17378160634468694d79
Nate Trick <trick.nate@gmail.com>
2016-10-26 12:11:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+function appendKitten(name){
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function prependKitten(name){
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+function removeLastKitten(){
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray
+}
+function removeFirstKitten(){
+  var newArray = kittens.slice(1)
+  return newArray
+}
