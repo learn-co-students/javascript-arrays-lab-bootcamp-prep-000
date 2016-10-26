32d0c8e9f66c2803f882a8d7ce9187c7f0a2ea2b
Joe Browm <jaybrizle@gmail.com>
2016-10-26 11:47:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
