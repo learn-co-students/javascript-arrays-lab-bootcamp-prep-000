881d80b6bc3844f46b31cc9f6449a24f41929ba2
jason brown <j.grant.brown@gmail.com>
2016-10-28 18:29:13 +0000

    Done.

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens= kittens.slice(0, kittens.length -1);
-  return kittens
+  lesskittens= kittens.slice(0, kittens.length -1);
+  return lesskittens
 }
 function removeFirstKitten(){
-  kittens=kittens.slice(1);
-  return kittens
+  lesskittens=kittens.slice(1);
+  return lesskittens
 }
