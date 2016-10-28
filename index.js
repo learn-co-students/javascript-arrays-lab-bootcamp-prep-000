952e955b07c278c19b269b76365f74342b39b21a
jason brown <j.grant.brown@gmail.com>
2016-10-28 18:33:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name){
-
-return kittens.shift(name);
+kittens.push(name);
+return kittens
 }
 
 function destructivelyPrependKitten(name){
