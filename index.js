465423ef984358e069e91d8ccc64364656247e7f
Viswada Yangala <yangalav@go.stockton.edu>
2016-10-29 17:11:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  var arr = [...kittens, name];
+  return arr;
+}
+
+function prependKitten(name){
+  var arr = [name, ...kittens];
+  return arr;
+}
+
+function removeLastKitten() {
+  var arr = kittens.slice(0, kittens.length - 1);
+  return arr;
+}
+
+function removeFirstKitten() {
+  var arr = kittens.slice(1);
+  return arr;
+}
