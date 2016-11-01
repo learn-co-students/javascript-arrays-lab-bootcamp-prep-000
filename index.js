2b9c8e7ceb1c84888cb6ad465543681447e11450
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-01 17:17:19 +0000

    Done.

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function removeLastKitten(name) {
   return kittens.slice(0,2)
 }
 
-function removeFirstKitten(name) {
-  return kittens.slice(0, kittens.length -1)
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
