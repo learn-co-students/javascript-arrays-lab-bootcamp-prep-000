f286e189d0a84a9b884de67a6d968bca8e366d7b
Andrew Wick <a.wick17@gmail.com>
2016-11-04 14:36:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kittenss = [...kittens, name]
+  return kittenss
+}
+
+function prependKitten(name) {
+  var kittenss = [name, ...kittens]
+  return kittenss
+}
+
+function removeLastKitten() {
+  var len = kittens.length
+  var kittenss = kittens.slice(0, len - 1)
+  return kittenss
+}
+
+function removeFirstKitten() {
+  var kittenss = kittens.slice(1)
+  return kittenss
+}
