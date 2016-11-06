da5c692c23f9bd2ac220c54d51b291d4c61cc429
Ryan Schoolfield <rp0school@gmail.com>
2016-11-06 01:33:17 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var kitten = kittens.push(name)
-  return kitten;
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
