0df924dc77dcf097caa4ade4821a282516a22b6b
Daniel Seehausen <daniel.seehausen@gmail.com>
2016-11-09 20:54:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+//var kittens = array(['Milo', 'Otis', 'Garfield']);
+
+function destructivelyAppendKitten(name) {
+    kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+    kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+    kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+    kittens.shift();
+}
+
+function appendKitten(name) {
+    var kits = kittens.slice()
+    kits.push(name)
+    return kits
+}
+
+function prependKitten(name) {
+    var kits = kittens.slice()
+    kits.unshift(name)
+    return kits
+}
+
+function removeLastKitten() {
+    var kits = kittens.slice();
+    kits.pop();
+    return kits
+}
+
+function removeFirstKitten() {
+    var kits = kittens.slice();
+    kits.shift();
+    return kits
+}
