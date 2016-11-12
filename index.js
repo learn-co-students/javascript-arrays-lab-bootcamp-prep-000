80c579be5da249ad2d700c431071a04bded3829f
Patrick Ylitalo <paylitalo@gmail.com>
2016-11-12 19:27:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var newKittens
+  return newKittens = [...kittens, name]
+}
+
+function prependKitten(name){
+  var newKitten; return newKitten = [name, ...kittens]
+}
+
+function removeLastKitten(){
+  var newKitten; return newKitten = kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+  var newKitten; return newKitten = kittens.slice(1,kittens.length)
+}
