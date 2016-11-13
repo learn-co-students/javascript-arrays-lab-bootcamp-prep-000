92664c6272bcfd9d79a7b26118c05335dfb9a2d5
Stefan Culafic <culaficstefan@gmail.com>
2016-11-13 00:22:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+function kit(){
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+kit();
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+kit();
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+kit();
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+kit();
+function appendKitten(name){
+  var array = [...kittens, name]
+  return array;
+}
+function prependKitten(name){
+  var array = [name, ...kittens]
+  return array;
+}
+function removeLastKitten(){
+  var array = kittens.slice(0, kittens.length - 1)
+  return array;
+}
+function removeFirstKitten(){
+  var array = kittens.slice(1, kittens.length)
+  return array;
+}
