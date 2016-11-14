16d2d574c725539a2a8eaf864da981fe88e8e996
Clark moss <cmoss@mail2business.com>
2016-11-14 03:53:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+var kitten =  [ 'Milo', 'Otis', 'Garfield' ]
+kittens .push  ('Ralph')
+return kitten ;
+
+}
+function  destructivelyPrependKitten(name) {
+var kitten =  [ 'Milo', 'Otis', 'Garfield' ]
+kittens .unshift ("Bob")
+return  kitten
+
+}
+function destructivelyRemoveLastKitten(){
+var kitten = [ 'Milo', 'Otis', 'Garfield' ]
+kittens .pop("Garfield")
+return kitten
+}
+
+function destructivelyRemoveFirstKitten(){
+var kitten = [ 'Milo', 'Otis', 'Garfield' ]
+kittens.shift('Milo')
+return kitten
+
+}
+function appendKitten(name) {
+var kitten = ['Milo' ,'Otis', 'Garfield']
+kitten = [...kitten,'Broom']
+return kitten
+
+}
+ function  prependKitten(name) {
+var kitten = ["Milo", "Otis", "Garfield"]
+kitten = ['Arnold',...kitten]
+return kitten
+
+   }
+
+function removeLastKitten() {
+var kitten =["Milo", "Otis", "Garfield"]
+kitten = kitten.slice(0,kitten.length -1)
+return kitten
+ }
+
+ function removeFirstKitten() {
+ var kitten = ["Milo", "Otis", "Garfield"]
+kitten= kitten.slice(1)
+return kitten
+ }
