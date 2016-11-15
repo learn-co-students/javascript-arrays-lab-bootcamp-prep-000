99c7deaba64ea4a717f92e1019b51c65c2cab9f4
Catherine Luse <catherine.luse@gmail.com>
2016-11-15 03:35:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name);
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, -1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
