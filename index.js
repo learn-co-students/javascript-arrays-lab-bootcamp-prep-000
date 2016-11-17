a7f8ae6b71900f10902d03f71e765499a5b11978
Devon Weldon <devon.w.weldon@gmail.com>
2016-11-17 02:16:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArr = [...kittens, name];
+  return newArr;
+}
+
+function prependKitten(name){
+  var newArr2 = [name, ...kittens];
+  return newArr2;
+}
+
+function removeLastKitten(){
+  var newArr3 = kittens.slice(0, kittens.length-1);
+  return newArr3;
+}
+
+function removeFirstKitten(){
+  var newArr4 = kittens.slice(1);
+  return newArr4;
+}
