08b9fec9977e46b12ba3702c914c58d23208dea3
Michael Lane <mikeyd.lane@gmail.com>
2016-11-20 04:39:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name)
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
