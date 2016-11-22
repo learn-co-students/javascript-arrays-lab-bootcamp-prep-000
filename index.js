cd791918391bd5e2634b6b60e952cf9df55a9547
Naomi Meyer <naomiemeyer@gmail.com>
2016-11-22 23:16:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+  }
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+  }
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+  }
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+  }
+function appendKitten(name){
+  var appKit = [...kittens, 'Broom'];
+  return appKit;
+  }
+function prependKitten(name){
+  var prepKit = ['Arnold', ...kittens];
+  return prepKit;
+  }
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length -1);
+  }
+function removeFirstKitten(){
+  return kittens.slice(-2);
+  }
