0c9f60b0fbaf03885170129a3743cf792f1ccde8
Pak Belalang <opblang@yandex.com>
2016-11-24 16:00:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+let kittens = ["Milo", "Otis", "Garfield"]
+
+let destructivelyAppendKitten = x => {
+  kittens.push(x)
+  return kittens
+}
+
+let destructivelyPrependKitten = x => {
+  kittens.unshift(x)
+  return kittens
+}
+
+let destructivelyRemoveLastKitten = () => {
+  kittens.pop()
+  return kittens
+}
+
+let destructivelyRemoveFirstKitten = () => {
+  kittens.shift()
+  return kittens
+}
+
+let appendKitten = x => {
+  let xs = kittens.slice()
+  xs.push(x)
+  return xs
+}
+
+let prependKitten = x => {
+  let xs = kittens.slice()
+  xs.unshift(x)
+  return xs
+}
+
+let removeLastKitten = () => {
+  let xs = kittens.slice()
+  xs.pop()
+  return xs
+}
+
+let removeFirstKitten = () => {
+  let xs = kittens.slice()
+  xs.shift()
+  return xs
+}
