a9cfb990e9b0ed1510ede3d346e2418393dbc8ed
Michael Cacciano <mbcacciano@gmail.com>
2016-11-25 17:26:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+};
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+};
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+};
+
+function appendKitten(name) {
+  var newKittensArray = [...kittens, name];
+  return newKittensArray;
+};
+
+function prependKitten(name) {
+  var newKittensArray = [name, ...kittens];
+  return newKittensArray;
+};
+
+function removeLastKitten() {
+  var newKittensArray = kittens.slice(0, kittens.length - 1);
+  return newKittensArray;
+};
+
+function removeFirstKitten() {
+  var newKittensArray = kittens.slice(1);
+  return newKittensArray;
+};
