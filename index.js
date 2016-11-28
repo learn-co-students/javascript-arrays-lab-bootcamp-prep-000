e835b062deddb8e27be2314b5d7ed21268819fcd
Bernard Maling <bernardmalingio@gmail.com>
2016-11-28 08:33:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return name
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return name
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return name
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return name
+}
+
+function appendKitten(name){
+  var cats = [name]
+  cats = [...kittens, name]
+  return cats
+}
+
+function prependKitten(name){
+  var cats = [name]
+  cats = [name, ...kittens]
+  return cats
+}
+
+function  removeLastKitten(name){
+  var cats = [name]
+  cats = kittens.slice(0, kittens.length - 1)
+  return cats
+}
+
+function removeFirstKitten(name){
+  var cats = [name]
+  cats = kittens.slice(1)
+  return cats
+}
