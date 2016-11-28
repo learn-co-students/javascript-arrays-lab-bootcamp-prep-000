793360b2d3972ae0865e8176012cb6b36a804e0d
Jeremy Tie <jeremyhlt@hotmail.com>
2016-11-28 09:21:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = name =>
+{
+  kittens.push(name);
+  return kittens;
+}
+
+var destructivelyPrependKitten = name =>
+{
+  kittens.unshift(name);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = () =>
+{
+  kittens.pop();
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = () =>
+{
+  kittens.shift()
+  return kittens;
+}
+
+var prependKitten = name =>
+{
+  return [name, ...kittens];
+}
+
+var appendKitten = name =>
+{
+  return [...kittens, name];
+}
+
+var removeLastKitten = () =>
+{
+  return kittens.slice(0, kittens.length - 1);
+}
+
+var removeFirstKitten = () =>
+{
+  return kittens.slice(1);
+}
