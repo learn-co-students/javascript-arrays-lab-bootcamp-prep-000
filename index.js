6a6fd351be0239ecc11b2452737397538bca3b35
Kelly McCutchen <kmccutchen123@gmail.com>
2016-11-28 06:21:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 const app = "I don't do much."
+
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+
+function appendKitten(name) {
+//  var ar = ["Milo", "Otis", "Garfield"]
+
+  var ar = [name]
+  ar = [...kittens, name]
+  //ar.push(name)
+
+  return ar
+}
+
+
+function prependKitten(name) {
+
+  var ar = [name, ...kittens]
+  return ar
+
+}
+
+function removeLastKitten() {
+  var ar = [...kittens]
+  ar.pop()
+  return ar
+}
+
+function removeFirstKitten() {
+  var ar = [...kittens]
+  ar.shift()
+  return ar
+
+}
