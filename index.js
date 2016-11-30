781016580176c92f22114caaa6b431c76a30959c
Molly Gelsey <mag242@gmail.com>
2016-11-30 22:48:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+    return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.concat(name);
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
