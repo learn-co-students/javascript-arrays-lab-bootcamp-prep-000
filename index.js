deb33a86dde4a06be16bd43bf7e87b8f8871a8dd
Aidan Bane <aidanfbane@gmail.com>
2016-12-02 02:48:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens];
+  newArray.push(name);
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = [...kittens];
+  newArray.unshift(name);
+  return newArray;
+}
+
+function removeLastKitten() {
+  var nArray = [...kittens];
+  nArray.pop();
+  return nArray;
+}
+
+function removeFirstKitten() {
+  var nArray = [...kittens];
+  nArray.shift();
+  return nArray;
+}
