0fe88e2d270968ba43440eb9bb26070c3f001c97
Germaine Pannell <glpannell@icloud.com>
2016-12-07 03:39:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+};
+
+function appendKitten(name) {
+  var otherKittens = [];
+  kittens.forEach(function(el) {
+    otherKittens.push(el);
+  });
+  otherKittens.push(name);
+  return otherKittens;
+};
+
+function prependKitten(name) {
+  var otherKittens = [];
+  otherKittens.unshift(name);
+  kittens.forEach(function(el) {
+    otherKittens.push(el);
+  });
+  return otherKittens;
+};
+
+function removeLastKitten() {
+  var filterOffLast = function(el, index, array) {
+    if(index !== (array.length - 1)) {
+      return el;
+    }
+  };
+  var otherKittens = kittens.filter(filterOffLast);
+  return otherKittens;
+};
+
+function removeFirstKitten() {
+  var filterOffFirst = function(el, index, array) {
+    if(index !== 0){
+      return el;
+    }
+  };
+  var otherKittens = kittens.filter(filterOffFirst);
+  return kittens.filter(filterOffFirst);
+};
