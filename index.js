60aa83faab70a1ebd73e6a04efa77591f552f948
Darnell Gillead <dlgillead@gmail.com>
2016-12-07 02:32:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+};
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+};
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+};
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, 2);
+  return newKittens;
+};
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+};
