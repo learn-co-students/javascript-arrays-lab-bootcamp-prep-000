1702eefd6d5718b6d9e29630cae81efd8a645af8
Brian Bohme <bohmebrian@gmail.com>
2016-12-11 19:24:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var cats = new Array();
+  cats = [...kittens, name];
+  return cats
+}
+
+function prependKitten(name) {
+  var cats = new Array();
+  cats = [name, ...kittens];
+  return cats
+}
+
+function removeLastKitten() {
+  var cats = new Array();
+  cats = kittens.slice(0, (kittens.length - 1));
+  return cats
+}
+
+function removeFirstKitten() {
+  var cats = new Array();
+  cats = kittens.slice(1);
+  return cats
+}
