81d8028fb7d4b5fd5ddcc5de18c0f851679c4aaa
Scott Plunkett <scottyplunkett@gmail.com>
2016-12-15 23:24:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+};
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+};
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+};
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+};
+function appendKitten(name) {
+  var array = [...kittens, name]
+  return array
+};
+function prependKitten(name) {
+  var array = [name, ...kittens]
+  return array
+};
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+};
+function removeFirstKitten() {
+  return kittens.slice(1)
+};
