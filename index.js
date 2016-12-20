1b30badcead2f94da6510bfc459c3a25808e3345
Shelly Xu <xushell@gmail.com>
2016-12-20 04:41:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function kittens (){
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens;
+}
+
+
+function destructivelyAppendKitten(name){
+  var a = kittens;
+  a.push(name);
+  return a;
+}
+
+function destructivelyPrependKitten(name){
+  var a = kittens;
+  a.unshift(name);
+  return a;
+}
+
+function destructivelyRemoveLastKitten(){
+  var a = kittens;
+  a.pop();
+  return a;
+}
+
+function destructivelyRemoveFirstKitten(){
+  var a = kittens;
+  a.shift();
+  return a;
+}
+
+function appendKitten(name){
+var a = kittens;
+a = [...a, name];
+return a;
+}
+
+function prependKitten(name){
+var a = kittens;
+a = [name, ...a];
+return a;
+}
+
+function removeLastKitten(){
+var a = kittens;
+a = a.slice(0,a.length-1);
+return a;
+}
+
+function removeFirstKitten(){
+var a = kittens;
+a = a.slice(1);
+return a;
+}
