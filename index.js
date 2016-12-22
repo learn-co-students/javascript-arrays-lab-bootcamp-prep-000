f4b2a277db8aece37320bfce34c289402705f87d
Saad Ahmed <dropheaven@live.com>
2016-12-22 03:33:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 const app = "I don't do much."
+
+var kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+
+  return kittens;
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+
+  return kittens;
+}
+
+function appendKitten (name) {
+  return [...kittens, name];
+}
+
+function prependKitten (name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten () {
+  var newKittens = [
+    'Milo',
+    'Otis',
+    'Garfield'
+  ];
+
+  newKittens.pop();
+
+  return newKittens;
+}
+
+function removeFirstKitten () {
+  var newKittens = [
+    'Milo',
+    'Otis',
+    'Garfield'
+  ];
+
+  newKittens.shift();
+
+  return newKittens;
+}
