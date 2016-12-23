7ff7fe910490f4109bb65f9f6b846d4172f23d90
IKSHVAKU SINGAPURAM <ixs21870@ucmo.edu>
2016-12-23 09:37:45 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ var kittens =["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
 
-  return [kittens,name];
+  return kittens.push(name);
 
+}
+
+function destructivelyRemoveLastKitten(){
+
+  return kittens.pop();
+}
+
+ function destructivelyPrependKitten(name){
+
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveFirstKitten(){
+
+  return kittens.shift(name);
+}
+
+function appendKitten(name)
+{
+
+  return [...kittens,name];
+
+
+
+}
+
+function prependKitten(name){
+  return [name,...kittens];
+}
+function removeLastKitten(){
+
+return kittens.slice(0,kittens.length-1);
+}
+function removeFirstKitten(){
+  return kittens.slice(1,kittens.length);
 }
