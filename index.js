b61d9a5a34e279abd5b38a9e6f283bc57bc16413
James Langford <derivative7117@live.com>
2016-12-25 03:32:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  return kittens.unshift();
+}
+function destructivelyAppendKitten(name){
+    return kittens.push();
+}
+function  destructivelyRemoveLastKitten(name){
+    return kittens.pop();
+}
+function destructivelyRemoveFirstKitten(name){
+    return kittens.shift();
+}
+funtion prependKitten(name){
+   return [kittens,...name]
+}
