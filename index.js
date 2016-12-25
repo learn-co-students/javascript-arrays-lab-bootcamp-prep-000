6eee3cde750993840cb741e6ada1458fb2fa2410
James Langford <derivative7117@live.com>
2016-12-25 04:36:29 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendKitten(name){
   return kittens.unshift();
 }
 function destructivelyAppendKitten(name){
-    return kittens.push();
+    return kittens.push(name);
 }
 function  destructivelyRemoveLastKitten(name){
     return kittens.pop();
@@ -13,6 +13,22 @@ function  destructivelyRemoveLastKitten(name){
 function destructivelyRemoveFirstKitten(name){
     return kittens.shift();
 }
-funtion prependKitten(name){
-   return [kittens,...name]
+function prependKitten(name){
+   return [name,...kittens]
+}
+function appendKitten(name){
+   return [...kittens,name];
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+function removeLastKitten(name){
+  return kittens.pop();
+}
+function removeFirstKitten(name){
+  return kittens.slice(1);
+}
+
+function removeLastKitten(name){
+  return kittens.slice(0,kittens.length  - 1);
 }
