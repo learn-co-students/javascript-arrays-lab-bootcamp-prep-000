0c0ec6f61c539d930e7315fed2286aa79f487072
Gerardo Portela <gportela@gmail.com>
2016-12-28 04:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function removeFirstKitten()
+{
+  var firstkitty=kittens.slice(1)
+  return firstkitty
+}
+function removeLastKitten()
+{
+  var lastkitten=kittens.slice(0,kittens.length-1)
+  return lastkitten
+}
+
+function appendKitten(name)
+{
+  return [...kittens, name]
+}
+
+function prependKitten(name)
+{
+  return [name,...kittens]
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens=kittens.slice(1)
+  return kittens
+}
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+  return kittens
+}
