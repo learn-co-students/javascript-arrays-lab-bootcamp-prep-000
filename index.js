81c5ccae36fb1c6c22d1acaea5eb2008ebcabf0d
Luis F. Estevez <luis.fdez.e@gmail.com>
2016-12-29 22:10:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(){
+  kittens= [...kittens, "Ralph"]
+  return kittens;
+}
+
+function destructivelyPrependKitten(){
+  kittens = ["Bob", ...kittens]
+  return kittens;
+}
+
+function appendKitten(){
+
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function prependKitten(){
+  const moreKittens = ["Arnold",...kittens];
+  return moreKittens;
+}
+function appendKitten(){
+  const moreKittens = [...kittens,"Broom"];
+  return moreKittens;
+}
+
+function removeLastKitten(){
+  const moreKittens = kittens.slice(0, kittens.length - 1);
+  return moreKittens;
+}
+
+function removeFirstKitten(){
+  const moreKittens = kittens.slice(1, kittens.length);
+  return moreKittens;
+}
