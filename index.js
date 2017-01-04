dd734900738695a952be7d63bec4098a14e360f1
Elvis Delahoz <elvisdelahoz@gmail.com>
2017-01-04 01:19:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+const app = "I don't do much."; /*
+destructivelyAppendKitten(name) {
+
+} // appends a kitten to the end of the kittens array
+destructivelyAppendKitten("kittenName1");
+
+destructivelyPrependKitten(name) {
+  
+} // prepends a kitten to the beginning of the kittens array: 
+destructivelyPrependKitten("kittenName1");
+destructivelyRemoveLastKitten() {
+  
+} // removes the last kitten from the kittens array:  
+destructivelyRemoveLastKitten("deadKitname");
+destructivelyRemoveFirstKitten() {
+  
+} // removes the First kitten from the kittens array
+destructivelyRemoveFirstKitten("kitname");
+appendKitten(name) {
+  
+} // appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+appendKitten("newkit");
+prependKitten(name) {
+  
+} // prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+prependKitten("addkit");
+removeLastKitten(kitwhat) {
+  
+} // removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+removeLastKitten("kitwhat");
+removeFirstKitten(kitwhat) {
+  
+} // removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
+removeFirstKitten("kitwhat");
+*/
