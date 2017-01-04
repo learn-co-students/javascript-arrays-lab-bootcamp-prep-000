17270827895a45b053354b64d908de616c0ee4f9
Elvis Delahoz <elvisdelahoz@gmail.com>
2017-01-04 02:25:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const app = "I don't do much."; 
-var kittens = ["Milo", "Otis", "Garfield"]
-var name = "";
-destructivelyAppendKitten(name) { 
-  kittens.push(`name`);
+// const app = "I don't do much."; 
+var kittens = ["Milo", "Otis", "Garfield"];
+// name = "";
+function destructivelyAppendKitten(name) { 
+  kittens.push(name);
   return kittens;
 } // appends a kitten to the end of the kittens array
-destructivelyAppendKitten("endKittenName"); /*
-
-destructivelyPrependKitten(name) {
-  
+destructivelyAppendKitten("Ralph"); 
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
 } // prepends a kitten to the beginning of the kittens array: 
-destructivelyPrependKitten("kittenName1");
-destructivelyRemoveLastKitten() {
-  
-} // removes the last kitten from the kittens array:  
-destructivelyRemoveLastKitten("deadKitname");
-destructivelyRemoveFirstKitten() {
-  
-} // removes the First kitten from the kittens array
-destructivelyRemoveFirstKitten("kitname");
-appendKitten(name) {
-  
+destructivelyPrependKitten("Bob"); 
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+} // Function removes the last kitten from the kittens array. No argument
+destructivelyRemoveLastKitten();  
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;  
+} // Function removes the First kitten from the kittens array. No argument.
+destructivelyRemoveFirstKitten(); 
+function appendKitten(name) {
+  tmparrary = [...kittens, `name`];
+  return tmparrary;
 } // appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
-appendKitten("newkit");
+appendKitten("Broom"); /*
 prependKitten(name) {
   
 } // prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
