3830e6ada0bf3cb6ae69a9af154faa581ac1c20e
Elvis Delahoz <elvisdelahoz@gmail.com>
2017-01-04 02:52:00 +0000

    Done.

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) { 
   kittens.push(name);
   return kittens;
-} // appends a kitten to the end of the kittens array
+} // appends a kitten to the end of the kittens array (append Ralph)
 destructivelyAppendKitten("Ralph"); 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
-} // prepends a kitten to the beginning of the kittens array: 
+} // prepends a kitten to the beginning of the kittens array(unshift Bob) 
 destructivelyPrependKitten("Bob"); 
 function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
-} // Function removes the last kitten from the kittens array. No argument
-destructivelyRemoveLastKitten();  
+} // Function removes the last kitten from the kittens array (Ralph). No argument
+destructivelyRemoveLastKitten(); 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;  
-} // Function removes the First kitten from the kittens array. No argument.
-destructivelyRemoveFirstKitten(); 
+} // Function removes the First kitten from the kittens array(shift Bob) No argument.
+destructivelyRemoveFirstKitten();  
 function appendKitten(name) {
-  tmparrary = [...kittens, `name`];
-  return tmparrary;
+  myarray = [...kittens, `name`];
+  var mynewarray = myarray;
+  return mynewarray;
 } // appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
 appendKitten("Broom"); /*
-prependKitten(name) {
+function prependKitten(name) {
   
 } // prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
 prependKitten("addkit");
