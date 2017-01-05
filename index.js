55c64d9ec1c3ca85acc0bb9cad47614495e7aedc
Ursh Khan <urshkhan@gmail.com>
2017-01-05 05:20:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
-var kittens = ['Milo', 'Otis', 'Garfield']
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name)
 {
   kittens.push(name)
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyPrependKitten(name)
 {
   kittens.unshift(name)
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyRemoveLastKitten()
 {
   kittens.pop()
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyRemoveFirstKitten()
 {
   kittens.shift()
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function appendKitten(name)
 {
   return [...kittens, name]
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function prependKitten(name)
 {
   return [name, ...kittens]
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function removeLastKitten()
 {
   return kittens.slice(0, kittens.length -1)
 }
 
-var kittens = ['Milo', 'Otis', 'Garfield']
 
 function removeFirstKitten()
 {
