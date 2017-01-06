bb14a01e2e278adb1e28dde5a08ef8ebc87f3dab
Haim Khaymovich <haim46@gmail.com>
2017-01-06 01:44:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+var kittens=["Milo", "Otis", "Garfield"]
+kittens;
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+destructivelyAppendKitten("Ralph")
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+destructivelyPrependKitten("Bob")
+
+function destructivelyRemoveLastKitten() {
+kittens.pop()
+return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+
+}
+
+function appendKitten(name){
+  var kitten = [...kittens,name]
+         return kitten
+}
+appendKitten("Broom")
+
+function prependKitten(name){
+  var kitten=[name,...kittens]
+         return kitten
+}
+prependKitten("Arnold")
+
+function removeLastKitten(){
+    var kitten=kittens.slice(0,kittens.length-1)
+         return kitten
+}
+removeLastKitten()
+function removeFirstKitten(){
+  var kitten=kittens.slice(1)
+       return kitten
+}
+removeFirstKitten()
