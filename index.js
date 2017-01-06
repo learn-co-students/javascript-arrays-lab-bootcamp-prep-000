26f8bb68a203ef3e646fa7ee69a23ebc1a2c52ce
O programm <goprocode@gmail.com>
2017-01-06 17:21:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendKitten(name){
+   kittens.push(name);
+   return kittens ;
+ }
+destructivelyAppendKitten("Ralph");
+
+
+function destructivelyPrependKitten(name){
+kittens.unshift(name);
+return kittens ;
+}
+destructivelyPrependKitten("Bob");
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+return kittens ;
+}
+
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+return kittens ;
+}
+
+function appendKitten(name){
+var newArray = [...kittens, name];
+return newArray ;
+}
+appendKitten("Broom");
+
+function prependKitten(name){
+var newArray = [name, ...kittens];
+return newArray ;
+}
+prependKitten("Arnold");
+
+function removeLastKitten(){
+var change = kittens.slice(0, kittens.length -1);
+return change ;
+}
+
+function removeFirstKitten(){
+var change = kittens.slice(1);
+return change;
+}
