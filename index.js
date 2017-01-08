04e346649365cf1095d4a3ef80e85c9284b40976
Leibel Hecht <leibelhecht@gmail.com>
2017-01-08 06:18:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name){
+  var newArr =  [...kittens, name];
+  return newArr;
+}
+function prependKitten(name){
+  var newArr = [name, ...kittens];
+  return newArr;
+}
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1);
+}
