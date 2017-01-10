87c8a7650354b440b4ccc1cd45fe434d3c010637
Kelsey Huse <kelseyhuse30@gmail.com>
2017-01-10 01:57:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = function(name){
+  kittens.push(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyPrependKitten = function(name){
+  kittens.unshift(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyRemoveLastKitten = function(){
+  kittens.pop();
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyRemoveFirstKitten = function(){
+  kittens.shift();
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var appendKitten = function(name){
+  return [...kittens, name];
+}
+
+var prependKitten = function(name){
+  return [name, ...kittens];
+}
+
+var removeLastKitten = function(){
+  return kittens.slice(0, kittens.length - 1);
+}
+
+var removeFirstKitten = function(){
+  return kittens.slice(1);
+}
