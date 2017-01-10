791c3be5677256f9b381dbca3081527f1de14a99
Sekllan ChenRuan <xirenchen94@gmail.com>
2017-01-10 19:38:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function  destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
