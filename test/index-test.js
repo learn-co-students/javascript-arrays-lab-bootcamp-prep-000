65201f7976e208f7bd905c0988523482b2ca843a
Kevin Niemeyer <kevin_niemeyer@msn.com>
2017-01-11 23:30:29 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,6 @@ describe('Arrays', function() {
   beforeEach(function() {
     window.kittens = ['Milo', 'Otis', 'Garfield'];
   });
-
   describe('kittens', function() {
     it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
