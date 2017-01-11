95aeb2c39ac72b306cffe31e2e7d23c18a2786a1
John Cibula <johnjcibula@gmail.com>
2017-01-11 23:48:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name){
+kittens.push(name);
+};
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+};
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+};
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+};
+function appendKitten (name) {
+  var newKitten = [kittens + name];
+  return newKitten;
+};
