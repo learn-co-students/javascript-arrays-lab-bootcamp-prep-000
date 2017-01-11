253f1ec8b9f06cb1f1c95b9670204fe721494286
Carlos Quinones <cquinones100@gmail.com>
2017-01-11 15:52:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten(name){
+  var newArray = kittens.slice(0, -1);
+  return newArray;
+}
+
+function removeFirstKitten(name){
+  var newArray = kittens.slice(1);
+  return newArray;
+}
