037ebad1a86f33b2bb57890d76ab318344e99125
John Cibula <johnjcibula@gmail.com>
2017-01-12 00:04:27 +0000

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ function destructivelyRemoveFirstKitten(){
   kittens.shift();
 };
 function appendKitten (name) {
-  var newKitten = [kittens + name];
-  return newKitten;
+   return [...kittens,name]
+};
+function prependKitten (name) {
+  return [name, ...kittens];
+};
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length - 1);
+};
+function removeFirstKitten(){
+  return kittens.slice(1);
 };
