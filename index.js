afeca6091c84212efe926fbd84d276cc8e5de83b
Derrick Chu <dchu74@gmail.com>
2017-01-12 00:31:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
-const app = "I don't do much."
-
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name){
-
-kittens.push("Ralph")
-return kittens;
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
-
-  kittens.unshift("Bob")
+  kittens.unshift("Bob");
   return kittens;
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop()
+  kittens.pop();
   return kittens;
 }
 
 function destructivelyRemoveFirstKitten() {
-  kittens.shift()
+  kittens.shift();
   return kittens;
-
 }
 
 function appendKitten(name) {
-  return [...kittens, name];
+  return [...kittens, "Broom"];
 }
 
 function prependKitten(name) {
-  return [name, ...kittens];
+  return ["Arnold", ...kittens];
 }
 
 function removeLastKitten() {
-  return ["Milo","Otis"];
-
+  return kittens.slice(0, kittens.length-1);
 }
 
 function removeFirstKitten() {
-  return ["Otis", "Garfield"];
+  return kittens.slice(1);
 }
