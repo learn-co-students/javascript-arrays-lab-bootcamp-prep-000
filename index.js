f0d9da7c1540f21ae5a317e98f21de3661c4c202
Banu Yilmaz <paksoybanu83@gmail.com>
2017-01-12 21:12:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name){
+  var newArr = [...kittens, name];
+  return newArr;
+}
+function prependKitten(name){
+ var newArr = [name, ...kittens];
+ return newArr;
+}
+function removeLastKitten(){
+  var newArr= kittens.slice(0, kittens.length-1);
+  return newArr;
+}
+function removeFirstKitten(){
+  var newArr = kittens.slice(1);
+  return newArr;
+}
