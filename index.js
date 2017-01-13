9967c6f200aa590d1860122b63f1eb583f8947e9
Matt Jackson <matjack9@gmail.com>
2017-01-13 20:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+};
+kittens.pop();
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+};
+kittens.shift();
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+};
+kittens.push("Garfield");
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+};
+kittens.unshift("Milo");
+
+function appendKitten(name) {
+  return [...kittens, name]
+};
+
+function prependKitten(name) {
+  return [name, ...kittens]
+};
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+};
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+};
