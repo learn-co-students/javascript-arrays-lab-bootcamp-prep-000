79c9c811fc825f7ea47953616831bbfbbe51acc2
Phillip Fine <phillipfine@yahoo.com>
2017-01-16 23:47:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {kittens.push(name)}
+function destructivelyPrependKitten(name) {kittens.unshift(name)}
+function destructivelyRemoveLastKitten(name) {kittens.pop(name)}
+function destructivelyRemoveFirstKitten(name) {kittens.shift(name)}
+function appendKitten(name) {var newArray = [...kittens, name]; return newArray}
+function prependKitten(name) {var newArray1 = [name, ...kittens]; return newArray1}
+function removeLastKitten() {var newArray2 = kittens.slice(0,- 1); return newArray2}
+function removeFirstKitten() {var newArray3 = kittens.slice(1); return newArray3}
