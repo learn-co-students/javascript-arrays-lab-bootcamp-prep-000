f835b6deaef26842c4423a54a4c2dfddf5ff3a35
Elianna O'Shea <elianna.ros@gmail.com>
2017-01-17 21:53:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeFirstKitten() {
+  kittens.slice(0)
+  return kittens
+}
+
+function removeLastKitten(name) {
+  name = kittens.slice(0, kittens.length - 1)
+  return name
+}
+
+function removeFirstKitten(name) {
+  name = kittens.slice(1)
+  return name
+}
