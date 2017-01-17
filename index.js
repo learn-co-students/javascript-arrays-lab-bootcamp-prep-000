cd2e824820ce5f8504f7a42c8ffd345454a15a88
Jingru Zhang <zhang.jingru.93@gmail.com>
2017-01-17 13:12:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name)
+}
+
+var destructivelyPrependKitten = (name) => {
+  kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens.pop()
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  kittens.shift()
+}
+
+var appendKitten = (name) => {
+  return [...kittens, name]
+}
+
+var prependKitten = (name) => {
+  return [name, ...kittens]
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, 2)
+}
+
+var removeFirstKitten = () => {
+  return kittens.slice(1)
+}
