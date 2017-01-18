bfc74b13e568592fb7e9c3ab4e20ca1afbb04069
Conlin McManus <conlinmcmanus@gmail.com>
2017-01-18 14:43:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  kittens.push("Ralph")
+  console.log(kittens)
+}
+
+function destructivelyPrependKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  kittens.unshift("Bob")
+  console.log(kittens)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  kittens.pop()
+  console.log(kittens)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  kittens.shift()
+  console.log(kittens)
+}
+
+function appendKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  var apkittens = [...kittens, "Broom"]
+  return apkittens
+}
+
+function prependKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  var prkittens = ["Arnold", ...kittens]
+  return prkittens
+}
+
+function removeLastKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  var rlkitten = kittens.slice(0, 2)
+  return rlkitten
+}
+
+function removeFirstKitten() {
+  kittens = ["Milo", "Otis", "Garfield"]
+  var rfkitten = kittens.slice(1)
+  return rfkitten
+}
