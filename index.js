fb23c8fc9a01bd4af563853124c7638c3f6725d8
Paul Snider <pbsnider@gmail.com>
2017-01-18 02:47:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'] ;
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+}
+
+function appendKitten(name) {
+  var new_kitten = [name] ;
+  var new_kittens = kittens.concat(new_kitten)
+  return new_kittens;
+}
+
+function prependKitten(name) {
+  var new_kitten = [name] ;
+  var new_kittens = new_kitten.concat(kittens)
+  return new_kittens;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length)
+}
