63a67363bc6300b030867941b90cb2a2a95d4cb0
Andrea Graham <andrea.f.graham@gmail.com>
2017-01-18 16:47:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+
+
+function destructivelyAppendKitten(){
+  kittens.push('Ralph');
+}
+
+funtion destructivelyPrependKitten(){
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten()){
+  return [...kittens, "Broom"];
+}
+
+function prependKitten()){
+  return ["Arnold", ...kittens];
+}
+
+function removeLastKitten()){
+    return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten()){
+    return kittens.slice(1);
+}
