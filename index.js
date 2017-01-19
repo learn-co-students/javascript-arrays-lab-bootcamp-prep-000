fef4f2a62f947ae2d2ce9587c9e28252f93e244f
Himalay Sunuwar <i@himalay.com.np>
2017-01-19 09:01:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much."
+let kittens = ["Milo", "Otis", "Gerfield"]
+
+const destructivelyAppendKitten = name => kittens.push(name)
+
+const destructivelyPrependKitten = name => kittens.unshift(name)
+
+const destructivelyRemoveLastKitten = () => kittens.pop()
+
+const destructivelyRemoveFirstKitten = () => kittens.shift()
+
+const appendKitten = name => [...kittens, name]
+
+const prependKitten = name => [name, ...kittens]
+
+const removeLastKitten = () => kittens.slice(0, kittens.length - 1)
+
+const removeFirstKitten = () => kittens.slice(1)
