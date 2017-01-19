e0c5933149c6b2b0af4d7576ee872a8752578715
Alex Newton <alexnewton42@gmail.com>
2017-01-19 20:05:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = function(name) {
+	kittens.push(name)
+	return kittens
+}
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var destructivelyRemoveLastKitten = function() {
+  kittens.pop()
+  return kittens
+}
+
+var destructivelyRemoveFirstKitten = function() {
+  kittens.shift()
+  return kittens
+}
+
+var appendKitten = function(name) {
+    var newKittens1 = [...kittens, name]
+    return newKittens1
+}
+
+var prependKitten = function(name) {
+  var newKittens2 = [name, ...kittens]
+  return newKittens2
+}
+
+var removeLastKitten = function() {
+  var takeKittens1 = kittens.slice(0, kittens.length - 1)
+  return takeKittens1
+}
+
+var removeFirstKitten = function() {
+  var takeKittens2 = kittens.slice(1)
+  return takeKittens2
+}
