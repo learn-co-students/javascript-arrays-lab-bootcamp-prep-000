f97976abce8dda947fe1c2096cb08e23e4c04157
Tom Maher <tomjmaher4@gmail.com>
2017-01-19 06:30:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  var newcat = [...kittens, name]
+  return newcat
+}
+function prependKitten(name){
+  var newercat = [name, ...kittens]
+  return newercat
+}
+function removeLastKitten(){
+  var deadcat = kittens.slice(0, kittens.length -1)
+  return deadcat
+}
+function removeFirstKitten(){
+  var deadcat2 = kittens.slice(1, kittens.length)
+  return deadcat2
+}
