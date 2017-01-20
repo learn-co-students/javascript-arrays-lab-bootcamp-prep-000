6aae27a22a7ed43ccffcf88944791468e0057cf0
Calandra Zellner <calandrazellner@gmail.com>
2017-01-20 18:34:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  var moreKittens = [...kittens, name];
+  return moreKittens;
+}
+
+function prependKitten(name){
+  var moreKittens = [name, ...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten(){
+  var newkittens = [...kittens];
+  newkittens.pop();
+  return newkittens;
+}
+
+function removeFirstKitten(){
+  var newkittens = [...kittens];
+  newkittens.shift();
+  return newkittens;
+}
