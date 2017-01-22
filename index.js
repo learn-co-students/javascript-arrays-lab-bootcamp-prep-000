69328d33586b7eaf1f8daa4e8d1e1624492da46a
Ariel Herman <aeh.herman@gmail.com>
2017-01-22 22:05:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+};
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+};
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+};
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+};
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray
+};
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray
+};
+
+function removeLastKitten(){
+  var theArray = kittens.slice(0, kittens.length - 1);
+  return theArray
+};
+
+function removeFirstKitten(){
+  var newArray = kittens.slice(1);
+  return newArray
+};
