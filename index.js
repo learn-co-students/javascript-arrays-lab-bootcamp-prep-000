35e3247bee9871b7e25b2b687f4214b55c3d7cf1
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-01-23 22:14:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  var newArray = kittens.push(name);
+  return newArray;
+}
+function destructivelyPrependKitten(name){
+  var newArray = kittens.unshift(name);
+  return newArray;
+}
+function destructivelyRemoveLastKitten(){
+  var newArray = kittens.pop(name);
+  return newArray;
+
+}
+function destructivelyRemoveFirstKitten(){
+  var newArray = kittens.shift(name);
+  return newArray;
+}
+function appendKitten(name){
+  var newArray = (..."name");
+  return newArray;
+}
