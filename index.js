8cc61df0c1ba504a4844a40fc4ea768b9bdb7ab2
Nathaniel Richardson <richardson.j.nathaniel@gmail.com>
2017-01-23 22:57:26 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ function destructivelyRemoveFirstKitten(){
   return newArray;
 }
 function appendKitten(name){
-  var newArray = (..."name");
+  var newArray = [...kittens, name]
+  return newArray;
+}
+function prependKitten(name){
+  var newArray = [name, ...kittens]
+  return newArray;
+}
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray;
+}
+function removeFirstKitten() {
+  var newArray = kittens.slice(1);
   return newArray;
 }
