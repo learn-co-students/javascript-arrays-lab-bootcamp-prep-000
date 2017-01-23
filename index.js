3af85ec31b4e9b89a2c6904b6e3b62a750336422
Lanitta Horner <lanittad@hotmail.com>
2017-01-23 17:53:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(){
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(){
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+ var kitten =[...kittens, name]
+ return kitten
+}
+
+function prependKitten(name){
+  var kitten = [name, ...kittens]
+  return kitten
+}
+
+function removeLastKitten(){
+ return [...kittens.slice(0, 2)]
+
+}
+
+function removeFirstKitten(){
+  return [...kittens.slice(1)]
+}
