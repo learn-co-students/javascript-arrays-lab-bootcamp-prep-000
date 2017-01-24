4938806dddbdbdbf392d1fcd506f8a768b0b8676
stephen chun <stephenchun91@gmail.com>
2017-01-24 09:05:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+Array = [ "Milo", "Otis", "Garfield" ]
+
+var kittens = [ "Milo", "Otis", "Garfield" ]
+console.log(kittens)
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var kittens1 = [...kittens, name]
+  return kittens1
+}
+
+function prependKitten(name) {
+  var kittens2 = [name, ...kittens ];
+  return kittens2
+}
+
+function removeLastKitten() {
+  var kittens1 = kittens.slice(0, kittens.length-1);
+  return kittens1
+}
+
+function removeFirstKitten() {
+  var kittens1 = kittens.slice(1)
+  return kittens1
+}
