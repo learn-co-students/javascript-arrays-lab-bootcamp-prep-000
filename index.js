701364dfcc08ce12b8e2c8d750ae4d5466e35d43
Risha Shetty <reeks_05@yahoo.co.in>
2017-01-25 08:54:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name)
+{
+    return  kittens.push(name)
+}
+function destructivelyPrependKitten(name)
+{
+    kittens.unshift(name)
+    return kittens
+}
+function destructivelyRemoveLastKitten(name) {
+    return kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray
+}
+function prependKitten(name) {
+  var newArray = [name, ...kittens]
+  return newArray
+}
+function removeLastKitten() {
+  var newArray = kittens.slice (0, kittens.length - 1)
+  return newArray
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
