c7c8ee800df6b2c7686126d0b85faddee4c04244
Christofer Herlin <christofer.herlin@gmail.com>
2017-01-25 11:25:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = new Array();
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newArray;
+  newArray = [...kittens, name];
+  return newArray
+}
+
+function prependKitten(name) {
+  var newArray;
+  newArray = [name, ...kittens];
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray;
+  newArray = kittens.slice(0, kittens.length - 1);
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray;
+  newArray = kittens.slice(1)
+  return newArray
+}
