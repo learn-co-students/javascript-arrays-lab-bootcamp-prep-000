5e01553bf99a2c56461d13bd70dbd9d8d5d74783
Clinton Sexton <sextonclinton@gmail.com>
2017-01-27 02:46:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(el) {
+  kittens.push(el);
+}
+
+function destructivelyPrependKitten(el) {
+  kittens.unshift(el);
+}
+
+function destructivelyRemoveFirstKitten(el) {
+  kittens.shift();
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function appendKitten(el) {
+  var arr = [...kittens, el];
+  return arr;
+}
+
+function prependKitten(el) {
+  var arr = [el,...kittens];
+  return arr;
+}
+
+function removeLastKitten() {
+  var a = [...kittens];
+  a.pop();
+  return a;
+}
+
+function removeFirstKitten() {
+  var b = [...kittens];
+  b.shift();
+  return b;
+}
