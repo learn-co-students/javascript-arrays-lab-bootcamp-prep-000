9857a4e849b11397ad67d75864e7f5d7c1cbd8c2
Chibueze Osuoji <chibz2006@yahoo.com>
2017-01-27 14:27:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+ kittens.push(name)
+ kittens;
+ return kittens;
+}
+
+function destructivelyPrependKitten(name)
+{
+ kittens.unshift(name);
+ kittens;
+ return kittens;
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+ kittens.pop(name);
+ kittens;
+ return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name)
+{
+ kittens.shift(name);
+ kittens;
+ return kittens;
+}
+
+function appendKitten(name)
+{
+  const moreKittens = [...kittens, name];
+  moreKittens;
+  return moreKittens;
+}
+
+function prependKitten(name)
+{
+  const morekittens = [name, ...kittens];
+  morekittens;
+  return morekittens;
+}
+
+function removeFirstKitten()
+{
+  const morekittens = kittens.slice(1);
+  morekittens;
+  return morekittens;
+}
+
+function removeLastKitten()
+{
+  const morekittens = kittens.slice(0, kittens.length - 1);
+  morekittens;
+  return morekittens;
+}
