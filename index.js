a2cb802a7b9322e71ffb364658ab7f3187fa1d9d
Julie Widman <juliewdmn@gmail.com>
2017-01-30 15:19:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop("Garfield")
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift("Milo")
+  return kittens
+}
+
+function appendKitten(name) {
+return[...kittens,"Broom"]
+}
+
+function prependKitten(name) {
+return["Arnold",...kittens]
+}
+
+function removeLastKitten() {
+  var kittens = ["Milo", "Otis"]
+  return kittens
+}
+
+function removeFirstKitten() {
+  var kittens = ["Otis", "Garfield"]
+  return kittens
+}
