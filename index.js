de152d5e87414401ec67e610f3f7e24448dc0413
Bashir Awaty <bashir.awaty@hotmail.com>
2017-01-31 07:38:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = function(){
+ kittens.push('Ralph');
+return kittens;
+}
+
+var destructivelyPrependKitten = function(){
+ kittens.unshift("Bob");
+return kittens;
+}
+
+var destructivelyRemoveLastKitten = function(){
+ kittens.pop();
+return kittens;
+}
+
+var destructivelyRemoveFirstKitten = function(){
+ kittens.shift();
+return kittens;
+}
+
+var appendKitten = function(){
+
+var kitten = [...kittens, 'Broom']
+return kitten;
+};
+
+var prependKitten = function(){
+
+var kitten = ["Arnold",...kittens]
+return kitten;
+};
+
+var removeLastKitten = function(){
+
+var kitten = kittens.slice(0,2)
+return kitten;
+};
+
+var removeFirstKitten = function(){
+
+var kitten = kittens.slice(1)
+return kitten;
+};
