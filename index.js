f062b62d79d7ff481dfe65ee374e6ad3a4d309bd
Calvin Lee <calvinkflee@gmail.com>
2017-02-01 22:17:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  return(kittens.push(name))
+}
+
+function destructivelyPrependKitten(name){
+  return(kittens.unshift(name))
+}
+
+function destructivelyRemoveLastKitten(){
+  return(kittens.pop())
+}
+
+function destructivelyRemoveFirstKitten(){
+  return(kittens.shift())
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+  return(kittens.slice(0, kittens.length -1))
+}
+
+function removeFirstKitten(){
+  return(kittens.slice(1))
+}
+
