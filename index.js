6667da12a2eafb0a1e65bf2122f1d0baf52d5d36
Ellen Park <ellenspark.ux@gmail.com>
2017-02-01 20:54:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+kittens.unshift(name);
+return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var appended = [...kittens, name]
+  return appended;
+}
+
+function prependKitten(name) {
+  var prepended = [name, ...kittens]
+  return prepended;
+}
+
+function removeLastKitten() {
+  var removedLK = kittens.slice(0, kittens.length - 1);
+  return removedLK;
+}
+
+function removeFirstKitten() {
+  var removedFK = kittens.slice(1);
+  return removedFK;
+}
