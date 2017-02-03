5c69734fa2723d72e2211d3d525497108ddfa0fd
Terrance Koar <terrancekoar@gmail.com>
2017-02-03 05:04:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
