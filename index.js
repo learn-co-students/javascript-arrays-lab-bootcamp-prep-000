2ae8928b392e646ac4fd561d1f04e1f04bd02a8c
Gary Payne <gary.payne67@ntlworld.com>
2017-02-03 16:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name);
+}
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name);
+}
+
+var destructivelyRemoveLastKitten = function(name) {
+  kittens.pop(name);
+}
+
+var destructivelyRemoveFirstKitten = function() {
+  kittens.shift(name);
+}
+
+var appendKitten = function(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+var prependKitten = function(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+var removeLastKitten = function() {
+  var newKittens = kittens.slice(0,2);
+  return newKittens;
+}
+
+var removeFirstKitten = function() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
