cba71efbdbece0499d6b1ea0e22f48afaa896d28
daniel byun <byunsta@gmail.com>
2017-02-04 18:21:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+
+  kittens.push(name);
+  return kittens;
+
+}
+
+function destructivelyRemoveLastKitten(name) {
+
+  kittens.pop(name);
+  return kittens;
+
+}
+
+function destructivelyPrependKitten(name) {
+
+  kittens.unshift(name);
+  return kittens;
+
+}
+
+function destructivelyRemoveFirstKitten() {
+
+  kittens.shift(name);
+  return kittens;
+
+}
+
+function appendKitten(name) {
+
+  var kitten = kittens.slice(0);
+  kitten.push(name);
+  return kitten;
+
+}
+
+function prependKitten(name) {
+
+  var kitten = kittens.slice(0);
+  kitten.unshift(name);
+  return kitten;
+
+}
+
+function removeLastKitten(name) {
+
+  var kitten = kittens.slice(0);
+  kitten.pop(name);
+  return kitten;
+
+}
+
+function removeFirstKitten(name) {
+
+  var kitten = kittens.slice(0);
+  kitten.shift(name);
+  return kitten;
+
+}
