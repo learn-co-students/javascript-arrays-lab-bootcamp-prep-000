b3b9ffd107e24d7257cd0ab50cf20f1f9740560a
Tayeb Mc <gomail654@gmail.com>
2017-02-04 20:43:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(){
+     kittens.push("Ralph");
+     return kittens
+  }
+
+  var kittens = ["Milo", "Otis", "Garfield"];
+  function destructivelyPrependKitten(){
+       kittens.unshift("Bob");
+       return kittens
+  }
+
+  var kittens = ["Milo", "Otis", "Garfield"];
+  function destructivelyRemoveLastKitten(){
+       kittens.pop("Garfield");
+       return kittens
+  }
+   var kittens = ["Milo", "Otis", "Garfield"];
+  function destructivelyRemoveFirstKitten(){
+       kittens.shift("Milo");
+       return kittens
+  }
+
+  var kittens = ["Milo", "Otis", "Garfield"];
+    function appendKitten(){
+      Kittens = new Array("var Kittens", "Broom");
+          return Kittens
+    }
+
+  var Kittens;
+  function prependKitten(){
+  var Kittens = new Array("Milo", "Otis", "Garfield");
+    Kittens.unshift("Arnold");
+   return Kittens
+  }
+  var Kittens;
+  function removeLastKitten(){
+  var Kittens = new Array("Milo", "Otis", "Garfield");
+    Kittens.pop("Milo");
+   return Kittens
+  }
+var  Kittens;
+  function removeFirstKitten(){
+  var Kittens = new Array("Milo", "Otis", "Garfield");
+    Kittens.shift("Milo");
+   return Kittens
+  }
