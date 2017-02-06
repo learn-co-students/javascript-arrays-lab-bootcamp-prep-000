b9596b81024b7e2fb4efb053fb13cc2a31187e25
Noah Fulkerson <noah.fulkerson@live.com>
2017-02-06 13:48:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens = ['Milo', 'Otis', 'Garfield']
+  kittens.push(name)
+};
+
+function destructivelyPrependKitten(name) {
+  kittens = ['Milo', 'Otis', 'Garfield']
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens = ['Milo', 'Otis', 'Garfield']
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens = ['Milo', 'Otis', 'Garfield']
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var kitties = [...kittens, name]
+  return kitties
+}
+
+function prependKitten(name) {
+  var kitties = [name, ...kittens]
+  return kitties
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
