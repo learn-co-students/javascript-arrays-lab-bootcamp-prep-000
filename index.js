8f8d2b7be6d765154865891871e38c2cd4a3f4a0
Austin Anderson <1austinm@sbcglobal.net>
2017-02-06 00:51:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(a) {
+  kittens.push(a)
+}
+
+function destructivelyPrependKitten(a) {
+  kittens.unshift(a)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(a) {
+  return [...kittens, a]
+}
+
+function prependKitten(a) {
+  return [a, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
