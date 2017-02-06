27b7a9c5283a447ca9b54d10c2029b5d7900cde4
Nicholas Daniele <n.daniele@gmail.com>
2017-02-06 16:49:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens = [...kittens, name];
+}
+
+function destructivelyPrependKitten(name) {
+  kittens = [name, ...kittens];
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
