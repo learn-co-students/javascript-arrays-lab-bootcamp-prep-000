be8842a5b8134083fa84987027832399f5eff954
Rinda Ko <rinda.ko@gmail.com>
2017-02-07 04:26:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+var morekittens = kittens.push(name);
+return morekittens;
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return[name, ...kittens];
+}
+
+function removeLastKitten(){
+  var lesskittens = kittens.slice(0,kittens.length-1)
+  return lesskittens;
+}
+
+function removeFirstKitten(){
+  var lesskittens = kittens.slice(1);
+  return lesskittens;
+}
