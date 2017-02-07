e5f95e2bdbaa062fb147691d137e12f622590c18
Jason  Rosso <jasonrosso@gmail.com>
2017-02-07 07:09:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+};
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+};
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = [...kittens,name]
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [name,...kittens]
+  return newArray;
+}
+
+function removeLastKitten(){
+return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
