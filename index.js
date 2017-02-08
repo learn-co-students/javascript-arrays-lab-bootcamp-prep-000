793478621402e7f3a0335b518d7f4c6b5ef50c86
Dane Ford <daneford22@gmail.com>
2017-02-08 01:01:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  var kittensApp = [...kittens, name]
+  return kittensApp
+  }
+function prependKitten(name) {
+  var kittensPrep = [name, ...kittens]
+  return kittensPrep
+}
+function removeFirstKitten() {
+  var kittensFirst = kittens.slice(1)
+  return kittensFirst
+}
+function removeLastKitten() {
+  var kittensLast = kittens.slice(0, kittens.length - 1)
+  return kittensLast
+}
