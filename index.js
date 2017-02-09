1fe74c7e0cc747fc882bcf4155c7d392f752f61d
Penn Wu <phw.dental@gmail.com>
2017-02-09 12:20:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name);
+  return kittens;
+};
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name);
+  return kittens;
+};
+var destructivelyRemoveLastKitten = function(name) {
+  kittens.pop(name);
+  return kittens;
+};
+var destructivelyRemoveFirstKitten = function(name) {
+  kittens.shift(name);
+  return kittens;
+};
+var appendKitten = function(name) {
+  return [...kittens, name];
+};
+var prependKitten = function(name) {
+  return [name, ...kittens];
+};
+var removeLastKitten = function() {
+  return kittens.slice(0, kittens.length - 1);
+};
+var removeFirstKitten = function() {
+  return kittens.slice(1, 3);
+};
+destructivelyRemoveLastKitten(name);
+destructivelyAppendKitten(name);
+destructivelyPrependKitten(name);
+destructivelyRemoveFirstKitten(name);
