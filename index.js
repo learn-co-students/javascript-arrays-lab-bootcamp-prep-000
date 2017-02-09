662361d2a6ac85af3d617ddb3085ffce744f9909
Mandy Losk <losk.m.jchs@gmail.com>
2017-02-09 00:15:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+
+  kittens.push(name)
+  return kittens;
+}
+
+
+function destructivelyPrependKitten(name) {
+
+  kittens.unshift (name)
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+
+  kittens.pop(name)
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+
+  kittens.shift()
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kittens=[
+    'Milo',
+    'Otis',
+    'Garfield',
+    'Broom'
+  ]
+  return kittens;
+}
+
+function prependKitten() {
+  var kittens=[
+    'Arnold',
+    'Milo',
+    'Otis',
+    'Garfield'
+  ]
+  return kittens;
+}
+
+function removeLastKitten() {
+ var kittens=[
+   'Milo',
+   'Otis',
+ ]
+ return kittens;
+}
+
+function removeFirstKitten() {
+  var kittens=[
+      'Otis',
+      'Garfield'
+    ]
+    return kittens;
+}
