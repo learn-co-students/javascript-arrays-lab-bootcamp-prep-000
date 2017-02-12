171aa6b73471ae1ffaa4d14230b424618d318fa6
Danielle Wood <play_in_da_rain@hotmail.com>
2017-02-12 15:51:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+var kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'];
+  function appendKitten(name) {
+    return [...kittens,name];
+  }
+  function prependKitten(name) {
+    return [name,...kittens]
+  }
+  function destructivelyAppendKitten(name) {
+    return kittens.push(name);
+  }
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
