39305e76e46a540659c96db0b780b7ea2461060d
Kenny Hearn <wkhearn@gmail.com>
2017-02-13 23:28:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var a = [...kittens, name];
+  return a;
+}
+
+function prependKitten(name){
+  var a = [name, ...kittens];
+  return a;
+}
+
+function removeLastKitten(){
+  var a = kittens.slice(0, kittens.length -1);
+  return a;
+}
+
+function removeFirstKitten(){
+  var a = kittens.slice(1);
+  return a;
+}
