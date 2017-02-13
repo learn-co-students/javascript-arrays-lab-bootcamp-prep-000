8c0614b6ea96c5cfc2672344fd2a3a1bc87e6127
Lindsay Richman <lrichman@gmail.com>
2017-02-13 22:01:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens=['Milo', 'Otis', 'Garfield'];
+
+function kittens() {
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newArr=kittens.slice();
+  newArr.push(name);
+  return newArr;
+}
+
+function prependKitten(name) {
+  var newArr=kittens.slice();
+  newArr.unshift(name);
+  return newArr;
+}
+
+function removeLastKitten(name) {
+  var newArr=kittens.slice();
+  newArr.pop();
+  return newArr;
+}
+
+function removeFirstKitten(name) {
+  var newArr=kittens.slice();
+  newArr.shift();
+  return newArr;
+}
