8e7bc6618369be2ba1bb24d9094a6f40d2819cca
Christoffer Stenshøj <c.stenshoj@gmail.com>
2017-02-14 07:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(element) {
+  kittens.push(element);
+  return kittens;
+}
+function destructivelyPrependKitten(element){
+  kittens.unshift(element);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+function appendKitten(element){
+  var myKittens = [...kittens, element];
+  return myKittens;
+}
+function prependKitten(element) {
+  var myKittens = [element, ...kittens];
+  return myKittens;
+}
+function removeLastKitten() {
+  var myKittens = kittens.slice(0, kittens.length - 1);
+  return myKittens;
+}
+function removeFirstKitten() {
+  var myKittens = kittens.slice(1);
+  return myKittens;
+}
