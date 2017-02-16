5f46e2c75e021eb4357267d035a798ae871e5eeb
Peter Helstrom <phelstrom@gmail.com>
2017-02-16 00:26:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+//var kittens;
+
+function destructivelyAppendKitten(name) {
+    return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop(name);
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
