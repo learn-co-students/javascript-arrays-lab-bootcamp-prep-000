bd3e43b915ced478dba779052643a272f8054d13
Marina Hoashi <marinahoashi@gmail.com>
2017-02-17 18:06:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten];
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, -1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
