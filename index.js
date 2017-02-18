c6d23ca9e87d78598f80f4945e82b3c789da8f93
Ryan Do <falcsmike7@gmail.com>
2017-02-18 13:04:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift(1);
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  var newkittens = kittens.slice(0, kittens.length - 1);
+  return newkittens;
+}
+
+function removeFirstKitten() {
+  var newkittens = kittens.slice(1);
+  return newkittens;
+}
