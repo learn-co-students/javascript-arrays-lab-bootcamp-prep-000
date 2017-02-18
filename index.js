1fc87434a56c0e2ccd8c07ad1205ddc1e53070f1
Ammo Can <ammo@2-drops.com>
2017-02-18 19:25:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var kitten = kittens;
+  kitten = [...kittens, name];
+  return kitten;
+}
+
+function prependKitten(name) {
+  var kitten = kittens;
+  kitten = [name, ...kittens];
+  return kitten;
+}
+
+function removeLastKitten() {
+  var kitten = kittens;
+  kitten = kittens.slice(0, kittens.length - 1);
+  return kitten;
+}
+
+function removeFirstKitten() {
+  var kitten = kittens;
+  kitten = kittens.slice(1);
+  return kitten;
+}
