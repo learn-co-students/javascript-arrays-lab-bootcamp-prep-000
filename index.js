d4ecf4b05083793606aeb0a4b1671e56b2637824
Marianna Katsika <marianna.katsika@gmail.com>
2017-02-19 00:41:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push('Ralph')
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob')
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+ var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name) {
+return [...kittens, 'Broom']
+}
+
+function prependKitten(name) {
+return ["Arnold", ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
