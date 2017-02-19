c2530d02e5d831ed7a23f8ba9f3854796c94c0b3
Zack Enders <zenders672@gmail.com>
2017-02-19 06:05:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(0);
+}
+function appendKitten(name) {
+  var moreKittens = kittens;
+  moreKittens = [...kittens, name];
+  return moreKittens;
+}
+function prependKitten(name) {
+  var moreKittens = kittens;
+  moreKittens = [name, ...kittens];
+  return moreKittens;
+}
+function removeLastKitten() {
+  var lessKittens = kittens;
+  lessKittens = kittens.slice(0, kittens.length - 1);
+  return lessKittens;
+}
+function removeFirstKitten() {
+  var lessKittens = kittens;
+  lessKittens = kittens.slice(1);
+  return lessKittens;
+}
