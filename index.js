8c631bb3ea79564a1ddb60043ba42f240b22427d
Hardik Shah <hardevshah32@gmail.com>
2017-02-20 00:15:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(element)
+{kittens.push(element)
+return kittens}
+
+function destructivelyPrependKitten(element)
+{kittens.unshift(element)
+return kittens}
+
+function destructivelyRemoveLastKitten(element){
+kittens.pop()
+return kittens}
+
+function destructivelyRemoveFirstKitten (element){
+kittens.shift()
+return kittens}
+
+function appendKitten(element, array){
+  return [...kittens, element]
+}
+
+function prependKitten(element, array){
+  return [element, ...kittens]
+}
+
+function removeFirstKitten(element){
+    var kit1 = kittens.slice(1, 3)
+    return kit1
+    return kittens
+}
+
+function removeLastKitten(element){
+    var kit2 = kittens.slice(0, 2)
+    return kit2
+    return kittens
+}
