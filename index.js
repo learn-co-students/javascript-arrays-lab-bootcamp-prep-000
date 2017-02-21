93280ab80b596186050dc499a35aa76d96be9972
Kairav Parekh <kparekh01@gmail.com>
2017-02-21 20:07:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var new_array = [...kittens, name];
+  return new_array;
+}
+
+function  prependKitten(name){
+  var new_array = [name, ...kittens];
+  return new_array;
+}
+
+function removeLastKitten(){
+  var new_array = kittens.slice(0, (kittens.length - 1));
+  return new_array;
+}
+
+function removeFirstKitten(){
+  var new_array = kittens.slice(1);
+  return new_array;
+}
