e19c789bdfab648eb3ccadac0c875c296357650e
William Juseck <will.juseck@gmail.com>
2017-02-22 18:27:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(element) {
+  kittens.push(element)
+  return kittens
+}
+
+function destructivelyPrependKitten(element) {
+  kittens.unshift(element)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(element) {
+  kittens.pop(element)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(element) {
+  kittens.shift(element)
+  return kittens
+}
+
+function appendKitten(element) {
+  return [...kittens, element]
+}
+
+function prependKitten(element) {
+  return [element, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
