e2feaf4b8520f210d262e9c4aea60379697a2a76
Monika Williams <monika.m.williams@gmail.com>
2017-02-23 13:51:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+const kittensValue = ["Milo","Otis","Garfield"];
+var kittens = kittensValue;
+function destructivelyAppendKitten(element) {
+  kittens.push(element);
+}
+function destructivelyPrependKitten(element){
+  kittens = [element,...kittensValue];
+}
+function destructivelyRemoveLastKitten(){
+  kittens = [...kittensValue];
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens=[...kittensValue];
+  kittens.shift();
+}
+function removeLastKitten(){
+  var answer =[...kittensValue];
+  answer.pop();
+  return answer;
+}
+function removeFirstKitten(){
+  var answer =[...kittensValue];
+  answer.shift();
+  return answer;
+}
+function appendKitten(name){
+  kittens = [...kittensValue];
+  return [...kittensValue,name];
+}
+function prependKitten(name){
+  kittens = [...kittensValue];
+  return [name,...kittensValue];
+}
