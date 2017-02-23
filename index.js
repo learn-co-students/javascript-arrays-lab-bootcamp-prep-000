68098c42fef0645c560281f12fe4865b564c5c4d
Steven Hui <stevenhhui@gmail.com>
2017-02-23 21:14:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+   kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+
+function appendKitten(name){
+  var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function removeLastKitten(name){
+  return kittens.slice(0, kittens.length - 1)
+}
