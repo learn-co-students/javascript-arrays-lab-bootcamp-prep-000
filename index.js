1b347b1def670ed7c086ff0786580be77c16988a
Neil Patel <neilptl7@gmail.com>
2017-02-23 17:29:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens=["Milo", "Otis", "Garfield"];
+var destructivelyAppendKitten=(name)=>{
+var newkittens=kittens.push(name)
+  return newkittens
+}
+var destructivelyPrependKitten=(name)=>{
+  var newkittens=kittens.unshift(name)
+  return newkittens
+}
+var destructivelyRemoveFirstKitten=(name)=>{
+  var newkittens=kittens.shift(name)
+  return newkittens
+}
+var destructivelyRemoveLastKitten=(name)=>{
+  var newkittens=kittens.pop(name)
+  return newkittens
+}
+var appendKitten=(name)=>{
+  var newkittens=[...kittens, name]
+  return newkittens
+}
+var prependKitten=(name)=>{
+  var newkittens=[name,...kittens]
+  return newkittens
+}
+var removeLastKitten=(name)=>{
+  var newkittens=kittens.slice(0, kittens.length -1)
+  return newkittens
+}
+var removeFirstKitten=(name)=>{
+  var newkittens=kittens.slice(1)
+  return newkittens
+}
