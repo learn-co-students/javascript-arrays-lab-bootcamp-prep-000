993dbf9c23052ae6fa1243a4c84e021eb82f3ab9
Killian Piraro <kpiraro@gmail.com>
2017-02-23 20:33:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens
+}
+
+function removeLastKitten() {
+    var noLastKitten = kittens.slice(0, kittens.length - 1);
+    return noLastKitten;
+}
+
+function removeFirstKitten() {
+  var noFirstKitten = kittens.slice(1);
+  return noFirstKitten
+}
