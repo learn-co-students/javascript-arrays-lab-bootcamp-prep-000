88b0643f6ac03bb02ba682c54c134e5681a464d8
Sarah Tennis <sarah.n.tennis@gmail.com>
2017-02-24 03:32:37 +0000

    Done.

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
+  kittens = ["Milo", "Otis", "Garfield"]
   kittens.push(name)
+  return kittens
 }
 
 function destructivelyPrependKitten(name){
+  kittens = ["Milo", "Otis", "Garfield"]
   kittens.unshift(name)
+  return kittens
 }
 
 function destructivelyRemoveLastKitten(){
+  kittens = ["Milo", "Otis", "Garfield"]
   kittens.pop()
+  return kittens
 }
 
 function destructivelyRemoveFirstKitten(){
+  kittens = ["Milo", "Otis", "Garfield"]
   kittens.shift()
+  return kittens
 }
 
 function appendKitten(name){
