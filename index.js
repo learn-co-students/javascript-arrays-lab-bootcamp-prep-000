a06e4058fb7d8bd4e6e259b93311bf74bf8b3a1a
Trac Nguyen <nguyendtrac@gmail.com>
2017-02-25 07:12:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var kitten2 = [...kittens, name]
+  return kitten2
+}
+
+function prependKitten(name) {
+  var kitten2 = [name, ...kittens]
+  return kitten2
+}
+
+function removeLastKitten() {
+  var kitten2 = kittens.slice(0, kittens.length - 1)
+  return kitten2
+}
+
+function removeFirstKitten() {
+  var kitten2 = kittens.slice(1)
+  return kitten2
+}
