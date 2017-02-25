621665ad4e5621931f6ec2d3ac880c4defa7e0b9
Josh Smith <chessperson0809@gmail.com>
2017-02-25 05:57:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name)
+{
+    kittens.push(name);
+    return kittens;
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+return kittens;
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  var y = kittens.shift();
+  return y;
+}
+
+function appendKitten(name)
+{
+  var x = [...kittens, name]
+  return x;
+}
+
+function prependKitten(name)
+{
+  var x = [name, ...kittens]
+  return x;
+}
+
+function removeLastKitten()
+{
+  var y = kittens.slice(0,2);
+  return y;
+}
+
+function removeFirstKitten()
+{
+  var x = kittens.slice(1);
+  return x;
+}
