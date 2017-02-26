36578b2c35235abfa73bb11b75a9bc9e6fd459ab
Paul Brown <paabrown@gmail.com>
2017-02-26 16:44:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens2 = [name, ...kittens]
+  return kittens2
+}
+
+function removeFirstKitten() {
+  var kittens2 = kittens.slice(1)
+  return kittens2
+}
+
+function removeLastKitten() {
+  var kittens2 = kittens.slice(0, kittens.length -1)
+  return kittens2
+}
