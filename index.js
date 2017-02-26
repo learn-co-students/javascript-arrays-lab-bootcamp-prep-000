e02ee2d2ec8aa6e33a89e6a5b15e783384fcc455
Abbi Aldrich <araldrich9@gmail.com>
2017-02-26 19:00:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
