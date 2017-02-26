0ffb6e2ca5730daf9a927d6f532b75e0be280446
Nick C <ncommella@gmail.com>
2017-02-26 23:36:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKat = [...kittens, name];
+  return newKat;
+}
+
+function prependKitten(name) {
+  var prepKat = [name, ...kittens];
+  return prepKat;
+}
+
+function removeLastKitten() {
+  var noLastKat = kittens.slice(0, kittens.length - 1);
+  return noLastKat;
+}
+
+function removeFirstKitten(){
+  var noFirstKat = kittens.slice(1);
+  return noFirstKat;
+}
