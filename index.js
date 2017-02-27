f7b450b1ae512d8c7f2ac237f4a10bc38efe8a9a
Adesegun Adedeji <adesegunadedeji@gmail.com>
2017-02-27 18:31:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten () {
+kittens.push("Ralph");
+return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+kittens.unshift("Bob");
+return kittens;
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+return[...kittens, "Broom"];
+  //return kittens;
+}
+
+function prependKitten(name){
+  return ["Arnold",...kittens];
+}
+
+function removeLastKitten() {
+  var lgth = kittens.length;
+return kittens.slice(0,lgth-1)  ;
+}
+function  removeFirstKitten()  {
+return  kittens.slice(1);
+}
