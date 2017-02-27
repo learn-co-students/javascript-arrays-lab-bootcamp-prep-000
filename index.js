330f42337a18cbcf7a611e59d2544f3e1f196e98
Briana Gross <brianagross@me.com>
2017-02-27 19:43:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
