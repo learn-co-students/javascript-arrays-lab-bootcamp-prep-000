dcfce2481c61e85b3c2d79c5043c11f0bb92aaa3
Corey Hosch <corey.hosch@gmail.com>
2017-02-27 18:36:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
