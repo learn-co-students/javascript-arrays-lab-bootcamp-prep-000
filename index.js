ec93791346cd3410e3e0bac21b4d8dc92ceaf211
Jeanne Erickson Cooley <cool0020@gmail.com>
2017-02-28 22:14:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+    return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+    return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+    return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+    return kittens.shift();
+}
+
+function appendKitten(kittens, name) {
+    const newArray = [kittens, ...name];
+    return newArray;
+}
