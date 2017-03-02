3ad512d43d23ff9dbf5cd07ec5d2176dea233be2
Bart Versluijs <bartv@vedicium.com>
2017-03-02 13:39:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name);
+  return kittens;
+}
+
+var destructivelyPrependKitten = name => {
+  kittens.unshift(name);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens = kittens.slice(0, kittens.length-1)
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  kittens = kittens.slice(1)
+  return kittens;
+}
+
+var appendKitten = name => {
+  return [...kittens, name];
+}
+
+var prependKitten = name => {
+  return [name, ...kittens];
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, kittens.length-1);
+}
+
+var removeFirstKitten = () => {
+  return kittens.slice(1);
+}
