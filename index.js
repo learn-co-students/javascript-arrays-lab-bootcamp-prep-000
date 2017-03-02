1fdb2c705348678ce31d8a081e936f42ff256a16
Oliver Mead <olivermmead@gmail.com>
2017-03-02 16:25:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+
+
+function destructivelyAppendKitten(Ralph){
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(Bob){
+  kittens.unshift(Bob)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(Broom){
+  var kittens1 = [...kittens, "Broom"]
+  return kittens1
+}
+
+function prependKitten(Arnold){
+  var kittens2 = ["Arnold", ...kittens]
+  return kittens2
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
