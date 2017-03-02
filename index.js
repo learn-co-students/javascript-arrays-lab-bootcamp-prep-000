ae2e40745d0c9f927922c67b7dc43657248f504a
Dave Parker <ctccobra@yahoo.com>
2017-03-02 19:25:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop("Garfield");
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift("Milo");
+}
+function appendKitten(Broom){
+    appendKitten = [...kittens, "Broom"]
+  return appendKitten;
+  }
+function prependKitten(Arnold){
+    prependKitten = ["Arnold", ...kittens]
+    return prependKitten
+}
+function removeLastKitten(){
+   removeLastKitten = [ "Milo", "Otis" ]
+   return removeLastKitten
+}
+function removeFirstKitten(){
+    removeFirstKitten = [ "Otis", "Garfield" ]
+    return removeFirstKitten
+}
