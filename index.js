8dc45406b7b0c3abb88227b7877d49c3e7763f90
Jack Xu <jackexyou@gmail.com>
2017-03-04 20:47:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  var new_kittens = kittens.slice(0, kittens.length - 1)
+  return new_kittens
+}
+
+function removeFirstKitten(name) {
+  var new_kittens = kittens.slice(1)
+  return new_kittens
+}
