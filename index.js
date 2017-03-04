24f8488e55d14f6f6aa50e92133e951bf9ffa2d0
Alex Calice <macciak@gmail.com>
2017-03-04 10:57:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+destructivelyAppendKitten('ciak');
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+destructivelyPrependKitten('Jon');
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+destructivelyRemoveLastKitten('Last');
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+destructivelyRemoveFirstKitten('First');
+
+var newArr = [];
+function appendKitten(name) {
+  newArr = kittens.concat(name);
+  return newArr;
+}
+appendKitten('pete');
+
+function prependKitten(name){
+  var newArr2 = [];
+  newArr2.unshift(name);
+  newArr2 = newArr2.concat(kittens);
+  return newArr2;
+}
+appendKitten('bob');
+
+function removeLastKitten(){
+  var newArr2 = [];
+  newArr2 = newArr2.concat(kittens);
+  newArr2.pop();
+  return newArr2;
+}
+appendKitten();
+
+function removeFirstKitten(){
+  var newArr2 = [];
+  newArr2 = newArr2.concat(kittens);
+  newArr2.shift();
+  return newArr2;
+}
+appendKitten();
