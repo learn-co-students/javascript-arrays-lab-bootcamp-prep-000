ab3ab526f24cced2c801d9e708e7305dcb9c1322
Lucas Kisabeth <lukey.mac@outlook.com>
2017-03-05 06:14:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten() {
+  kittens.push('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name]
+  return moreKittens
+}
+
+function prependKitten(name) {
+  var moreKittensAtBeginning = [name, ...kittens]
+  return moreKittensAtBeginning
+}
+
+function removeLastKitten() {
+  var lessKittens = kittens.slice(0, kittens.length - 1)
+  return lessKittens
+}
+
+function removeFirstKitten() {
+  var lessKittensAtBeginning = kittens.slice(1)
+  return lessKittensAtBeginning
+}
