726d3c44b951130b77e3c549aecc9aa482c656b0
Eunjoo Byeon <mirareunjoo@gmail.com>
2017-03-07 23:04:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten (name) {
+  kittens.push (name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift (name);
+  return kittens; 
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop (name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kittensNew = [...kittens, name];
+  return kittensNew;
+}
+
+function prependKitten(name){
+  var kittensNew = [name, ...kittens];
+  return kittensNew;
+}
+
+function removeLastKitten(){
+  return kittens.slice (0, kittens.length -1);
+
+}
+
+function removeFirstKitten(){
+  return kittens.slice (1);
+}
