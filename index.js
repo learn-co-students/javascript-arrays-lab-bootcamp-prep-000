7aac4f010048846caa830b8d14b3f9f30be38257
Shalin Scupham <shalin.scupham@gmail.com>
2017-03-08 06:18:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens=["milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var array = [...kittens, name];
+  return array
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
