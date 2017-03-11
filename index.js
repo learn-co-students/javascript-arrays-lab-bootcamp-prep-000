b73c998414f6b04bbaab3abaae72e1756f96ee87
Joseph Doyle <j.doyle345@gmail.com>
2017-03-11 09:47:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1);
+}
