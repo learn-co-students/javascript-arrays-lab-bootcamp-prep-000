ee9fa0811abfd9350728aa8d2292d0178caf618b
Tomas Pozo <tomaspozogarzon@gmail.com>
2017-03-11 21:52:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = (name) => {
+  kittens.push(name);
+  return kittens;
+}
+
+var destructivelyPrependKitten = (name) => {
+  kittens.unshift(name);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = () => {
+  kittens.pop();
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = () => {
+  kittens.shift();
+  return kittens;
+}
+
+var appendKitten = (name) => {
+  return [...kittens, name];
+}
+
+var prependKitten = (name) => {
+  return [name, ...kittens];
+}
+
+var removeLastKitten = () => {
+  return kittens.slice(0, -1);
+}
+
+var removeFirstKitten = () => kittens.slice(1);
