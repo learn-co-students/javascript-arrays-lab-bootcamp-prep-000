95915343128fdae6c335679332ebbc6b2ae6c9bc
Abdul Rahman Quwatli <aznq2005@yahoo.com>
2017-03-11 04:46:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-const app = "I don't do much."
+/*const app = "I don't do much."
+kittens=['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+  return kittens;
+}
+
+function destructivelyPrependKitten(name)
+{
+  var Kittens=kittens
+  Kittens.unshift(name)
+  return Kittens;
+}
+
+function destructivelyRemoveLastKitten()
+{
+  var Kittens=kittens
+  Kittens.pop()
+  return Kittens;
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  var Kittens=kittens
+  Kittens.shift();
+  return Kittens;
+}
+
+function appendKitten(name)
+{
+  var Kittens=kittens
+  Kittens.push(name)
+  return Kittens;
+}
+
+function prependKitten(name)
+{
+  var Kittens=kittens
+  Kittens.unshift(name)
+  return kittens;
+}
+
+function removeLastKitten()
+{
+  var Kittens=kittens
+  Kittens.pop()
+  return kittens;
+}
+
+function removeFirstKitten()
+{
+  var Kittens=kittens
+  Kittens.shift()
+  return kittens;
+}*/
