8894c84e339009bc72c7918c113f24a7ac94b837
Yesharge Abrha <yeshargeabrha@gmail.com>
2017-03-15 03:35:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens =["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+var foo = [...kittens, name]
+return foo
+}
+function prependKitten(name) {
+  var foo = [name, ...kittens]
+  return foo
+}
+function removeLastKitten() {
+  var foo = kittens.slice(0, kittens.length -1)
+  return foo
+}
+function removeFirstKitten() {
+  var foo = kittens.slice(1)
+  return foo
+}
