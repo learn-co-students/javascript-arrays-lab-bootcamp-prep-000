824e1c843c6f6ffba3777c13fbed01c61cb95f80
Narcita Hamilton <majesticwaterfall11@yahoo.com>
2017-03-15 02:14:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){ // destructivelyAppendKitten('Ralph')
+  kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependKitten(name) {
+kittens.unshift (name);
+return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveLastKitten(name){
+kittens.pop ();
+return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+ function destructivelyRemoveFirstKitten(name){
+  kittens.shift(1);
+}
+
+
+function appendKitten(name){
+  return [...kittens,"Broom"];
+}
+
+function prependKitten(name) {
+   return ["Arnold", ...kittens];
+ }
+
+
+  function removeLastKitten(name) {
+      kittens.slice (0, -1);
+      return kittens.slice (0, -1);
+ }
+
+function removeFirstKitten(name){
+       kittens.slice (1);
+       return kittens.slice (1);
+ }
