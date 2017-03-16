e722f6349e2d33a2fd53bfeea693f22277f603db
johnshelb <jsnd11@gmail.com>
2017-03-16 18:07:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,76 @@
-const app = "I don't do much."
-function destructivelyAppendKitten(name){
-kittens.push(name)
-  return kittens
+// const app = "I don't do much."
+// function destructivelyAppendKitten(name){
+// kittens.push(name)
+//   return kittens
+// }
+//
+// function destructivelyPrependKitten(name){
+//   kittens.unshift(name)
+//   return kittens
+// }
+//
+// function destructivelyRemoveLastKitten(){
+//   kittens.pop()
+//   return kittens
+// }
+//
+// function destructivelyRemoveFirstKitten(){
+//   kittens.shift()
+//   return kittens
+// }
+// function appendKitten(name){
+//   var newKittens = [...kittens,name]
+//   return newKittens
+// }
+//
+// function prependKitten(name){
+//   var newKittens = [name,...kittens]
+//   return newKittens
+// }
+//
+// function removeLastKitten(){
+//   var newKittens = kittens.slice(0,-1)
+//   return newKittens
+// }
+//
+// function removeFirstKitten() {
+//   var newKittens = kittens.slice(1)
+//   return newKittens
+// }
+
+
+function destructivelyAppendKitten(name) {
+   return kittens.push(name)
 }
 
-function destructivelyPrependKitten(name){
-  kittens.unshift(name)
-  return kittens
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
 }
 
-function destructivelyRemoveLastKitten(){
-  kittens.pop()
-  return kittens
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
 }
 
-function destructivelyRemoveFirstKitten(){
-  kittens.shift()
-  return kittens
-}
-function appendKitten(name){
-  var newKittens = [...kittens,name]
-  return newKittens
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
 }
 
-function prependKitten(name){
-  var newKittens = [name,...kittens]
-  return newKittens
+function appendKitten(name) {
+   var new_array =[ ...kittens, name]
+   return new_array
 }
 
-function removeLastKitten(){
-  var newKittens = kittens.slice(0,-1)
-  return newKittens
+function prependKitten(name) {
+  var new_array = [name, ...kittens]
+  return new_array
+}
+
+function removeLastKitten() {
+  var new_array = kittens.slice(0,-1)
+  return new_array
 }
 
 function removeFirstKitten() {
-  var newKittens = kittens.slice(1)
-  return newKittens
+  var new_array = kittens.slice(1)
+  return new_array
 }
