a15e2bfdf4825f27f0a2814fccefe0540c5e779a
Mollie Stein <mollie.stein@gmail.com>
2017-03-16 02:11:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+//  newarray = kittens;
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newarray;
+  newarray = kittens.slice(0);
+  newarray.push(name);
+  return newarray;
+}
+
+function prependKitten(name) {
+  var newarray;
+  newarray = kittens.slice(0);
+  newarray.unshift(name);
+  return newarray;
+}
+
+function removeLastKitten() {
+  var newarray;
+  newarray = kittens.slice(0);
+  newarray.pop();
+  return newarray;
+}
+
+function removeFirstKitten() {
+  var newarray;
+  newarray = kittens.slice(0);
+  newarray.shift();
+  return newarray;
+}
