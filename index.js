7c42dab928f22cab908a0931a64eac631d88c8fb
Chris Lee <blessedlee@gmail.com>
2017-03-17 01:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var new_kittens = [...kittens, name];
+  return new_kittens;
+}
+
+function prependKitten(name) {
+  var new_kittens = [name, ...kittens];
+  return new_kittens;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length);
+}
