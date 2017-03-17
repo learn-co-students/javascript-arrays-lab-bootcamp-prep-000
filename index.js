98058e589a30c72714ddd66d8ec61ba443bd40f8
mdr Dun <nyoriolo@gmail.com>
2017-03-17 14:44:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newkittens = [...kittens, name]
+  return newkittens
+}
+
+function prependKitten(name) {
+  var newkittens = [name, ...kittens]
+  return newkittens
+}
+
+function removeLastKitten() {
+  var newkittens = kittens.slice(0, kittens.length - 1)
+  return newkittens
+}
+
+function removeFirstKitten() {
+  var newkittens = kittens.slice(1)
+  return newkittens
+}
