ea32afc11e94cc8d3d771778144eec0e231fda89
Dustin Astrin <dustastrin@gmail.com>
2017-03-18 18:28:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var kitten = [...kittens, name]
+  return kitten
+}
+
+function prependKitten(name) {
+  var kitten = [ name, ...kittens]
+  return kitten
+}
+
+function removeLastKitten() {
+  var kitten = kittens.slice(0, kittens.length - 1)
+  return kitten
+}
+
+function removeFirstKitten() {
+  var kitten = kittens.slice(1, kittens.length)
+  return kitten
+}
