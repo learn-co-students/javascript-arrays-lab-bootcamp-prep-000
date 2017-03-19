262ad254de53ab41e30ca28b9860c51763a21e8a
Milly Rodriguez <millz118@gmail.com>
2017-03-19 22:03:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var kitties = [...kittens, name]
+  return kitties
+}
+
+function prependKitten(name) {
+  var kittiess = [name, ...kittens]
+  return kittiess
+}
+
+
+function removeFirstKitten() {
+  var kittensz = kittens.slice(1)
+  return kittensz
+}
+
+function removeLastKitten() {
+  var kkittens = kittens.slice(0, kittens.length - 1)
+  return kkittens
+}
