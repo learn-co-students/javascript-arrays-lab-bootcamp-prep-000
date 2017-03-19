45b30ea1b049c2cee3e7867c645057b3c3e5722c
Derek Loyer <derek.loyer@gmail.com>
2017-03-19 22:08:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+var destructivelyAppendKitten = function(name){
+  kittens.push(name)
+}
+
+var destructivelyPrependKitten = function(name){
+  kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = function(name){
+  kittens.pop(name)
+}
+
+var destructivelyRemoveFirstKitten = function(name){
+  kittens.shift(name)
+}
+
+var appendKitten = function(name){
+  return [...kittens, name]
+}
+
+var prependKitten = function(name){
+  return [name, ...kittens]
+}
+
+var removeLastKitten = function(name){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+var removeFirstKitten = function(name){
+  return kittens.slice(1)
+}
