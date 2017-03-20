c07e250e51e1d414dd88a5b0934fe8d7f5fecff1
Edgar Pavlovsky <edgarpavlovsky@gmail.com>
2017-03-20 22:09:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ['Milo','Otis','Garfield']
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var k2 = [...kittens,name]
+  return k2
+}
+
+function prependKitten(name){
+  var k2 = [name,...kittens]
+  return k2
+}
+
+function removeLastKitten(){
+  var k2 = kittens.slice(0,kittens.length - 1)
+  return k2
+}
+
+function removeFirstKitten(){
+  var k2 = kittens.slice(1)
+  return k2
+}
