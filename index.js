9f2ddef09b17ae2573cb63fc9e7bf395986d8c53
Stephanie Seib <sseib138@gmail.com>
2017-03-21 20:18:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(){
+kittens.push("Ralph")
+};
+
+
+function destructivelyPrependKitten(){
+kittens.unshift("Bob")
+};
+
+function destructivelyRemoveLastKitten(){
+kittens.pop()
+};
+
+function destructivelyRemoveFirstKitten(){
+kittens = kittens.slice(1)
+};
+
+function appendKitten(){
+return[...kittens, "Broom"]
+}
+
+function prependKitten(){
+return["Arnold",...kittens]
+}
+
+function removeLastKitten(){
+return  kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+return  kittens.slice(1)
+}
