ad094ad26d09b84bfb554d17304d6d8e907a52bc
Justin Kahn <jdavidkahn@gmail.com>
2017-03-21 00:17:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+kittens.push(name);
+return kittens;
+}
+
+function destructivelyPrependKitten(name){
+kittens.unshift(name);
+return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var moreKittens=[...kittens]
+  moreKittens.push(name)
+  return moreKittens
+
+}
+
+function prependKitten(name){
+var moreKittens=[...kittens]
+moreKittens.unshift(name);
+return moreKittens;
+}
+
+function removeLastKitten(){
+  var lessKittens=[...kittens]
+  lessKittens.pop();
+  return lessKittens;
+}
+
+function removeFirstKitten(){
+  var lessKittens=[...kittens]
+  lessKittens.shift();
+  return lessKittens;
+}
