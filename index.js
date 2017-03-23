eb7c32c9415e87f614a0bfbe8bd7af52037c4f1d
Luis Fernando Gutiérrez Romo <is696372@iteso.mx>
2017-03-23 20:18:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens = ['Milo', 'Otis', 'Garfield']
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+function appendKitten(name){
+  return [...kittens, name] //puts name at first
+}
+function prependKitten(name){ //puts name at end
+  return [name, ...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length -1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
