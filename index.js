2d17e41273bc2344095dc20a1d67adfb953f07cd
Deborah Johnson <debjohnson_11@yahoo.com>
2017-03-25 17:16:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name) {
+
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name){
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
