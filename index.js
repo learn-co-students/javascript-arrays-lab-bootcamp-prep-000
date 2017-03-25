c94e64f560e5ef85fa49000b5b9ebcf4dc2e22b2
Dan Blondell <danblondell@gmail.com>
2017-03-25 19:09:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name];
+  return moreKittens
+}
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
+
+function removeLastKitten() {
+  var moreKittens = kittens.slice(0, kittens.length - 1);
+  return moreKittens
+}
+
+function removeFirstKitten() {
+  var moreKittens = kittens.slice(1);
+  return moreKittens
+}
