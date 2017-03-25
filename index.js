3fe1fe8c6442d36d8c3862a80b1cb94e2b51ce41
Prajit Gopal <gprajit@gmail.com>
2017-03-25 19:45:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  name = [...kittens, "Broom"]
+  return name
+}
+
+function prependKitten(name) {
+  name = ["Arnold", ...kittens]
+  return name
+}
+
+function removeLastKitten(name) {
+  var removed = kittens.slice(0, kittens.length - 1)
+  return removed
+}
+
+function removeFirstKitten(name) {
+  var removed = kittens.slice(1)
+  return removed
+}
