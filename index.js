c335af64ab7eef2e97b6dedcb83e34fda93893b5
Rick Love <ricklove@outlook.com>
2017-03-26 22:25:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(array){
+kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(array){
+kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(array){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(array){
+  kittens.shift();
+}
+function appendKitten(array){
+  var newKittens = [...kittens, array];
+  return newKittens;
+  }
+
+function prependKitten(array){
+  var newKitten = [array, ...kittens];
+  return newKitten;
+}
+
+function removeLastKitten(array){
+return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten(array){
+  return kittens.slice(1);
+}
