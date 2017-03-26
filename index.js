1f68aea9fea374c520e43d8e553da7acaceb7095
David Schlauderaff <dschlaud@gmail.com>
2017-03-26 01:25:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var moreKittens = [...kittens, name]
+  return moreKittens
+}
+
+function prependKitten(name){
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
+
+function removeLastKitten(){
+  var lessKittens = kittens.slice(0, kittens.length - 1)
+  return lessKittens
+}
+
+function removeFirstKitten(){
+  var lessKittens = kittens.slice(1)
+  return lessKittens
+}
