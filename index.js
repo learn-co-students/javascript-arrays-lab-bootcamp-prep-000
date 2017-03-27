facdee20f050cfae478939efea82f24db0a95018
Sophia L <soph915@gmail.com>
2017-03-27 01:41:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+
+function appendKitten(name){
+  var kittens1 = [...kittens,name];
+  return kittens1
+}
+
+function prependKitten(name){
+  var kittens1 = [name,...kittens];
+  return kittens1
+}
+
+function removeLastKitten(){
+  var kittens1 = kittens.slice(0,-1)
+  return kittens1
+}
+
+function removeFirstKitten(){
+  var kittens1 = kittens.slice(1)
+  return kittens1
+}
