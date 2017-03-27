1a69ce7feab09e3545b7fe2c233712a545dfe7a9
Ben Cooley <bmcooley@gmail.com>
2017-03-27 03:16:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = [...kittens];
+  newArray.pop(); return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = [...kittens];
+  newArray.shift(); return newArray;
+}
