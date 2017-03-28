701cf908b051984465e3e75de5a1a3c5fbb80d8f
mcpccampo <mcp.ccampo@gmail.com>
2017-03-28 22:39:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var array = [...kittens, name]
+  return array;
+}
+
+function prependKitten(name){
+  var array = [name, ...kittens]
+  return array;
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
