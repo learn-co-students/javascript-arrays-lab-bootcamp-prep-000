e17afe7e3c72139b6d08686b0e9619417b041b19
arjb <arjb@users.noreply.github.com>
2017-03-29 12:30:01 +0100

    New Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(array, name) {
 	array.push(name)
