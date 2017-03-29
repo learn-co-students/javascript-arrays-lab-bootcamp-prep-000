db4226835b687a65289917187e21eeef10532c57
arjb <arjb@users.noreply.github.com>
2017-03-29 11:24:23 +0100

    New Commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAddKitten(element) {
+	kittens.push(element)
+	return kittens
+}
+
+function destructivelyPrependKitten(element) {
+	kittens.unshift(element)
+	return kittens
+}
+
+function destructivelyRemoveLastKitten(element) {
+	kittens.pop()
+	return kittens
+}
+
+function destructivelyRemoveFirstKitten(element) {
+	kittens.shift()
+	return kittens
+}
+
+function appendKitten (element) {
+	return [...kittens, element]
+}
+function prependKitten (element) {
+	return [element, ...kittens]
+}
+function removeLastKitten (element) {
+	var newArray = kittens
+	newArray.pop()
+	return newArray
+}
+function removeFirstKitten (element) {
+	var newArray = kittens
+	newArray.shift()
+	return newArray
+}
