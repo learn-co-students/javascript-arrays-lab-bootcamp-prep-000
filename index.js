e0b0dec423ac231b552d99f97ded26d7ec3abcbc
Thomas Cheung <cheungth25@gmail.com>
2017-03-29 20:02:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
