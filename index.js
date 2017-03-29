7aea93bbe82c8bf59ab965f37e44c1b254963c91
Noah Summers <summers.noah@gmail.com>
2017-03-29 08:07:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
