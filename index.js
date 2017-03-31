e1bd3e3882534471e507c65e416a5a8f008e2d28
Dan Strong <dangaston@gmail.com>
2017-03-31 05:04:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var new_kittens = kittens.slice();
+  new_kittens.push(name);
+  return new_kittens;
+}
+
+function prependKitten(name) {
+  var new_kittens = kittens.slice();
+  new_kittens.unshift(name);
+  return new_kittens;
+}
+
+function removeLastKitten() {
+  var new_kittens = kittens.slice();
+  new_kittens.pop();
+  return new_kittens;
+}
+
+function removeFirstKitten() {
+  var new_kittens = kittens.slice();
+  new_kittens.shift();
+  return new_kittens;
+}
