e34d8cd2ff848e917474c36855bc6882d900d6e4
Rosie Martinez <rosiemin11@gmail.com>
2017-04-03 03:20:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+
+
+function destructivelyAppendKitten(array, name){
+  newArray = array.push(name)
+  return newArray
+}
+
+function destructivelyPrependKitten(array, name){
+  array.unshift(name)
+  return array
+}
+
+function destructivelyRemoveLastKitten(array){
+  array.pop()
+  return array
+}
+
+function destructivelyRemoveFirstKitten(array){
+  array.shift()
+  return array
+}
+
+function appendKitten(array, name){
+  return [...array, name]
+}
+
+function prependKitten (array, name){
+  return [name, ...array]
+}
+
+function removeLastKitten(array, name){
+  var newArray = array
+  return newArray.slice(0, newArray.length-1)
+}
+
+function removeFirstKitten(array){
+  var newArray = array
+  return newArray.slice(1)
+}
