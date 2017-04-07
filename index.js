336d9995ce5b31abbd9918c119861185559e9bbe
Joe Cutajar <jcutajar12@gmail.com>
2017-04-07 14:08:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+kittens.push(name);
+
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+ kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+var newKittens = [...kittens]
+ newKittens.push(name)
+return newKittens
+}
+
+function prependKitten(name){
+var newKittens = [...kittens]
+newKittens.unshift(name)
+return newKittens
+}
+
+function removeLastKitten() {
+  var newKitten = [...kittens]
+  newKitten.pop()
+  return newKitten
+}
+
+function removeFirstKitten() {
+  var newKitten = [...kittens]
+  newKitten.shift()
+  return newKitten
+}
