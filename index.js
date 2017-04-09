a0a22eae3db983639939b7d199e4978906516f17
Y J <yjachmann@gmail.com>
2017-04-09 20:31:46 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1)
+  return newArray
+}
