edca901575a35c9e7623f537c38ff2dd43951f06
Chinmay Lonkar <clonkified@gmail.com>
2017-04-09 06:17:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  window.kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  window.kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  window.kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  window.kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...window.kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...window.kittens];
+}
+
+function removeLastKitten() {
+  return window.kittens.slice(0, window.kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return window.kittens.slice(1);
+}
