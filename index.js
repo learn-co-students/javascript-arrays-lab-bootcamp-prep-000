217fb354610f2e210ec2475bdc44bee76802f2fa
Richard Steinbach <steinbach.dick@gmail.com>
2017-04-09 04:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(name)
+}
+function appendKitten(name){
+  var kittens2 = [...kittens, 'Broom']
+  return kittens2
+}
+function prependKitten(name){
+  var kittens2 = ['Arnold', ...kittens]
+  return kittens2
+}
+function removeLastKitten(){
+  var kittens2 = kittens.slice(0, kittens.length - 1)
+  return kittens2
+}
+function removeFirstKitten(){
+  var kittens2 = kittens.slice(1)
+  return kittens2
+}
