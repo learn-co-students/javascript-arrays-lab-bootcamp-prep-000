df80150af68996b0646638b288ae18a5305a5a4e
Avery Carty <avery.carty7@gmail.com>
2017-04-09 14:08:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+    kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.push(name)
+    return kittens
+}
+
+function destructivelyPrependKitten(name) {
+    kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.unshift(name)
+    return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+    kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.pop()
+    return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+    kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.shift()
+    return kittens
+}
+
+function appendKitten(name) {
+    var kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.push(name)
+    var newLitter = kittens
+    return newLitter
+}
+
+function prependKitten(name) {
+    var kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.unshift(name)
+    var newLitter = kittens
+    return newLitter
+}
+
+function removeLastKitten() {
+    var kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.pop()
+    var newLitter = kittens
+    return newLitter 
+}
+
+function removeFirstKitten() {
+    var kittens = ['Milo', 'Otis', 'Garfield']
+    kittens.shift()
+    var newLitter = kittens
+    return newLitter
+}
