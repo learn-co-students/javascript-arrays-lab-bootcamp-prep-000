c20aa48b800301650f237a8b72b0d3ac3a93069a
Sullivan Nolan <nolakat@gmail.com>
2017-04-14 00:20:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+var kitties = [...kittens, name];
+return kitties;
+}
+
+function prependKitten(name){
+  var kitties = [name, ...kittens];
+  return kitties;
+}
+
+function removeLastKitten(){
+  var kitties = kittens.slice(0, 2);
+  return kitties;
+}
+
+function removeFirstKitten(){
+  var kitties = kittens.slice(-2);
+  return kitties;
+}
