3d0bd53c06610372521451bc58361bdf3128282c
Benjamin Coleman <hello@benjamincoleman.me>
2017-04-16 20:31:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+let kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+    kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  let newKittens = [...kittens];
+  newKittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  let newKittens = [...kittens];
+  newKittens.shift();
+  return newKittens;
+}
