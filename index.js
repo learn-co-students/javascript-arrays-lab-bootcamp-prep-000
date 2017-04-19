6421324575177ccb7dbf456159e35e9bc7e14d0b
Brian Lane <mr.brianlane@gmail.com>
2017-04-19 12:35:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var array = [...kittens, name]
+  return array
+}
+
+function prependKitten(name) {
+  var array = [name, ...kittens]
+  return array
+}
+function removeLastKitten(){
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray;
+}
+
+function removeFirstKitten(){
+  var array = kittens.slice(1)
+  return array
+}
