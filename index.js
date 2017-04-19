a11096a77f683deab898b25afb409a3415a379e7
Hassan  Mahad <hmahad0723@gmail.com>
2017-04-19 22:19:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return name;
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
