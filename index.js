4bef9de3b95084e313a8ab6b91362f88ebd9741d
JD Leshan <stritch@gmail.com>
2017-04-19 20:55:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push(name);
+  return newArray;
+}
+function removeLastKitten(name){
+  var newArray = kittens.slice();
+  newArray.pop(name);
+  return newArray;
+}
+function removeFirstKitten(){
+  var newArray = kittens.slice();
+  newArray.shift(name);
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = kittens.slice();
+  newArray.unshift(name);
+  return newArray;
+
+}
