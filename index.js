9795868ffe28cc51aa3f7107d78a86bc8d07dfbc
Emilio da Costa <emidac@gmail.com>
2017-04-20 05:11:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  var a = kittens
+  return a.slice(0,a.length-1)
+}
+
+function removeFirstKitten(){
+  var a = kittens
+  return a.slice(1)
+}
