46d28a779dff3f128535ac70f336593310ed3686
Daiane Bushey <daianebushey@gmail.com>
2017-04-20 21:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  return kittens.push("Ralph");
+};
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift("Bob");
+};
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+};
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+};
+
+function appendKitten(name){
+  var appendedList = [...kittens, "Broom"];
+  return appendedList;
+};
+
+function prependKitten(name){
+  var prependedList = ["Arnald", ...kittens];
+  return prependedList;
+};
+
+function removeLastKitten(){
+  var removedLast = kittens.slice(-1);
+  return removedLast;
+};
+
+function removeFirstKitten(){
+  var removedFirst = kittens.slice(1);
+  return removedFirst;
+};
