087a150e9c649507d820493e4d7a8a6443f50edc
Jeremy Snow <jeremysnow404@yahoo.com>
2017-04-20 17:45:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+function destructivelyAppendKitten (kitten) {
+  kittens.push(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten]
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
