1ef5c2c57211fdbdbec1a326733dc5ef82e68120
Paul Arias <ariaspaul@gmail.com>
2017-04-22 16:56:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newKittens = new Array();
+  for (var i=0;i<kittens.length ; i++) {
+    newKittens.push(kittens[i]);
+  }
+  newKittens.push(name);
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = new Array();
+  for (var i=0;i<kittens.length ; i++) {
+    newKittens.push(kittens[i]);
+  }
+  newKittens.unshift(name);
+  return newKittens
+}
+
+function removeLastKitten(name) {
+  var newKittens = new Array();
+  for (var i=0;i<kittens.length ; i++) {
+    newKittens.push(kittens[i]);
+  }
+  newKittens.pop();
+  return newKittens
+}
+
+function removeFirstKitten(name) {
+  var newKittens = new Array();
+  for (var i=0;i<kittens.length ; i++) {
+    newKittens.push(kittens[i]);
+  }
+  newKittens.shift();
+  return newKittens
+}
