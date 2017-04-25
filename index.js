e0d906b42c569bd353eabeb2359265b0f1e87f31
darryl most <dmost1@gulls.salisbury.edu>
2017-04-25 20:18:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift()
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop()
+}
+
+function removeFirstKitten()
+{
+  kittens.shift()
+}
+
+function appendKitten(name)
+{
+  return [...kittens, name]
+}
+
+function prependKitten(name)
+{
+  return [name, ...kittens]
+}
+
+function removeLastKitten()
+{
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten()
+{
+  return kittens.slice(1)
+}
