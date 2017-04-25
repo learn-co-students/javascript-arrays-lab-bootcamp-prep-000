b61526f096ad0f8a8f077734b968516d9ac86fd4
shuangfei zhou <shuangfei.zhou128@gmail.com>
2017-04-25 20:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten ()
+{
+  kittens.push("Ralph");
+}
+function destructivelyPrependKitten ()
+{
+  kittens.unshift("Bob");
+}
+function destructivelyRemoveLastKitten ()
+{
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten ()
+{
+  kittens.shift();
+}
+function appendKitten ()
+{
+  const moreKittens = [...kittens,"Broom"];
+  return moreKittens;
+}
+function prependKitten ()
+{
+  const moreKittens = ["Arnold",...kittens];
+  return moreKittens;
+}
+function removeLastKitten ()
+{
+  var lessKittens = kittens.slice (0, kittens.length - 1 )
+  return lessKittens;
+}
+function removeFirstKitten ()
+{
+  var lessKittens = kittens.slice (1)
+  return lessKittens;
+}
