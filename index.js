476bfacaaea2d41a30d7aa5eb95ae79540e5f95d
Jennifer Watson <jennifer.watson@me.com>
2017-04-29 13:11:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function Arrays() {
+  kittens();
+}
+
+function kittens() {
+  var kittens = ["Milo",
+                "Otis",
+                "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  //TODO: write code
+}
+
+function destructivelyPrependKitten(name) {
+  //TODO: write code
+}
+
+function destructivelyRemoveLastKitten() {
+  //TODO: write code
+}
+
+function destructivelyRemoveFirstKitten() {
+  //TODO: write code
+}
+
+function appendKitten(name) {
+  //TODO: write code
+}
+
+function prependKitten(name) {
+  //TODO: write code
+}
+
+function removeLastKitten() {
+  //TODO: write code
+}
+
+function removeFirstKitten() {
+  //TODO: write code
+}
