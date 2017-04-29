91f2eb7739f3b617569d18e956107432637b8336
Jennifer Watson <jennifer.watson@me.com>
2017-04-29 14:17:29 +0000

    Done.

--- index.js
+++ index.js
@@ -25,56 +25,20 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  moreKittens = [...kittens, name];
+  var moreKittens = [...kittens, name];
   return moreKittens;
 }
-/* appends a kitten to the kittens array and return
-s a new array, leaving the kittens array unchanged:
 
-      Error: Expected [ 'Milo', 'Otis', 'Garfield', 'Broom' ] to equal [ 'Milo'
-, 'Otis', 'Garfield' ]
-      + expected - actual
-
-       [
-         "Milo"
-         "Otis"
-         "Garfield"
-      -  "Broom"
-    ]                   */
 function prependKitten(name) {
-  return kittens = [name,...kittens];
+  var evenMoreKittens = [name,...kittens];
+  return evenMoreKittens;
 }
-/* prepends a kitten to the kittens array and retu
-rns a new array, leaving the kittens array unchanged:
-
-      Error: Expected [ 'Arnold', 'Milo', 'Otis', 'Garfield' ] to equal [ 'Milo
-', 'Otis', 'Garfield' ]
-      + expected - actual
-
-       [
-      -  "Arnold"
-         "Milo"
-         "Otis"
-         "Garfield"
-       ]                        */
 
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  kittens = kittens.slice(1);
-  return kittens;
+  var lessKittens = kittens.slice(1);
+  return lessKittens;
 }
-/* removes the first kitten from the kittens array
- and returns a new array, leaving the kittens array unchanged:
-
-      Error: Expected [ 'Otis', 'Garfield' ] to equal [ 'Milo', 'Otis', 'Garfie
-ld' ]
-      + expected - actual
-
-       [
-      +  "Milo"
-         "Otis"
-         "Garfield"
-       ]     */
