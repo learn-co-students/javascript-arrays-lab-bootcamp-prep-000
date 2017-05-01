a0aaa1445d63398c78aac0a88e315bd22593ae0c
luke Winkelmann <lgwink89@yahoo.com>
2017-05-01 23:51:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+ kittens.push(name)
+return kittens;
+};
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+};
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+};
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+};
+function appendKitten(name){
+  return [...kittens, name]
+};
+function prependKitten(name){
+  var kittens =  ['Milo', 'Otis', 'Garfield'];
+  return [name, ...kittens]
+};
+function removeLastKitten(){
+  var kittens =  ['Milo', 'Otis', 'Garfield'];
+  return kittens.slice(0, kittens.length - 1);
+};
+function removeFirstKitten(){
+    var kittens =  ['Milo', 'Otis', 'Garfield'];
+    return kittens.slice(1);
+};
