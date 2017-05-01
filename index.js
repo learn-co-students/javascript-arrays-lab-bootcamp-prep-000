b36cb6fdac2d497b013981e0fcdc3faf63910f7e
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-01 10:54:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+function appendKitten(name) {
+  return [...kittens, name];
+}
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+function removeLastKitten() {
+  var a = kittens.slice(0, kittens.length - 1)
+  return a;
+}
+function removeFirstKitten() {
+  var a = kittens.slice(1)
+  return a;
+}
