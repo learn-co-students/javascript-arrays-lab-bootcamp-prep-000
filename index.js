6ea4ea8300cf59536d4d7067fa68784dee968911
Jamie Smith <jamiesmith357@gmail.com>
2017-05-01 20:20:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var Ralph = "Ralph";
+
+function destructivelyAppendKitten(Ralph) {
+  kittens.push(Ralph);
+
+  return kittens;
+}
+
+function destructivelyPrependKitten(Ralph) {
+  kittens.unshift(Ralph);
+
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+
+  return kittens;
+}
+
+var Broom = "Broom";
+
+function appendKitten(Broom) {
+  var newKittens = [...kittens, Broom];
+
+  return newKittens;
+}
+
+var Arnold = "Arnold";
+
+function prependKitten(name) {
+  var extraKittens = [Arnold, ...kittens];
+
+  return extraKittens;
+}
+
+function removeLastKitten() {
+  var lessKittens = kittens.slice(0, kittens.length -1);
+
+  return lessKittens;
+}
+
+function removeFirstKitten() {
+  var missingKitten = kittens.slice(1);
+
+  return missingKitten;
+}
