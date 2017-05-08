1495669e9b11041d8e0df3708eaa0cd0d4538500
Jared Mitchell <jared.mitchell@yahoo.com>
2017-05-08 14:58:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var a = [...kittens,name]
+  return a
+}
+
+function prependKitten(name) {
+  var a = [name,...kittens]
+  return a
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length-1)
+}
