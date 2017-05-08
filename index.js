d44e5e4a71a61f71c948a80b37fe8d7751b37b0d
Allan Moreno <allanm93@gmail.com>
2017-05-08 00:34:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (element) {
+  return kittens.push(element)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten (element) {
+  return kittens.unshift(element)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten (element) {
+  return kittens.pop(element)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveFirstKitten (element) {
+  return kittens.shift(element)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten (element) {
+  return [...kittens, element]
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function prependKitten (element) {
+  return [element, ...kittens]
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeLastKitten (element) {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten (element) {
+  return kittens.slice(1)
+}
