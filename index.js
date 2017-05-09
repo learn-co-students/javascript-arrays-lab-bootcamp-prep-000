ab9857d974a67302d22df07cb95358bc2cf81595
Pasqualino Petillo <patpetillo@gmail.com>
2017-05-09 00:36:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittensTwo = [...kittens, name]
+  return kittensTwo
+}
+
+function prependKitten(name) {
+  var kittensTwo = [name, ...kittens]
+  return kittensTwo
+}
+
+function removeLastKitten() {
+  var kittensTwo = kittens.slice(0, (kittens.length - 1))
+  return kittensTwo
+}
+
+function removeFirstKitten() {
+  var kittensTwo = kittens.slice(1, kittens.length)
+  return kittensTwo
+}
+
