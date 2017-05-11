d9c70f50a566fad37d6d850be1071a233ed3f212
MaryBeth Dunlap <marybeth_dunlap@yahoo.com>
2017-05-11 19:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name){
+  var kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens = [...kittens, "Broom"];
+  return kittens
+}
+
+function prependKitten(name){
+  var kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens = ["Arnold", ...kittens];
+  return kittens
+}
+
+function removeLastKitten(name){
+  return kittens.slice(0,2)
+}
+
+function removeFirstKitten(name){
+  return kittens.slice(1)
+}
