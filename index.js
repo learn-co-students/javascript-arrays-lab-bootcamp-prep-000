4422919959ea9a41c6a72629f881fef47f5bf03d
Clay Tunica-El <clay.tunicael@yahoo.com>
2017-05-11 05:53:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+ return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name)
+{
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten()
+{
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  return kittens.shift()
+}
+
+function appendKitten(name)
+{
+  var MoreKittens
+  return MoreKittens = [...kittens,name]
+}
+
+function prependKitten(name)
+{
+  var MoreKittens
+  return MoreKittens = [name,...kittens]
+}
+
+function removeLastKitten()
+{
+  var LessKittens
+  return LessKittens = kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten()
+{
+  var LessKittens
+  return LessKittens = kittens.slice(1)
+}
