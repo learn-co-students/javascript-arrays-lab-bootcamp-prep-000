24b7caedda03a92bd5c38536c6faa468d906d566
Christian Oliva <coliva92@gmail.com>
2017-05-11 20:20:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  var kits = [...kittens, name];
+  return kits;
+}
+function prependKitten(name){
+  var kits = [name, ...kittens];
+  return kits;
+}
+function removeLastKitten(){
+  var kits = kittens.slice(0, kittens.length-1);
+  return kits;
+}
+function removeFirstKitten(){
+  var kits = kittens.slice(1);
+return kits;
+}
