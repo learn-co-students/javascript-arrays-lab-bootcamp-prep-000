d1848de9e78f3ec5f5a091426bc1708bc99b9ede
Sam Ojling <s.ojling@gmail.com>
2017-05-15 14:31:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  const arr = [...kittens, name];
+  return arr;
+}
+
+function prependKitten(name) {
+  const arr = [name, ...kittens];
+  return arr;
+}
+
+function removeLastKitten() {
+  const arr = kittens.slice(0, kittens.length - 1);
+  return arr;
+}
+
+function removeFirstKitten() {
+  const arr = kittens.slice(1, kittens.length);
+  return arr;
+}
