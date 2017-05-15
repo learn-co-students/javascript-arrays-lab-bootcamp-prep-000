579a003a394e41d018816f5d4dce37361ff709ca
Amanda Wehrman <arwehrman@gmail.com>
2017-05-15 18:21:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten () {
+  kittens.push("Ralph")
+}
+function destructivelyPrependKitten () {
+  kittens.unshift("Bob")
+}
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten () {
+  kittens.shift()
+}
+function appendKitten(name) {
+var newKittens =  [...kittens]
+newKittens.push(name)
+return newKittens
+}
+function prependKitten(name) {
+var newKittens =  [...kittens]
+newKittens.unshift(name)
+return newKittens
+}
+function removeLastKitten() {
+var newKittens = [...kittens]
+newKittens.pop()
+return newKittens
+}
+function removeFirstKitten() {
+var newKittens = [...kittens]
+newKittens.shift()
+return newKittens
+}
