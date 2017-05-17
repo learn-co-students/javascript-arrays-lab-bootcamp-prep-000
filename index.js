bb860907d556f1c83bb4f8170760df9a7bc92aa6
Steve Moore <stevemoorewebdev@gmail.com>
2017-05-17 19:01:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+ return kittens
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+ return kittens
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+ var temp = [...kittens, name]
+ return temp
+}
+
+function prependKitten(name) {
+ var temp = [name, ...kittens]
+ return temp
+}
+
+function removeLastKitten() {
+  var temp = kittens.slice(0,kittens.length-1)
+ return temp
+}
+
+function removeFirstKitten() {
+  var temp = kittens.slice(1)
+  return temp
+}
