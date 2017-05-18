980c39f5808516b3da04bfa9a3a35a360fcd79bf
Sarah Schaller <sarcsch@aol.com>
2017-05-18 00:24:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name)
+  return kittens
+};
+
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name)
+  return kittens
+};
+
+
+var destructivelyRemoveLastKitten = function(name) {
+  kittens.pop(name)
+  return kittens
+};
+
+
+var destructivelyRemoveFirstKitten = function(name) {
+  kittens.shift()
+  return kittens
+};
+
+
+var appendKitten = function(name) {
+  return [...kittens, name]
+};
+
+
+var prependKitten = function(name) {
+  return [name, ...kittens]
+};
+
+
+var removeLastKitten = function(name) {
+  return kittens.slice(0, kittens.length - 1)
+};
+
+
+var removeFirstKitten = function(name) {
+  return kittens.slice(1)
+};
