b33c84cb990036aa4eea6a4eaf997c65f32a7dd0
Carol Greene <carolgreene3@gmail.com>
2017-05-23 23:01:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  [...kittens, name]
+  return [...kittens, name]
+}
+function prependKitten(name) {
+  [name,...kittens]
+  return [name,...kittens]
+}
+function removeLastKitten() {
+  kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten() {
+  kittens.slice(1)
+  return kittens.slice(1)
+}
