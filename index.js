87438bc49e3ee88bfa9a2ee0142ae24097681c2c
elle lee <eelzil77@gmail.com>
2017-05-24 02:10:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name)
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(name)
+  return kittens
+}
+function appendKitten(name){
+  var newKittens = [... kittens]
+  newKittens.push(name)
+  return newKittens
+}
+function prependKitten(name){
+  var newKittens = [...kittens]
+  newKittens.unshift(name)
+  return newKittens
+}
+function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop(name)
+  return newKittens
+}
+function removeFirstKitten(){
+var newKittens = [...kittens]
+newKittens.shift(name)
+return newKittens
+}
