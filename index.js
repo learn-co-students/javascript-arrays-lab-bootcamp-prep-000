cc7efa5b4b2dec8cb8a5190c65852a9aa12ef60a
Sarizad Mohd Ali <sarizad@icloud.com>
2017-05-25 13:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens = kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newKittens = kittens
+  return [...newKittens, name]
+}
+
+function prependKitten(name) {
+  var newKittens = kittens
+  return [name, ...newKittens]
+}
+
+function removeLastKitten() {
+  var newKittens = kittens
+  newKittens.pop()
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens
+  newKittens.shift()
+  return newKittens
+}
