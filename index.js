b27c61ef3556082e1755a299bf278e029ade0088
Joseph Moloughney <jmoloughney25@gmail.com>
2017-05-30 19:19:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens;
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens;
+}
+var kittens = ['Milo', 'Otis', 'Garfield', 'Broom'];
+function appendKitten(name){
+  return [...kittens, name]
+}
+function prependKitten(name){
+  return [name, ...kittens]
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1)
+}
