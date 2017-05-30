52bc2d54662ca15149f8f19c7a5d12a1dc5776c6
Benjamin Geyer <brgeyer49@gmail.com>
2017-05-30 18:11:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(n){
+  kittens.push(n)
+}
+function destructivelyPrependKitten(n){
+  kittens.unshift(n)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(n){
+  return [...kittens,n]
+}
+function prependKitten(n){
+  return [n,...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
