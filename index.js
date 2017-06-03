aa69e98e1336dd6e5db77986f3049d9df2233098
Eric Rosenberg <eric.rosenberg77@gmail.com>
2017-06-03 14:57:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(array){
+  return kittens.push('Ralph')
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function destructivelyPrependKitten(array){
+  return kittens.unshift('Bob')
+}
+
+function appendKitten(a){
+  var freshKittens = [...kittens, a]
+  return freshKittens
+}
+
+function prependKitten(a){
+  var freshKittens = [a, ...kittens]
+  return freshKittens
+}
+
+function removeLastKitten(){
+  var freshKittens = kittens.slice(0, kittens.length - 1);
+  return freshKittens
+}
+
+function removeFirstKitten(){
+  var freshKittens = kittens.slice(1);
+  return freshKittens
+}
