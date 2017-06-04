d7c3d5d8c60737cab2ecd59385d501d4a39d073a
Milber Ferreira <milberferreira@hotmail.com>
2017-06-04 07:43:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function prependKitten(name) {
+  var newKittens =  [name, ...kittens]
+  return newKittens
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
+}
