fb79bd1cb4236cead68a26eb3f8de890e2cc874c
Lindsey Trainor Trainor <lindseytrainor77@gmail.com>
2017-06-05 22:58:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten(){
+
+var newKitten = kittens.slice()
+newKitten.shift()
+return newKitten
+
+
+}
+
+function removeLastKitten(){
+
+var newKitten = kittens.slice()
+newKitten.pop()
+return newKitten
+
+
+}
+
+function prependKitten(name){
+  var newKitten = kittens.slice()
+  newKitten.unshift(name)
+  return newKitten
+}
+
+function appendKitten(name){
+  var newKitten = kittens.slice()
+  newKitten.push(name)
+  return newKitten
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
