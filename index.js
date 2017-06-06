59ca51eba7c71a8968200cac747b750169baa9e4
Poke Sam <pokesam10031@yahoo.com>
2017-06-06 03:17:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(e){
+  kittens.push(e)
+  return kittens
+}
+
+function destructivelyPrependKitten(e){
+  kittens.unshift(e)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(e){
+  kittens.pop(e)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(e){
+  kittens.shift(e)
+  return kittens
+}
+
+function appendKitten(e){
+  return [...kittens,e]
+}
+
+function prependKitten(e){
+  return [e, ...kittens]
+}
+
+function removeLastKitten(e){
+  return kittens.slice(0, kittens.length - 1)
+
+}
+
+function removeFirstKitten(e){
+return kittens.slice(1)
+}
