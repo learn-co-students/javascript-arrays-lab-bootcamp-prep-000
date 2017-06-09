5a720140216ce1bacae904ebe237f6aebb2434c3
Alec Alba <alecgalba@gmail.com>
2017-06-09 18:48:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop("Garfield")
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift("Otis")
+  return kittens
+}
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, "Broom"]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens2 = ["Arnold", ...kittens]
+  return kittens2
+}
+
+function removeLastKitten() {
+  var kittens2 = kittens.slice(0, 2)
+  return kittens2
+}
+
+function removeFirstKitten() {
+  var kittens2 = kittens.slice(1)
+  return kittens2
+}
