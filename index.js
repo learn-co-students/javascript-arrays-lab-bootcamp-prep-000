e421f05fd27502e36a26216ae3c14878ed92c36c
Roslyn McKenna <roslynayers@gmail.com>
2017-06-09 11:56:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function kittens() {
+  return kittens
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.slice();
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.slice();
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten(name) {
+  var newKittens = kittens.slice();
+  newKittens.pop(name);
+  return newKittens;
+}
+
+function removeFirstKitten(name) {
+  var newKittens = kittens.slice();
+  newKittens.shift(name);
+  return newKittens;
+}
