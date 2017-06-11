61fb94a9eae3b83fe5f7ccc75f55b47eee0c5444
Inca Curwen <incacurwen@gmail.com>
2017-06-11 12:26:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  }
+
+  function destructivelyRemoveLastKitten() {
+    kittens.pop()
+  }
+
+  function destructivelyRemoveFirstKitten() {
+    kittens.shift()
+  }
+
+ function appendKitten(name) {
+  var changedArray = [...kittens, name]
+  return changedArray
+ }
+
+ function prependKitten(name) {
+  var changedArray = [name, ...kittens]
+  return changedArray
+ }
+
+ function removeLastKitten() {
+  var changedArray = kittens.slice(0, kittens.length - 1)
+  return changedArray
+ }
+
+function removeFirstKitten() {
+  var changedArray = kittens.slice(1)
+  return changedArray
+}
