689b58c5dd1b167eef225572b3697b743f72af6c
Sikander Chowhan <sikanderchowhan@gmail.com>
2017-06-12 21:54:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(array, element) {
+  kittens.push("Ralph")
+  return kittens
+}
+function destructivelyPrependKitten(array, element) {
+  kittens.unshift("Bob")
+  return kittens
+}
+function destructivelyRemoveLastKitten(array) {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(array, element) {
+  return [...kittens, "Broom"]
+}
+function prependKitten(array, element) {
+  return ["Arnold", ...kittens]
+}
+function removeLastKitten(array) {
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(array) {
+  return kittens.slice(1)
+}
