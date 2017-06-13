c24bd423c7b425c717b69a4227ebddf6370466de
Levi Shemtov <leviuruguay@gmail.com>
2017-06-13 18:52:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+function removeLastKitten(name) {
+  return kittens.slice(0, -1);
+}
+function removeFirstKitten(name){
+  return kittens.slice(1, kittens.length)
+}
