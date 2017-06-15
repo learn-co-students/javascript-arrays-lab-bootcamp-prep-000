9c41af208da15482412def99b7c0a07516485e26
Anna Bush <annabush092@gmail.com>
2017-06-15 13:10:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+}
+
+function appendKitten(name)
+{
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name)
+{
+  return [name, ...kittens];
+}
+
+function removeLastKitten()
+{
+  return kittens.slice(0, 2);
+}
+
+function removeFirstKitten()
+{
+  return kittens.slice(1, kittens.length);
+}
