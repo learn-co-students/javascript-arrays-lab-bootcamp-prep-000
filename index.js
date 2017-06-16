2d33b622429813c870724a04553196bb366daa5f
James Miele <jnmiele@verizon.net>
2017-06-16 16:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
+
+// function destructivelyAddElementToEndOfArray(currentArray, element) {
+//   currentArray = currentArray.push(element)
+//   console.log(currentArray)
+// }
+
+function destructivelyPrependKitten(name) {
+	return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+	return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+	return kittens.shift()
+}
+
+function appendKitten(name) {
+	var newArray = [...kittens, "Broom"]
+	return newArray
+}
+
+function prependKitten(name) {
+	var newArray = [name, ...kittens]
+	return newArray
+}
+
+function removeLastKitten() {
+	var newArray = kittens.slice(0, kittens.length - 1)
+	return newArray
+}
+
+function removeFirstKitten() {
+	var newArray = kittens.slice(1)
+	return newArray
+}
