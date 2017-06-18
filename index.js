facd43eee1e42a7b30394f344759a0af8d87d71a
Rongrong Liu <rrliu1990.polyu@gmail.com>
2017-06-18 05:33:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
