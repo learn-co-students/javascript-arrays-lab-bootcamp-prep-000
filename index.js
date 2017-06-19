843b9372647786a616f8b7497e3cce0bf47f70f6
Bo Keopaseut <bopk@hawaii.edu>
2017-06-19 00:31:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ['Milo', 'Othis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKitten = [...kittens,name];
+  return newKitten;
+}
+
+function prependKitten(name) {
+  var newKitten = [name, ...kittens];
+  return newKitten;
+}
+
+function removeLastKitten() {
+   var newKitten = kittens.slice(0,-1);
+   return newKitten;
+}
+
+function removeFirstKitten() {
+   var newKitten = kittens.slice(-2);
+   return newKitten;
+}
