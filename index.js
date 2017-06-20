a1315a8a740c202a0480261e4c1951e5f757943b
Kelsey Ojima <kmojima@gmail.com>
2017-06-20 22:35:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return kittens
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return kittens
+}
