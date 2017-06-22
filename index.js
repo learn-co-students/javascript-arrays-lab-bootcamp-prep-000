1ca79fd2b8f0d4df5c00058b57c1be3d8390e825
Dasha Nikitina <dashadariallc@gmail.com>
2017-06-22 07:50:53 +0000

    Done.

--- index.js
+++ index.js
@@ -26,3 +26,13 @@ function prependKitten(name){
   var newKittens = [name, ...kittens]
   return newKittens;
 }
+
+function removeLastKitten(){
+var newKittens = kittens.slice(0, kittens.length-1);
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
