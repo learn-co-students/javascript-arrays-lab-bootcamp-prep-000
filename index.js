7b2b72ae4b4ec1cc26e0005847eb3a4578aaa2db
Ben Reuling <benreuling@hotmail.com>
2017-06-22 03:16:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+function appendKitten(name){
+  var kittens2 = [...kittens,name]
+  return kittens2
+}
+function prependKitten(name){
+  var kittens2 = [name,...kittens]
+  return kittens2
+}
+function removeLastKitten(){
+  var kittens2 = kittens.slice(0,kittens.length-1)
+  return kittens2
+}
+function removeFirstKitten(){
+  var kittens2 = kittens.slice(1)
+  return kittens2
+}
