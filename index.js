5d94ae7e899ca4a26a82bcef39fb9fe34448f65e
Sikander Chowhan <sikanderchowhan@gmail.com>
2017-06-23 01:12:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
-  return kittens
 }
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
-  return kittens
 }
 function destructivelyRemoveLastKitten() {
   kittens.pop()
-  return kittens
 }
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
-  return kittens
 }
 function appendKitten(name) {
   return [...kittens, name]
