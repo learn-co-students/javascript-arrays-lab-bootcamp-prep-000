d648cb014e9c128280b9eef5ffca0f34c38ad0b8
Meghan Nash <meghannenash@gmail.com>
2017-06-27 01:26:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+function removeLastKitten(name) {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+function removeFirstKitten(name) {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
