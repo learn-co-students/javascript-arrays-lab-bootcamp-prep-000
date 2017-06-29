0d756d024bbcfa9e924f4dcd64e885a4bc14e46d
B G <bcgraves8@gmail.com>
2017-06-29 00:46:21 +0000

    Done.

--- index.js
+++ index.js
@@ -17,5 +17,21 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  newkittens = [kittens, ...name];
+  var newkittens = [];
+  newkittens = [...kittens, name];
+  return newkittens
+}
+
+function prependKitten(name) {
+  var newkittens = [];
+  newkittens = [name, ...kittens];
+  return newkittens
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
