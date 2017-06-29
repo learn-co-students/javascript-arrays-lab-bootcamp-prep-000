2dda6c91321168ee170b0cb6439abfe7992f0ff3
Morgan Green <morgreen2123@gmail.com>
2017-06-29 17:26:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+
+function kittens(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  return kittens
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var newArray = []
+  newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name){
+  var newArray = []
+  newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+  var newArray = []
+  newArray = newArray.slice(newArray.length - 1)
+  return newArray
+}
+
+function removeFirstKitten(){
+  var newArray = []
+  newArray = newArray.slice(1)
+  return newArray
+}
