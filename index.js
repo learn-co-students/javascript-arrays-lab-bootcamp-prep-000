3986d2e2dcc4e1c3f74b087fa30d7e3440f892a7
Tyler MacDonald <tmacdonald1@gmail.com>
2017-06-29 03:35:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield",
+]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kitties = [...kittens, "Broom"];
+  return kitties;
+}
+
+function prependKitten(name) {
+  var kitties = ["Arnold", ...kittens];
+  return kitties;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
