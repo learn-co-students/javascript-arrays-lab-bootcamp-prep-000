dc46ca303497c3671779733ba71f5a43e56941c5
Dylan Kershaw <dylankershaw@me.com>
2017-07-02 22:34:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var appendedKittens = [...kittens, name];
+  return appendedKittens;
+}
+
+function prependKitten(name) {
+  var prependedKittens = [name, ...kittens];
+  return prependedKittens;
+}
+
+function removeLastKitten() {
+  var lastKittenRemoved = kittens.slice(0, kittens.length - 1);
+  return lastKittenRemoved;
+}
+
+function removeFirstKitten() {
+  var firstKittenRemoved = kittens.slice(1);
+  return firstKittenRemoved;
+}
