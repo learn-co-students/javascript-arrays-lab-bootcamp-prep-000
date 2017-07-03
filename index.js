e2108dac6b537928a6553d35ddbc465eb1a756b6
Wei-Chun Sheu <weichunsheu@gmail.com>
2017-07-03 21:01:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var soln=kittens.slice(0)
+  soln.push(name)
+  return soln
+}
+
+function prependKitten(name){
+  var soln=kittens.slice(0)
+  soln.unshift(name)
+  return soln
+}
+
+function removeLastKitten(){
+  var soln=kittens.slice(0,kittens.length-1)
+  return soln
+}
+function removeFirstKitten(){
+  var soln=kittens.slice(1)
+  return soln
+}
