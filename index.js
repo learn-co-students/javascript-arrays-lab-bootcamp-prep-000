11ce031f7cce22f58b65a433be2f8bb3732f075a
Christina Pan <christina.pl.pan@gmail.com>
2017-07-08 18:51:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  const newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  const newKittens = [...kittens];
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten() {
+  const newKittens = [...kittens];
+  newKittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  const newKittens = [...kittens];
+  newKittens.shift();
+  return newKittens;
+}
