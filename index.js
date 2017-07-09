bc2aa8e49b7b5bfc0b7a3d02e7d71be362dcb6d8
Ashlee Crusco <ashleecrusco@gmail.com>
2017-07-09 21:52:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newKits = kittens.slice(0);
+  newKits.push(name)
+  return newKits;
+}
+
+function prependKitten(name) {
+  var newKitsPre = kittens.slice(0);
+  newKitsPre.unshift(name);
+  return newKitsPre;
+}
+
+function removeLastKitten() {
+  var newKitsRe = kittens.slice(kittens[0]);
+  newKitsRe.pop();
+  return newKitsRe;
+}
+
+function removeFirstKitten() {
+  var newKitsFirst = kittens.slice(1);
+  return newKitsFirst;
+}
