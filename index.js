3cfc2ebc6a990f1ad9f6de88b1cee0afb6b3de62
Gene Yoo <gene.y.yoo@gmail.com>
2017-07-11 21:18:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name]
+  return moreKittens
+}
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens]
+  return moreKittens
+}
+
+function removeLastKitten() {
+  var moreKittens = kittens.slice(0, kittens.length - 1)
+  return moreKittens
+}
+
+function removeFirstKitten() {
+  var moreKittens = kittens.slice(1)
+  return moreKittens
+}
