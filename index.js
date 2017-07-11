6a2824e92a236740cb39df00ccb9b47a20bd8c5c
John Eckert <johnteckert@gmail.com>
2017-07-11 22:29:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 const app = "I don't do much."
+
+var kittens = ['milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+
+  kittens.push(name);
+
+  return kittens;
+}
+
+function  destructivelyPrependKitten(name) {
+
+  kittens.unshift(name);
+
+  return kittens;
+
+}
+
+function destructivelyRemoveLastKitten(name) {
+
+  kittens.pop(name);
+
+  return kittens;
+
+}
+
+function destructivelyRemoveFirstKitten(name) {
+
+  kittens.shift(name);
+
+  return kittens;
+
+}
+
+function appendKitten(name) {
+
+  var moreKittens = [...kittens, name];
+
+  return moreKittens;
+
+}
+
+function prependKitten(name) {
+
+  var moreKittens = [name, ...kittens];
+
+  return moreKittens;
+
+}
+
+function removeLastKitten() {
+
+  var moreKittens = kittens.slice(0, kittens.length - 1);
+
+  return moreKittens;
+
+}
+
+function removeFirstKitten() {
+
+  var moreKittens = kittens.slice(1);
+
+  return moreKittens;
+
+}
