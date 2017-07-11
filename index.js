4766ae08661ed3c7045bcc792ed19e3b749273e0
Sharowynn Wilson <snwilson@csuchico.edu>
2017-07-11 02:39:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+ kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+function appendKitten(array,name){
+var newArray = [...kittens,name];
+return newArray;
+}
