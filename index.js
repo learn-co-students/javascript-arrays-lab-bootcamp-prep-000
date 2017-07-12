10f1543312e3f1b1f621a6df36cd982be9fe3eb9
Dorothy  Eves <dorothyeves7@gmail.com>
2017-07-12 01:48:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+   kittens.push(name);
+   return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
