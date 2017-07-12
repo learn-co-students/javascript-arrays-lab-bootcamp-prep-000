0d864226dcc9c7f97df552c670543fba95a8867a
Dorothy  Eves <dorothyeves7@gmail.com>
2017-07-12 02:47:27 +0000

    Done.

--- index.js
+++ index.js
@@ -31,13 +31,12 @@ function prependKitten(name){
 
 function removeLastKitten(){
   kittens.slice(0, kittens.length-1);
-  kittens = kittens.slice(0, kittens.length-1);
   return kittens
 }
 /*
 function removeFirstKitten(){
   kittens.slice(1);
   kittens = kittens.slice(1);
-  return kittens
+  return kittens;
 }
 */
