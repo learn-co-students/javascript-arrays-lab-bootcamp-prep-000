6b0ed04519a0c1a2065224a567cd644b84b13f3d
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-07-14 19:13:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newArr = [...kittens, name];
+  return newArr;
+}
+
+function prependKitten(name) {
+  var newArr = [name, ...kittens];
+  return newArr;
+}
+
+function removeLastKitten() {
+  var newArr = [...kittens];
+  newArr.pop();
+  return newArr;
+}
+
+function removeFirstKitten() {
+  var newArr = [...kittens];
+  newArr.shift();
+  return newArr;
+}
