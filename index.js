747fcf60e658b3bebd697d4859a89b8757d0cc86
Laura Webber <wwebby1@gmail.com>
2017-07-15 21:10:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-const app = "I don't do much."
+//var kittens = ['Milo','Otis','Garfield']
+
+function prependKitten(kittens,name){
+  //var kittens = ['Milo','Otis','Garfield']
+  return [name, ...kittens]
+}
+
+function appendKitten(kittens,name){
+  //var kittens = ['Milo','Otis','Garfield']
+  return [...kittens, name]
+}
+
+function destructivelyAppendKitten(kittens,name){
+  //var kittens = ['Milo','Otis','Garfield']
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(kittens,name){
+  //var kittens = ['Milo','Otis','Garfield']
+  kittens.unshift(name)
+  return kittens
+}
+
+function removeLastKitten(kittens){
+  //var kittens = ['Milo','Otis','Garfield']
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(kittens){
+  //var kittens = ['Milo','Otis','Garfield']
+  return kittens.slice(1)
+}
+
+function destructivelyRemoveFirstKitten(kittens){
+  //var kittens = ['Milo','Otis','Garfield']
+  kittens.shift()
+  return kittens
+}
+function destructivelyRemoveLastKitten(kittens){
+  //var kittens = ['Milo','Otis','Garfield']
+  kittens.pop()
+  return kittens
+}
