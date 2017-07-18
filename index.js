4c5be62c7419d9adc744c90a4e18ed02573bb31c
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-07-18 18:34:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-const app = "I don't do much."
+
+var kittens =
+[
+"Milo",
+"Otis",
+"Garfield"
+]
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+  kittens.pop(name)
+  return kittens
+
+}
+
+
+function removeLastKitten()
+{
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function destructivelyRemoveFirstKitten(name)
+{
+  kittens.shift(name)
+  return kittens
+}
+
+function removeFirstKitten()
+{
+  return kittens.slice(1)
+}
+
+function appendKitten(name)
+{
+  return [...kittens, name]
+}
+
+function prependKitten(name)
+{
+  return [name, ...kittens]
+}
