5ccff0dfe3024078ac94295a6cd793dd0f954237
Jennifer Argote <jenniarg21@gmail.com>
2017-07-18 19:05:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, name];
+  return moreKittens;
+}
+
+function prependKitten(name) {
+  var moreKittens = [name, ...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten() {
+  var lessKittens = kittens.slice(0, kittens.length - 1);
+  return lessKittens;
+}
+
+function removeFirstKitten() {
+  var lessKittens = kittens.slice(1);
+  return lessKittens;
+}
