559a4ff0bb08e30a148af5aa35751c8d1b2da3b4
Jeremy Lindstrom <dekard@gmail.com>
2017-07-19 12:51:30 +0000

    All tests pass

--- index.js
+++ index.js
@@ -6,6 +6,42 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
-//test
 
-test
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens]
+  newKittens.push(name)
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [...kittens]
+  newKittens.unshift(name)
+  return newKittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function removeLastKitten() {
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
+}
