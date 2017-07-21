90aaac52d79770987869c98b6996f558cd747990
Stanley Huang Huang <stanhua33@gmail.com>
2017-07-21 08:58:57 +0000

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ function destructivelyRemoveFirstKitten() {
     return kittens
 }
 function appendKitten(name) {
-  kittens.push(name)
-  return kittens.slice(-4)
+var newArray = kittens.slice()
+  newArray.push(name)
+  return newArray
+}
+function prependKitten(name) {
+var newArray = kittens.slice()
+  newArray.unshift(name)
+  return newArray
+}
+function removeLastKitten() {
+var newArray = kittens.slice()
+newArray.pop(name)
+return newArray
+}
+function removeFirstKitten() {
+var newArray = kittens.slice()
+newArray.shift(name)
+return newArray
 }
