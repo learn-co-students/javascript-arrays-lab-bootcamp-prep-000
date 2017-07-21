3ae81dd26a9acc96a599d6866a79b4d30a7e2a57
Stanley Huang Huang <stanhua33@gmail.com>
2017-07-21 08:17:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+    return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+    return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+    return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+    return kittens
+}
+function appendKitten(name) {
+  kittens.push(name)
+  return kittens.slice(-4)
+}
