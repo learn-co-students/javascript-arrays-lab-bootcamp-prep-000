a0a649cf0b527c665d01484c5257b884e7a5fbba
Buena Chan <cybuena@gmail.com>
2017-07-24 01:02:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+};
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+};
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+};
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+};
+
+function appendKitten(name){
+  var newKittens = [...kittens, name];
+  return newKittens
+};
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens];
+  return newKittens
+};
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens
+};
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1, kittens.length);
+  return newKittens
+}
