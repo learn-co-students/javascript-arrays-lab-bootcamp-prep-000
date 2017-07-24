8eec287dee79e573792b2a0e6bdc0325a1d673c0
rb2ep <rbasu412@gmail.com>
2017-07-24 12:49:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+/*
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]*/
+
 const app = "I don't do much."
+
+
+function kittens(){
+    var kittens = [
+    "Milo",
+    "Otis",
+    "Garfield"
+  ]
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+kittens()
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+kittens()
+
+function destructivelyRemoveLastKitten(){
+    kittens.pop()
+}
+kittens()
+
+function destructivelyRemoveFirstKitten(){
+    kittens.shift()
+}
+
+kittens()
+
+function appendKitten(name){
+  var kittens2 = [...kittens,name]
+  return kittens2
+}
+
+function prependKitten(name){
+  var kittens2 = [name,...kittens]
+  return kittens2
+}
+
+function removeLastKitten(){
+  var kittens2 = kittens.slice(0, kittens.length-1)
+  return kittens2
+}
+
+function removeFirstKitten(){
+  var kittens2 = kittens.slice(1)
+  return kittens2
+}
