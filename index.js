39ceeb55f5bda6d57dd4018f71b874e9aecf2410
Daniel Wiehoff <daniel.wiehoff+flatiron@gmail.com>
2017-07-26 16:42:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var temp = kittens.slice();
+  temp.push(name);
+  return temp;
+}
+
+function prependKitten(name) {
+  var temp = kittens.slice();
+  temp.unshift(name);
+  return temp;
+}
+
+function removeLastKitten() {
+  var temp = kittens.slice();
+  temp.pop();
+  return temp;
+}
+
+function removeFirstKitten() {
+  var temp = kittens.slice();
+  temp.shift();
+  return temp;
+}
