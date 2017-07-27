df9527de254d0f61bdf49e6b222b7eb9852a4f86
Philip Allen <philipwallen1@gmail.com>
2017-07-27 19:52:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var answer = [];
+  answer = [...kittens];
+  answer.push(name);
+  return answer;
+}
+
+function prependKitten(name) {
+  var answer = [];
+  answer = [...kittens];
+  answer.unshift(name);
+  return answer;
+}
+
+function removeLastKitten() {
+  var answer = [];
+  answer = [...kittens];
+  answer.pop();
+  return answer;
+}
+
+function removeFirstKitten() {
+  var answer = [];
+  answer = [...kittens];
+  answer.shift();
+  return answer;
+}
