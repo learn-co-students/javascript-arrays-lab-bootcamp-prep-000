fbaa36217a8bdb94f1088506c4b39f67487ca48d
William Hatcher <wjeffhatcher@gmail.com>
2017-07-28 15:58:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+var destructivelyAppendKitten = function(x) {
+  kittens.push(x);
+  return kittens;
+}
+
+var destructivelyPrependKitten = function(x) {
+  kittens.unshift(x);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = function() {
+  kittens.pop();
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = function() {
+  kittens.shift();
+  return kittens;
+}
+
+var appendKitten = function(x) {
+  var newKittens = [...kittens, x];
+  return newKittens;
+}
+
+var prependKitten = function(x) {
+  var newKittens = [x, ...kittens];
+  return newKittens;
+}
+
+var removeLastKitten = function() {
+  var newKittens = kittens.slice(0,kittens.length-1);
+  return newKittens;
+}
+
+var removeFirstKitten = function() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
