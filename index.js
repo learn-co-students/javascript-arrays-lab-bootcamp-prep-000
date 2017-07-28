865fcee7267af510041058408933acd585e1e70b
Victor Roybal Roybal <vroybal1@gmail.com>
2017-07-28 23:50:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+function appendKitten(name) {
+  var array = []
+  array = [...kittens, name]
+  return array
+}
+function prependKitten(name){
+  var array = [];
+    return array = [name ,...kittens]
+}
+function removeLastKitten() {
+  var array = [];
+  array = kittens.slice(0, kittens.length -1)
+  return array
+}
+function removeFirstKitten(){
+  var array = [];
+  array = kittens.slice(1)
+  return array
+}
