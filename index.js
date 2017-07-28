78db80fa2e0d3f33b2096a5cd20e3ff22b847811
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-07-28 21:00:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+var name = name;
+var myKittens = kittens;
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens };
+
+function destructivelyPrependKitten(name)  {
+  kittens.unshift(name)
+  return kittens };
+
+  function destructivelyRemoveFirstKitten() {
+    kittens.shift();
+    return kittens  }
+
+    function destructivelyRemoveLastKitten() {
+      kittens.pop();
+      return kittens}
+
+function appendKitten(name) {
+  return [...kittens, name] }
+
+  function prependKitten(name) {
+    return [name, ...kittens] }
+
+
+    function removeFirstKitten() {
+        myKittens = kittens;
+        return myKittens.slice(1)  }
+
+    function removeLastKitten() {
+      myKittens = kittens;
+      return myKittens.slice(0, myKittens.length - 1)   }
+
+//
