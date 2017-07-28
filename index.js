7d1bfe8659177e58277a517e00edb6e2d37fac0e
Joseph Villa <jvitalian1983@gmail.com>
2017-07-28 16:36:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(array, element){
+  kittens.push("Ralph")
+  return array
+}
+
+function destructivelyPrependKitten(array, element){
+  kittens.unshift("Bob")
+  return array
+}
+
+function destructivelyRemoveLastKitten(array, element){
+  kittens.pop(-1)
+  return array
+}
+
+function destructivelyRemoveFirstKitten(array, element){
+  kittens.shift(0)
+  return array
+}
+
+function appendKitten(array, element){
+  array = [...kittens, "Broom"]
+  return array
+}
+
+function prependKitten(array, element){
+  array = ["Arnold", ...kittens]
+  return array
+}
+
+function removeLastKitten(array, element){
+  array = kittens.slice(0, kittens.length -1)
+  return array
+}
+
+function removeFirstKitten(array, element){
+  array = kittens.slice(1)
+  return array
+}
