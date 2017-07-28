ac1a38cbe94bba569ff3b5b11a8916b664b81377
Guillermo <miranda_hav@yahoo.es>
2017-07-28 17:53:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var result = kittens.slice(0);
+  result.push(name);
+  return result;
+}
+
+function prependKitten(name){
+  var result = kittens.slice(0);
+  result.unshift(name);
+  return result;
+}
+
+function removeLastKitten(){
+  var result = kittens.slice(0);
+  result.pop();
+  return result;
+}
+
+function removeFirstKitten(){
+  var result = kittens.slice(0);
+  result.shift();
+  return result;
+}
