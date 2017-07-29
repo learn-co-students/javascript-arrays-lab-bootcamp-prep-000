42b6f2ac9f60c642876ae4406ee2c50578aabb31
Jamshidbek Mirzakhalov <mirzakhalov@mail.usf.edu>
2017-07-29 13:46:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+const app = "I don't do much"
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(element){
+  kittens.push(element);
+  return kittens
+
+}
+
+function destructivelyPrependKitten(element){
+  kittens.unshift(element);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(element){
+  kittens.pop(element);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(element){
+  kittens.shift(element);
+  return kittens
+}
+
+function appendKitten(element){
+  return [...kittens, element];
+}
+
+function prependKitten(element){
+  return [element, ...kittens]
+}
+
+function removeLastKitten(element){
+  return kittens.slice(0, kittens.length  - 1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1, kittens.length);
+}
