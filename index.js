bf314e54aa1253c3b133e26ff3a4d154a8a7dc30
Chad Montoya <chadmontoya21@yahoo.com>
2017-07-30 04:40:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+ kittens.push(name)
+
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+function appendKitten(name) {
+var result = [kittens.length]
+result.push(name)
+return result
+}
