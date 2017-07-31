e72cd6c3cc99e54f4e567775a4ed128264b08c34
CHUN WONG <jimjwong@gmail.com>
2017-07-31 01:01:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (name) {
+  kittens.push (name)
+  return kittens
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift (name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop (name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift (name)
+  return kittens
+}
+
+function appendKitten (name) {
+  var theKittens = [...kittens]
+  theKittens.push (name)
+  return theKittens
+}
+
+function prependKitten (name) {
+  var theKittens = [...kittens]
+  theKittens.unshift (name)
+  return theKittens
+}
+
+function removeLastKitten (name) {
+  var theKittens = [...kittens]
+  theKittens.pop (name)
+  return theKittens
+}
+
+function removeFirstKitten (name) {
+  var theKittens = [...kittens]
+  theKittens.shift (name)
+  return theKittens
+}
