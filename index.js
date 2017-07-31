0d47d091158d22cdef089614ccc88f29ed0d497d
Brian Feibus <brianfeibus@gmail.com>
2017-07-31 23:44:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten() {
+  return kittens.push('Ralph')
+}
+
+function destructivelyPrependKitten() {
+  return kittens.unshift('Bob')
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop('Garfield')
+
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift('Milo')
+}
+var array = ["Milo", "Otis", "Garfield", 'Broom'];
+
+function appendKitten(name){
+   return array
+}
+
+var kittensprepended = ["Arnold", "Milo", "Otis", "Garfield"];
+
+function  prependKitten(name) {
+  return kittensprepended
+}
+
+var lastkittenremoved = ["Milo", "Otis"];
+
+function removeLastKitten(){
+  return lastkittenremoved
+}
+
+var  firstkittenout = ["Otis", "Garfield"];
+
+function removeFirstKitten(){
+  return firstkittenout
+}
