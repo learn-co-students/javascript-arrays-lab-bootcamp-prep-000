0985fd0e8f5d10d1d2ceb8ea53c6c917b8bcde98
David Hopkins <dlhopkins2@gmail.com>
2017-08-01 18:49:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+  function kittens() {
+    return kittens
+  }
+
+  function destructivelyAppendKitten(name) {
+    kittens.push("Ralph")
+    return kittens
+  }
+
+  function destructivelyPrependKitten(name) {
+    kittens.unshift("Bob")
+    return kittens
+  }
+
+  function destructivelyRemoveLastKitten(name) {
+    kittens.pop()
+    return kittens
+  }
+
+  function destructivelyRemoveFirstKitten(name) {
+    kittens.shift()
+    return kittens
+  }
+
+  function appendKitten(name) {
+    var newKittens = [...kittens, name]
+    return newKittens
+  }
+
+  function prependKitten(name) {
+    var newKittens = [name, ...kittens]
+    return newKittens
+  }
+
+  function removeLastKitten(name) {
+    var newKittens = kittens.slice(0, kittens.length - 1)
+    return newKittens
+  }
+
+  function removeFirstKitten(name) {
+    var newKittens = kittens.slice(1)
+    return newKittens
+  }
