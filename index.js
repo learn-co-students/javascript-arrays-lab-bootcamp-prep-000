ec30bd46b9bdbb9430e073aa7ac8a47b7a7498cf
Richard Chang <rchang9999@yahoo.com>
2017-08-02 21:58:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const app = "I don't do much."
-
+var kittens=['Milo', 'Otis', 'Garfield']
 
 function kittens(){
-  return kittens=['Milo', 'Otis', 'Garfield']
+  return kittens
 }
 
 function destructivelyAppendKitten(){
-  var kittens=['Milo', 'Otis', 'Garfield']
-  return kitten.push("Ralph")
+  return kittens.push("Ralph")
 }
 
 function destructivelyPrependKitten(){
-var kittens=['Milo', 'Otis', 'Garfield']
-return kitten.shift("Bob")
+  return kittens.unshift("Bob")
 }
 
+
+
 function destructivelyRemoveLastKitten(){
-var kittens=['Milo', 'Otis', 'Garfield']
-return kitten.pop()
+return kittens.pop()
 }
 
 function destructivelyRemoveFirstKitten(){
-  var kittens=['Milo', 'Otis', 'Garfield']
-  return kitten.shift()
+  return kittens.shift()
 }
 
-function appendKitten(){
-  var kittens=['Milo', 'Otis', 'Garfield']
-  var newArray=[kittens.slice(0,kittens.length),"Broom"]
+function appendKitten(name){
+  var newArray=[...kittens, name]
 
   return newArray
+}
+
+function prependKitten(name){
+  var newArray=[name,...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
 }
