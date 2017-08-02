b02ee144eb0d69302878ded3bede3ca9a70451cc
Richard Chang <rchang9999@yahoo.com>
2017-08-02 21:30:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+
+function kittens(){
+  return kittens=['Milo', 'Otis', 'Garfield']
+}
+
+function destructivelyAppendKitten(){
+  var kittens=['Milo', 'Otis', 'Garfield']
+  return kitten.push("Ralph")
+}
+
+function destructivelyPrependKitten(){
+var kittens=['Milo', 'Otis', 'Garfield']
+return kitten.shift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+var kittens=['Milo', 'Otis', 'Garfield']
+return kitten.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  var kittens=['Milo', 'Otis', 'Garfield']
+  return kitten.shift()
+}
+
+function appendKitten(){
+  var kittens=['Milo', 'Otis', 'Garfield']
+  var newArray=[kittens.slice(0,kittens.length),"Broom"]
+
+  return newArray
+}
