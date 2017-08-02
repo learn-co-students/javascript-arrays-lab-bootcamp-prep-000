818577e8f5b4a07d07d6e86e17ab4710ae442ec0
Juliet <jktallmer2@gmail.com>
2017-08-02 20:36:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+   kittens.push(name);
+   return kittens;
+ }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(name)
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens = [name,...kittens]
+  return newKittens
+}
+
+function removeLastKitten(name){
+  var newKittens = kittens.slice (0, kittens.length - 1)
+  return newKittens
+}
+
+function removeFirstKitten(name){
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
