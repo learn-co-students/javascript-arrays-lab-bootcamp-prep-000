de865a0faed06c506abb97630e9f6975fb5df88a
adriennekinman <adrienne.kinman@gmail.com>
2017-08-03 00:51:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [(name), ...kittens]
+}
+
+function removeLastKitten(){
+  var newkitten = kittens.slice(0, kittens.length - 1)
+  return newkitten
+}
+
+function removeFirstKitten(){
+  var newkittens = kittens.slice(1)
+  return newkittens
+}
