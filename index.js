1bda8c509b491d91332ae1c46e24b3a5e900ea07
George Yudin <george.yudin@yahoo.com>
2017-08-04 06:52:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+kittens.push(name);
+}
+function destructivelyPrependKitten(name) {
+kittens.unshift(name);
+}
+function destructivelyRemoveFirstKitten() {
+kittens.shift();
+}
+function destructivelyRemoveLastKitten() {
+kittens.pop();
+}
+
+function appendKitten(name) {
+var kittens2 = kittens.concat(name);
+return kittens2;
+}
+
+function prependKitten(name) {
+return [name,...kittens];
+}
+
+function removeFirstKitten() {
+  var kittens3 = kittens.slice(1);
+  return kittens3;
+}
+function removeLastKitten() {
+  var kittens3 = kittens.slice(0, kittens.length - 1);
+  return kittens3;
+}
