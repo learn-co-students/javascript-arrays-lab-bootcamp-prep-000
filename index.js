cf4743d5f904bd6fc640bd327b8a36fe2291002d
Michael Hirschberger <mlhirschberger@gmail.com>
2017-08-06 19:53:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, name];
+  return kittens2;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function prependKitten(name) {
+  var kittens2 = [name, ...kittens];
+  return kittens2;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function removeLastKitten() {
+  var kittens2 = kittens.slice(0, kittens.length - 1);
+  return kittens2;
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function removeFirstKitten() {
+  var kittens2 = kittens.slice(1);
+  return kittens2;
+}
