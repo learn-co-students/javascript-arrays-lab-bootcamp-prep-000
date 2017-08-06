82d01cf64d81185cabdbc123adcd572d83c5d9a9
Adil Asif <asifadilanis@gmail.com>
2017-08-06 01:20:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var result = [...kittens, name];
+  return result;
+}
+
+function prependKitten(name) {
+  var result = [name, ...kittens];
+  return result;
+}
+
+function removeLastKitten() {
+  var result = kittens.slice(0, kittens.length-1);
+  return result;
+}
+
+function removeFirstKitten() {
+  var result = kittens.slice(1);
+  return result;
+}
