3fe8cda4ae34e99df15de3bdef50fdca29432cf5
MarkoBresecke <mb@soul-sign.de>
2017-08-08 10:15:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+//var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(kitten) {
+  kittens.pop(kitten)
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift(kitten)
+}
+
+function appendKitten(kitten) {
+  var kittensNonDestructive = kittens.slice()
+  kittensNonDestructive.push(kitten)
+  return(kittensNonDestructive)
+}
+
+function prependKitten(kitten) {
+  var kittensNonDestructive = kittens.slice()
+  kittensNonDestructive.unshift(kitten)
+  return(kittensNonDestructive)
+}
+
+function removeLastKitten(kitten) {
+  var kittensNonDestructive = kittens.slice()
+  kittensNonDestructive.pop(kitten)
+  return(kittensNonDestructive)
+}
+
+function removeFirstKitten(kitten) {
+  var kittensNonDestructive = kittens.slice()
+  kittensNonDestructive.shift(kitten)
+  return(kittensNonDestructive)
+}
