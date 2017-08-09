d163f5f476e4cd9cb0edd5de9e3bfc3fbae097be
brentleeper <brentleeper@me.com>
2017-08-09 15:56:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+console.log(`test ${kittens}`)
+
+function destructivelyAppendKitten(kitten){
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(kitten){
+  var newKittens = [...kittens, kitten]
+  return newKittens
+}
+
+function prependKitten(kitten){
+  var newKittens = [kitten, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return  kittens.slice(1)
+}
