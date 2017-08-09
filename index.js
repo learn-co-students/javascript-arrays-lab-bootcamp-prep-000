0b9eff1e65be0c967535824f7f27b476d49cd5f5
Dana  G <giordano.dana@gmail.com>
2017-08-09 03:59:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+destructivelyAppendKitten('Ralph')
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+
+}
+destructivelyPrependKitten('Ralph')
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+destructivelyRemoveLastKitten()
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+destructivelyRemoveFirstKitten()
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+function appendKitten(name) {
+  var Boo = [...kittens, name]
+  return Boo
+}
+appendKitten('Broom')
+
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+function prependKitten(name) {
+  var Boop = [name, ...kittens]
+  return Boop
+}
+prependKitten("Arnold")
+
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+function removeLastKitten() {
+  var Beep = kittens.splice(-1)
+  return Beep
+}
+removeLastKitten()
