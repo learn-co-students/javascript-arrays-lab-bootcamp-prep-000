0c4e0652b03de706c9799b6238e45b3d6fe6dd5e
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-08-10 18:35:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+// unshift is destructive and adds
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+// shift is destructive and removes first
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+//work on this
+function appendKitten(name){
+  return [...kittens, name]
+}
+function prependKitten(name){
+  return [name, ...kittens]
+}
+// work on this
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
