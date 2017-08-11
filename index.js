7cf724795e856936ea779a79c115cb3d58ff5979
Vanessa George <vanessa.s.george@gmail.com>
2017-08-11 16:51:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return [ ...kittens, name]
+}
+
+function prependKitten(name) {
+    return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  var newkittens = kittens.slice(0, kittens.length -1)
+  return newkittens
+}
+
+function removeFirstKitten() {
+  var newkittens = kittens.slice(1, kittens.length)
+  return newkittens
+}
