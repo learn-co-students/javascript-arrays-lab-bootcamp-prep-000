2607ed3affb0369088babb8ae5078f61f78e848a
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-11 17:20:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+
+function kittens(){
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens
+}
+
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+   var kittens2 = kittens.slice(0,2)
+   return kittens2
+}
+
+function removeFirstKitten(){
+   var kittens2 = kittens.slice(1,3)
+   return kittens2
+}
