e558d9d0c4805339abf32dda4bf992daf67b2e7e
Haley Park <hparkit@gmail.com>
2017-08-12 23:35:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+//kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var newkitten = [...kittens, name]
+  return newkitten
+}
+
+function prependKitten(name) {
+  var newkitten = [name, ...kittens]
+  return newkitten
+}
+
+function removeLastKitten() {
+  var newkitten = kittens.slice(0, kittens.length - 1)
+  return newkitten
+}
+
+function removeFirstKitten() {
+  var newkitten = kittens.slice(1)
+  return newkitten
+}
