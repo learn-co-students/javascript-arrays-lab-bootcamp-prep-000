462cdc6f9347609b7217239e2fa0fc93f98fd3e6
Joshua Wasserman Wasserman <wassermj@union.edu>
2017-08-14 23:37:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray
+}
+function prependKitten(name){
+  var newArray = [name ,...kittens]
+  return newArray
+}
+function removeLastKitten(){
+  var newArray = [...kittens]
+  newArray.pop()
+  return newArray
+}
+function removeFirstKitten(){
+  var newArray = [...kittens]
+  newArray.shift()
+  return newArray
+}
