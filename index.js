3848edfb13ce020acf35a853f73f2d955c0f5e30
Dakota Lillie <bewguy101@gmail.com>
2017-08-15 16:48:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
