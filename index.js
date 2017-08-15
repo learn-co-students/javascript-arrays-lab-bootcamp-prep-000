edf4ea30420940196fbec5208477df690a208b6b
Jon Edgeworth Edgeworth <jredgeworth@gmail.com>
2017-08-15 23:38:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var c = [...kittens, name];
+  return c;
+}
+
+function prependKitten(name) {
+  var c = [name, ...kittens];
+  return c;
+}
+
+function removeFirstKitten() {
+  var c = kittens.slice(1);
+  return c;
+}
+
+function removeLastKitten() {
+  var c = kittens.slice(0, kittens.length -1);
+  return c;
+}
