cd6f4ee2c6410a0b90f97eabfbbf7a5ed650f2ab
Andras Lenart <andraslenart0@gmail.com>
2017-08-16 17:28:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten() {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, length.kittens - 1)
+
+}
+
+function removeFirstKitten() {
+  var kittens1 = kittens
+  kittens1.slice()
+  return kittens1
+}
