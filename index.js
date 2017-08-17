6a9370a6a2963978a39b1975c7697a7e0034a105
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-08-17 02:42:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten (name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten (name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return  kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten (name){
+  var array=[...kittens, name]
+  return array
+}
+
+function prependKitten (name){
+  var array= [name, ...kittens]
+  return array
+}
+
+function removeLastKitten (name){
+  kittens.slice(0, kittens.length -1)
+}
