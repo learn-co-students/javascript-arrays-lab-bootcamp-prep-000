ffa94ede6db4aca8f11a24cd4a6d6af662d3f55d
VICTOR Alfonzo <yassenca@gmail.com>
2017-08-19 00:37:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(Bob){
+  kittens.unshift(Bob)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(Garfield){
+  kittens.pop(Garfield)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(Bob){
+  kittens.shift(Bob)
+  return kittens
+}
+
+function appendKitten(name){
+  return [...kittens, "Broom"]
+}
+function prependKitten(name){
+  return ["Arnold",...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
