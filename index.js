bff936817977640e725f8e0ecc5e370aa7dc147e
zarli tun burrow <zarli1977@gmail.com>
2017-08-19 17:19:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(element) {
+  kittens.push(element)
+  return kittens
+}
+
+function destructivelyPrependKitten(element) {
+  kittens.unshift(element)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(element) {
+  kittens.pop(element)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(element) {
+  kittens.shift(element)
+  return kittens
+}
+
+function appendKitten(element) {
+  return [...kittens, "Broom"]
+}
+
+function prependKitten(element) {
+  return ["Arnold", ...kittens]
+}
+
+function removeLastKitten(element) {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(element) {
+  return kittens.slice(1)
+}
