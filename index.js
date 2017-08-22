00d7863e1748eb981d54549ebd9d144f12a2fcd7
Ibro C <wengibz@gmail.com>
2017-08-22 17:26:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+var kittens=["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name) {  return kittens.push(name)}
+
+function appendKitten (name){ return [...kittens, name]}
+
+function destructivelyPrependKitten(name) { return kittens.unshift(name)}
+
+function prependKitten(name) {  return [name, ...kittens]}
+
+function destructivelyRemoveLastKitten() {  return kittens.pop()}
+
+function removeLastKitten() { return kittens.slice(0,kittens.length-1)}
+
+function destructivelyRemoveFirstKitten() { return kittens.shift()}
+
+function removeFirstKitten() {  return kittens.slice(1)}
