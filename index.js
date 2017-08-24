754b5470fdb9c11fb1953dac78c4b9daecbe719c
Damien Richcreek <richcreekd@live.com>
2017-08-24 06:13:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+const kittens = [
+  `Milo`,
+  `Otis`,
+  `Garfield`
+]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newArray = kittens.concat(name)
+  return newArray
+}
+
+function prependKitten(name) {
+  var moreKittens = [name,...kittens]
+  return moreKittens
+}
+
+function removeLastKitten() {
+  var evenMoreKittens = kittens.slice(0, kittens.length - 1)
+  return evenMoreKittens
+}
+
+function removeFirstKitten() {
+  var oneLessKitten = kittens.slice(1)
+  return oneLessKitten
+}
