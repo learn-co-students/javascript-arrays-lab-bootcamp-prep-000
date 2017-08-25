170e7e718677de58e93d92e93692e8735f34927b
Andrey Gribanov <gribanovandrew@gmail.com>
2017-08-25 20:11:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const app = "I don't do much."
 
-function kittens() {
-  var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+
+// destructivelyAppendKitten("Boris")
+
+//
+// console.log(appendKitten("Boris"));
