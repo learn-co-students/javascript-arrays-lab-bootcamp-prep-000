621d07caac6055a11efa4e612789ddb809080239
Robert Tabares <robtabares92@gmail.com>
2017-08-26 23:41:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift()
+}
+function appendKitten(name){
+  var oneMore = [...kittens, 'Broom']
+  return oneMore
+}
+function prependKitten(name){
+  var lastOne = ['Arnold', ...kittens]
+  return lastOne
+}
+function removeLastKitten(){
+  var remL = kittens.slice(0, kittens.length -1)
+  return remL
+}
+function removeFirstKitten(){
+  var remF = kittens.slice(1)
+  return remF
+}
