2acb354857252c17de5594881496525d885e516c
Marlon DuPain <marlonsaddress@gmail.com>
2017-08-27 17:36:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-var kittens = ["Milo", "Otis", "Garfield"];
+var kittens = [`Milo`, `Otis`, `Garfield`];
 
-function destructivelyAppendKitten(name) {
-  return kittens.push(name);
+function destructivelyAppendKitten(element){
+  return kittens.push(element);//adds element to end of array
 }
 
-function destructivelyPrependKitten(name) {
-  return kittens.unshift(name);
+function destructivelyPrependKitten(element){
+  return kittens.unshift(element);//adds element to beginning of array
 }
 
-function destructivelyRemoveLastKitten() {
-  return kittens.pop();
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()//removes last element from array
 }
 
-function destructivelyRemoveFirstKitten() {
-  return kittens.shift();
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()//removes first eleemnt from array
 }
 
-function appendKitten(name) {
-  return [...kittens, name];
+function appendKitten(element){
+  var appendedArray = [...kittens, element];
+  return appendedArray;//adds element to end and returns new array
 }
 
-function prependKitten(name) {
-  return [name, ...kittens];
+function prependKitten(element){
+  var prependedArray = [element, ...kittens];
+  return prependedArray;//adds element to beginning and returns new array
 }
 
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1);
+function removeLastKitten(){
+  var lastKittenRemoved = kittens.slice(0, -1);
+  return lastKittenRemoved;//removes last element and returns array
 }
 
-function removeFirstKitten() {
-  return kittens.slice(1);
+function removeFirstKitten(){
+  var firstKittenRemoved = kittens.slice(1);
+  return firstKittenRemoved;//removes frist element and returns new array
 }
