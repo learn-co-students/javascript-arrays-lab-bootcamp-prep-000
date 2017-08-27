e9323be6e2411569ec44dc2b393e2ff6d2608e07
Sani Abubakar <egbaojo@gmail.com>
2017-08-27 08:10:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+var name = "Ralph"
+function kittens() {
+  return kittens
+}
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(1)
+  return kittens
+}
+function appendKitten(name) {
+  var kitten = [...kittens,'Broom']
+  return kitten
+}
+function prependKitten(name) {
+  var kitten = ['Arnold', ...kittens]
+  return kitten
+}
+function removeLastKitten() {
+ var kitten = kittens.slice(0, -1)
+ return kitten
+}
+function removeFirstKitten() {
+  var kitten = kittens.slice(-2)
+  return kitten
+}
