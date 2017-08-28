8da5a961a49e5fd270a4f4ae291b1b178d81fc48
Jon Wu <jon@j-wu.com>
2017-08-28 22:40:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kitten){
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(kitten){
+  return kittens.pop(kitten)
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+  return kittens.shift(kitten)
+}
+
+function appendKitten(kitten){
+  var new_kittens
+  return new_kittens = [...kittens, kitten]
+}
+
+function prependKitten(kitten){
+  var new_kittens
+  return new_kittens = [kitten, ...kittens]
+}
+
+function removeLastKitten(kitten){
+  var new_kittens
+  return new_kittens = kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(kitten){
+  var new_kittens
+  return new_kittens = kittens.slice(1)
+}
