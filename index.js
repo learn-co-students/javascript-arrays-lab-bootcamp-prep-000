eeadcd10abedde5a9622739a7dcd7d95b6128dd3
Ryan Mascolo <rjmascolo@gmail.com>
2017-08-28 23:31:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var new_kittens = kittens
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  var new_kittens = kittens
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  var new_kittens = kittens.slice(0, kittens.length -1)
+  return new_kittens
+}
+function removeFirstKitten(){
+  var new_kittens = kittens.slice(1)
+  return new_kittens
+}
