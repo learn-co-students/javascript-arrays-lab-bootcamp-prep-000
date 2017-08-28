f0f4bc85d70af64af9db1e0f0b03cdefd1942074
aakkad <ayla_akkad@hotmail.com>
2017-08-28 15:37:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(){
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(){
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(){
+  var appendKittens = [...kittens, "Broom"]
+  return appendKittens
+}
+
+function prependKitten(){
+  var prependKittens = ["Arnold", ...kittens]
+  return prependKittens
+}
+
+function removeLastKitten(){
+  var lastkitten = kittens.slice(0, kittens.length - 1)
+  return lastkitten
+}
+
+function removeFirstKitten(){
+  var removekitten = kittens.slice(1)
+  return removekitten
+}
