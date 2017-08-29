c04837a2de2de0ff7ce5593537c7ae0778fcd3af
Cayla Schwartz <cayschwartz@gmail.com>
2017-08-29 23:52:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(element){
+  kittens.push(element)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+
+}
+
+function prependKitten(name){
+  return[name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
