648ef4ee8b706a9b5a32ab1591a7f0c04ea829b3
Joshua Burke <burke.joshua.james@gmail.com>
2017-08-31 23:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(element) {
+  kittens.push(element)
+}
+
+function destructivelyPrependKitten(element) {
+  kittens.unshift(element)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(element) {
+  return [...kittens, element]
+}
+
+function prependKitten(element) {
+  return [element, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length)
+}
