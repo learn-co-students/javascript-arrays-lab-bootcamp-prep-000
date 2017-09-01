951f6becb81c80c79a55fe99dd37dcdcd68c3d50
Chris Snyder <christopher.snyder.m+flatiron@gmail.com>
2017-09-01 00:59:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var a2 = [...kittens, name];
+  return a2;
+}
+
+function prependKitten(name) {
+  var a2 = [name, ...kittens];
+  return a2;
+}
+
+function removeLastKitten() {
+  var a2 = kittens.slice(0,kittens.length-1);
+  return a2;
+}
+
+function removeFirstKitten() {
+  var a2 = kittens.slice(1,kittens.length);
+  return a2;
+}
