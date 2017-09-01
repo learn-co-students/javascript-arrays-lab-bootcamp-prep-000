d0d3be11728dd10bd86434fa095a1b42f3a79f16
Christian Chamars <c.chamars@gmail.com>
2017-09-01 00:53:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+  return kittens
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens = kittens.slice(1)
+  return kittens
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
