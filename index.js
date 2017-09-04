76031d90520da0189c1e4ab1a627fd8e93becfb8
Katherine He <sakura_saki321@yahoo.com>
2017-09-04 17:16:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+ var kittens2 = [...kittens, name]
+ return kittens2
+}
+
+function prependKitten(name) {
+ var kittens2 = [name, ...kittens]
+ return kittens2
+}
+
+function removeLastKitten() {
+  var kittens2 = kittens.slice(0, kittens.length-1)
+  return kittens2
+}
+
+function removeFirstKitten() {
+  var kittens2 = kittens.slice(1)
+  return kittens2
+}
