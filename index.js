5122ca897b18f113bbc161dd7d3e90e9fb75879e
Crista Cooper <aine3d@gmail.com>
2017-09-05 01:55:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var newarray = [...kittens, name]
+  return newarray
+}
+
+function prependKitten(name) {
+  var newarray = [name, ...kittens]
+  return newarray
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
