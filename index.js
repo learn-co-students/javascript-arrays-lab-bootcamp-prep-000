386bba3149e581c17ebffc764c5ef7ec0b03b549
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 01:12:18 +0000

    Done.

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
 return [...kittens, name]
 }
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
