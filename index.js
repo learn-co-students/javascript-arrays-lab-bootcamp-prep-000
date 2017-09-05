504c877a6a2990da7b3ea5b431bf8c2d5e496171
James Abels <abelsj60@gmail.com>
2017-09-05 00:24:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+
+    return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newArr = [...kittens, name];
+
+  return newArr;
+}
+
+function prependKitten(name) {
+  var newArr = [name, ...kittens];
+
+  return newArr;
+}
+
+function removeLastKitten() {
+  var newArr = kittens.slice(0, kittens.length - 1);
+
+  return newArr;
+}
+
+function removeFirstKitten() {
+  var newArr = kittens.slice(1);
+
+  return newArr;
+}
