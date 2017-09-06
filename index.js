8dcb26815b7ea75f337787ddac58b48b8844bc6a
Kayla Hansen <shiverr@gmail.com>
2017-09-06 01:35:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop("Garfield");
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift("Milo");
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1);
+}
