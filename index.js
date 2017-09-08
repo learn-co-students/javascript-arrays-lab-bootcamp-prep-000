e74fe1c95096f2ab23e49d6e65b16dfd87ff3606
gitluky <jluk87@gmail.com>
2017-09-08 21:46:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(name)
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens = [name,...kittens]
+  return newKittens
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0,-1)
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
