0767aef87580f52ebcc488891860dc93b0fffc59
Renee Tracy <renee.upandaway@gmail.com>
2017-09-09 07:35:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+
+function appendKitten(name) {
+  var newkittens = [...kittens, name];
+  return newkittens
+}
+
+
+function prependKitten(name) {
+  var newkittens = [name, ...kittens];
+  return newkittens
+}
+
+
+function removeLastKitten() {
+  var newkittens = kittens.slice(0, kittens.length -1);
+  return newkittens
+}
+
+
+function removeFirstKitten() {
+  var newkittens = kittens.slice(1);
+  return newkittens
+}
