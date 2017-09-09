83e0b1042cb605daad388bee0f14e3047c618c29
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-09 20:55:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+// var kittens = [
+//     'kitten1',
+//     'kitten2',
+//     'kitten3'
+// ]
+function destructivelyAppendKitten(name) {
+    kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+    kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten() {
+    kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+    kittens.shift()
+}
+function appendKitten(name) {
+    const new_array = [...kittens, name]
+    return new_array
+}
+function prependKitten(name) {
+    const new_array = [name, ...kittens]
+    return new_array
+}
+function removeLastKitten() {
+    const new_array = kittens.slice(0, kittens.length - 1)
+    return new_array
+}
+function removeFirstKitten() {
+    const new_array = kittens.slice(1)
+    return new_array
+}
