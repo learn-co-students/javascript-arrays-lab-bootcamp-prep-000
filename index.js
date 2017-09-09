b55d7749d7b1b8db8030b3766389d19a62faac74
Danny Avery <danny.w.avery@gmail.com>
2017-09-09 14:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(cat) {
+  return kittens.push(cat)
+}
+
+function destructivelyPrependKitten(cat) {
+  return kittens.unshift(cat)
+}
+
+function destructivelyRemoveLastKitten(cat) {
+  return kittens.pop(cat)
+}
+
+function destructivelyRemoveFirstKitten(cat) {
+  return kittens.shift(cat)
+}
+
+function appendKitten(cat) {
+  const addKitten = [...kittens, cat]
+  return addKitten
+}
+
+function prependKitten(cat) {
+  const addKitten = [cat, ...kittens]
+  return addKitten
+}
+
+function removeLastKitten() {
+  const removeKitten = kittens.slice(0, 2)
+  return removeKitten
+}
+
+function removeFirstKitten() {
+  const removeKitten = kittens.slice(-2)
+  return removeKitten
+}
