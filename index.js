416c37b10f48dde305528ac89d330d24d9b73690
Emma Konrad <emma.konrad92@gmail.com>
2017-09-10 04:01:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(element) {
+  kittens.push(element);
+  return kittens
+}
+
+function destructivelyPrependKitten(element) {
+  kittens.unshift(element);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(element) {
+  kittens.pop(element);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(element) {
+  kittens.shift(element);
+  return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
