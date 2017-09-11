4027527ba4bc633ee27a62d45fcaad0daefb5180
Brianna Antuna <bdantuna@gmail.com>
2017-09-11 16:56:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+var kittems = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+  return kittens
+
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var moreKittens = [...kittens, 'Broom']
+  return moreKittens
+}
+
+function prependKitten(name){
+  var moreKittens = ['Arnold', ...kittens]
+  return moreKittens
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(-2)
+}
