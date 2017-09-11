bd4cd8168796d6ff72dd6524ea930fb8558ad1fc
Scott Friedman <scottfriedman00@gmail.com>
2017-09-11 03:12:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten(x){
+  kittens.push(x)
+}
+
+function destructivelyPrependKitten(x){
+  kittens.unshift(x)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens = kittens.slice(1)
+}
+
+function appendKitten(name){
+  return [...kittens,name]
+}
+
+function prependKitten(name){
+
+  return [name,...kittens]
+}
+
+function removeLastKitten(){
+  var z = kittens.slice(0,kittens.length-1)
+  return z
+}
+
+function removeFirstKitten(){
+  var z = kittens.slice(1)
+  return z
+}
