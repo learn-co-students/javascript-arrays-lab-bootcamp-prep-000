928272c0a0bc07171f06f1ea98de0e216b2100ba
Kathia Mgwaba <mgwaba68@gmail.com>
2017-09-11 02:38:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+window.kittens = ['Milo', 'Otis', 'Garfield'];
+
+
+function appendKitten(cat){
+  const addKitten = [... kittens, cat]
+  return addKitten
+}
+
+function destructivelyAppendKitten(cat){
+ const addKitten = kittens.push(cat)
+  return addKitten
+}
+
+function destructivelyPrependKitten(name){
+  const addKitten = kittens.unshift(name)
+  return addKitten
+}
+
+function destructivelyRemoveLastKitten(cat){
+  const removeKitten = kittens.pop(cat)
+  return removeKitten
+}
+
+function destructivelyRemoveFirstKitten(cat){
+  const removeKitten = kittens.shift(cat)
+  return removeKitten
+}
+
+function prependKitten(name){
+   const addKitten = [name,... kittens]
+  return addKitten
+}
+
+function removeLastKitten(cat){
+ const removeKitten = kittens.slice(0, kittens.length -1)
+  return removeKitten
+}
+
+function removeFirstKitten(cat){
+  const removeKitten = kittens.slice(1)
+  return removeKitten
+}
