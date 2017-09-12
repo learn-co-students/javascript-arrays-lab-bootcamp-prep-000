74528d266f5ed533143df9fa5c6fb5e79724dae1
Shadrack Limbua <shadrack.limbua@gmail.com>
2017-09-12 23:06:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(a) {
+
+  kittens.push(a)
+
+  return kittens
+}
+
+function destructivelyPrependKitten(a) {
+
+  kittens.unshift(a)
+
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+
+  kittens.pop()
+
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten () {
+
+  kittens.shift()
+
+  return kittens
+}
+
+function appendKitten(a) {
+
+  var newArr = kittens.concat(a)
+  return newArr
+}
+
+function prependKitten (a) {
+
+  var newArr = kittens.slice(0, kittens.length)
+
+  newArr.unshift(a)
+
+  return newArr
+}
+
+function removeLastKitten () {
+
+  var newArr = kittens.slice(0, kittens.length-1)
+
+  return newArr
+}
+
+function removeFirstKitten() {
+
+  var newArr = kittens.slice(1,kittens.length)
+
+  return newArr
+}
