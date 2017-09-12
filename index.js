9d7f402e30a9c35e0622a9e893747852fc7d5094
Abigail Messmer <abbie.messmer@gmail.com>
2017-09-12 00:33:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+var kittens = [1,2,3]
+
+function destructivelyAppendKitten(kitten){
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten(kitten){
+  return kittens.pop(kitten)
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+  return kittens.shift(kitten)
+}
+
+function appendKitten(kitten){
+  return [...kittens, kitten]
+}
+
+function prependKitten(kitten){
+  return [kitten, ...kittens]
+}
+
+function removeLastKitten(kitten){
+  var newKittens = kittens.slice(0,kittens.length - 1)
+  return newKittens
+}
+
+function removeFirstKitten(kitten){
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
