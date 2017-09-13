54db45f9030ef1037bb5f123cab0a10cfa054231
Lauren  Novotny <laurenmn213@gmail.com>
2017-09-13 21:11:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var akittens = [...kittens, name]
+  return akittens
+}
+
+function prependKitten(name) {
+  var bkittens = [name, ...kittens]
+  return bkittens
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length -1)
+
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+
+}
