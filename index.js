3563304ceda257f9114c0ef59e68644b8036cba7
James Perrin <countrydawgg@gmail.com>
2017-09-13 05:37:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 const app = "I don't do much."
-//var kittens= ["Milo", "Otis", "Garfield"];
 
+// Appends a kitten to the end of the kittens array
+// Expect:["Milo", "Otis", "Garfield", "Ralph"]
 function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
 
+// Prepends a kitten to the beginning of the kittens array
+// Expect: ["Bob", "Milo", "Otis", "Garfield"]
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
 
+// Removes the last kitten from the kittens array
+// Expect: ["Milo", "Otis"]
 function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
 }
 
+// Removes the First kitten from the kittens array
+// Expect: ["Otis", "Garfield"]
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
 
+// Appends a kitten to the kittens array and returns a new array,
+// leaving the kittens array unchanged
+// Expect: ["Milo", "Otis", "Garfield", "Broom"]
 function appendKitten(name) {
   return [...kittens, name];
 }
 
+// Prepends a kitten to the kittens array and returns a new array,
+// leaving the kittens array unchanged
+// Expect: ["Arnold", "Milo", "Otis", "Garfield"]
 function prependKitten(name) {
   return [name, ...kittens];
 }
 
+// Removes the last kitten in the kittens array and returns a new array,
+// leaving the kittens array unchanged
+// Expect : ["Milo", "Otis"]
 function removeLastKitten() {
   return kittens.slice(0, 2);
 }
 
+// Removes the first kitten from the kittens array and returns a new array,
+// leaving the kittens array unchanged
+// Expect: ["Otis", "Garfield"]
 function removeFirstKitten() {
   return kittens.slice(1, 3);
 }
