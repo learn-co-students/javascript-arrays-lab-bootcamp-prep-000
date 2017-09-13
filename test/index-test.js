d6ecccb7aed7b56be8e4afcabd86e670be03fb8d
JCS <joshsad2@gmail.com>
2017-09-13 07:03:51 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 describe('Arrays', function() {
   beforeEach(function() {
     window.kittens = ['Milo', 'Otis', 'Garfield'];
-  });
+  }); 
 
   describe('kittens', function() {
     it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
