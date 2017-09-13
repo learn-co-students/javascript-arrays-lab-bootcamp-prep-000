3ab37453f3bdce3f7e72d99c63d957637ea8a11f
Jessica Greene <jessica0greene@gmail.com>
2017-09-13 14:31:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const app = "I don't do much."
+
+function Arrays(){
+  kittens = ['Milo', 'Otis', 'Garfield'];
+}
+
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+  Arrays()
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+  Arrays()
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+  Arrays()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+  Arrays()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+  Arrays()
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+  Arrays()
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1 )
+  Arrays()
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+  Arrays()
+}
