59026036ae613dfba7c10991c2b75fb81a39a2d4
Keith Getty <keithgetty@yahoo.com>
2017-09-14 22:27:05 +0000

    Done.

--- index.js
+++ index.js
@@ -21,5 +21,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return (...kittens, name);
+  return [...kittens, name];
 }
+
+function prependKitten(name) {
+   return [name, ...kittens];
+ }
+
+ function removeLastKitten() {
+    return kittens.slice(0, kittens.length - 1);
+  }
+
+  function removeFirstKitten() {
+     return kittens.slice(1);
+   }
