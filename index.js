d0fc6f5d6591960ec4f4f7661cd8cb0d59538018
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-14 04:07:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  return [...kittens, "Broom"]
+  return kittens
+}
+function prependKitten(name) {
+  return ["Arnold", ...kittens]
+  return kittens
+}
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length-1)
+  return kittens
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+  return kittens
+}
