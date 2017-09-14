908c1b886e798928e340fb332b23ea7fe867026f
Chris Winge <chris.winge@gmail.com>
2017-09-14 02:42:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(){
+  var newKittens = [...kittens]
+  newKittens.push("Broom")
+  return newKittens
+}
+
+function prependKitten(name){
+   var newKittens = [...kittens]
+   newKittens.unshift(name)
+   return newKittens
+ }
+
+function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
+}
