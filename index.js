1b2873c891abf6b5c8b9ef4c65111c5f39b022de
ammarita <marita.lasmane@gmail.com>
2017-09-14 10:00:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+};
+
+function appendKitten(name) {
+  var kittens1 = [...kittens, name]
+  return kittens1
+};
+
+function prependKitten(name) {
+  var kittens1 = [name, ...kittens]
+  return kittens1
+};
+
+function removeLastKitten() {
+  var kittens1 = kittens.slice(0, kittens.length - 1)
+  return kittens1
+};
+
+function removeFirstKitten() {
+  var kittens1 = kittens.slice(1)
+  return kittens1
+};
