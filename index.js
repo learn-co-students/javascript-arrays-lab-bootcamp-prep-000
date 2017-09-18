53d066875c2a0900fbc09ef72e92b4852e6978c2
yesI <1317154549@qq.com>
2017-09-18 02:54:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens = [name].concat(kittens)
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+
+
+function prependKitten(name) {
+  var a = [name].concat(kittens);
+  return a;
+}
+
+
+
+function appendKitten(name) {
+  var k = kittens.slice();
+  k.push(name)
+  return k;
+}
+function removeLastKitten() {
+  var a = kittens.slice();
+  a.pop()
+  return a;
+}
+
+function removeFirstKitten() {
+  let a = kittens.slice();
+  a.shift();
+  return a;
+}
