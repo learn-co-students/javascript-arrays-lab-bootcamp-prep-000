e41cecabe4d32a9b4c8e8716269c5b44b112ea07
macro6461 <mattcroak718@gmail.com>
2017-09-18 14:16:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArr = [...kittens, name];
+  return newArr;
+}
+
+function prependKitten(name){
+  var newArr = [name, ...kittens];
+  return newArr;
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
