bc3f1b3e3aedcdd08657301f47071e3ee000629c
Thomas Viles <thomas.c.viles.jr@gmail.com>
2017-09-19 22:06:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+  var newKittens = [...kittens,name]
+  return newKittens
+}
+function prependKitten(name){
+  var newkittens = [name,...kittens]
+  return newkittens
+}
+function removeLastKitten(){
+  var a = kittens.slice(0,kittens.length - 1)
+  return a
+}
+function removeFirstKitten(){
+  var b = kittens.slice(1,kittens.length)
+  return b
+}
