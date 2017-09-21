f5d419748d5815305adc197629bae48bb85e5de1
AIT ASSOU <pasarela70@hotmail.com>
2017-09-21 05:51:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  var popped = kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  var shifted = kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  var temp = kittens.slice(0, kittens.length-1);
+  return temp;
+}
+
+function removeFirstKitten() {
+  var temp = kittens.slice(1-kittens.length);
+  return temp;
+}
