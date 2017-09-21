d90fe9f28f65bb5d3aa14dddd80b0c42e8205516
Annika Noren <amenoren@gmail.com>
2017-09-21 21:12:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+var newKittens = new Array()
+
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name){
+  newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten(){
+  newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens
+}
+
+function removeFirstKitten(){
+  newKittens = kittens.slice(1)
+  return newKittens
+}
