0262145771205d4a0e75bc646020533863fa2121
Charla Archie <ceansky@gmail.com>
2017-09-21 14:46:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  console.log(kittens)
+ }
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  console.log(kittens)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  console.log(kittens)
+ }
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  console.log(kittens)
+}
+function appendKitten(kitten) {
+  const moreKittens = [...kittens,kitten];
+  return moreKittens
+}
+function prependKitten(kitten) {
+  const frontKittens = [kitten,...kittens];
+  return frontKittens
+}
+function removeLastKitten(kitten) {
+  const removeLast = kittens.slice(0,kittens.length-1);
+  return removeLast
+}
+function removeFirstKitten(kitten) {
+  const removeFirst = kittens.slice(-2);
+  return removeFirst
+}
