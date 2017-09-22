f51abb05cde7b95d79ddb39f9ef3f18ffa611974
Molly Soeder <mollysoeder@gmail.com>
2017-09-22 22:59:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name,myArray){
+  kittens.push(name);
+  return myArray;
+}
+
+function destructivelyPrependKitten(name,myArray){
+  kittens.unshift(name);
+  return myArray;
+}
+
+function destructivelyRemoveLastKitten(name,myArray){
+  kittens.pop();
+  return myArray;
+}
+
+function destructivelyRemoveFirstKitten(name,myArray){
+  kittens.shift();
+  return myArray;
+}
+
+function appendKitten(name,myArray){
+   return [...kittens, name];
+}
+
+function prependKitten(name,myArray){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name,myArray){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(name,myArray){
+  return kittens.slice(1)
+}
