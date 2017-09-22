3d3fa6f708e7b087188d37a04a7c06da550e22cb
Tara Johnson <tara.e.johnson@gmail.com>
2017-09-22 04:09:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (kitten) {
+  kittens.push(kitten)
+}
+
+function destructivelyPrependKitten (kitten) {
+  kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift()
+}
+
+function appendKitten (kitten) {
+  var moreKittens = [...kittens, kitten]
+  return moreKittens
+}
+
+function prependKitten (kitten) {
+  var moreKittens = [kitten, ...kittens]
+  return moreKittens
+}
+
+function removeLastKitten () {
+  var lessKittens = kittens.slice(0,kittens.length-1)
+  return lessKittens
+}
+
+function removeFirstKitten () {
+  var lessKittens = kittens.slice(1)
+  return lessKittens
+}
