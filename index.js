5758b2d4f2d4a40381efb8425e45cd2e7aba6f06
alexis101 <kondilisalexis@gmail.com>
2017-09-24 21:41:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens
+}
+function destructivelyRemoveLastKitten(array){
+  kittens.pop("Garfield");
+  return kittens
+}
+function destructivelyRemoveFirstKitten(array){
+  kittens.shift("Milo");
+  return kittens
+}
+function appendKitten(name){
+  var newkittens= [...kittens, "Broom"];
+  return newkittens
+}
+function prependKitten(name){
+  var newkittens =["Arnold", ...kittens]
+  return newkittens
+}
+function removeLastKitten(){
+var newKittens = [...kittens]
+newKittens.pop()
+return newKittens
+}
+function removeFirstKitten(){
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
+}
