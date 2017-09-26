50d95b26f8a3ae8a0f3e07631a239b22486f9e8d
Michael Wong <mik9080@gmail.com>
2017-09-26 21:07:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(n){
+  return [...kittens,n]
+}
+function prependKitten(n){
+  return [n,...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
