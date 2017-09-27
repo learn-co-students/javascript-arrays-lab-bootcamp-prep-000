882a63d5f0a18333f65cf7e76d00abee646c8a45
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-27 21:21:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function kittens() {
+  return kittens
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var fineKittens = [...kittens];
+  fineKittens.push(name)
+  return fineKittens
+}
+
+function prependKitten(name) {
+  var fineKittens = [...kittens];
+  fineKittens.unshift(name)
+  return fineKittens
+}
+
+function removeLastKitten(name) {
+  var fineKittens = [...kittens];
+  fineKittens.pop(name)
+  return fineKittens
+}
+
+function removeFirstKitten(name) {
+  var fineKittens = [...kittens];
+  fineKittens.shift(name)
+  return fineKittens
+}
