cfecb7e55aeebb2f5c9bd72e7073a46ce0145929
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-02 23:23:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(name) {
+  var sliceKitten = kittens.slice(0, kittens.length - 1);
+  return sliceKitten;
+}
+
+function removeFirstKitten() {
+  var slicedKitten = kittens.slice(1);
+  return slicedKitten;
+}
