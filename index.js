a0c7bd5892607a127e30a6cce6f845a2d699ffdf
geoffcraz <geoffcraz@gmail.com>
2017-10-04 19:48:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+const kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+ var destruction = (kittens.push(`${name}`))
+  return destruction
+}
+
+function destructivelyPrependKitten(name) {
+  var prepend = (kittens.unshift(`${name}`))
+  return prepend
+}
+
+function destructivelyRemoveLastKitten() {
+  var pop = (kittens.pop())
+  return pop
+}
+
+function destructivelyRemoveFirstKitten() {
+  var remove = (kittens.shift())
+  return remove
+}
+
+function appendKitten(name) {
+  var moreKitties = [...kittens, `${name}`]
+  return moreKitties
+}
+
+function prependKitten(name) {
+  return [`${name}`, ...kittens]
+}
+
+function removeLastKitten(){
+ return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+ return kittens.slice(1)
+}
