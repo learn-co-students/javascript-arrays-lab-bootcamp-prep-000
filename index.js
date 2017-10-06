fe80f0bf8d9e4c9f9f89a41ccea375eb960372b0
Ghalyah Al-Ansari <gaa291@nyu.edu>
2017-10-06 07:21:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+var kittens= ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+  var NewKittens= [...kittens]
+  NewKittens.push(name)
+  return NewKittens
+
+}
+function prependKitten(name){
+  var NewKittens =[...kittens]
+  NewKittens.unshift(name)
+  return NewKittens
+
+}
+function removeLastKitten(){
+  var NewKittens =[...kittens]
+  NewKittens.pop()
+  return NewKittens
+}
+function removeFirstKitten(){
+  var NewKittens =[...kittens]
+  NewKittens.shift()
+  return NewKittens
+}
