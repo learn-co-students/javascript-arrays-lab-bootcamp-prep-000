d1e8e426d5aef5e5abfda8d81a9235cfb0748565
Sen Tezeno <sen63092@yahoo.com>
2017-10-06 20:09:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name){
-kittens.push(name)
-return kittens};
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
 
-function destructivelyPrependKitten(name){
-kittens.unshift(name)
-return kittens};
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
 
-function destructivelyRemoveLastKitten(name){
-kittens.pop(name)
-return kittens};
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
 
-function destructivelyRemoveFirstKitten(name){
-kittens.shift(name)
-return kittens};
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
 
-function appendKitten(name){
-  var newKittens = [...kittens]
-  newKittens.push(name)
-  return newKittens};
+function appendKitten(name) {
+  return [...kittens, name]
+}
 
-  function prependKitten(name){
-    var newKittens = [...kittens]
-    newKittens.unshift(name)
-    return newKittens};
+function prependKitten(name) {
+  return [name, ...kittens]
+}
 
-function removeLastKitten(name){
-      var newKittens = [...kittens]
-      newKittens.pop(name)
-      return newKittens};
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
 
-      function removeFirstKitten(name){
-        var newKittens = [...kittens]
-        newKittens.shift(name)
-        return newKittens};
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
