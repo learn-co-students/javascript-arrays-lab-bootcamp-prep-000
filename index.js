fe8cb6e084a1ac73291a0414554c85a81cbb3074
blue shadow <anujose1989@gmail.com>
2017-10-06 04:48:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+destructivelyAppendKitten('Ralph');
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+destructivelyPrependKitten("Bob")
+
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+destructivelyRemoveLastKitten();
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+destructivelyRemoveFirstKitten();
+
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+appendKitten("Broom");
+
+function prependKitten(name){
+  return [name, ...kittens ];
+}
+prependKitten("Arnold");
+
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1);
+}
+removeLastKitten();
+
+function removeFirstKitten(){
+  return kittens.slice(1,kittens.length);
+}
+removeFirstKitten();
