19d7ecc7b1f38b18f8372519d467a64180188911
John DANQUAH-BOATENG <yjbdanquah@gmail.com>
2017-10-06 09:05:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(index){
+kittens.push(index)
+//console.log(myArr)
+return kittens
+}
+function destructivelyPrependKitten(index){
+return kittens.unshift(index)
+}
+function destructivelyRemoveLastKitten(index){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+  }
+function appendKitten(index){
+  var newArray = kittens.concat(index)
+  return newArray
+}
+function prependKitten(index){
+  var newArray = [index, ...kittens]
+  return newArray
+}
+function removeLastKitten(){
+  //kittens = kittens.splice(0,2))
+return kittens.slice(0,kittens.length-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1,kittens.length)
+}
