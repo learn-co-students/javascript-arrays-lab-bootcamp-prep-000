3b6badb50154ba1e390c8d807fe6cc36498b82b2
nbisigna <nbisigna@gmail.com>
2017-10-10 03:45:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+
+
+function appendKitten (name) {
+  var cats = [...kittens, name]
+  return cats
+
+}
+
+function prependKitten (name) {
+  var cats = [name, ...kittens]
+  return cats
+
+}
+
+function removeLastKitten () {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten () {
+  return kittens.slice(1)
+}
