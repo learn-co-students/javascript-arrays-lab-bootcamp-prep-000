d3c8827e2facdeb9661487a767a044f3389bbcd1
Jacob Miller <jake0miller@gmail.com>
2017-10-16 03:59:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(ele){
+  kittens.push(ele)
+  return kittens
+}
+
+function destructivelyPrependKitten(ele){
+  kittens.unshift(ele)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(ele){
+  return [...kittens,ele]
+}
+
+function prependKitten(ele){
+  return [ele,...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
