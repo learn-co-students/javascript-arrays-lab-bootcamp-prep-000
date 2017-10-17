6b10554e724bcf6b4cff6e0678cbbe157495cdcf
matt hagner <mhagner77@gmail.com>
2017-10-17 21:45:51 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function kittens(){
-    var kittens = ["Milo", "Otis", "Garfield"];
-    return kittens
-}
-
 function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens
