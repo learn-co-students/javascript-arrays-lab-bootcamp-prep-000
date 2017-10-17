081882a1e3472f1689a034fa04172a5ba3eef39e
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-17 22:19:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = [...kittens, "Broom"]
+  return kittens
+}
+
+function prependKitten(name){
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens = ["Arnold", ...kittens]
+  return kittens
+}
+
+function removeLastKitten(name){
+  var kittens = ["Milo", "Otis"]
+  kittens.slice(-1)
+  return kittens
+}
+
+function removeFirstKitten(name){
+  var kittens = ["Otis", "Garfield"]
+  kittens.slice(0)
+  return kittens
+}
