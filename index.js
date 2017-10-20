6aaa37b4ec51e2f0aa2a16df95e497284cf8adbc
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-20 15:12:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.slice(0, kittens.length + 1);
+  newKittens.push(name);
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.slice(0, kittens.length + 1);
+  newKittens.unshift(name);
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1, kittens.length + 1);
+  return newKittens
+}
