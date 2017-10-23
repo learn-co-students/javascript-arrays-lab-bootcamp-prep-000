5cdd3d4ecfb61bb844f41d0a56bf52e5083535e3
Nirali Shah <hellonirali@gmail.com>
2017-10-23 04:01:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  return kittens.concat(name);
+}
+
+function prependKitten(name){
+  var arr = [name];
+  return arr.concat(kittens.slice());
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1,kittens.length)
+}
