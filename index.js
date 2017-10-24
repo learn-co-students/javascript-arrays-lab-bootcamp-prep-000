38a9ee9bd759b1cbd0acf9dfc7b9e3869d45e55e
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-24 18:28:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
-function destructivelyAppendKitten(array, kitten) {
-  array.push(kitten)
-  return array
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(kitten) {
+  kittens.pop(kitten)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift(kitten)
+  return kittens
+}
+
+function appendKitten(kitten) {
+  return [...kittens,kitten]
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens]
+}
+
+function removeLastKitten(kitten) {
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten(kitten) {
+  return kittens.slice(1)
 }
