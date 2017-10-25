07c0b593cfcb75072947b8607b12c4e32458cb16
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-25 23:17:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+console.log(kittens.length);
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+   kittens.pop();
+   return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(0);
+  return kittens;
+}
+
+function appendKitten(name){
+  var morekittens = [...kittens, name];
+  return morekittens;
+}
+
+function prependKitten(name) {
+  var morekittens = [name, ...kittens];
+  return morekittens
+}
+
+function removeLastKitten() {
+  var morekittens = kittens.slice(0, kittens.length -1);
+  return morekittens;
+}
+
+function removeFirstKitten() {
+  var morekittens = kittens.slice(1);
+  return morekittens;
+}
