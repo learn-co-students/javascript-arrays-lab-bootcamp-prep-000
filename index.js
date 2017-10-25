05cba891eacc527dde89847876a21aef929dca5e
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:23:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const app = "I don't do much."
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
+// appends a kitten to the end of the kitten array (mutate)
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-   return kittens
+  return kittens;
 }
 
+// prepends a kitten to the beginning of the kittens array (mutate)
 function destructivelyPrependKitten(name) {
-  kittens.unshift(name)
-    return kittens
+  kittens.unshift(name);
+  return kittens;
 }
 
+// removes the last kitten from the kittens array (mutate)
 function destructivelyRemoveLastKitten() {
-  kittens.pop()
-   return kittens
+  kittens.pop();
+  return kittens;
 }
 
+// removes the first kitten from the kittens array (mutate)
 function destructivelyRemoveFirstKitten() {
-  kittens.shift()
-   return kittens
+  kittens.shift();
+  return kittens;
 }
 
+// appends a kitten array and returns a new array, leaving the kittens array unchanged
 function appendKitten(name) {
-  return [...kittens, name]
+  return [...kittens, name];
 }
 
+// prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
 function prependKitten(name) {
-  return [name, ...kittens]
+  return [name, ...kittens];
 }
 
+// removes the last kitten in the kittens array and returns a new array, leaving the array unchanged
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
 }
 
+// removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
 function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens.slice(1);
 }
