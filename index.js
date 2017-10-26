2f4b5e5d295d40c8328085a2934adb419021d50b
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-10-26 18:13:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name);
+}
+
+function appendKitten(name){
+  var newKittens= [...kittens];
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens=[...kittens];
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten(name){
+  var newKittens=[...kittens];
+  newKittens.pop(name);
+  return newKittens;
+}
+
+function removeFirstKitten(name){
+  var newKittens=[...kittens];
+  newKittens.shift(name);
+  return newKittens;
+}
