4a95c3eff13be2372564fdf082c28258592095c0
Christopher Donati <cdonati89@gmail.com>
2017-10-28 17:59:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+}
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+}
+function destructivelyRemoveLastKitten(kitten) {
+  kittens.pop(kitten);
+}
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift(kitten);
+}
+function appendKitten(kitten) {
+  return [...kittens, kitten];
+}
+function prependKitten(kitten) {
+  return [kitten, ...kittens];
+}
+function removeLastKitten() {
+  return kittens.slice(0, 2);
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
