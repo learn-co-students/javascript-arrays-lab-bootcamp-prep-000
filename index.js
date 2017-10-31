f46217b96be9172e468dfc2b1d8a9e18a507fe5c
Holden Altaffer <haltaffer@gmail.com>
2017-10-31 00:39:00 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,25 @@ function destructivelyRemoveLastKitten() {
   return kittens
 }
 
-function destructivelyRemoveLastKitten() {
+function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+};
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens]
+};
+
+function removeLastKitten() {
+return kittens.slice(0, kittens.length - 1);
+
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+
 }
