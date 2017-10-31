80b544a92b979eb9bd920ffcf871c7df6aa7031a
Fernando R González <rumbaut2288@gmail.com>
2017-10-31 23:16:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name){
+  var aux = kittens.slice();
+  aux.push(name);
+  return aux;
+}
+
+function prependKitten(name){
+  var aux = kittens.slice();
+  aux.unshift(name);
+  return aux;
+}
+
+function removeLastKitten(){
+  var aux = kittens.slice();
+  aux.pop();
+  return aux;
+}
+
+function removeFirstKitten(){
+  var aux = kittens.slice();
+  aux.shift();
+  return aux;
+}
