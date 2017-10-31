5293e0bb06454179bc1d67a1ff346fbd618094f0
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-10-31 15:09:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  var array = [...kittens, name]
+  return array
+}
+
+function prependKitten(name){
+  var array = [name,...kittens]
+  return array
+}
+
+function removeLastKitten(){
+  var array = kittens.slice(0, kittens.length - 1)
+  return array
+}
+
+function removeFirstKitten(){
+  var array = kittens.slice(1)
+  return array
+}
