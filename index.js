fbd2607da5998fdfa6b5ec1fb34461da7713a44f
Onur Eker <yonureker@gmail.com>
2017-11-01 23:38:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function appendKitten(name){
+  var exArray = [...kittens, name]
+  return exArray
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+  var newMambo = kittens.slice(0, kittens.length - 1)
+  return newMambo
+}
+
+function removeFirstKitten(){
+  var newJambo = kittens.slice(1, kittens.length)
+  return newJambo
+}
