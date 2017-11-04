c3513b1bbeaab6080ccafbd9323c5338077fdf94
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-11-04 15:40:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+    kittens.push(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+var kittens = ["Milo", "Otis", "Garfield"];
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
+var kittens = ["Milo", "Otis", "Garfield"];
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+var kittens = ["Milo", "Otis", "Garfield"];
+function removeLastKitten(){
+  var newArray = kittens.slice(0, kittens.length -1);
+  return newArray;
+}
+var kittens = ["Milo", "Otis", "Garfield"];
+function removeFirstKitten(){
+  var newArray = kittens.slice(1);
+  return newArray;
+}
