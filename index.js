aee9899d78db5cf7454f5d569f02977bdab3b95a
Alex Denton <alex.jdenton@gmail.com>
2017-11-10 16:41:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(element) {
+  kittens.push (element)
+}
+
+ function destructivelyPrependKitten(element) {
+   kittens.unshift (element)
+ }
+
+ function destructivelyRemoveLastKitten(element) {
+    kittens.pop (element)
+ }
+
+ function destructivelyRemoveFirstKitten(element) {
+    kittens.shift (element)
+ }
+
+function appendKitten (element) {
+  return kittens.concat(element)
+}
+
+function prependKitten (element) {
+  return [(element),...kittens]
+}
+
+function removeLastKitten () {
+  return kittens.slice (0,-1)
+}
+
+function removeFirstKitten () {
+  return kittens.slice(1)
+}
