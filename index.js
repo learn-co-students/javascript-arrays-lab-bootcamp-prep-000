b15469284a0207b18e47a69e7e539b2934899496
Tony Boswell <tboswell@kc.rr.com>
2017-11-10 18:13:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+// creates a function that appends an element to the end of the kittens array
+// destructive example
+
+function destructivelyAppendKitten(a) {
+  kittens.push(a);
+  return kittens;
+}
+
+// creates a function that prepends an element to the beginning the kittens
+// array destructive example
+function destructivelyPrependKitten(a) {
+  kittens.unshift(a);
+  return kittens;
+}
+
+// creates a function that removes the last element from the kittens array
+// destructive example
+function destructivelyRemoveLastKitten(a) {
+  kittens.pop(a);
+  return kittens;
+}
+
+// creates a function that removes the First element from the kittens array
+// destructive example
+function destructivelyRemoveFirstKitten(a) {
+  kittens.shift(a);
+  return kittens;
+}
+
+//  creates a function that appends an element to the kittens array and returns
+// a new array  non-destructive/non mutating example
+function appendKitten(a) {
+  var myArray = [...kittens, a];
+  return myArray;
+}
+
+// creates a function that prepends an element to the kittens array and returns
+// a new array non-destructive/non mutating example
+function prependKitten(a) {
+  var myArray = [a, ...kittens];
+  return myArray;
+}
+
+// creates a function that removes the last element in the kittens array and
+// returns a new array non-destructive/non mutating
+function removeLastKitten() {
+  var myArray = kittens.slice(0, kittens.length -1);
+  return myArray;
+}
+
+// creates a function that removes the first element in the kittens array and
+// returns a new array non-destructive/non mutating
+function removeFirstKitten() {
+  var myArray = kittens.slice(1);
+  return myArray;
+}
