f58d38d8365ae017a05bd7f0919b8aaf968103d1
Angela <wang.angela90@gmail.com>
2017-11-10 01:04:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+
+function destructivelyAppendKitten (name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name] //spread operator that takes the kittens array and add 'name'
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten(name) {
+  var newArray = kittens.slice(0); // another method > var newArray = [...kittens]
+  newArray.pop();
+  return newArray;
+}
+
+function removeFirstKitten(){
+  var newArray = [...kittens]
+  newArray.shift();
+  return newArray;
+}
