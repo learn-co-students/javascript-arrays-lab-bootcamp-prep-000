d1320c877b45af4b3759463ba0f8b9aa935756c6
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 23:48:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+// var kittens = window.kittens; // ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kitten) {
+  window.kittens.push(kitten);
+  // return window.kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  window.kittens.unshift(kitten)
+}
+
+function destructivelyRemoveLastKitten() {
+  window.kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  window.kittens.shift()
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten]
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens. slice(1)
+}
