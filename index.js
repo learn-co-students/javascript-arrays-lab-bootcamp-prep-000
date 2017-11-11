e39ac44368067d4a3beabb112d74760e1354518b
Shianne Lee <shiannelee@live.ca>
2017-11-11 01:26:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+function appendKitten(name){
+  var k = [...kittens, name]
+  return k
+}
+function prependKitten(name){
+  var k = [name, ...kittens]
+  return k
+}
+function removeLastKitten(){
+var k = kittens.slice(0, kittens.length-1)
+return k
+}
+function removeFirstKitten(){
+var k = kittens.slice(1)
+return k
+}
