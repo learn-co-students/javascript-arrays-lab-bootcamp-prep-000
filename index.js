703eed08da7cc6e6c54a9a851dc993465677d416
Tim Sander <tomzander88@yahoo.com>
2017-11-12 19:36:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+function kittens () {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift(name)
+}
+function appendKitten (name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = [...kittens, name]
+  return kittens
+}
+function prependKitten (name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = [name, ...kittens]
+  return kittens
+}
+function removeLastKitten (name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+kittens = kittens.splice(0, kittens.length - 1)
+  return kittens
+}
+function removeFirstKitten (name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = kittens.splice(1)
+  return kittens
+}
