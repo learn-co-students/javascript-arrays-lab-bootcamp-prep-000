30e4b2e3e3f8278dbec09b02e2a1ad70aafe1c44
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-13 16:38:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(`${name}`);
+  var newKittens = kittens.slice();
+  return newKittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(`${name}`);
+  var newKittens = kittens.slice();
+  return newKittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  var newKittens = kittens.slice();
+  return newKittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  var newKittens = kittens.slice();
+  return newKittens;
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens, `${name}`];
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = [`${name}`,...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0,kittens.length-1);
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
