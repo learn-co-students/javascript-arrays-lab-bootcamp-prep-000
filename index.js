4025b3896809a1da9609ddc21f6030520d21871d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-13 16:47:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function prependKitten(name) {
+return [name, ...kittens]
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeLastKitten() {
+  return kittens.slice(0,-1)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
