4b45d3b80145697c0511a9790cbcad5b442167ed
Mario Rodrigues <mariorodjr92@gmail.com>
2017-11-14 05:26:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  name = kittens.push('Ralph')
+  return name
+}
+
+function destructivelyPrependKitten(name) {
+  name = kittens.unshift('Bob')
+  return name
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  name = [...kittens, "Broom"]
+  return name
+}
+
+function prependKitten(name) {
+  name = ["Arnold", ...kittens]
+  return name
+}
+
+function removeLastKitten() {
+  name = kittens.slice(0, kittens.length - 1)
+  return name
+}
+
+function removeFirstKitten() {
+  name = kittens.slice(1)
+  return name
+}
