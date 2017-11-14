175fa7e3260500040e70db2521a8a71854dd02d7
Ben Gantz <bengantz@gmail.com>
2017-11-14 07:59:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(newKitten) {
+  kittens.push(newKitten)
+}
+
+function destructivelyPrependKitten(newKitten) {
+  kittens.unshift(newKitten)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(newKitten) {
+  return [...kittens, `${newKitten}`]
+}
+
+function prependKitten(newKitten) {
+  return [`${newKitten}`, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
