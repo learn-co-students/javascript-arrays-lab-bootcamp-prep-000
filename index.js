52c05505802f9aac47759ccbd99495d50fef9cf0
James Ferguson <ncjamesferguson@gmail.com>
2017-11-14 15:36:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+function appendKitten(name) {
+  var freshkit = [...kittens, name]
+  return freshkit;
+}
+function prependKitten(name) {
+  var freshkit = [name, ...kittens]
+  return freshkit;
+}
+function removeLastKitten() {
+ var freshkit = kittens.slice(0, kittens.length-1) 
+ return freshkit
+}
+function removeFirstKitten() {
+  var freshkit = kittens.slice(1)
+  return freshkit
+}
