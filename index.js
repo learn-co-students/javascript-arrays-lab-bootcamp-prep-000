771dd1c09f09e41e431ee421017ba1a54449dab6
E Chao <elbert.chao@gmail.com>
2017-11-15 00:46:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+let kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  let array = kittens.slice(0);
+  array.push(name);
+  return array
+}
+function preppendKitten(name){
+  let array = kittens.slice(0);
+  array.unshift(name);
+  return array
+}
+function removeLastKitten(){
+  let array = kittens.slice(0);
+  array.pop();
+  return array
+}
+function removeFirstKitten(){
+  let array = kittens.slice(0);
+  array.shift();
+  return array
+}
