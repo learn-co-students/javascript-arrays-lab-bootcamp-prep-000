dcae5df09b4fed0757856a0ba3e67bc42fc3144d
Truman Lahr <trumanlahr@gmail.com>
2017-11-15 15:27:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  const newKit = [...kittens, name];
+  return newKit
+}
+
+function prependKitten(name) {
+  const newKit = [name, ...kittens];
+  return newKit
+}
+
+function removeLastKitten() {
+  const newKit = kittens.slice(0, kittens.length - 1)
+  return newKit
+}
+
+function removeFirstKitten() {
+  const newKit = kittens.slice(1);
+  return newKit
+}
