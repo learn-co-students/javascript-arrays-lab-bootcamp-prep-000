52e86a4a8562a920fc5a9ed185fbb59d166e15cd
Jay G <playerjayho@gmail.com>
2017-11-16 19:59:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 const app = "I don't do much."
 
-function destructivelyRemoveLastKitten(name) {
-    return array.pop(name)
-
-}
-
-function destructivelyAppendKitten() {
-
-}
-
-function removeFirstKitten() {
-
-}
-
-function destructivelyPrependKitten(kittens) {
-
-
+function destructivelyPrependKitten(name) {
+      kittens.unshift(name)
+      return kittens
 }
 
 
 function destructivelyRemoveLastKitten() {
       kittens.pop()
       return kittens
+}
+
+function destructivelyAppendKitten(name) {
+    kittens.push(name)
+    return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+      kittens.shift()
+      return kittens
+}
+
+function appendKitten(name) {
+    var xd =  [...kittens, name]
+      return xd
+}
+
+function prependKitten(name) {
+  var xd =  [name, ...kittens]
+    return xd
+}
+
+
+
+
+
+
+function removeFirstKitten() {
+      var xa = kittens;
+        xa.shift();
+
+      return xa;
+}
+
+function removeLastKitten() {
+      var xe = kittens;
+        xe.pop();
+
+      return xe;
 }
