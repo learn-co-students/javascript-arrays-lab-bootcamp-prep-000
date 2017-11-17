55945edde62655f6a207febced6a8f74a3156599
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-17 11:32:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function kittens(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+  var newKittens=[...kittens,name]
+  return newKittens
+}
+function prependKitten(name){
+  var newKittens=[name,...kittens]
+  return newKittens
+}
+function removeLastKitten(){
+  var oneLessKitten= kittens.slice(0, kittens.length -1)
+  return oneLessKitten
+}
+function removeFirstKitten(){
+var newKittens= kittens.slice(1)
+return newKittens
+}
