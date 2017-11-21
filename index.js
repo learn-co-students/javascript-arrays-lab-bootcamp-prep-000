f8622279c1ab879827d8015fa8225f6e94bc3c67
Richard Loera <richexp9@gmx.com>
2017-11-21 20:39:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0,kittens.length -1)
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1)
+  return newArray
+}
