76f79b0fe1c2005bb766d715094e4c318643cd05
jim <reverseleft@gmail.com>
2017-11-25 09:51:07 +0000

    Done.

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function prependKitten(name)
 
 function removeLastKitten()
 {
-  return kittens.slice(0,kittens.length - 1);
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten()
