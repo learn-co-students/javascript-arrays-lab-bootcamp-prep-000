796f8b36433b9e26f9ee1bc332dddabd53d20dd6
Cas <lydiahamil@yahoo.com>
2017-11-28 10:05:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten() {
+return [...kittens, "Broom"]
+}
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
