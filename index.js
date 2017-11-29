fc12e4737106ac7e1af106dd90c63ebfa8a424ac
J.D. Bean <jonathandbean@gmail.com>
2017-11-29 22:49:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift(1)
+}
+
+function prependKitten(name) {
+  var morekittens =  [name, ...kittens]
+  return morekittens
+}
+
+function appendKitten(name) {
+  var morekittens = [...kittens, name]
+  return morekittens
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
