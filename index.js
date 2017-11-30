0b2fb753f14174d18388ae5dc9e0fac89268bc13
Marvin Andrade <marvin@lionguardsecurity.com>
2017-11-30 15:14:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 const app = "I don't do much."
+
+
+ function destructivelyAppendKitten(name){
+
+    kittens.push(name);
+
+
+
+ }
+
+
+function destructivelyPrependKitten(name){
+
+  kittens.unshift(name);
+}
+
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+
+function destructivelyRemoveFirstKitten(){
+
+  kittens.shift();
+}
+
+
+function appendKitten(name) {
+
+var kittens1 = kittens;
+
+kittens1 = [...kittens1, name];
+
+return kittens1;
+
+
+
+}
+
+
+function prependKitten(name)
+{
+  var kittens1 = kittens;
+
+  kittens1 = [ name,...kittens1];
+
+  return kittens1;
+
+
+}
+
+function removeFirstKitten() {
+ var kittens1 = kittens;
+
+  kittens1 = kittens.slice(1);
+
+  return kittens1;
+}
+
+function removeLastKitten() {
+ var kittens1 = kittens;
+
+  kittens1 = kittens.slice(0,-1);
+
+  return kittens1;
+}
