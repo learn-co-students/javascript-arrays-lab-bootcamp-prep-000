2de4172a4f37c829df908a2d458d0d061e1008c9
Angelo Cigna <acigna1982@gmail.com>
2017-12-01 00:27:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]
+
+function destructivelyAppendKitten(){
+  kittens.push ("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(){
+  kittens.unshift("Bob")
+  return kittens
+
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+
+function appendKitten(){
+  var moreKittens = [...kittens, "Broom"]
+  return moreKittens
+}
+
+function prependKitten(){
+  var furtherKittens = ["Arnold", ...kittens]
+  return furtherKittens
+}
+
+function removeLastKitten(){
+  var allTheKittens = kittens.slice(0,kittens.length - 1)
+  return allTheKittens
+}
+
+function removeFirstKitten(){
+  var noMoreKittens = kittens.slice(1)
+  return noMoreKittens
+}
