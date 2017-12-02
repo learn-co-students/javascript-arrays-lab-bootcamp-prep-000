8b406c9d8607b3a9cd4270e4b0398a0a3cbc5ed3
Devin Moore <devin.moore@volken.org>
2017-12-02 02:44:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kitten = [...kittens, name]
+  return kitten
+}
+
+function prependKitten(name) {
+  var kitten = [name, ...kittens]
+  return kitten
+}
+
+function removeLastKitten() {
+  var kitten = kittens.slice(0, kittens.length-1)
+  return kitten
+}
+
+function removeFirstKitten() {
+  var kitten = kittens.slice(1)
+  return kitten
+}
