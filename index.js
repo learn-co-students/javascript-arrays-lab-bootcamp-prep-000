4493b145d2c74414580e22c879c934ca126e1e9d
Anna Hotter <annajhotter@gmail.com>
2017-12-04 04:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+var name = "Broom"
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  return [...kittens, 'Broom']
+}
+
+function prependKitten(name) {
+  return ['Arnold', ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten() {
+  retuen kittens.slice(0)
+}
