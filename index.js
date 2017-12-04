a1ba468b739ae3ebf6ac7a2927d945d164776f6a
Allison Tzadok <allisontzadok@gmail.com>
2017-12-04 06:57:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+ kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var newKitten = [...kittens,"Broom"]
+  return newKitten;
+}
+
+function prependKitten(name){
+  var newKitten = ["Arnold",...kittens]
+  return newKitten;
+}
+
+function removeLastKitten(name){
+  var newKitten = kittens;
+  return newKitten.slice(0, newKitten.length - 1);
+}
+
+function removeFirstKitten(name){
+  var NewKitten = kittens
+  return NewKitten.slice(1);
+}
