f701cd5b2a72c6dda3b433017a97e6a1d5af54d2
Brian Embry <b.a.embry@gmail.com>
2017-12-05 14:22:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+ // return(kittens)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+ // return(kittens)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+function appendKitten(name) {
+  var kittens2 = [...kittens.slice(0, kittens.length), name]
+  return(kittens2)
+}
+function prependKitten(name) {
+  var kittens2 = [name, ...kittens]
+  return(kittens2)
+}
+function removeLastKitten() {
+  var kittens2 = kittens.slice(0, kittens.length - 1)
+  return(kittens2)
+}
+function removeFirstKitten() {
+  var kittens2 = kittens.slice(1)
+  return(kittens2)
+}
+
