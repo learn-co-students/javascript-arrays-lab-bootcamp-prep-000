804d616770f4c4332a6bf57fbb07715d250889ce
Rollin Metzger <rollin.metzger@gmail.com>
2017-12-05 18:00:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+ var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens1 = [name, ...kittens]
+  return kittens1
+}
+
+function removeLastKitten() {
+  var kittens3 = kittens.slice(0, kittens.length - 1)
+  return kittens3
+}
+
+function removeFirstKitten() {
+  var kittens4 = kittens.slice(1)
+  return kittens4
+}
