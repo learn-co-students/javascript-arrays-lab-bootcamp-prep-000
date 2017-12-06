d120355a25c3575161a36519acaf1c3d48377f68
melissames <melissa.sames@gmail.com>
2017-12-06 19:18:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var newArr = kittens.slice()
+  newArr.push(name)
+  return newArr
+}
+
+function prependKitten(name){
+  var newArr = kittens.slice()
+  newArr.unshift(name)
+  return newArr
+}
+
+function removeLastKitten(){
+  var newArr = kittens.slice()
+  newArr.pop()
+  return newArr
+}
+
+function removeFirstKitten(){
+  var newArr = kittens.slice()
+  newArr.shift()
+  return newArr
+}
