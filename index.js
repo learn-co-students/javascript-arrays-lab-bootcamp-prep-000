41b998805fe37e7267d9e263e6927a3fd917313f
Vas Musca <vas@execute.plus>
2017-12-09 14:11:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1); 
+}
+
