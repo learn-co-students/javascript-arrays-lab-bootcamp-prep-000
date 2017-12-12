195d647143bfcd15592a2f573cd02a4116914221
Mariya <mariya.emv@gmail.com>
2017-12-12 21:31:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push(name);
+  return newArray;
+}
+
+function prependKitten(name){
+  var newKittenArray = kittens.slice();
+  newKittenArray.unshift(name);
+  return newKittenArray;
+}
+
+function removeLastKitten(){
+  var newKittenArray = kittens.slice(0, -1);
+  return newKittenArray;
+}
+
+function removeFirstKitten(){
+  var newKittenArray = kittens.slice(1);
+  return newKittenArray;
+}
