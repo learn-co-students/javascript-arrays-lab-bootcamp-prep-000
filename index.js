221cdbc401134e36801c2a7cac2ea0962a913204
William Spangler <bspangs@hotmail.com>
2017-12-13 17:36:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+kittens = ["Milo", "Otis", "Garfield"]; //global, is this way to reset?
+
+//appends a kitten to the end of the kittens array
+function destructivelyAppendKitten(name){
+kittens.push(name);  
+}
+
+//ralph
+function destructivelyPrependKitten(name){
+kittens.unshift(name);  
+} 
+
+//expects ["Milo", "Otis"]
+function destructivelyRemoveLastKitten()
+{kittens.pop()}
+
+//expects ["Otis", "Garfield"]
+function destructivelyRemoveFirstKitten()
+{kittens.shift()}
+
+function appendKitten(name){
+return [...kittens,name];
+}
+
+function prependKitten(name){
+return [name,...kittens];
+}
+
+function removeLastKitten (){
+return  kittens.slice(0,kittens.length-1 );
+}
+
+function removeLastKitten (){
+return kittens.slice(0,kittens.length-1 );
+}
+
+function removeFirstKitten(){
+return kittens.slice(1);  
+}
