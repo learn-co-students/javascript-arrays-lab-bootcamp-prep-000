c6d576fd41b9ef8b5164f6a579332bc5ccbb6d2c
Ingrid Wong <yw340@georgetown.edu>
2017-12-14 18:06:06 +0000

    Done.

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(name){
   kittens.push(name);
-  return kittens;
 }
 
 function destructivelyPrependKitten(name){
   kittens.unshift(name);
-  return kittens;
 }
 
 function destructivelyRemoveLastKitten(){
   kittens.pop();
-  return kittens;
 }
 
 function destructivelyRemoveFirstKitten(){
   kittens.shift();
-  return kittens;
 }
 
 function appendKitten(name){
