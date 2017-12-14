3f9d521d36ba0f336757390c62fb4106f1fc7dcd
Jon Osterman <suni.abraham@gmail.com>
2017-12-14 14:24:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kitten = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kitten
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop;
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+  kitten.shift;
+  return kitten
+}
+
+function appendKitten(kitten, name){
+  return [...kitten,name]
+}
+
+function prependKitten(kitten, name){
+  return [name,...kitten]
+}
+
+function removeFirstKitten(kitten){
+  return kitten.slice(1)
+}
+
+function removeLastKitten(kitten){
+  return kitten.slice(0,kitten.length-1)
+}
