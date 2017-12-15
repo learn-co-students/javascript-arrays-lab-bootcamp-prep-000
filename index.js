5e2dc4c451efa755989c8a116899107f00fdb878
Jatin Rungta <jatinrungta27@gmail.com>
2017-12-15 13:59:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(arr)
+{
+  kittens=[...kittens,arr]
+}
+function destructivelyPrependKitten(arr)
+{
+  kittens.unshift(arr)
+}
+function destructivelyRemoveLastKitten(arr){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(arr){
+  kittens.shift()
+}
