7750c988491a5975c2709a62b4811dd852985eba
Kalpana Swaminathan <kalpana294@gmail.com>
2017-12-18 21:45:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var kittens = ["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(value){
+ kittens.push(value);
+ return kittens
+  
+}
+kittens = ["Milo","Otis","Garfield"]
+ function destructivelyPrependKitten(value){
+   kittens.unshift(value);
+   return kittens
+ }
+ kittens = ["Milo","Otis","Garfield"]
+ function destructivelyRemoveLastKitten(){
+   kittens.pop();
+   return kittens
+ }
+ kittens = ["Milo","Otis","Garfield"]
+ function destructivelyRemoveFirstKitten(){
+   kittens.shift();
+   return kittens
+ }
+ kittens = ["Milo","Otis","Garfield"]
+ function appendKitten(value){
+  var kittens1 = kittens.concat(value)
+  return kittens1
+ }
+ 
+ function prependKitten(value){
+   var kittens2 = [value, ...kittens]
+   return kittens2
+ }
+  function removeLastKitten(){
+    var kittens3 = kittens.slice(0,2)
+    
+    return kittens3
+  }
+  function removeFirstKitten(){
+    var kittens4 = kittens.slice(1)
+    return kittens4
+  }
