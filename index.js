788ab6f1bde6f5f8b01a04da8e7688adffe0adb3
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 06:16:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 const app = "I don't do much."
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name) {
+  var name = 'Ralph'
+  kittens.push(name)
+}
+
+kittens()
+Arrays()
+
+function destructivelyPrependKitten(name) {
+  var name = 'Bob'
+  kittens.unshift(name)
+}
+
+kittens()
+Arrays()
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+kittens()
+Arrays()
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+kittens()
+Arrays()
+
+function appendKitten(name) {
+  var name = 'Broom'
+  return [...kittens, name]
+}
+
+kittens()
+Arrays()
+
+function prependKitten(name) {
+  var name = 'Arnold'
+  return [name, ...kittens]
+}
+
+kittens()
+Arrays()
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+kittens()
+Arrays()
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
