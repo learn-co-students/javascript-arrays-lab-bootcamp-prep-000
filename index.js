87bdddec6c4f323d4b832259a00c5d67783ed785
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-20 02:16:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.map(function(x) {
+    return x;
+  });
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.map(function(x) {
+    return x;
+  });
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.map(function(x) {
+    return x;
+  });
+  newKittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.map(function(x) {
+    return x;
+  });
+  newKittens.shift();
+  return newKittens;
+}
