851341fa4f8fe0739c99a38d82d5035cb0f78115
David McGrann <dpmcgrann@gmail.com>
2017-12-21 18:29:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
+
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Ralph')
+  return kittens
+}
