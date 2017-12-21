266bfc8b3292848b2de7c6d9c4958a3a7007d3f4
Ronald Bennett <bennettrd318@gmail.com>
2017-12-21 00:06:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+ var kitOnEnd = kittens.push(name);
+ return kitOnEnd;
+}
+
+function destructivelyPrependKitten(name) {
+  var kitOnFront = kittens.unshift(name);
+  return kitOnFront;
+}
+
+function destructivelyRemoveLastKitten() {
+  var removeLastKitten = kittens.pop();
+  return removeLastKitten;
+}
+
+function destructivelyRemoveFirstKitten() {
+  var removeFirstKitten = kittens.shift();
+  return removeFirstKitten;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+  }
+  
+  function removeFirstKitten() {
+    return kittens.slice(1, kittens.length + 1);
+  }
