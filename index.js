ae0b86f43bdf5d5c656a87692c1ee763125dde94
David McGrann <dpmcgrann@gmail.com>
2017-12-21 18:50:03 +0000

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,43 @@ function destructivelyAppendKitten(name) {
 var kittens = ['Milo', 'Garfield', 'Otis']
 
 function destructivelyPrependKitten(name) {
-  kittens.unshift('Ralph')
+  kittens.unshift('Bob')
   return kittens
+}
+
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(2)
+  return kittens
+}
+
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(0)
+  return kittens
+}
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function appendKitten(name) {
+  return [...kittens, 'Broom']
+}
+
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function prependKitten(name) {
+  return ['Arnold', ...kittens]
+}
+
+var kittens = ['Milo', 'Garfield', 'Otis']
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function removeFirstKitten() {
+  return kittens.slice(1)
 }
