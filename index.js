607f2dd510a6f021fb8f0afd9bb9bc09ffcacbab
Steven Harms <steven.harms@flatironschool.com>
2017-12-22 16:46:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+var kittens = [];
+
+var destructivelyAppendKitten = (n) => kittens.push(n);
+var destructivelyPrependKitten = (n) => kittens.unshift(n);
+var destructivelyRemoveLastKitten = () => kittens.pop();
+var destructivelyRemoveFirstKitten = () => kittens.shift();
+var appendKitten = (n) => [...kittens, n];
+var prependKitten = (n) => [n, ...kittens];
+var removeLastKitten = () => kittens.slice(0, kittens.length - 1);
+var removeFirstKitten = () => kittens.slice(1);
