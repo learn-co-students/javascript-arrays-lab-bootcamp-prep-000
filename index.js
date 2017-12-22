71c584d130234cce1a15fc81f45cd1144ac7affb
winter mutt <sejongsan@hotmail.com>
2017-12-22 19:56:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const app = "I don't do much."
 
-var kittens = ["Milo","Otis","Garfield"];
-
 function destructivelyAppendKitten(name){
-  kittens.push("Ralph");
-  return kittens;
+  kittens.push(name);
 }
 
 function destructivelyPrependKitten(name){
-  kittens.unshift("Bob");
-  return kittens;
+  kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(){
   kittens.pop();
-  return kittens;
 }
 
 function destructivelyRemoveFirstKitten(){
   kittens.shift();
-  return kittens;
 }
 
 function appendKitten(name){
-  return [...kittens, "Broom"];
+  return [...kittens, name];
 }
 
 function prependKitten(name){
-  return ["Arnold", ...kittens];
+  return [name, ...kittens];
 }
 
 function removeLastKitten(){
