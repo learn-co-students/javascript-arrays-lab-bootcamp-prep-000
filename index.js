6c345e49de0358d3da1a4af829f8e0e6a8a13e39
meinnes <innesmercean@gmail.com>
2017-12-22 03:18:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+const kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+return kittens;
+}
+
+function appendKitten(name){
+let newKittens = [...kittens, name];
+return newKittens;
+}
+
+function prependKitten(name){
+let newKittens = [name, ...kittens];
+return newKittens;
+}
+
+function removeLastKitten(){
+return kittens.slice(0, -1);
+}
+
+function removeFirstKitten(){
+return kittens.slice(1);
+}
