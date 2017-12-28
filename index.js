8894071cbfe4984d4db8dab26ded58adeb561204
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-28 03:09:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+const kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name){
+  const moreKittens = [...kittens, "Broom"];
+  return moreKittens
+}
+
+function prependKitten(name){
+  const moreKittens = ["Arnold", ...kittens];
+  return moreKittens
+}
+
+function removeLastKitten(){
+  const fewerKittens = kittens.slice(0, 2);
+  return fewerKittens
+}
+
+function removeFirstKitten(){
+  const fewerKittens = kittens.slice(-2);
+  return fewerKittens
+}
