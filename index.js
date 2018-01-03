6c2dc2ff7aadf29efc187a0f7b36e6b2093a640c
Alec German <alec_german@yahoo.com>
2018-01-03 02:22:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var array = [...kittens, name];
+  return array;
+}
+
+function prependKitten(name) {
+  var array = [name, ...kittens];
+  return array;
+}
+
+function removeLastKitten(name) {
+  var array = kittens.slice(0, kittens.length - 1);
+  return array;
+}
+
+function removeFirstKitten(name) {
+  var array = kittens.slice(1);
+  return array;
+}
