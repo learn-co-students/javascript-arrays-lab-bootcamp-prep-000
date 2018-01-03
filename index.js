e289efbb3d79ed408f400dae8e5578bcb7256d17
Szewah Chin <swgrleong@gmail.com>
2018-01-03 19:27:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function kittens () {
+  return kittens
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
