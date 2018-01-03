673aa9be42d1be159ebff644d7b4c63de40c5ff9
Szewah Chin <swgrleong@gmail.com>
2018-01-03 20:10:58 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ function kittens () {
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newArr = kittens.slice()
+  newArr.push(name)
+  return newArr
+}
+
+
+function prependKitten(name){
+  var newArr = kittens.slice()
+  newArr.unshift(name)
+  return newArr
+}
+
+function removeLastKitten() {
+  var newArr = kittens.slice()
+  newArr.pop(name)
+  return newArr
+}
+
+function removeFirstKitten() {
+  var newArr = kittens.slice()
+  newArr.shift(name)
+  return newArr
+}
