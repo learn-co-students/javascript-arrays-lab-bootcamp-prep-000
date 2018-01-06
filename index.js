61d89f363ae83b7f0ecd20adcef39fbe9b62e27a
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-06 18:43:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var a = [...kittens];
+  a.push(name);
+  return a;
+}
+
+function prependKitten(name){
+  var a = [...kittens];
+  a.unshift(name);
+  return a;
+}
+
+function removeLastKitten(){
+    var a = [...kittens];
+    a.pop();
+    return a;
+}
+
+function removeFirstKitten(){
+  var a = [...kittens];
+  a.shift();
+  return a;
+}
