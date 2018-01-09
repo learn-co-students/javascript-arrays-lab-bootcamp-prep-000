2574730f71bbd1aa37e2ad4442cf665acec58de7
Rosie Wang <rosiedwang@yahoo.com>
2018-01-09 21:40:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-var kittens
+var kittens = ["Milo","Otis","Garfield"]
 function destructivelyAppendKitten(name)
+{
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten()
+{
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten()
+{
+  return kittens.shift()
+}
+function appendKitten(name)
+{
+  var changed = [...kittens,name]
+  return changed
+}
+function prependKitten(name)
+{
+  var changed = [name,...kittens]
+  return changed
+}
+function removeLastKitten()
+{
+  var last = kittens[kittens.length-1]
+  
+}
+function removeFirstKitten()
 {
   
 }
