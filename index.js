16486ae69f4cd9cca97451354dd3417537e2487f
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-09 01:38:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(kittenString) {
+  kittens.push(kittenString)
+  return kittens
+}
+
+function destructivelyPrependKitten(kittenString) {
+  kittens.unshift(kittenString)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(Broom) {
+  return [...kittens, Broom]
+}
+
+function prependKitten(Broom) {
+  return [Broom, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
