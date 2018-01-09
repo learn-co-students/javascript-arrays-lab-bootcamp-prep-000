6f2e3bd2267a44c3f83cca1a352dac2306f6edb3
Dan Brett <boristhellama99@gmail.com>
2018-01-09 23:53:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var array2 = [...kittens, name];
+  return array2;
+}
+
+function removeFirstKitten() {
+  var array2 = kittens.slice(1);
+  return array2;
+}
+
+function prependKitten(name) {
+  var array2 = [name, ...kittens];
+  return array2;
+}
+
+function removeLastKitten() {
+  var array2 = kittens.slice(0, kittens.length - 1);
+  return array2;
+}
