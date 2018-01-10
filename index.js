1c9594ab614de3a29ad265fd744722d0102461f1
Kimon Assim <breatheloud@icloud.com>
2018-01-10 17:15:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kitten){
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten){
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(kitten){
+  var arr = [ ...kittens, kitten ];
+  return arr;
+}
+
+function prependKitten(kitten){
+  var arr = [ kitten, ...kittens ];
+  return arr;
+}
+
+function removeLastKitten(){
+  var arr = kittens.slice( 0, kittens.length - 1 );
+  return arr;
+}
+
+function removeFirstKitten(){
+  var arr = kittens.slice ( 1, kittens.length );
+  return arr;
+}
