43f15fa22c767054ad01cc683ceb4ba83e49dcf7
p huang <huang.pien@gmail.com>
2018-01-10 04:05:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+window.kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(array){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(array){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  window.kittens1=[...kittens,name]
+  return kittens1
+}
+
+function prependKitten(name){
+  window.kittens1=[name,...kittens]
+  return kittens1
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
