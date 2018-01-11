c97c6365ed8e884eca2c5463344167696b5ea279
Andrés Fernando  Foronda Espinal <andresforonda.af@gmail.com>
2018-01-11 21:14:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop(name);
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  var newKitten = [...kittens, name];
+  return newKitten
+}
+function prependKitten(name){
+  var newKitten = [name, ...kittens];
+  return newKitten
+}
+function removeLastKitten(){
+  var newKitten = [...kittens];
+  newKitten.pop();
+  return newKitten
+}
+function removeFirstKitten(){
+  var newKitten = [...kittens];
+  newKitten.shift();
+  return newKitten
+}
