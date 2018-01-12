31ec40e8c4d81d0bcb0580e289b6ad106c0f80bf
Mangesh Kumar <kumarmangesh55@gmail.com>
2018-01-12 16:42:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+var kittens=["Milo", "Otis", "Garfield"];
+
+function appendKitten(name)
+{
+  return [...kittens,name];
+}
+function prependKitten(name)
+{
+  return [name,...kittens];
+}
+function removeFirstKitten()
+{
+  return kittens.slice(1);
+}
+function removeLastKitten()
+{
+  return kittens.slice(0,kittens.length-1);
+}
+function destructivelyAppendKitten(name)
+{
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name)
+{
+  return kittens.unshift(name);
+} 
+
+function destructivelyRemoveLastKitten(name)
+{
+  return kittens.pop(name);
+} 
+
+function destructivelyRemoveFirstKitten(name)
+{
+  return kittens.shift(name);
+} 
