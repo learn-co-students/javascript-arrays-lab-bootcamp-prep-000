1fa49cc6f588e4f94e2818089dbc01ff5fbe6dd0
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-14 04:00:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  var outputArray = [...kittens, name]
+  return outputArray
+}
+function prependKitten(name) {
+  var outputArray = [name, ...kittens]
+  return outputArray
+}
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
