b65fdb53429ef1adf5b696c7ca00f1c015180abc
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-14 15:50:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+   window.kittens.push(name)
+   return
+}
+function destructivelyPrependKitten(name) {
+   window.kittens.unshift(name)
+   return
+}
+function destructivelyRemoveLastKitten() {
+  window.kittens.pop()
+  return
+}
+function destructivelyRemoveFirstKitten() {
+  window.kittens.shift()
+  return
+}
+function removeLastKitten() {
+  return window.kittens.slice(0,kittens.length -1)
+}
+function appendKitten(name) {
+  return [...window.kittens, name]
+}
+function prependKitten(name) {
+  return [name, ...window.kittens]
+}
+function removeFirstKitten() {
+  return window.kittens.slice(1)
+}
