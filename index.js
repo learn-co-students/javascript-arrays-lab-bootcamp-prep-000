bb9d675f93c2dbd8a9b4ae59276961f091af4e66
Kate <kate.dubitski@gmail.com>
2018-01-14 18:08:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  var newKittens = [...kittens,name];
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens = [...kittens];
+  newKittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  var newKittens = [...kittens];
+  newKittens.shift();
+  return newKittens;
+}
