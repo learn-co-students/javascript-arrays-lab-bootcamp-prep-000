1fa83ed72bddd59506b723e4202ac3c68bbcf390
Greg Stimson <stimsong@yahoo.com>
2018-01-15 19:34:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var appKittens = [...kittens, name];
+  return appKittens;
+}
+
+function prependKitten(name) {
+  var preKittens = [name, ...kittens];
+  return preKittens;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
