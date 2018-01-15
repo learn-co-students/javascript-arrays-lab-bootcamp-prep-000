8e71554dbfe18a3c25998e2f6753a6cedcfeb525
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 13:40:43 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ function destructivelyRemoveFirstKitten() {
   return kittens
 }
 
+function appendKitten(name) {
+  var newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1)
+  return newArray
+}
