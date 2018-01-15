6af92298bf7df8dfed13f0ae763e3ec2446e521d
Iho Somnam <ihosomnam4741@gmail.com>
2018-01-15 15:15:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const app = "I don't do much."
+kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  name = ["Milo", "Otis", "Garfield", "Ralph"]
+  name.shift("Ralph")
+}
