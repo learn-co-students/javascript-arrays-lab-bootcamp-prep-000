15a9ea5a7f97c387f5d97a4792029f9e71bb0786
Sarah F <sarahelayna@gmail.com>
2018-01-16 07:19:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var lastKitten = [...kittens, name];
+  return lastKitten;
+}
+
+function prependKitten(name) {
+  var firstKitten = [name, ...kittens];
+  return firstKitten;
+}
+
+function removeLastKitten() {
+  var removeLastKitty = kittens.slice(0, kittens.length - 1);
+  return removeLastKitty;
+}
+
+function removeFirstKitten() {
+  var removeFirstKitty = kittens.slice(1);
+  return removeFirstKitty;
+}
