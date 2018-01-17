750e6c5e63235114719527aafd36991f526ba6aa
Mark Grant <mark.rd.grant@gmail.com>
2018-01-17 12:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
