3815bcbc9d2d93841c3fc85d982a7d7ed31fd0dc
semih Fatih <s_fatihh@hotmail.com>
2018-01-17 23:33:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = function(name){
+  kittens.push(name);
+  return kittens;
+}
+
+var destructivelyPrependKitten = function(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = function(){
+  kittens.pop();
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = function(){
+  kittens.shift();
+  return kittens
+}
+
+var appendKitten = function(name){
+  var array = [...kittens];
+  array.push(name);
+  return array;
+}
+
+var prependKitten = function(name){
+  
+  var array =[...kittens];
+  array.unshift(name);
+  return array;
+}
+
+var removeLastKitten = function(){
+  var array =[...kittens];
+  array.pop();
+  return array;
+}
+
+var removeFirstKitten= function(){
+  var array = [...kittens];
+  array.shift();
+  return array;
+  
+}
