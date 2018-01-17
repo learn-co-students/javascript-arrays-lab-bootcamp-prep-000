c8ded676ad6e9e24e62c8cd7b43c040dd4b92c9f
Carine Tomassian <carinetomassian@gmail.com>
2018-01-17 07:53:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var addKittenToEnd = [...kittens, name];
+  return addKittenToEnd;
+}
+
+function prependKitten(name) {
+  var addKittenToBeginning = [name, ...kittens];
+  return addKittenToBeginning;
+}
+
+function removeLastKitten() {
+  var byeLastKitten = kittens.slice(0, kittens.length-1);
+  return byeLastKitten;
+}
+
+function removeFirstKitten() {
+  var byeFirstKitten = kittens.slice(1);
+  return byeFirstKitten;
+}
