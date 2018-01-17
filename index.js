b69441d46a9cdcfd3ab8ad4949aed8bb9fae3397
Mark Grant <mark.rd.grant@gmail.com>
2018-01-17 12:18:08 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+    kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
