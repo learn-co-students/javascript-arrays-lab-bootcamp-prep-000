43ec190c70c0af79afc46c5b78fc2a5d11ff10fc
Laurents Mohr <laurents.mohr@whu.edu>
2018-01-18 02:25:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var arr = [...kittens, name];
+  return arr;
+}
+
+function prependKitten(name) {
+  var arr = [name, ...kittens];
+  return arr;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
