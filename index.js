ab13a05ed323540d230a1229983196e30744f7f1
Emily J Cain <emilycain@ymail.com>
2018-01-18 16:16:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  name = "Ralph";
+  return kittens.push(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten(name) {
+  name = "Bob";
+  kittens.unshift(name);
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function appendKitten(name) {
+  name = "Broom";
+  return [...kittens, name];
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function prependKitten(name) {
+  name = "Arnold";
+  return [name, ...kittens];
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
