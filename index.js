bb12fc7669f8e961cc9000ba67ad1710f43bd9e3
Douglas Craig <dcraig112@gmail.com>
2018-01-19 15:25:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
-const app = "I don't do much."
-
-var kittens =["Milo", "Otis", "Garfield"]
-var newKitten = "Ralph"
-
-function destructivelyAppendKitten(newKitten) {
-	kittens.push(newKitten)
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+	kittens.push(name)
 	return kittens
 }
-
-function destructivelyPrependKitten(newKitten)	{
-	kittens.unshift(newKitten)
+function destructivelyPrependKitten(name) {
+	kittens.unshift(name)
 	return kittens
 }
-
 function destructivelyRemoveLastKitten() {
-	kittens.pop()
-	return kittens
+  return kittens.pop()
 }
-
-function destructivelyRemoveFirstKitten() { //removes the First kitten from the kittens array:
-	kittens.shift()
-	return kittens
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
 }
-
-function appendKitten(newKitten) {
-	var newArray = [...kittens, newKitten]
-	return newArray
+function appendKitten(name) {
+  var newArray = [...kittens, name]
+  return newArray
 }
-
-function prependKitten(newKitten) {
-	var newArray = [newKitten, ...kittens]
-	return newArray
+function prependKitten(name) {
+  var newArray = [name, ...kittens]
+  return newArray
 }
-
 function removeLastKitten() {
-	var newArray = kittens.slice(0, kittens.length -1)
-	return newArray
+  var newArray = kittens.slice(0, kittens.length -1)
+  return newArray
 }
-
 function removeFirstKitten() {
-	var newArray = kittens.slice(1)
-	return newArray
+  var newArray = kittens.slice(1)
+  return newArray
 }
