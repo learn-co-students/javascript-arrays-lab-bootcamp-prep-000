40b8037e84da6d8c51523dea1fc0674c6d5d398c
Ronald DeBeras <rdeberas@gmail.com>
2018-01-20 18:37:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+var kittens = ['Milo', 'Otis', 'Garfield', 'Ralph'];
+
+ function destructivelyPrependKitten(name) {
+kittens.unshift(name)
+  return kittens
+ }
+var kittens = ['Bob', 'Milo', 'Otis', 'Garfield'];
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+var kittens = ['Milo', 'Otis'];
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+var kittens = ['Milo', 'Otis', 'Garfield', 'Broom'];
+
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push(name)
+  return newArray
+}
+var kittens = ['Arnold', 'Milo', 'Otis', 'Garfield'];
+
+function prependKitten(name) {
+return ["Arnold", ...kittens]
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function removeLastKitten(name) {
+  var newArray = kittens.slice();
+  newArray.pop()
+  return newArray
+}
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function removeFirstKitten(name) {
+  var newArray = kittens.slice();
+  newArray.shift()
+  return newArray
+}
