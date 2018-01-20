16d6c4cde84316cebfd8a7c565f317374aa4b1ad
Bill Monney <billyemmel@gmail.com>
2018-01-20 22:53:23 +0000

    Done.

--- index.js
+++ index.js
@@ -23,3 +23,12 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
  return [...kittens, name];
 }
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
