53551cade453205777ae863e5e63e8d3bde56c0a
Yan Hankin <hankinyb@gmail.com>
2018-01-20 19:38:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(array) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens3 = [name, ...kittens]
+  return kittens3
+}
+
+function removeLastKitten(array) {
+  var kittens4 = kittens.slice(0,2)
+  return kittens4
+}
+
+function removeFirstKitten(array) {
+  var kittens5 = kittens.slice(1)
+  return kittens5
+}
