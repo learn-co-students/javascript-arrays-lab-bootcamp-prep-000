f5528d597d13eba35d0f184a654f3c11c46a5d86
Raymond W. Robinson <raymondrobinson123@gmail.com>
2018-01-20 21:41:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(){
+  const newKittens = [...kittens, "Broom"]
+  return newKittens
+}
+
+function prependKitten(){
+  const newKittens = ["Arnold", ...kittens]
+  return newKittens
+}
+
+function removeLastKitten(){
+  const newKittens = kittens.slice(0, kittens.length -1)
+  return newKittens
+}
+
+function removeFirstKitten(){
+  const newKittens = kittens.slice(1)
+  return newKittens
+}
