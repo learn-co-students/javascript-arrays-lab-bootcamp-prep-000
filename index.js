f20ece16d4b7ca2d912b35d4e58fec5e3ef0e62c
Betsy Sallee <ensallee@gmail.com>
2018-01-20 19:36:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var moreKittens= [...kittens, "Broom"];
+  return moreKittens;
+}
+
+function prependKitten(name){
+  var evenMoreKittens=["Arnold", ...kittens];
+  return evenMoreKittens;
+}
+
+function removeLastKitten(){
+  var fewerKittens=kittens.slice(0, kittens.length-1);
+  return fewerKittens;
+}
+
+function removeFirstKitten(){
+  var evenFewerKittens=kittens.slice(1);
+  return evenFewerKittens;
+}
