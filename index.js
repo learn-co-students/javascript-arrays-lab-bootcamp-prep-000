9d849c5bbf4b94d527b636d0456da9263c20d2fa
Bill Monney <billyemmel@gmail.com>
2018-01-20 22:50:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name) {
+   kittens.push(`${name}`)
+   // destructively adds name to end of kittens array
+   return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(`${name}`)
+  // destructively adds name to beginning of kittens array
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+// destructively removes last name in kittens array
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  //destructively removes first name from kittens array
+  return kittens
+}
+function appendKitten(name) {
+ return [...kittens, name];
+}
