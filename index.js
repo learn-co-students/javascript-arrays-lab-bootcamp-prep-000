d7114a79cb7b58c2ce312fa40b310fe44c6fdaff
Sujata Roberts <sujata.roberts@gmail.com>
2018-01-22 05:40:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+var kittens =
+  ["Milo",
+  "Otis",
+  "Garfield"
+  ]
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+ function appendKitten(name) {
+  var newArray = kittens.slice(0)
+  newArray.push(name);
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = kittens.slice(0)
+  newArray.unshift(name);
+  return newArray;
+}
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1)
+  return newArray;
+}
