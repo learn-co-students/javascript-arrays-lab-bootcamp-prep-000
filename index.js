4511408dc60128720109d32b5b3752f9aebbafe6
Darwin Smith <darwin911@gmail.com>
2018-01-23 04:46:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Garfield", "Stevie", "Protein"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var moreKittensEnd = [...kittens, name]
+  return moreKittensEnd
+}
+
+function prependKitten(name) {
+  var moreKittensBeginning = [name, ...kittens]
+  return moreKittensBeginning
+}
+
+function removeLastKitten() {
+  var popLastKitten =  kittens.slice(0, kittens.length - 1)
+  return popLastKitten
+}
+
+function removeFirstKitten() {
+  var popFirstKitten = kittens.slice(1)
+  return popFirstKitten
+}
