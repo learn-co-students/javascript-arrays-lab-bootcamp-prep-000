4ad0254545681d9577fca0cf8045331926c16b29
Scott Silverman <scottms724@gmail.com>
2018-01-23 16:29:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  var morekittens = [name, ...kittens]
+  return morekittens
+}
+
+function removeLastKitten() {
+  var lesskittens = kittens.slice(0, 2)
+  return lesskittens
+}
+
+function removeFirstKitten() {
+  var lesskittens = kittens.slice(1, kittens.length + 1)
+  return lesskittens
+}
