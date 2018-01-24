3d9204ac4dd90b7243fcff101a2921102533c17e
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-24 00:26:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+   kittens.push(name);
+   return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var k = [...kittens, 'Broom'];
+  return k;
+}
+
+function prependKitten(name) {
+  var k = [ 'Arnold',...kittens];
+  return k;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, 2)
+}
+  
+  
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+  
+  
+  
+  
