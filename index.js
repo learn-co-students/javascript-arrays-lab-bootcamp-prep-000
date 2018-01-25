45e26fbfc2c76413e33cd28e80248165394100dd
Mark Stephens <mstephens@trackeleven.com>
2018-01-25 13:00:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+function appendKitten(name){
+  var arrayNew = [...kittens, name];
+  return arrayNew;
+}
+function prependKitten(name){
+  var arrayNew = [name, ...kittens];
+  return arrayNew;
+}
+function removeLastKitten(name){
+  var arrayNew = kittens.slice(0,2);
+  return arrayNew;
+}
+function removeFirstKitten(name){
+  var arrayNew = kittens.slice(1,3);
+  return arrayNew;
+}
