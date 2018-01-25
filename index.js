e8c68a70c2f802383eb6ec4f45128d2ea743f576
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-25 20:31:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+	kittens.push(name);
+	return kittens;
+}
+function destructivelyPrependKitten(name){
+	kittens.unshift(name);
+	return kittens;
+}
+function destructivelyRemoveLastKitten(){
+	kittens.pop();
+	return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+	kittens.shift();
+	return kittens;
+}
+function appendKitten(name){
+	var macke = [...kittens];
+	macke.push(name);
+  return macke;
+}
+function prependKitten(name){
+	var macke = [...kittens];
+	macke.unshift(name);
+  return macke;
+}
+function removeLastKitten(){
+	var macke = [...kittens];
+	macke.pop();
+  return macke;
+}
+function removeFirstKitten(){
+	var macke = [...kittens];
+	macke.shift();
+  return macke;
+}
