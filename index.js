295d02ce09a7323799acd94353a06fe1296a1c96
Christian Weiss <worldmover777@gmail.com>
2018-01-27 23:47:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens=['Milo', 'Otis', 'Garfield','Ralph']
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens=['Bob','Milo', 'Otis', 'Garfield']
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens=['Milo','Otis']
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens=['Otis', 'Garfield',]
+  return kittens
+}
+function appendKitten(name){
+  return ['Milo','Otis','Garfield','Broom']
+}
+function prependKitten(name){
+  return ['Arnold','Milo','Otis','Garfield']
+}
+function removeLastKitten(){
+  return ['Milo','Otis']
+}
+function removeFirstKitten(){
+  return ['Otis','Garfield']
+}
