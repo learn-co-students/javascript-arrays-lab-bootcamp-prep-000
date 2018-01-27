3d9e7da5a61610da4fe419722de8eb188ec31b5c
jairaj shetty <jayuchintu@gmail.com>
2018-01-27 06:41:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens=["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+}
+function appendKitten(name){
+ var new1= kittens.slice();
+new1.push(name);
+return new1 ;
+
+}
+function prependKitten(name){
+ var new1= kittens.slice();
+new1.unshift(name);
+return new1 ;
+}
+function removeLastKitten(name){
+   var new1= kittens.slice();
+new1.pop(name);
+return new1 ;
+}
+function removeFirstKitten(name){
+   var new1= kittens.slice();
+new1.shift(name);
+return new1 ;
+}
