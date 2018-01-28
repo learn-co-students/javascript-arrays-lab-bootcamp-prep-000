615dd9657350aaa6e462cb24d96ee07faa3c3cfc
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 08:50:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(cat) {
+  return kittens.push(cat)
+}
+
+function destructivelyPrependKitten(cat) {
+  return kittens.unshift(cat)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(cat) {
+  return [...kittens, cat]
+}
+
+function prependKitten(cat) {
+  return [cat, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
