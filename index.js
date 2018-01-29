3d8f5f9c873cedbe239b0b1ab448888fef53097b
Cutina Woof <gretchenista@gmail.com>
2018-01-29 20:12:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+destructivelyAppendKitten(element) {
+   kittens.push(element)
+}
+
+destructivelyPrependKitten(element) {
+    kittens.unshift(element)
+}
+
+destructivelyRemoveLastKitten(element) {
+  kittens.pop(element)
+}
+
+destructivelyRemoveFirstKitten(element) {
+  kittens.shift(element)
+}
+
+appendKitten(name) {
+  return [...kittens, name]
+}
+
+prependKitten(name) {
+  return [name, ...kittens]
+}
+
+removeLastKitten() {
+ return kittens.slice(0, kittens.length -1)
+}
+
+removeFirstKitten() {
+  return kittens.slice(1)
+}
