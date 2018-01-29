87f9f9c21aec59f1e7275125e0926657081ba3df
Charles Mitchell <charlesmitchell@protonmail.com>
2018-01-29 19:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]; 
+
+var destructivelyAppendKitten = function(name){
+  kittens.push(name); 
+  return kittens; 
+}
+
+var destructivelyPrependKitten = function(name){
+  kittens.unshift(name);
+  return kittens; 
+}
+
+var destructivelyRemoveLastKitten = function(name){
+  kittens.pop();
+  return kittens; 
+}
+
+var destructivelyRemoveFirstKitten = function(name){
+  kittens.shift();
+  return kittens; 
+}
+
+var appendKitten = function(name){
+  var k = [...kittens, name]; 
+  return k; 
+  
+}
+
+var prependKitten = function(name){
+  var k = [name, ...kittens]; 
+  return k; 
+} 
+var removeLastKitten = function(name){
+var k = kittens.slice(0, kittens.length-1); 
+return k; 
+}
+var removeFirstKitten = function(name){
+  var k = kittens.slice(1);
+  return k; 
+  
+}
