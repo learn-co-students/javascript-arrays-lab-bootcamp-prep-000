684bd86e0eb45c52ae8e01fa7c05e0d5101c4838
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-01-29 00:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+ kittens.push(name);
+ return kittens;
+}
+function destructivelyPrependKitten(name){
+ kittens.unshift(name);
+ return kittens;
+}
+function destructivelyRemoveLastKitten(){
+ kittens.pop();
+ return kittens;
+}
+function destructivelyRemoveFirstKitten()
+{kittens.shift();
+ return kittens;
+}
+function appendKitten(name){
+const morekittens = [...kittens,name] 
+ return morekittens;
+}
+function prependKitten(name){
+const morekittens = [name,...kittens] 
+ return morekittens;
+}
+function removeLastKitten(name){
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
