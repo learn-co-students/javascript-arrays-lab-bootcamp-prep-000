0898b067cb4876a238db70517a85ffeec6864758
Deep Patel <deepgpatel93@gmail.com>
2018-02-01 01:16:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 const app = "I don't do much."
+function Arrays()
+{
+ var kittens = ['Milo', 'Otis', 'Garfield']
+ return kittens
+}
+
+function destructivelyAppendKitten(Ralph)
+{
+  kittens.push(Ralph)
+  return kittens
+}
+
+function destructivelyPrependKitten(Bob)
+{
+  kittens.unshift(Bob)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(Broom)
+{
+  return [...kittens, Broom]
+}
+
+function prependKitten(Arnold)
+{
+  return [Arnold, ...kittens]
+}
+
+function removeLastKitten()
+{
+  return [...kittens.slice(0,2)]
+}
+
+function removeFirstKitten()
+{
+  return [...kittens.slice(1)]
+}
+
+
+
+
+
+
