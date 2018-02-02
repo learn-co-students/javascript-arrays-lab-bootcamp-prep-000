ddea540224a172922b93b0ced5e7f12084b98bcd
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 20:04:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var addKittyToEnd = [...kittens, name];
+  return addKittyToEnd;
+}
+
+function prependKitten(name) {
+  var addKittyToFront = [name, ...kittens];
+  return addKittyToFront;
+}
+
+function removeLastKitten() {
+  var byeByeLastKitty = kittens.slice(0, kittens.length - 1);
+  return byeByeLastKitty;
+}
+
+function removeFirstKitten() {
+  var byeByeFirstKitty = kittens.slice(1);
+  return byeByeFirstKitty;
+}
