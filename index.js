ca4a45e7c111d551da4ed9ff29a7fb71c70af9d4
Ryan Ghaida <ryaboug@gmail.com>
2018-02-03 23:18:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield']
+// global variable
+
+function destructivelyAppendKitten(name) {
+
+kittens.push(name)
+
+  return kittens
+}
+
+
+function destructivelyPrependKitten(name) {
+
+kittens.unshift(name)
+
+return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+
+  kittens.pop()
+
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+
+  kittens.shift()
+
+  return kittens
+}
+
+
+function appendKitten(name){
+
+  var kittens2 = [...kittens,name]
+
+  return kittens2
+}
+
+function prependKitten(name){
+
+  var kittens2 = [name, ...kittens]
+
+  return kittens2
+}
+
+function removeLastKitten() {
+
+var kittens2 = kittens.slice(0, 2)
+
+return kittens2
+}
+
+
+function removeFirstKitten() {
+
+  var kittens2 = kittens.slice(1)
+
+  return kittens2
+}
