6b01f8e574dae9183ed9e1b665717cbf624041e9
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 17:00:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  return [...kittens, name]
+  }
+  
+function prependKitten(name) {
+return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length-1)
+  }
+  
+function removeFirstKitten(name) {
+  return kittens.slice(1,kittens.length)
+}
