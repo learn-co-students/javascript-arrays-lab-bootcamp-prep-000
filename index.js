a91b650b75d851c8a0eb11f76e11cd2e66664927
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-07 22:41:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var cats = [...kittens,"Broom"]
+   return cats
+}
+
+function prependKitten(name) {
+  var lions = ["Arnold", ...kittens]
+  return lions
+}
+
+function removeLastKitten() {
+  var kitten = kittens.slice(0, kittens.length-1)
+  return kitten
+}
+
+function removeFirstKitten() {
+  var goats = kittens.slice(1)
+  return goats
+}
