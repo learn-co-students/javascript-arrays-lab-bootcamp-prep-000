f622fe8fe0dfeb2874c18c1a896e8040da0c6133
David Markel <davidgmarkel@gmail.com>
2018-02-07 17:50:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function kittens() {
+   return kittens;
+ }
+
+ function destructivelyAppendKitten(name) {
+    return kittens.push(name);
+  }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, 2);
+}
+
+function removeFirstKitten() {
+    return kittens.slice(1);
+}
