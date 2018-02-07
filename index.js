8d9eb80c6974863fb52f35882da48e3f60e99702
Adam Towers <hello@adamtowers.design>
2018-02-07 14:33:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(array) {
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten(array) {
+  kittens.unshift('Bob');
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(array) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(array, element) {
+  return [...kittens, 'Broom'];
+}
+
+function prependKitten(array) {
+  return ['Arnold', ...kittens];
+}
+
+function removeLastKitten(array) {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(array) {
+  return kittens.slice(1);
+}
