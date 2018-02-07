884ae42966476345654c504dad49dbab3dfc074f
Wade Pedersen <wade.m.pedersen@gmail.com>
2018-02-07 16:41:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var kittens = []
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+}
+function appendKitten(name) {
+  var array = [...kittens,name]
+  return array
+}
+function prependKitten(name) {
+  var array = [name,...kittens]
+  return array
+}
+function removeLastKitten(name) {
+  var array = kittens.slice(0, kittens.length - 1)
+  return array
+}
+function removeFirstKitten(name) {
+  var array = kittens.slice(1)
+  return array
+}
