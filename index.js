9e0263abf6c0fdcd2034ac9e95483ee102f7dc21
Jonathan Lin <jlin8293@gmail.com>
2018-02-08 19:34:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  console.log(kittens);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten(name) {
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+
+function removeFirstKitten(name) {
+  var newArray = kittens.slice(1, kittens.length);
+  return newArray
+}
