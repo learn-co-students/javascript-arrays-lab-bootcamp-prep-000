da06af0fc90e7d751802b38d7b59bf92bd241660
Ronald Tuzo <rontuz@icloud.com>
2018-02-14 17:11:52 +0000

    Done.

--- index.js
+++ index.js
@@ -25,40 +25,15 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [ 'Arnold', 'Milo', 'Otis', 'Garfield' ];
 }
-function removeLastKitten() {
-//  return function Arrays() {
-  var kittens = ['Milo', 'Otis', 'Garfield'];
-  return kittens;
-}
-function destructivelyAppendKitten(name) {
-  kittens.push("Ralph");
-  return kittens;
-}
-function destructivelyPrependKitten(name) {
-  kittens.unshift("Bob");
-  return kittens;
-}
-function destructivelyRemoveLastKitten() {
-  kittens.pop();
-  return kittens;
-}
-function destructivelyRemoveFirstKitten() {
-  kittens.shift();
-  return kittens;
-}
-function appendKitten(name) {
-  return [ 'Milo', 'Otis', 'Garfield', 'Broom' ];
-} 
-function prependKitten(name) {
-  return [ 'Arnold', 'Milo', 'Otis', 'Garfield' ];
-}
+//function removeLastKitten() {
+//var kittens = ['Milo', 'Otis', 'Garfield'];
+//return kittens;
+//}
 function removeLastKitten() {
   return kittens.slice(0, -1);
  }
 function removeFirstKitten() {
-  //var kitten;
-//  kitten = ['Milo', 'Otis', 'Garfield'];
-  return kittens.slice(1);
+   return kittens.slice(1);
  }
  
 
