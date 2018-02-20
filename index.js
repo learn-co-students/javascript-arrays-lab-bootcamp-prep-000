f178cd7a748b35a2abbdd9eb5816eb09e3ad506a
yossi katzman <issoyk@gmail.com>
2018-02-20 18:38:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var name1 = [...kittens, name]
+  return name1
+}
+
+function prependKitten(name) {
+  var name1 = [name, ...kittens]
+  return name1
+}
+
+function removeLastKitten() {
+  var kittensbad = kittens.slice(0, kittens.length - 1);
+  return kittensbad
+}
+
+function removeFirstKitten() {
+  var kitt = kittens.slice(1)
+  return kitt
+}
