bf6391a9ebb7a95cde0882b74e288cd28bbf6c6d
Michael Kroth <mkroth65@gmail.com>
2018-02-21 15:40:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+   var newKittens = kittens.slice(0);
+   newKittens.push(name);
+   return newKittens;
+}
+ 
+function prependKitten(name){
+  var newKittens = kittens.slice(0);
+  newKittens.unshift(name);
+  return newKittens;
+}
+ 
+function removeLastKitten(){
+  var newKittens = kittens.slice(0);
+  newKittens.pop();
+  return newKittens;
+}
+ 
+function removeFirstKitten(){
+  var newKittens = kittens.slice(0);
+  newKittens.shift();
+  return newKittens;
+}
