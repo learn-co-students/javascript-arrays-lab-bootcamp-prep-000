caf47f1d70ca4bac495be4a0825ce76f6237c0f5
Ricardo Ebbers Carneiro Leão <recl@cin.ufpe.br>
2018-02-23 21:37:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+const kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var arr2 = [...kittens, name];
+  return arr2;
+}
+
+function prependKitten(name){
+  var arr2 = [name, ...kittens];
+  return arr2;
+}
+
+function removeLastKitten(){
+  var arr2 = kittens.slice(0, kittens.length - 1);
+  return arr2;
+}
+
+function removeFirstKitten(){
+  var arr2 = kittens.slice(1);
+  return arr2;
+}
