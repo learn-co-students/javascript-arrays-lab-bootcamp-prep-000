abea4ccaa89c38e97913d1e2e587fb0f11113cca
Sandy Yeung <sandynism@gmail.com>
2018-02-24 21:26:44 +0000

    Done.

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(name) {
-  return kittens.push(name)
+  return kittens.push(name) //adds kitten to end
 }
 
 function destructivelyPrependKitten(name) {
-  return kittens.unshift(name)
+  return kittens.unshift(name) //adds kitten to beginning
 }
 
 function destructivelyRemoveLastKitten() {
-   return kittens.pop()
+  return kittens.pop() //removes last kitten
 }
 
 function destructivelyRemoveFirstKitten() {
-    return kittens.shift()
+  return kittens.shift() //removes first kitten
 }
 
 function appendKitten(name) {
-    return [...kittens, name]
+  return [...kittens, name] //use ...spread operator to preserve original array
 }
 
 function prependKitten(name) {
-    return [name, ...kittens]
+  return [name, ...kittens] //put spread operator at the end
 }
 
 function removeLastKitten() {
-  return kittens.slice(0,kittens.length-1)
+  return kittens.slice(0, kittens.length-1) //removes only one kitten at the end of array
 }
 
-function removeFirstKitten(){
-  return kittens.slice(1)
+function removeFirstKitten() {
+  return kittens.slice(1) //remove first kitten
 }
