3149e39f9e686441d59e5df421358ea48ff38b7a
Aaron Gibson <aaron.gibson13@gmail.com>
2018-02-24 16:14:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(entry) {
+  //access an array, add an element to the end and return mutated new array
+  
+  kittens.push(entry)
+  return kittens
+}
+
+function destructivelyPrependKitten(entry) {
+  //acces kittens, add an element to beginning and return the mutated kittens array
+  
+  kittens.unshift(entry)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(entry) {
+  var cats = [...kittens, entry]
+  return cats
+}
+
+function prependKitten(entry) {
+  var cats = [entry, ...kittens]
+  return cats
+}
+
+function removeLastKitten() {
+  var cats = kittens.slice(0, kittens.length - 1)
+  return cats
+}
+
+function removeFirstKitten() {
+  var cats = kittens.slice(1)
+  return cats
+}
