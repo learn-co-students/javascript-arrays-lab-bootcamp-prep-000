78c2fa4725858ed0321c85086af2c29e7db9de19
Nancy Jo Chang <enciie@gmail.com>
2018-02-26 05:36:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+}
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push("Broom")
+  return newArray;
+}
+function prependKitten(name){
+  var newArray = kittens.slice();
+  newArray.unshift("Arnold")
+  return newArray;
+}
+function removeLastKitten(){
+  var newArray = kittens.slice();
+  newArray.pop()
+  return newArray;
+}
+function removeFirstKitten(){
+  var newArray = kittens.slice();
+  newArray.shift()
+  return newArray;
+}
