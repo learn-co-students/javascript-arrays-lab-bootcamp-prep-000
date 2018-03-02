fca32df4ee772659bd472df95963a58b9c2965ce
guptaco@ymail.com <guptaco@ymail.com>
2018-03-02 02:19:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(){
+kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(){
+kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop("Garfield")
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift("Bob")
+}
+
+function appendKitten(){
+  var newKittens = kittens.slice()
+  newKittens.push("Broom")
+  return newKittens
+}
+
+function prependKitten(){
+  var newKittens = kittens.slice()
+  newKittens.unshift("Arnold")
+  return newKittens
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice()
+  newKittens.pop("Garfield")
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice()
+  newKittens.shift("Arnold")
+  return newKittens
+}
