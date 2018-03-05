9c2087ff3e1f167f15336ac8d5cf9a1ee1146985
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-05 21:08:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(n){
+  kittens.push(n)
+  return kittens
+}
+function destructivelyPrependKitten(n){
+  kittens.unshift(n)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens = kittens.slice(0,-1)
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+ return[...kittens,name] 
+}
+function prependKitten(name){
+  return [name, ...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
