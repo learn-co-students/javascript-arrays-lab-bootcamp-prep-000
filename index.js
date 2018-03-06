0c81ff3415c9ea56d17491bd05ee30848c926419
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 05:38:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name){
+kittens.push(name);
+} //appends a kitten to the end of the kittens array
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+} //prepends a kitten to the beginning of the kittens array
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+} //removes the last kitten from the kittens array
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+} //removes the First kitten from the kittens array
+function appendKitten(name){
+  return [...kittens, name]
+} //appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function prependKitten(name){
+  return [name, ...kittens]
+} //prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+} //removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+function removeFirstKitten(){
+return kittens.slice(1)
+} //removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
