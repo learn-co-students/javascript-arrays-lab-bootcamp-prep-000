ffec201a00fe4ac22eb4fda0e60f88a8a3263ad3
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-06 05:08:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(Buttons) {
+  kittens.push(Buttons);
+  return kittens
+}
+
+function destructivelyPrependKitten(Fluffy) {
+  kittens.unshift(Fluffy)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(Scratches) {
+  return [...kittens, Scratches];
+}
+
+function prependKitten(Meowington) {
+  return [Meowington, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
