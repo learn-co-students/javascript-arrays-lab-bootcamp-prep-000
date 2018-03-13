2ca70b884dfe0ca9b3fd883464e81bb977adbfc0
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 08:40:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 const app = "I don't do much."
+function destructivelyAppendKitten (name) {
+    kittens.push(name)
+    return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(kittens.length)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var arr = [...kittens, name]
+  return arr
+}
+
+function prependKitten(name) {
+  var arr = [name, ...kittens]
+  return arr
+}
+
+function removeLastKitten() {
+  var arr = kittens.slice(0, kittens.length -1)
+  return arr
+}
+
+function removeFirstKitten() {
+  var arr = kittens.slice(1)
+  return arr
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
