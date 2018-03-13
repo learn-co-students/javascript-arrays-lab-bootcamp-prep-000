3e7f33918a3515255641e3a2909c3484d96fbad7
austin mcclain <mcclainduer@gmail.com>
2018-03-13 05:35:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = [...kittens];
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function  removeLastKitten(name) {
+  var newKittens = [...kittens];
+  newKittens.pop(name);
+  return newKittens;
+}
+
+function removeFirstKitten(name) {
+  var newKittens = [...kittens];
+  newKittens.shift(name);
+  return newKittens;
+}
+
+
+destructivelyAppendKitten("Ralph");
+destructivelyPrependKitten("Karl");
+destructivelyRemoveLastKitten("kyle");
+destructivelyRemoveFirstKitten("jack");
