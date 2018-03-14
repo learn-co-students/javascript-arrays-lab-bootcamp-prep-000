1acad5a5f1191eae140b2cfeb8ca60a988a7330f
Brendan <brendangraff@gmail.com>
2018-03-14 23:24:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+}
+
+function appendKitten(name)
+{
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+
+function prependKitten(name)
+{
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+
+function removeLastKitten()
+{
+  var newKittens = [...kittens];
+  newKittens.pop();
+  return newKittens;
+}
+
+
+function removeFirstKitten()
+{
+  var newKittens = [...kittens];
+  newKittens.shift();
+  return newKittens;
+}
+
+
+
