e2249b8e036056d105ae07dd4a6cca9e1081cc86
jconway4 <jconwa4@illinois.edu>
2018-03-14 18:49:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name);
+};
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name);
+};
+
+var destructivelyRemoveLastKitten = function(name) {
+  kittens.pop();
+};
+
+var destructivelyRemoveFirstKitten = function(name) {
+  kittens.shift();
+};
+
+var appendKitten = function(name) {
+  var newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
+};
+
+var prependKitten = function(name) {
+  var newKittens = [...kittens];
+  newKittens.unshift(name);
+  return newKittens;
+};
+
+var removeLastKitten = function() {
+  var newKittens = [...kittens];
+  newKittens.pop();
+  return newKittens;
+};
+
+var removeFirstKitten = function() {
+  var newKittens = [...kittens];
+  newKittens.shift();
+  return newKittens;
+}
