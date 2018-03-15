28fe5705978a70a9af5c34d7f5cb90180925a69d
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 14:28:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+  ]
+  
+  function destructivelyAppendKitten(item) {
+    kittens.push(item)
+    return kittens
+  }
+
+  function destructivelyPrependKitten(item) {
+    kittens.unshift(item)
+    return kittens
+  }
+  
+  function destructivelyRemoveLastKitten() {
+    kittens.pop()
+    return kittens
+  }
+  
+  function destructivelyRemoveFirstKitten() {
+    kittens.shift()
+    return kittens
+  }
+  
+  function appendKitten(item) {
+    var kitty = [...kittens, item]
+    return kitty
+  }
+  
+  function prependKitten(item) {
+    var kitty = [item, ...kittens]
+    return kitty
+  }
+  
+  function removeLastKitten() {
+    var kitty = kittens.slice(0,kittens.length-1)
+    return kitty
+  }
+  
+  function removeFirstKitten() {
+    var kitty = kittens.slice(1, kittens.length)
+    return kitty
+  }
+
+  
+  
