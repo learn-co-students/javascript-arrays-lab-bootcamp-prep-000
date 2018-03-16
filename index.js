a0000024831f016c1c4ba1ff7c32d4dc10c2012c
Valerie Hudson <vhudsonphoto@gmail.com>
2018-03-16 01:35:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return name
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return name
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return name
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return name
+}
+function appendKitten(name) {
+  return [...kittens,name]
+}
+function prependKitten(name) {
+  return [name,...kittens]
+}
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length -1)
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
