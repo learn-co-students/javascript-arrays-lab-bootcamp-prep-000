fe6e64d4244cef6cb6bfe2ff24d547a7d3d68423
nblxanueb <a.e.pykhantsev@gmail.com>
2018-03-16 23:10:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+
+
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+
+}
+
+function removeLastKitten() {
+  var newKittens = [...kittens];
+  newKittens.pop();
+  return newKittens;
+
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+
+}
