c0b5b7245844663656313d4ee58b91c24eb445b5
Hollie Lambert <hollielambert@protonmail.com>
2018-03-18 16:36:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+}
+
+function appendKitten(name) {
+  let newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  let newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  let newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  let newKittens = kittens.slice(1);
+  return newKittens;
+}
