49915ee1a22ab15ffa81920346f84f50a9b6d8d1
Koua Thao <kouathao09@gmail.com>
2018-03-18 02:06:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(element) {
+  kittens.push(element);
+  // windows.kittens.push(element);
+}
+
+function destructivelyPrependKitten(element) {
+  kittens.unshift(element);
+}
+
+function destructivelyRemoveLastKitten(element) {
+  kittens.pop(element);
+}
+
+function destructivelyRemoveFirstKitten(element) {
+  kittens.shift(element);
+}
+function appendKitten(element) {
+  var newArray = [...kittens];
+  // var newArray = kittens;
+  newArray.push(element);
+  return newArray;
+}
+
+function prependKitten(element) {
+  var newArray = [...kittens];
+  newArray.unshift(element);
+  return newArray;
+}
+
+function removeLastKitten(element) {
+  var newArray = [...kittens];
+  newArray.pop(element);
+  return newArray;
+}
+
+function removeFirstKitten(element) {
+  var newArray = [...kittens];
+  newArray.shift(element);
+  return newArray;
+}
