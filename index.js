e1e4ef3226f61a0084c08655dc9680e5755063e4
corytveten <corytveten@gmail.com>
2018-03-19 22:29:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function appendKitten(name) {
+  return [name, ...kittens];
+}
