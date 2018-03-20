bd6e421c031b8939e498abf7a4651479a6bafb72
John Kalinauskas <jkalinauskas@live.com>
2018-03-20 17:41:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+kittens =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop("Ralph")
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift("Bob")
+}
+function appendKitten(name) {
+  return [...kittens, "Broom"]
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens]
+}
+function removeLastKitten(name) {
+  return kittens.slice (0, kittens.length - 1)
+}
+
+function removeFirstKitten(name) {
+ return kittens.slice (1) 
+}
+
+  
+
