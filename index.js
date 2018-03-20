09798b7a66aa368dfb70f2a55ae3271e9e9a4967
Brennan Fulmer <brennan.flmr@gmail.com>
2018-03-20 23:40:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function Arrays() {
+  kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  Arrays();
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  Arrays();
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  Arrays();
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  Arrays();
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  Arrays();
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name) {
+  // Arrays();
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten() {
+  // Arrays();
+  var newArray = kittens.slice(0, kittens.length - 1);;
+  return newArray;
+}
+
+function removeFirstKitten() {
+  // Arrays();
+  var newArray = kittens.slice(1);
+  return newArray;
+}
+
+
+
+
+
+
+
+
