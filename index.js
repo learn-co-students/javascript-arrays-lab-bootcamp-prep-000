ab0a9e3861f41ed0b8bcd50002feb7dc5c5212a4
A MacGregor <annabel.macgregor@gmail.com>
2018-03-21 21:04:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+//1 
+function destructivelyAppendKitten(name) {
+  kittens.push(`${name}`);
+  return kittens;
+}
+
+//2 
+function destructivelyPrependKitten(name) {
+  kittens.unshift(`${name}`);
+  return kittens;
+}
+
+//3 
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+//4 
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+//5
+function appendKitten(name) {
+  var appendKitten = [...kittens, `${name}`];
+  return appendKitten;
+}
+
+//6 
+function prependKitten(name) {
+  var prependKitten = [`${name}`, ...kittens];
+  return prependKitten;
+}
+
+//7 
+function removeLastKitten() {
+  var removeLastKitten = kittens.slice(0, kittens.length - 1);
+  return removeLastKitten;
+}
+
+//8 
+function removeFirstKitten() {
+  var removeFirstKitten = kittens.slice(1);
+  return removeFirstKitten;
+}
