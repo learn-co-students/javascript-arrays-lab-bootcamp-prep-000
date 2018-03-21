686f4726c8f2ca769e77d70be96ebee6e382870e
Christopher <christopherccm@msn.com>
2018-03-21 18:09:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob")
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten() {
+  var newKittens = [...kittens, "Broom"];
+  return newKittens;
+}
+
+function prependKitten() {
+  var newKittens = ["Arnold", ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.shift();
+  return newKittens;
+}
