6c9775c6e0c33fd61b12549a2bccac4d09698db7
Taylor August Giles <augustgiles8@gmail.com>
2018-03-22 00:50:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const app = "I don't do much."
+const app = "I don't do much.";
 
 function kittens() {
- var kittens = ["Milo", "Otis", "Garfield"];
- return kittens
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens;
 }
 
 function destructivelyAppendKitten(name) {
-  name = "Ralph"
-  return kittens.push(name)
+  return kittens.push(name);
 }
 
-  function destructivelyPrependKitten(name) {
-    name = "Bob"
-    return kittens.unshift(name)
-  }
-  
-  function destructivelyRemoveLastKitten() {
-    return kittens.pop()
-  }
-  
-  function destructivelyRemoveFirstKitten() {
-    return kittens.shift()
-  }
-  
-  function appendKitten(name) {
-    name = "Broom";
-    return [...kittens, "Broom"]
-  }
-  
-  function prependKitten(name) {
-    name = "Arnold";
-    return ["Arnold", ...kittens]
-  }
-  
-  function removeLastKitten() {
-    return kittens.slice(0,2)
-  }
-  
-  function removeFirstKitten() {
-    return kittens.slice(1)
-  }
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,2);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(-2);
+}
