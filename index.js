bc0050e1a1fa456773dca8214e352c59fa12283f
Crystalker <crystalsemen@gmail.com>
2018-03-22 03:39:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  let newArray = [...kittens]
+  newArray.pop();
+  return newArray;
+}
+
+function removeFirstKitten() {
+  let newArray = [...kittens]
+  return newArray.slice(1);
+}
