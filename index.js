60a325fa2d5a66daf10107ae2aa747d59430c00c
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-25 21:56:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependKitten(name) {
+   kittens.unshift(name)
+   return kittens;
+ }
+ 
+var kittens = ["Milo", "Otis", "Garfield"]; 
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+} 
+ 
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+} 
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function appendKitten(name) {
+  var kittens_a = [...kittens, name]
+  return kittens_a;
+}
+
+function prependKitten(name) {
+  var kittens_a = [name, ...kittens]
+  return kittens_a;
+}
+function removeLastKitten() {
+  var kittens_a = kittens.slice(0, kittens.length - 1)
+  return kittens_a;
+}
+ function removeFirstKitten() {
+   var kittens_a = kittens.slice(1, kittens.length)
+   return kittens_a;
+ } 
