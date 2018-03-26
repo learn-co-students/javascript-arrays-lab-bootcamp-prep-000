b14e673876a36f5d5b88d3ced4aecc0226518d8f
Michael Blandford <mdblandford@gmail.com>
2018-03-26 19:32:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(Ralph){kittens.push(Ralph);return kittens}
+function destructivelyPrependKitten(Bob){kittens.unshift(Bob);return kittens}
+function destructivelyRemoveLastKitten(Ralph){kittens.pop(Ralph);return kittens}
+function destructivelyRemoveFirstKitten(Bob){kittens.shift(Bob);return kittens}
+function appendKitten(Broom){return[...kittens,Broom]}
+function prependKitten(Arnold){return[Arnold,...kittens]}
+function removeLastKitten(Garfield){return kittens.slice(0,kittens.length-1)}
+function removeFirstKitten(Milo){return kittens.slice(1)}
