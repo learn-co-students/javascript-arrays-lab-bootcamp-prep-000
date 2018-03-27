b4f6f379500d48fbf105a31d384ed7be447b20e1
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-27 16:08:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(myName) {
+  kittens.push(myName)
+}
+
+function destructivelyPrependKitten(myName) {
+  kittens.unshift(myName)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift([1])
+}
+
+function appendKitten(myName) {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.push(myName)
+  return kittens
+}
+
+function prependKitten(myName) {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.unshift(myName)
+  return kittens
+}
+
+function removeLastKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.pop()
+  return kittens
+}
+
+function removeFirstKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.shift([1])
+  return kittens
+}
