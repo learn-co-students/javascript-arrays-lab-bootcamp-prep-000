4ecd51649d677c73d555e159c2520fd904900de6
Jackie First <jackiejeep@gmail.com>
2018-03-29 03:38:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens;
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens;
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+}
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+function removeLastKitten() {
+  var removeLastKitten = ["Milo", "Otis", "Garfield"];
+  removeLastKitten = removeLastKitten.slice(0, removeLastKitten.length -1);
+  return removeLastKitten;
+}
+function removeFirstKitten() {
+  var removeFirstKitten = ["Milo", "Otis", "Garfield"];
+  removeFirstKitten = removeFirstKitten.slice(1);
+  return removeFirstKitten;
+}
