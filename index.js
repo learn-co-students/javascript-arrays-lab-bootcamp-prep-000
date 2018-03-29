afb7152ca5546bdc7e9cfd3d9c144d0523dc2fe4
Nancy Jo Chang <enciie@gmail.com>
2018-03-29 01:03:02 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   kittens.push("Ralph");
-  return kittens;
 }
 function destructivelyPrependKitten(name){
   kittens.unshift("Bob");
