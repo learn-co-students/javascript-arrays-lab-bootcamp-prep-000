9bcce067bfaeb6a624e42dcf598c0c5dddf86cb5
Lorena <lorena.giddens@outlook.com>
2018-03-29 21:34:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+
+function removeLastKitten() {
+  var miloAndOtis = kittens.slice(0, 2);
+  return miloAndOtis;
+}
+
+function removeFirstKitten() {
+  var otisAndGarfield = kittens.slice(1, 3);
+  return otisAndGarfield;
+}
