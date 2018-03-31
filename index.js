08935e4a081ac3d27b3b58952fb213031310b802
Roigrosa <roigrosa88@gmail.com>
2018-03-31 21:57:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+var  kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];
+
+
+function destructivelyAppendKitten(name)
+{
+kittens.push(name)
+return kittens
+}
+
+
+function destructivelyPrependKitten(name)
+{
+
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+
+  kittens.pop()
+
+}
+
+function destructivelyRemoveFirstKitten(){
+
+  kittens.shift()
+
+}
