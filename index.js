6117727f693081667f7d0440e84221b8b3c70aec
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-01 15:31:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newAppend = kittens.slice();
+  newAppend.push(name);
+  return newAppend;
+}
+
+function prependKitten(name) {
+  var newAppend = kittens.slice();
+  newAppend.unshift(name);
+  return newAppend
+}
+
+function removeLastKitten() {
+  var newAppend = kittens.slice();
+  newAppend.pop();
+  return newAppend;
+}
+
+function removeFirstKitten() {
+  var newAppend = kittens.slice()
+  newAppend.shift();
+  return newAppend;
+}
