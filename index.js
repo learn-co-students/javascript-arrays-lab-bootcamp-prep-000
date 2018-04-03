4d866ad74d5e3171b449a3fa5294077e645cb15a
tommejas@gmail.com <tommejas@gmail.com>
2018-04-03 14:14:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+ kittens.push(name); 
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var array = [...kittens];
+  array.push(name);
+  return array;
+}
+
+function prependKitten(name) {
+  var array = [...kittens];
+  array.unshift(name);
+  return array;
+}
+
+function removeLastKitten() {
+  var array = [...kittens];
+  array.pop();
+  return array;
+}
+
+function removeFirstKitten() {
+  var array = [...kittens];
+  array.shift();
+  return array;
+}
+
+
+
