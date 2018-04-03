f76558104d482603d770d8f56c653bcea94b6f05
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-03 04:43:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten (name){
+
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten (name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten (){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten (){
+  return kittens.shift();
+}
+
+function appendKitten (name){
+  var newArray = [...kittens, name];
+  return newArray
+}
+
+function prependKitten (name){
+  var newArray = [name, ...kittens];
+  return newArray
+}
+
+function removeLastKitten (){
+  var newArray = kittens;
+  return newArray.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten (){
+  var newArray = kittens;
+  return newArray.slice(1); 
+
+}
