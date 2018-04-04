08fbbc6358be4f960cd0385ec8766ddb5234c24d
Enecas <hypercolorm@gmail.com>
2018-04-04 05:30:33 +0000

    Done.

--- index.js
+++ index.js
@@ -17,21 +17,29 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
+  //This is not required
   var kittensCute = [...kittens, name];
+  //return [...kittens, name];
   return kittensCute;
 }
 
 function prependKitten(name) {
+  //This is not required
   var kittensCute = [name, ...kittens];
+  //return [name, ...kittens];
   return kittensCute;
 }
 
 function removeLastKitten() {
+  //This is not required
   var kittensCute = kittens.slice(0, kittens.length - 1);
+  //return kittens.slice(0, kittens.length);
   return kittensCute;
 }
 
 function removeFirstKitten() {
+  //This is not required
   var kittensCute = kittens.slice(1);
+  //return kittens.slice(1);
   return kittensCute;
 }
