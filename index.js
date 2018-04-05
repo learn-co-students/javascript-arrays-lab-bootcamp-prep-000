0cb56c56c258895f0d6ed69d04b3214927ff338d
Lamido Tijjani <kinglamido@gmail.com>
2018-04-05 22:54:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much."
+ kittens = ['Milo', 'Otis', 'Garfield']
+
+
+
+
+
+
+
+
+
+ function destructivelyAppendKitten(name)
+ kittens.append(namee)
+ return kittens
+ end
+  
+  function destructivelyPrependKitten(name)
+  kittens.prepend(name)
+  return kittens
+  end
