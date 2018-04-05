5a4e61eacd45c7ee455763891c9061b36d3a330a
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-05 03:59:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  
+  return kittens.push(name);
 }
+destructivelyAppendKitten("Ralph");
 
 function destructivelyPrependKitten(name) {
-  
+  return kittens.unshift(name);
+}
+destructivelyPrependKitten("Bob");
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
 }
 
-function destructivelyRemoveLastKitten(name) {
-  
-}
-
-function destructivelyRemoveFirstKitten(name) {
-  
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
 }
 
 function appendKitten(name) {
-  
+  return [...kittens, name];
 }
 
 function prependKitten(name) {
-  
+  return [name, ...kittens];
 }
 
 function removeLastKitten() {
-  
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  
+  return kittens.slice(1);
 }
 
+//FUN FUN IN THE SUN
