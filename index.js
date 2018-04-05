dc846212afac9f3ea85eb706d9757c99ac362673
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-05 03:13:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten(name) {
+  
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
+
