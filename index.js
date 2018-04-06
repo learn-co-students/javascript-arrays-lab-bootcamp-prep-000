37408735b144807bdf9853db71ff8d339bfbe39d
Lamido Tijjani <kinglamido@gmail.com>
2018-04-06 02:44:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const app = "I don't do much."
  kittens = ['Milo', 'Otis', 'Garfield']
 
-
-
-
-
-
-
-
-
- function destructivelyAppendKitten(name)
- append.kittens(namee)
- return kittens
- end
+function destructivelyAppendKitten(name){
+ kittens.push(name)
+    return kittens
+}
+function destructivelyPrependKitten(name){
+ kittens.unshift(name)
+   return kittens
+}
   
+function destructivelyRemoveLastKitten(){
+ kittens.pop()
+  return kittens
+}
+  
+function destructivelyRemoveFirstKitten(name){
+ kittens.shift()
+  return kittens
+}
  
+function appendKitten(name){
+  return [...kittens, name]
+}
+  
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  var array = kittens
+  array = array.slice(0, array.length - 1)
+  return array
+}
+
+function removeFirstKitten(){
+  var array = kittens
+    array = array.slice(1, array.length)
+  return array
+}
