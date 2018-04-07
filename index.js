77be9c57722901ffdd353453d433784e904b8c2c
Hugo Meza <hugo_meza94@hotmail.com>
2018-04-07 04:09:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return;
+}
+
+function appendKitten(name){
+  var other = [...kittens, name];
+  return other;
+}
+
+function prependKitten(name){
+  var other = [name, ...kittens];
+  return other;
+}
+
+function removeLastKitten(){
+  var other = [...kittens];
+  other.pop();
+  return other;
+}
+
+function removeFirstKitten(){
+  var other = [...kittens];
+  other.shift();
+  return other;
+}
+
+
