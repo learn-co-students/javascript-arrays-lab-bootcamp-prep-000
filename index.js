c0f7ea22d627492af1148c55eee8d4a1e8c9630a
nomonkeysforu@gmail.com <nomonkeysforu@gmail.com>
2018-04-07 16:09:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens =["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten (name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  const arr2=kittens.concat(name)
+  return arr2
+}
+
+function prependKitten(name){
+  const arr2=[name,...kittens]
+  return arr2
+}
+
+function removeLastKitten(){
+  const arr2=kittens.slice(0,kittens.length-1)
+  return arr2
+}
+
+function removeFirstKitten(){
+  const arr2=kittens.slice(1)
+  return arr2
+}
