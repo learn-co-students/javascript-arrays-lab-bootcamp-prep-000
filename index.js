2344856441efa4c9628f2b89a3fc5664a891b799
gracey12051@gmail.com <gracey12051@gmail.com>
2018-04-07 22:11:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+function kittens() {
+  var kittens = ["Milo","Otis","Garfield"]
+  return kittens
+  }
+  function destructivelyAppendKitten(newKitten) {
+    kittens.push(newKitten)
+    return kittens
+  }
+  function destructivelyPrependKitten(newKitten){
+    kittens.unshift(newKitten)
+    return kittens
+  }
+  function destructivelyRemoveLastKitten(){
+    kittens.pop()
+    return kittens
+  }
+  function destructivelyRemoveFirstKitten(){
+    kittens.shift()
+    return kittens
+  }
+  function appendKitten(newKitten){
+    return [...kittens, newKitten]
+  }
+  function prependKitten(newKitten){
+    return [newKitten, ...kittens]
+  }
+  function removeLastKitten(){
+    var kittens2
+    kittens2 = kittens.slice(0,kittens.length-1)
+    return kittens2
+  }
+  function removeFirstKitten(){
+    var kittens2
+    kittens2 = kittens.slice(1)
+    return kittens2
+    
+  }
