6cc988947ab2e0c28a69cf57afdee102505c91f0
Ernesto ballon <ernestoballono@gmail.com>
2018-04-07 18:46:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+}
+function appendKitten(name){
+  return [ ...kittens,name]
+}
+function prependKitten(name){
+  return [ name,...kittens]
+}
+function removeLastKitten(){
+  var c = kittens.slice(0,kittens.length-1)
+  return c
+}
+function removeFirstKitten(){
+  var c = kittens.slice(1)
+  return c
+}
