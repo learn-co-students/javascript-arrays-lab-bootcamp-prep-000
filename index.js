69be31d6ef0f13d6a82d373ab390770aa14ff3f4
Kwaku J <gyazi@live.com>
2018-04-07 18:27:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var neww = [...kittens, name]
+  return neww
+}
+
+function prependKitten(name) {
+  var newarray = [name, ...kittens]
+  return (newarray)
+}
+
+function removeLastKitten() {
+  var last = kittens.slice(0, kittens.length - 1)
+  return last
+}
+
+function removeFirstKitten() {
+  var first = kittens.slice(1)
+  return first
+}
