91c86d24712ca0fbbf618e209ad17a4b1612bb51
Vicki Hamm <vicki.cello@gmail.com>
2018-04-09 22:43:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
+
+function accessElementInArray(kittens, index) {
+  return kittens[index];
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift([0]);
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1);
+}
+
+
+
+
+
+
