a255795a4b35d5e26d479026cf16f8eefd4bcc0c
bladeburris@gmail.com <bladeburris@gmail.com>
2018-04-09 18:03:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+ newArray = [kittens, name]
+ return newArray
+}
