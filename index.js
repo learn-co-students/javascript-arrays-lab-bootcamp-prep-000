95167a0c792f1efc35e97868b36c58a829bfe1e8
Kvasemi <alancho@gmail.com>
2018-04-10 20:05:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(b) {
+  kittens.push(b);
+  return kittens;
+}
+
+function destructivelyPrependKitten(b) {
+  kittens.unshift(b);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(b) {
+  var a = kittens.slice();
+  a.push(b);
+  return a;
+}
+
+function prependKitten(b) {
+  var a = kittens.slice();
+  a.unshift(b);
+  return a;
+}
+
+function removeLastKitten() {
+  var a = kittens.slice();
+  return a.slice(0, a.length - 1);
+}
+
+function removeFirstKitten() {
+  var a = kittens.slice();
+  return a.slice(1, a.length);
+}
