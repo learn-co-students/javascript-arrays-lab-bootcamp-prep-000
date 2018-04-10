c5bbc0930a05a7438b37af60f8a0f54bb6b65b6c
Brindha S <brinsree@gmail.com>
2018-04-10 01:19:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens=["Milo","Otis","Garfield"];
+function destructivelyAppendKitten(name){
+ kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+ kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name){
+ kittens.pop(name);
+}
+function destructivelyRemoveFirstKitten(name){
+kittens.shift(name);
+}
+function appendKitten(name){
+ return [...kittens,name];
+}
+function prependKitten(name){
+  return [name,...kittens];
+}
+function removeLastKitten(){
+ var array1=kittens.slice(0,kittens.length-1);
+ return array1;
+}
+function removeFirstKitten(){
+ var array1= kittens.slice(1);
+ return array1;
+}
