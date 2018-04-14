a7905b888e594a7460af3444b3e458a3ac3b4196
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-14 01:36:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+var kittens= ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  var morekittens= [...kittens, name]
+  return morekittens
+}
+function prependKitten(name) {
+  var morekittens= [name, ...kittens]
+  return morekittens
+}
+function removeFirstKitten() {
+  var morekittens= kittens.slice(1)
+  return morekittens
+}
+function removeLastKitten() {
+  var morekittens= kittens.slice(0, kittens.length-1)
+  return morekittens
+}
