f6b80a89386bc62d6eeebf571e7ebdb47cd608d8
Colin Brence <cbrence@ramapo.edu>
2018-04-14 17:21:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+kittens.push(name)
+return kittens
+}
+
+function destructivelyPrependKitten(name) {
+kittens.unshift(name)
+return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+kittens.shift(name)
+return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+kittens.pop(name)
+return kittens
+}
+
+function appendKitten(name){
+var moreKittens = kittens.slice(name);
+moreKittens.push(name)
+  return moreKittens
+}
+
+function prependKitten(name){
+var moreKittens = kittens.slice(name);
+moreKittens.unshift(name)
+  return moreKittens
+}
+
+function removeFirstKitten(name) {
+var moreKittens = kittens.slice(name);
+moreKittens.shift(name)
+return moreKittens
+}
+
+function removeLastKitten(name) {
+var moreKittens = kittens.slice(name);
+moreKittens.pop(name)
+return moreKittens
+}
