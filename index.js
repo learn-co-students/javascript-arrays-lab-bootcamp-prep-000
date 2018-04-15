36f1aa8eb9c56c4b5ca3f17939465844f58c3931
Shiri Mund <shiribogo@gmail.com>
2018-04-15 17:42:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+var kittens= ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+}
+
+function prependKitten(name) {
+  var newKittens= [name, ...kittens]
+  return newKittens
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
