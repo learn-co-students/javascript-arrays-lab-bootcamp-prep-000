967b657755c7abb12c51b4d8154b8a48f1ee4421
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-16 06:11:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name){
+  var newAppend = [...kittens, name]
+  return newAppend;
+}
+
+function prependKitten(name) {
+  var newPrepend = [name, ...kittens];
+  return newPrepend;
+}
+
+function removeLastKitten(){
+  var removal = kittens.slice(0, kittens.length - 1);
+  return removal;
+}
+
+function removeFirstKitten() {
+  var removeFirst = kittens.slice(1, kittens.length);
+  return removeFirst;
+}
