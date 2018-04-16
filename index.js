73df5872ae085fca37a23fbaa114ebb26c3e170e
Michael Mason <therealmichaelmason@gmail.com>
2018-04-16 21:22:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name){
-  kittens.push(name)
-  return kittens
+  return kittens.push(name)
 }
 
 function destructivelyPrependKitten(name){
-  kittens.unshift(name)
-  return kittens
+  return kittens.unshift(name)
 }
 
 function destructivelyRemoveLastKitten(){
-  kittens.pop()
-  return kittens
+  return kittens.pop()
 }
 
 function destructivelyRemoveFirstKitten(){
-  kittens.shift()
-  return kittens
+  return kittens.shift()
 }
 
 function appendKitten(name){
