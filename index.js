f65c76e5792ce8e2f7ef3b02ff82989ecbef6bdf
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-17 15:41:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+
+function appendKitten(name) {
+  var nKittens = [...kittens, name]
+  return nKittens
+}
+
+function prependKitten(name) {
+  var nKittens = [name,...kittens]
+  return nKittens
+}
+
+function removeLastKitten() {
+  var nKittens = kittens.slice(0,kittens.length -1)
+  return nKittens
+}
+
+function removeFirstKitten() {
+  var nKittens = kittens.slice(1,kittens.length)
+  return nKittens
+}
+
+/*
+["Philadelphia", ...cities]
+*/
