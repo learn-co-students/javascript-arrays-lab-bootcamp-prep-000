262f4f94950f46fc06537cd08be217c0880510ff
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-04-18 13:58:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var b = [...kittens, name]
+  return b
+}
+
+function prependKitten(name) {
+  var b = [name, ...kittens]
+  return b
+}
+
+function removeLastKitten() {
+  var b = kittens.slice(0,-1)
+  return b
+}
+
+function removeFirstKitten() {
+  var b = kittens.slice(1)
+  return b
+}
