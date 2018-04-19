4b0f4af68663932cb4778abff3d96237c19282a7
Dhiral Patel <dhiralpatel794@gmail.com>
2018-04-19 19:51:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newkitten = kittens.concat(name);
+  return newkitten;
+}
+
+function prependKitten(name) {
+  var unkitten = [name,...kittens];
+  return unkitten;
+}
+
+function removeLastKitten(name) {
+  var remkitten = kittens.slice(0,kittens.length - 1);
+  return remkitten;
+}
+
+function removeFirstKitten(name) {
+  var firkitten = kittens.slice(1);
+  return firkitten;
+}
