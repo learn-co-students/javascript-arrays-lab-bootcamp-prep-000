db74e211e5a0ed871c6a2bfda3ee6e4a9d40ff2a
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-19 13:17:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+  function destructivelyAppendKitten(name){
+    kittens.push(name);
+  }
+
+  function destructivelyPrependKitten(name){
+    kittens.unshift(name);
+  }
+
+  function destructivelyRemoveLastKitten() {
+    kittens.pop();
+  }
+
+  function destructivelyRemoveFirstKitten () {
+    kittens.shift();
+  }
+
+  function appendKitten(name) {
+    let newKittens = [...kittens];
+    newKittens.push(name);
+    return newKittens;
+  }
+  
+  function prependKitten(name){
+    let newKittens = [...kittens];
+    newKittens.unshift(name);
+    return newKittens;
+  }
+  
+  function removeLastKitten() {
+    let newKittens = [...kittens];
+    newKittens.pop();
+    return newKittens;
+  }
+
+  function removeFirstKitten() {
+    let newKittens = [...kittens];
+    newKittens.shift(name);
+    return newKittens;
+  }
