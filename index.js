c62e0287f8a14e6a08f02aff5694aff5f3ac52a1
malorie16 <malorie18@gmail.com>
2018-04-21 03:32:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  return [...kittens, 'Broom']
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function prependKitten(name) {
+  return ['Arnold', ...kittens]
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeLastKitten() {
+    return kittens.slice(0, kittens.length - 1)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function removeFirstKitten() {
+  return kittens.slice(-2)
+}
