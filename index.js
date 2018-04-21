126e52f0f16766e4bcfa3d7d15754a469eab60a7
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-21 15:44:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+}
+function appendKitten(name)
+{
+  var arr = [...kittens,name];
+  return arr;
+}
+function appendKitten(name)
+{
+  var arr = [...kittens,name];
+  return arr;
+}
+function prependKitten(name)
+{
+  var arr = [name,...kittens];
+  return arr;
+} 
+function removeLastKitten()
+{
+  var arr = kittens.slice(0,kittens.length-1);
+  return arr;
+} 
+function removeFirstKitten()
+{
+  var arr = kittens.slice(1,kittens.length);
+  return arr;
+} 
+
+ 
