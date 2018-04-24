e0a5562e0b863b3c0fe1114a3cb3929d9eb174a5
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-24 18:29:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+var name = 'Ralph'
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  [...kittens,"Broom"]
+  var kittens = [...kittens,"Broom"]
+  return kittens
+}
