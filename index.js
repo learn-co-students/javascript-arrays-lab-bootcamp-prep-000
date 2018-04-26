6beb4f288322979ab30add3495c39ef470f7967b
Jebediah Elliott <jebediahelliott@gmail.com>
2018-04-26 23:57:41 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ function destructivelyRemoveFirstKitten()
 }
 function appendKitten(name)
 {
-  var newArray = kittens.push(name)
+  var newArray = [...kittens, name]
+  return newArray
+}
+function prependKitten(name)
+{
+  var newArray = [name, ...kittens]
+  return newArray
+}
+function removeLastKitten()
+{
+  var newArray = kittens.slice(0, kittens.length - 1)
+  return newArray
+}
+function removeFirstKitten()
+{
+  var newArray = kittens.slice(1)
   return newArray
 }
