f6690e5984eba55d5e59bb374d8758baf5ca7c3e
Liz Gunter <midwestunicorn@gmail.com>
2018-04-27 19:49:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+};
+
+function appendKitten(name) {
+  var appendKitten = [...kittens, name];
+  return appendKitten;
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+};
+
+function prependKitten(name) {
+  var prependKitten = [name, ...kittens];
+  return prependKitten;
+};
+
+function removeLastKitten() {
+  removeLastKitten = kittens.slice(0, kittens.length - 1);
+  return removeLastKitten;
+};
+
+function removeFirstKitten() {
+  removeFirstKitten = kittens.slice(1);
+  return removeFirstKitten;
+};
