1e3e3ca306375d5f77aad236f693b1dc8a336b20
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-28 09:14:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kittensResult = [...kittens, name];
+  return kittensResult;
+}
+
+function prependKitten(name) {
+  var kittensResult = [name, ...kittens];
+  return kittensResult;
+}
+
+function removeLastKitten() {
+  var kittensResult = kittens.slice(0, kittens.length-1);
+  return kittensResult;
+}
+
+function removeFirstKitten() {
+  var kittensResult = kittens.slice(1);
+  return kittensResult;
+}
