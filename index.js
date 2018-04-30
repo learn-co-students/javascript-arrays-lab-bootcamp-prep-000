5bf44f90212f33e58d796a921e32558e02971a06
Sean Huang <hyprul@ucla.edu>
2018-04-30 01:13:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  var newarray = [...kittens, name]
+  return newarray
+}
+
+function prependKitten(name){
+  var newarray = [name, ...kittens]
+  return newarray
+}
+
+function removeLastKitten(){
+  var newarray = kittens.slice(0, kittens.length-1)
+  return newarray
+}
+
+function removeFirstKitten(){
+  var newarray = kittens.slice(1)
+  return newarray
+}
