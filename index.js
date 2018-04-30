f47d8d6dbc40bcc53524ccd9ffb0aaf17d91206f
Kenny Siu <siu.afmm@gmail.com>
2018-04-30 17:04:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArr = [];
+  newArr.push(...kittens, name);
+  return newArr;
+}
+function prependKitten(name){
+  var newArr = [];
+  newArr.unshift(name, ...kittens);
+  return newArr;
+}
+function removeLastKitten(){
+  var newArr = [];
+  newArr.push(...kittens); newArr.pop();
+  return newArr;
+}
+function removeFirstKitten(){
+  var newArr = [];
+  newArr.push(...kittens); newArr.shift();
+  return newArr;
+}
