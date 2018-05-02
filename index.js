85dcab7f3061a23522a8371b94b698c264739f14
Joseph Milliken <joseph97milliken@gmail.com>
2018-05-02 01:03:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+function copyAndFunc(array, action, argument){
+  const copy = array.slice();
+  copy[action](argument);
+  return copy;
+}
+
+var removeFirstKitten = () => copyAndFunc(kittens, 'shift');
+var removeLastKitten = () => copyAndFunc(kittens, 'pop');
+var prependKitten = name => copyAndFunc(kittens, 'unshift', name);
+var appendKitten = name => copyAndFunc(kittens, 'push', name);
+
+
+function destructiveFunc(array, action, argument){
+  array[action](argument);
+  return array;
+}
+
+var destructivelyRemoveFirstKitten = () => destructiveFunc(kittens, 'shift');
+var destructivelyRemoveLastKitten = () => destructiveFunc(kittens, 'pop');
+var destructivelyPrependKitten = name => destructiveFunc(kittens, 'unshift', name);
+var destructivelyAppendKitten = name => destructiveFunc(kittens, 'push', name);
