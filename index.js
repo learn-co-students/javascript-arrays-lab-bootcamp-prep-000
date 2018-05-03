67c09ebbbff9edd447ebac487ddc336bbb92dab9
Jee Yoon Lee <jlee66@wellesley.edu>
2018-05-03 23:40:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens = kittens.slice(1)
+  return kittens
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
