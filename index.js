6fd1a2bab6c921b0ada94b42e329c084a191737d
jmoypace <jmoypace@gmail.com>
2018-05-03 02:28:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+var newArray = kittens.slice();
+  newArray.push(name)
+  return newArray
+}
+function prependKitten(name){
+  var newArray = kittens.slice();
+  newArray.unshift(name)
+  return newArray
+}
+function removeLastKitten(){
+  var newArray = kittens.slice();
+  newArray.pop(name)
+  return newArray
+}
+function removeFirstKitten(){
+  var newArray = kittens.slice();
+  newArray.shift(name)
+  return newArray
+}
