ead6f2ac06c4ded652b85436d9cbbb6844c1e398
emy garcia <emygarcia1920@gmail.com>
2018-05-08 21:10:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const app = "I don't do much."
+var kittens = ['Milo','Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph')
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob')
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+[kittens, ... 'Broom']
+return kittens
+ 
+}
