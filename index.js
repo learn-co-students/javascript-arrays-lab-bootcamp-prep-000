4cbe4deb999e68978767f38691fefa01161585b9
shandamckinney <mckinney.shanda@gmail.com>
2018-05-08 18:45:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+window.kittens = ['Milo', 'Otis', 'Garfield']
+
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
