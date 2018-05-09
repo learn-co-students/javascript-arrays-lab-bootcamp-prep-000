f0e91135a541772a29f3c183c18b410944c8f764
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-09 17:37:01 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(n) {
   return kittens.push(n);
 }
+
+function destructivelyPrependKitten(n) {
+  return kittens.unshift(n);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(n) {
+  return [...kittens, n];
+}
+
+function prependKitten(n) {
+  return [n, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
