bcd3d52876e53ed3cd7ee6ccc2a94717641cc186
Christopher Beltran <christod.beltran@gmail.com>
2018-05-10 02:14:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+var kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'
+  ];
+  
+  
+function destructivelyAppendKitten(name) {
+  var newKittens = kittens.push(name);
+  return newKittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+}
+function appendKitten(name) {
+  
+}
+
+function appendKitten(name) {
+ var otherKittens = [...kittens, name];
+  return otherKittens;
+}
+
+function prependKitten(name) {
+  var moreKittens = [name,...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten() {
+  var soManyCats = kittens.slice(0, kittens.length-1);
+  return soManyCats;
+}
+
+function removeFirstKitten() {
+  var noMoreCats = kittens.slice(1);
+  return noMoreCats;
+}
