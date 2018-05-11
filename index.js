d2f3c6c0441b705b6c5ce40d4b53a5eee0addc50
Juliana Kang <julianajlk@gmail.com>
2018-05-11 00:14:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten];
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens];
+}
+
+function removeLastKitten(kitten) {
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten(kitten) {
+  return kittens.slice(1);
+}
