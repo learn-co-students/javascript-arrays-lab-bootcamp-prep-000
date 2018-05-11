132774adbdbcfb788aafd0b9904c55244748de80
Alex Zhang <alexgzhang92@gmail.com>
2018-05-11 00:02:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newKit = [...kittens, name];
+  return newKit;
+}
+
+function prependKitten(name) {
+  var newKit = [name, ...kittens];
+  return newKit;
+}
+
+function removeLastKitten() {
+  var newKit = kittens.slice(0, -1);
+  return newKit;
+}
+
+function removeFirstKitten() {
+  var newKit = kittens.slice(1);
+  return newKit;
+}
