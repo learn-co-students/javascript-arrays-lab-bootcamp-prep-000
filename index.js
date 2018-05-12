0074356ad8edd40125abb42a1b3e0731fedfb446
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-12 03:22:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name, element){
+	name.push(element);
+	return name
+}
+function destructivelyPrependKitten(name, element){
+  name.unshift(element);
+}
