50a5bf38a20454d495150fe55263a84ff2924f1a
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-12 06:05:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name, element){
-	name.push(element);
-	return name
+function destructivelyAppendKitten(name){
+	kittens.push(name);
+	return kittens
 }
-function destructivelyPrependKitten(name, element){
-  name.unshift(element);
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray
+}
+
+function removeLastKitten(){
+  kittens = [kittens.length - 1]
+  return kittens
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
 }
