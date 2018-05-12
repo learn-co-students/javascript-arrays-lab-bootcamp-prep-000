4c10e6b9641ba6f5023bca6454ea5f0a85dff74d
colin.jeske@gmail.com <colin.jeske@gmail.com>
2018-05-12 04:00:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
