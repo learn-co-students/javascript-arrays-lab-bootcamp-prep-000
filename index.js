e38d68e04ddcf1a74755ed2686c95eafe3c1f822
Cameron Orosco <cameron.orosco@outlook.com>
2018-05-13 08:41:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name, array) {
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name, array) {
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(-2)
+}
