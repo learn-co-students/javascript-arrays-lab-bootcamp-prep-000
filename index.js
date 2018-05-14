a987503c83d5844f311ac4847c66dfd03a51dda2
Bryant Torres <brt9029@gmail.com>
2018-05-14 22:51:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+function appendKitten(name){
+  return [...kittens, name]
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+function prependKitten(name){
+  return [name, ...kittens]
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
