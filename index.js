4ca6636625cf1704ab24eef70f0598be0ce94985
Sara Ledger <sara.ledger@live.com>
2018-05-17 01:21:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function kittens() {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function destructivelyAppendKitten(Ralph) {
+  kittens.push(Ralph)
+}
+
+function destructivelyPrependKitten(Bob) {
+  window.kittens.unshift(Bob)
+}
+
+function destructivelyRemoveLastKitten() {
+  window.kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  window.kittens.shift()
+}
+
+function appendKitten(Broom) {
+  return appendKitten = [...window.kittens, Broom]
+}
+
+function prependKitten(Arnold) {
+  return prependKitten = [Arnold, ...window.kittens]
+}
+
+//
+function removeLastKitten() {
+  return removeLastKitten = window.kittens.slice(0, window.kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return removeFirstKitten = window.kittens.slice(1)
+}
