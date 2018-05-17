983d56ab051a243a75780699b845cf9badc51bc6
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-17 15:26:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  window.kittens.push(name);
+  return window.kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  window.kittens.unshift(name);
+  return window.kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  window.kittens.pop();
+  return window.kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  window.kittens.shift();
+  return window.kittens;
+}
+
+function appendKitten(name) {
+  return [...window.kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...window.kittens];
+}
+
+function removeLastKitten() {
+  const new_array = window.kittens.slice(0, window.kittens.length - 1);
+  return new_array;
+}
+
+function removeFirstKitten() {
+  const new_array = window.kittens.slice(1);
+  return new_array;
+}
