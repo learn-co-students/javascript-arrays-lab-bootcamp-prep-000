ccc21b386fad1d6e3deda37ba32e0fa1cc4ef443
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-17 02:43:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten()
+{
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten() 
+{
+  return kittens.shift()
+}
+function appendKitten(name) 
+{
+  const kittens2 = [...kittens,name]
+  return kittens2
+}
+function prependKitten(name) {
+  const kittens2 = [name,...kittens]
+  return kittens2
+}
+function removeLastKitten() {
+  const kittensLast = kittens.slice(0,kittens.length -1)
+  return kittensLast
+}
+function removeFirstKitten() {
+  const kittensFirst = kittens.slice(1)
+  return kittensFirst
+}
