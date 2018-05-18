5369e6c530609df3d92ea9a773385055c91c9a79
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-18 04:14:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ var name = 'Ralph'
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
-  return kittens
+  
 }
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
-  return kittens
+
 }
 function destructivelyRemoveLastKitten() {
   kittens.pop()
-  return kittens
+  
 }
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
-  return kittens
+  
 }
 function appendKitten(name) {
  return [...kittens,name]
