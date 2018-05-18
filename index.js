f32bbb399de992865fa2f8ac67266bde76bc92bc
Taylor Tellman <taylortellman@gmail.com>
2018-05-18 21:14:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const app = "I don't do much."
 function destructivelyAppendKitten(name){
-kittens.push('Ralph')
-}
+  kittens.push(name);
+  return kittens}
 function destructivelyPrependKitten(name){
-  kittens.unshift('Bob')
-}
-function destructivelyRemoveLastKitten(name){
-  kittens.pop()
-}
-function destructivelyRemoveFirstKitten(name){
-  kittens.shift()
-}
-function appendKitten(name) {
- return  [...kittens, 'Broom']
- }
+  kittens.unshift(name);}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();}
+function appendKitten(name){
+     var newkittens = [...kittens, name];
+     return newkittens}
 function prependKitten(name){
-  return ['Arnold', ...kittens]
-}
-function removeLastKitten(name){
-  return kittens.slice(0, kittens.length-1)
-}
-function removeFirstKitten(name){
-  return kittens.slice(1)
-}
+       var newkittens = [name, ...kittens];
+       return newkittens}
+function removeLastKitten(){
+  var newkittens = kittens.slice(0,2);
+  return newkittens}
+  function removeFirstKitten(){
+    var newKittens = kittens.slice(1)
+    return newKittens
+  }
