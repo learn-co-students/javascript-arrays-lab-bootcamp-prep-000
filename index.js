71d512f83e0259668565cb7a43ed2567e75ee9bd
Evangel Teo <evangelteo@gmail.com>
2018-05-18 18:47:38 +0000

    Done.

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return [kittens,name]
+  return [...kittens,name]
 }
 
 function prependKitten(name) {
-  return [name,kittens]
+  return [name,...kittens]
 }
 
 function removeLastKitten() {
