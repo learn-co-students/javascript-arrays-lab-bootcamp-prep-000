efa3d52d8e5137e5ad15aea5880e9520f881aba7
AyanaZaire <ayanazaire@gmail.com>
2018-05-19 00:19:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+//destructive!!! Oh No!
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
+  return kittens
+}
+
+// NOT Destructive! :)
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
