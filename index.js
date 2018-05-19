9e22e6c07f27f3153fabf4d8dc6fd8daa56d9892
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-19 18:04:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var nKittens = kittens.slice();
+  nKittens.push(name);
+  return nKittens;
+}
+
+function prependKitten(name){
+  var nKittens = kittens.slice();
+  nKittens.unshift(name);
+  return nKittens;
+}
+
+function removeLastKitten(){
+  var nKittens = kittens.slice();
+  nKittens.pop();
+  return nKittens;
+}
+
+function removeFirstKitten(){
+  var nKittens = kittens.slice();
+  nKittens.shift();
+  return nKittens;
+}
+
+/*
+console.log(kittens);
+console.log(appendKitten("something"));
+console.log(kittens);
+*/
