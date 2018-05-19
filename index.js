ca6bd538c15ba694a34acd465a7c2fc6c7931641
Peter van Dijk <peter@petervandijk.net>
2018-05-19 17:07:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newKittens = kittens;
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten(name) {
+  var newKittens = kittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten(name) {
+  var newKittens = kittens.shift();
+  return newKittens;
+}
