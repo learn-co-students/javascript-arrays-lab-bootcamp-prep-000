610b2ea7f70d83a04007d391895b0e27d04b5a94
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-05-20 00:28:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+ function appendKitten(name){
+   return [...kittens,name] 
+   
+ }
+   
+ function destructivelyRemoveFirstKitten(){
+kittens.shift();
+return kittens
+ } 
+ 
+ function prependKitten (name){
+   return [name,...kittens]
+   
+ }
+
+function removeLastKitten(){
+  var newKittens = [...kittens]
+ newKittens.pop()
+  return newKittens
+ }
+ 
+ function removeFirstKitten(){
+   var newk=[...kittens]
+   newk.shift()
+   return newk;
+ }
