1012c6fea31e5c80b0c7cb05cc942bbc2923311b
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-21 17:11:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, name];
+  return kittens2;
+}
+
+function prependKitten(name) {
+  var kittens2 = [name, ...kittens];
+  return kittens2
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
