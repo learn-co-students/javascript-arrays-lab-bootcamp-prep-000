fda64a645930e0566d835f6fa7434aec28a59818
stefan.dorianmihai@yahoo.com <stefan.dorianmihai@yahoo.com>
2018-05-22 10:53:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten (name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten (name){
+  kittens.pop(name);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens;
+}
+function appendKitten (name){
+  var newKitArr = [... kittens];
+  newKitArr.push(name);
+  return newKitArr;
+}
+function prependKitten(name){
+  var newKitArr = [... kittens];
+  newKitArr.unshift(name);
+  return newKitArr;
+}
+function removeLastKitten(name){
+  var newKitArr = [... kittens];
+  newKitArr.pop(name);
+  return newKitArr;
+}
+function removeFirstKitten(name){
+  var newKitArr = [... kittens];
+  newKitArr.shift(name);
+  return newKitArr;
+}
