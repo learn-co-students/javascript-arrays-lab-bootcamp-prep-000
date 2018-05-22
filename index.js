aef1b6c463105db0f5ed08122b8488c558ba09ca
jinyuli91@gmail.com <jinyuli91@gmail.com>
2018-05-22 19:32:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  let newArray = [...kittens, name]
+  return newArray
+}
+
+function prependKitten(name){
+  let newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten(){
+  let newArray = kittens.slice(0, kittens.length - 1)
+  return newArray
+}
+
+function removeFirstKitten(){
+  let newArray = kittens.slice(1)
+  return newArray
+}
