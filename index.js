20749fb160f4ff76f1de6d7fb3ba04576fb94a42
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 19:09:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+var destructivelyAppendKitten = function(name) {
+  kittens.push(name)
+}
+
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name)
+}
+
+var destructivelyRemoveLastKitten = function() {
+  kittens.pop()
+}
