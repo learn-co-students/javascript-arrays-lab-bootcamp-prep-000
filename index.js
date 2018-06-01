a479d3a4e659d246c8167726dad0d4dc6b5ccb02
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-01 18:43:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(Ralph) {
+  return kittens.push(Ralph);
+}
+
+function destructivelyPrependKitten(Bob) {
+  return kittens.unshift(Bob);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop(); 
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(Broom) {
+  return [...kittens, Broom];
+}
+
+function prependKitten(Arnold) {
+  return [Arnold, ...kittens];
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length-1);
+}
