aabcf4bc14412379ccd47f96114a1c3e9fd7a409
Lassana Magassa <mystuff@lassanamagassa.com>
2018-06-03 08:38:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function  destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function   destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var appendedKitten = [...kittens, name]
+  return appendedKitten
+}
+
+function prependKitten(name) {
+  var prependedKitten = [name, ...kittens]
+  return prependedKitten
+}
+
+
+function removeLastKitten(name) {
+  var name = kittens
+  var removedLastKitten = name.slice(0, name.length - 1)
+  return removedLastKitten
+}
+
+function removeFirstKitten(name) {
+  var name = kittens
+  var removedFirstKitten = name.slice(1)
+  return removedFirstKitten
+}
+
+
+
