4c77c19dfdfc38b31c6f1376755e7705c78f7fd9
Cesarina Paula <cesarinapaula@gmail.com>
2018-06-04 23:04:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  var moreKittens = kittens.push(name);
+  return moreKittens;
+}
+
+function destructivelyPrependKitten(name){
+  var moreKittensInFront = kittens.unshift(name);
+  return moreKittensInFront;
+}
+
+function destructivelyRemoveLastKitten(){
+  var adoptedKitten = kittens.pop();
+  return adoptedKitten;
+}
+
+function destructivelyRemoveFirstKitten(){
+  var adoptedKittenFirst = kittens.shift();
+  return adoptedKittenFirst;
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten(name){
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+function removeFirstKitten(name){
+  var newArray = kittens.slice(1);
+  return newArray;
+}
