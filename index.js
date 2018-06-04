c93d6e336a95d3a6ed305f94cbbb797cdea7b2d5
Shafi Khalisdar <shafi.khalisdar@gmail.com>
2018-06-04 01:49:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens =[
+  "milo",
+  "Otis",
+  "Garfield"
+];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+};
+
+function destructivelyAppendKitten(name) {
+  kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+};
+
+function appendKitten(name) {
+  var newArray = [
+    kittens[0],
+    kittens[1];
+    kittens[2];
+    name
+  ];
+  return newArray;
+};
+
+function prependKitten(name) {
+  var newArray = [
+    name,
+    kittens[0],
+    kittens[1];
+    kittens[2];
+  ];
+  return newArray;
+};
