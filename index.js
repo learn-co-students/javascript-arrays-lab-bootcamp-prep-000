869e4ed120156c92d05677b05cbb8c14556e048b
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-06-04 20:03:16 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,37 @@ var kittens = [
   'Garfeld',
   ]
   
+function destructivelyAppendKitten(){
+  kittens.push('Ralph');
+  
+}
 
+  function destructivelyPrependKitten(){
+    kittens.unshift('Bob');
+  }
+  
+  function destructivelyRemoveLastKitten(){
+    kittens.pop('Garfield');
+  }
+  
+  function destructivelyRemoveFirstKitten(){
+    kittens.shift('Milo');
+  }
+  
+  function appendKitten(){
+    return ['Milo', 'Otis', 'Garfield', 'Broom'];
+  }
+  
+  function prependKitten(){
+    return ['Arnold', 'Milo', 'Otis', 'Garfield'];
+  }
+  
+  function removeLastKitten(){
+    return ['Milo', 'Otis'];
+  }
+  
+  function removeFirstKitten(){
+    return ['Otis', 'Garfield'];
+  }
+  
   
