081f4c710e79e68d0b0bf192b0afb7fc4e1d9fdd
michael <michaeltilman77@gmail.com>
2018-06-06 00:59:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  window.kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  window.kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  window.kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  window.kittens.shift()
+}
+function prependKitten(name){
+  return [name, ...window.kittens]
+}
+function appendKitten(name){
+  return [...window.kittens, name]
+}
+function removeLastKitten(){
+  return window.kittens.slice(0,window.kittens.length-1)
+}
+function removeFirstKitten(){
+  return window.kittens.slice(1)
+}
