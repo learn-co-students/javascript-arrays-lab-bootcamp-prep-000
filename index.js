98ce3a52e87d81cad0b755a6ed99269b27fd8a23
kevmsutton@hotmail.co.uk <kevmsutton@hotmail.co.uk>
2018-06-07 16:50:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten (Ralph){
+return (kittens.push(Ralph));
+}
+
+function destructivelyPrependKitten(Bob){
+return (kittens.unshift(Bob));
+}
+
+function destructivelyRemoveLastKitten(){
+return (kittens.pop());
+}
+
+function destructivelyRemoveFirstKitten(){
+return (kittens.shift());
+}
+
+function appendKitten(broom){
+return [...kittens, broom];
+}
+
+function prependKitten(max){
+return [max, ...kittens];
+}
+
+function removeFirstKitten(){
+return kittens.slice(1)
+}
+
+function removeLastKitten(){
+return kittens.slice(0, kittens.length-1)
+}
