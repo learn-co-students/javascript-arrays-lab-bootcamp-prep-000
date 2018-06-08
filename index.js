265666b78759ce790641503a2829e0c4e01545b2
firstboen@gmail.com <firstboen@gmail.com>
2018-06-08 14:51:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(name) {
+  var myKitt = kittens.shift(name)
+  return myKitt
+  
+}
+
+function destructivelyPrependKitten(name) {
+  var myKitt = kittens.unshift(name)
+  return myKitt
+}
