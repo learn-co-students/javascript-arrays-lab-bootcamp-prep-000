ff8cbe4de9ad1cdf02894237c2cc78621d98aa49
Joshua Suskin <jsuskin85@gmail.com>
2018-06-08 08:21:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 const app = "I don't do much."
+
+var kittens = [`Milo`,`Otis`,`Garfield`];
+
+function kittens(kittens) {
+  return kittens;
+}
+
+function destructivelyAppendKitten() {
+  kittens.push(`Ralph`);
+  return kittens;
+}
+
+kittens();
+
+function destructivelyPrependKitten() {
+  kittens.unshift(`Bob`);
+  return kittens;
+}
+
+kittens();
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+kittens();
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+kittens();
+
+function appendKitten() {
+  var newKittens = [...kittens, `Broom`];
+  return newKittens;
+}
+
+kittens();
+
+function prependKitten() {
+  var newKittens = [`Arnold`, ...kittens];
+  return newKittens;
+}
+
+kittens();
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length - 1);
+  return newKittens;
+}
+
+kittens();
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
+
+kittens();
