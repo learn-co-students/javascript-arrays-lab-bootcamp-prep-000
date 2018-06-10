16e319c3f2ba6db4b2d7964d50210433074f1dfc
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-10 23:57:36 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift()
   return kittens
 }
+function appendKitten(name) {
+  var temp = [...kittens, name]
+  return temp
+}
+function prependKitten(name) {
+  var temp = [name,...kittens]
+  return temp
+}
+function removeLastKitten() {
+  var temp = kittens.slice(0, 2)
+  return temp
+}
+function removeFirstKitten() {
+  var temp = kittens.slice(1)
+  return temp
+}
