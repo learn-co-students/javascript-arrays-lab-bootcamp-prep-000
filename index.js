c5f527cc259f8e11c30dadd622cc47d2773e5fbf
Andres Manco <andresmanco@gmail.com>
2018-06-10 07:34:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const app = "I don't do much."
+function kittens(){
+var kittens = ["Milo", "Otis", "Garfield"];
+return kittens;
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var arr = [...kittens, name];
+  return arr;
+}
+
+function prependKitten(name){
+  var arr = [name, ...kittens];
+  return arr;
+}
+
+function removeLastKitten(){
+  var arr = kittens;
+  arr.pop();
+  return arr;
+}
+
+function removeFirstKitten(){
+  var arr = kittens;
+  arr.shift();
+  return arr;
+}
