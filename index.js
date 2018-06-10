aadc895f2cc85e6cf30500ae42b0ac1bc4379996
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-10 23:19:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop() 
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
