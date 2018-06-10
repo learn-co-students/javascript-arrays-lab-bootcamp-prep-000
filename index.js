7a874a54a51c1694755a16c6423a7d91946e6d2e
Daniel Johnson <danielfrankjohnson@gmail.com>
2018-06-10 23:17:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function kittens () {
+  return kittens;
+}
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+function appendKitten(name) {
+  var newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
+}
+
+
