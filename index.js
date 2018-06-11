e1ff711c2dcd8176cdc8aec16ce0920974817585
Joshua Wessels <joshuawessels04@gmail.com>
2018-06-11 15:58:53 +0000

    Done.

--- index.js
+++ index.js
@@ -18,5 +18,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return kittens.push(name)
+ return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, 3)
 }
