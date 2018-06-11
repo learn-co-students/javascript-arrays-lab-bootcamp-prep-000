52dfb82f4950e75fd7b57365a6186cc81afc6a36
parisa pahlevan <paris.pahlevan@gmail.com>
2018-06-11 21:33:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name){
+  return kittens.push('Ralph')
+ }
+ var result = destructivelyAppendKitten(kittens)
+ console.log(result);
+ 
+ function destructivelyPrependKitten(name){
+   return kittens.unshift('Bob')
+ }
+ var result = destructivelyPrependKitten(kittens)
+ console.log(result);
+ 
+ function destructivelyRemoveLastKitten(name){
+  return kittens.pop('Garfield')
+ }
+ var result = destructivelyRemoveLastKitten(kittens)
+ console.log(result);
+ 
+ function destructivelyRemoveFirstKitten(name){
+  return kittens.shift('Milo')
+ }
+ var result = destructivelyRemoveFirstKitten(kittens)
+ console.log(result);
+ 
+ function appendKitten(name){
+   return [...kittens, 'Broom' ]
+  }
+  var result = appendKitten(kittens)
+  console.log(result);
+  
+  function prependKitten(name){
+   return ['Arnold', ...kittens]
+  }
+  var result = prependKitten(kittens)
+  console.log(result);
+
+function removeLastKitten(name){
+ return kittens.slice(0, kittens.length - 1 )
+}
+var result = removeLastKitten(kittens)
+console.log(result);  
+  
+function removeFirstKitten(name){
+ return kittens.slice(1) 
+}
+var result = removeFirstKitten(kittens)
+console.log(result);  
+  
+  
+  
+  
