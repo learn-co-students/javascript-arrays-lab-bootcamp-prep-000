34507cf4749546fb544698bdeded6790d77c4cc0
Ena Anicama <ena.v825@gmail.com>
2018-06-12 00:06:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name) {
+  
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+  kittens.shift();
+}
+
+function appendKitten(name) {
+
+  var arr = kittens.concat(name);
+  
+  return arr;
+}
+
+function prependKitten(name) {
+  
+  var arr = [name, ...kittens];
+  
+  return arr;
+}
+
+function removeLastKitten() {
+  
+  var arr = kittens.slice(0, -1);
+  
+  return arr;
+}
+
+function removeFirstKitten() {
+  
+  var arr = kittens.slice(1);
+  
+  return arr;
+}
+
+
+
+
