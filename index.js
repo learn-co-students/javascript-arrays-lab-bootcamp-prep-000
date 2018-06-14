51ea31626c88464377987d2aceeb4c7cf4d43ae9
Otabek Kamalov <otakam86@gmail.com>
2018-06-14 08:26:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+kittens = ['Milo', 'Otis', 'Garfield']
+destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+destructivelyRemoveLastKitten(){
+   kittens.pop();
+   return kittens;
+}
+destructivelyRemoveFirstKitten(){
+   kittens.shift();
+   return kittens;
+}
+appendKitten(name){
+  return [...kittens, "name"]
+}
+prependKitten(name){
+  return ["name",...kittens]
+}
+removeLastKitten(){
+return kittens.slice(-1)  
+}
+removeFirstKitten(){
+  return kittens.slice(1)
+}
+
+
+
+
+
+
