d8fb88996ec77a9a699e50c3c0566f20b03b4d84
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-14 09:02:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const app = "I don't do much."
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function kittens() {
+  var kittens = ['Milo', 'Otis', 'Garfield'];
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name) {
+  var kittensappended = [...kittens,name]
+  return kittensappended
+}
+
+function prependKitten(name) {
+  var kittensprepended = [name, ...kittens]
+  return kittensprepended
+}
+
+function removeLastKitten() {
+  var lastKitten = kittens.slice(0, kittens.length - 1);
+  return lastKitten
+}
+
+function removeFirstKitten() {
+  var firstKitten = kittens.slice(1);
+  return firstKitten
+}
