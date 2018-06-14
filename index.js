4c20fcd6be045d2adf8a8d3d42cd21594e6dbff5
Caitlin Lauer <caitlinrinn@gmail.com>
2018-06-14 17:32:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens.unshift("Bob");
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens.pop("Garfield");
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens.shift("Milo");
+  return kittens;
+}
+
+function appendKitten(kittens) {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens = [...kittens, "Broom"];
+  return kittens
+}
+
+function prependKitten(kittens) {
+  kittens = ["Milo", "Otis", "Garfield"];
+  kittens = ["Arnold", ...kittens];
+  return kittens
+}
+
+function removeLastKitten() {
+  kittens = ["Milo", "Otis", "Garfield"];
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten(kittens) {
+  kittens = ["Milo", "Otis", "Garfield"];
+  return kittens.slice(1)
+}
