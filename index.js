5b292ab445daa00f5d9656424d245bbd497906c6
Benjamin Jones <bljones36@gmail.com>
2018-06-16 16:32:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(array) {
+  kittens.push(array)
+}
+
+function destructivelyPrependKitten(array) {
+  kittens.unshift(array)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(array) {
+  var  output = [...kittens,array]
+  return output
+}
+
+function prependKitten(array) {
+  var  output = [array,...kittens]
+  return output
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,2)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
