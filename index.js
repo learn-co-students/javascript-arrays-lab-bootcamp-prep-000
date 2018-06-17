073b4bbcd156fbc6a497b0dc51eb42ed25fd60eb
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-17 01:32:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = [];
+  newArray = kittens.slice(0);
+  newArray.push(name);
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [];
+  newArray = kittens.slice(0);
+  newArray.unshift(name);
+  return newArray;
+}
+
+function removeLastKitten(){
+  var newArray = [];
+  newArray = kittens.slice(0);
+  newArray.pop();
+  return newArray;
+}
+
+function removeFirstKitten(){
+  var newArray = [];
+  newArray = kittens.slice(0);
+  newArray.shift();
+  return newArray;
+}
