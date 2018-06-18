b0d158a79097c627e3f2e8cbd2a97b22795925c4
Nikki M <nrmonegain@gmail.com>
2018-06-18 18:20:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'] 
+
+function kittens () {
+ return kittens
+}
+
+function destructivelyAppendKitten(array, name) {
+  array.push(name)
+  return array
+}
