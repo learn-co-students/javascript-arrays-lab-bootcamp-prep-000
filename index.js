25abd49ad84278891fd720e1f5c106e4390695bb
Ernest Kim <ekim86@gmail.com>
2018-06-21 20:08:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-const app = "I don't do much."
+function Arrays() {
+  kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  kittens = [...kittens, name];
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  kittens.pop();
+  return kittens;
+}
+
+function removeFirstKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  kittens.shift();
+  return kittens;
+}
