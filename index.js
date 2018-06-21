3f8e5e8cc7143daf64bff27e76919e7e8f9034eb
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-21 00:08:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const app = "I don't do much."
+var kittens =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(Ralph){
+  kittens.push("Ralph")
+  return kittens
+  
+}
+  
+function destructivelyPrependKitten(Bob){
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function appendKitten(Broom){
+  var newKittens = [...kittens]
+  newKittens.push("Broom")
+  return newKittens
+  
+}
+
+ function prependKitten(Arnold){
+  var newKittens = [...kittens]
+  newKittens.unshift("Arnold")
+  return newKittens
+    
+  }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+
+  
+function removeLastKitten(){
+var newKittens = [...kittens]
+newKittens.pop()
+return newKittens
+}
+
+function removeFirstKitten(){
+var newKittens = [...kittens]
+newKittens.shift()
+return newKittens
+}
