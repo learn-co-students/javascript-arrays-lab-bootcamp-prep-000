2e6a13ea48bab4ab5c65592294ab0e14d56ecd82
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-21 18:02:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph");
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob")
+  return kittens
+}
