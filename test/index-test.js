2d6d7607b84dc74eeca8a97e27d453943633f8e8
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-06-23 14:47:17 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -60,7 +60,7 @@ describe('Arrays', function() {
     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       expect(removeLastKitten()).toEqual(["Milo", "Otis"])
 
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+      expect(window.kittens).toEqual(["Milo", "Otis" , "Garfield"])
     })
   })
 
