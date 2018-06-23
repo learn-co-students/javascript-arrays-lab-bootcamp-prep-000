750d8f955fac94bf3c030f12245b5988ac011fc8
Rachel Dodell <rachel@racheldodell.com>
2018-06-23 18:37:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(kitten) {
+ return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten)
+
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens]
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten]
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1)
+}
