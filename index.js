3d086a9b5c097c1d586b8128eae1e112de42fd41
Sara S <ssantmier@gmail.com>
2018-06-24 14:21:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(Ralph) {kittens.push(Ralph);  return kittens} 
+
+function destructivelyPrependKitten(Bob) {kittens.unshift(Bob); kittens}
+
+function destructivelyRemoveLastKitten() {kittens.pop();}
+
+function destructivelyRemoveFirstKitten() {kittens.shift()}
+
+function appendKitten(Broom) {return [...kittens, Broom]}
+
+function prependKitten(Arnold) {return [Arnold, ...kittens]}
+
+function removeLastKitten() {return kittens.slice(0, kittens.length - 1)}
+
+function removeFirstKitten() {return kittens.slice(1)}
+
