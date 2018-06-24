e13fb97951f85d6ae1131b5ba54666e43a563e83
Padraig McDonald <pseamus1212@gmail.com>
2018-06-24 21:17:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const app = "I don't do much."
+var kittens = ['loki', 'charlie', 'char']
+function destructivelyAppendKitten(x) {
+  kittens.push(x)
+  return kittens
+}
+function destructivelyPrependKitten(x) {
+  kittens.unshift(x)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name) {
+  var newkittens = [...kittens, name]
+  return newkittens
+}
+function prependKitten(x) {
+  var newkittens = [x, ...kittens]
+  return newkittens
+}
+function removeLastKitten() {
+  var newkittens = kittens.slice(0, kittens.length - 1)
+  return newkittens
+}
+function removeFirstKitten() {
+  var newkittens = kittens.slice(1)
+  return newkittens
+}
