cb20ca13113bbb0b6bf8f5d5bd8cfe742c74a272
al chi <alchi1206@gmail.com>
2018-06-26 23:49:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  var myKittens = [...kittens, name];
+  return myKittens;
+}
+
+function prependKitten(name){
+ var myKittens = [name, ...kittens]
+ return myKittens;
+}
+
+function removeLastKitten(){
+var myKittens = kittens.slice (0,kittens.length -1)
+return myKittens;
+}
+
+function removeFirstKitten(){
+var myKittens = kittens.slice (1);
+  return myKittens;
+}
