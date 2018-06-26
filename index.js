ab6b33cd7b84c0d1656921adfa374decd03c141b
krivera1015 <krivera1015@gmail.com>
2018-06-26 16:10:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens;
+}
