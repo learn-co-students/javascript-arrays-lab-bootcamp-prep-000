84c8609a101e0aae024e0197b1fcd3d23d5a4f94
krivera1015 <krivera1015@gmail.com>
2018-06-26 17:51:55 +0000

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,33 @@ function destructivelyAppendKitten(name) {
   return kittens;
 }
 
-var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length -1);
 }
