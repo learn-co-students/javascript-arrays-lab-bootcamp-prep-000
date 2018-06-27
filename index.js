38611d6b70dc72123130a505a20013b5d7743fbd
Peter Traversa <peter.traversa@gmail.com>
2018-06-27 18:00:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(addition) {
+  kittens.push(addition);
+  return kittens;
+}
+
+function destructivelyPrependKitten(addition) {
+  kittens.unshift(addition);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(addition) {
+  var newKittens = [...kittens, addition];
+  return newKittens;
+}
+
+function prependKitten(addition) {
+  var newKittens = [addition, ...kittens];
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, length - 1);
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
