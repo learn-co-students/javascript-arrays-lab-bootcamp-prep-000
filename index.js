49c959d6d997cb9c5a479ae21b0dbb2fa4d4dda1
Roberto Busó-García <robertobuso@gmail.com>
2018-06-28 19:00:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten (array) { 
+  kittens.push ("Ralph");
+}
+
+function destructivelyPrependKitten (array) { 
+  kittens.unshift ("Bob");
+}
+
+function destructivelyRemoveLastKitten (array) { 
+  kittens.pop ();
+}
+
+function destructivelyRemoveFirstKitten (array) { 
+  kittens.shift ();
+}
+
+function appendKitten (array) { 
+  var newKittensArray = [...kittens, "Broom"];
+  return newKittensArray;
+}
+
+function prependKitten (array) { 
+  var newKittensArray = ["Arnold", ...kittens];
+  return newKittensArray;
+}
+
+function removeLastKitten (array) { 
+  var newKittensArray = kittens.slice (0, 2);
+  return newKittensArray;
+}
+
+function removeFirstKitten (array) { 
+  var newKittensArray = kittens.slice (1);
+  return newKittensArray;
+}
