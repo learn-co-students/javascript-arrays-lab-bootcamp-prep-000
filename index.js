25b8ab253e8442deb533259fcca526f21cbe97a2
Lucas Kabot <loyd.kim14@gmail.com>
2018-06-28 14:40:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'] //global scope
+
+function kittens () {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift(name)
+}
+
+function appendKitten (name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten () {
+ var newKittens = kittens.slice(0,2) 
+ return newKittens
+}
+
+function removeFirstKitten () {
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
