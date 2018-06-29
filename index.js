c4daa409b2a936a6d2b2a80539bf7b386d546032
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-29 06:51:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function kittens(){
+  return kittens;
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  [...kittens, "Broom"];
+}
+
+function prependKitten(name){
+  ["Arnold", ...kittens];
+}
+
+function removeLastKitten(){
+  kittens.slice(3);
+}
+
+function removeFirstKitten(){
+  kittens.slice(1);
+}
