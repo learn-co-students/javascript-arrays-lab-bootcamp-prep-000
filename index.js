890056798da566505663af955b1fd082955e6868
Valeria Ragonese <ribesnero80@gmail.com>
2018-07-01 17:51:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  return newKittens = kittens.push(name);
+  
+}
