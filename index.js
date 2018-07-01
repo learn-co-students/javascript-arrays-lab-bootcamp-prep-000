6a160bec3e3f354c6a61a5f0830ac2452cc1b74e
Thierry Dequin <thierrydequin@hotmail.com>
2018-07-01 23:43:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var   kittens;
+
+function kittens() {
+  kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  kittens();
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens();
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens();
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens();
+  kittens.shift();
+}
+
+  function appendKitten(name) {
+    kittens();
+    return [...kittens, name];
+}
+
+  function prependKitten(name) {
+    kittens();
+    return [name, ...kittens];
+}
+
+  function removeLastKitten() {
+    kittens();
+    return kittens.slice(0, kittens.length - 1);
+}
+
+  function removeFirstKitten() {
+    kittens();
+    return kittens.slice(1, kittens.length);
+}
