16f157a4d303576015502fd12825dc146ac5b1cc
Valeria Ragonese <ribesnero80@gmail.com>
2018-07-01 18:13:03 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,25 @@ function destructivelyRemoveFirstKitten(name){
 }
 
 function appendKitten(name){
-  return newKittens = kittens.push(name);
-  
+  var newKitten = kittens.slice();
+  newKitten.push(name);
+  return newKitten
+}
+
+function prependKitten(name){
+  var newKitten = kittens.slice();
+  newKitten.unshift(name);
+  return newKitten
+}
+
+function removeLastKitten(name){
+  var newKitten = kittens.slice();
+  newKitten.pop();
+  return newKitten
+}
+
+function removeFirstKitten(name){
+  var newKitten = kittens.slice();
+  newKitten.shift();
+  return newKitten
 }
