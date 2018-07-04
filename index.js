715b895a7f0baf3f8a3d9aa0bec08896615e334d
Jonathan Nguyen <jnnj12@yahoo.com>
2018-07-04 01:43:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(0)
+  return kittens
+}
+
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  var kittensP = [name, ...kittens]
+  return kittensP
+  
+}
+
+
+function removeLastKitten() {
+  return kittens.slice(0,2)
+
+}
+
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
