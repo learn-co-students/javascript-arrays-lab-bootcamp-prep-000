c12e902b7858d8fe67de446fe7f85041e993dbdf
Amy Zhou <aymeighz@gmail.com>
2018-07-09 21:50:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your functions and code here
+function kittens() {
+var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return name;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return name;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return name;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return name;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
