835b97cf0ff9b7bc5de59555bf933cbc3bea0af7
Alex Varone <alexvarone@ymail.com>
2018-07-11 18:57:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newCat = ["Broom"];
+  return kittens.concat(newCat);
+}
+
+// Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newCat = ["Broom"];
+  return kittens.concat(newCat);
+}
+
+function prependKitten(name) {
+  var newCat = ["Arnold"];
+  return newCat.concat(kittens);
+}
+
+
+function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
+}
+
+
+
+
