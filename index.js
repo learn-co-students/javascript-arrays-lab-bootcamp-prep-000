6a23e0f0484b3cd076bc4c03d8aee0097dbc55b6
Ryan McGovern <ryan.mcgovern0@gmail.com>
2018-07-11 11:37:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions and code here
+var destructivelyAppendKitten = function(name){
+  return kittens.push(name);
+};
+
+var destructivelyPrependKitten = function(name){
+  return kittens.unshift(name);
+};
+
+var destructivelyRemoveLastKitten = function(){
+  return kittens.pop();
+};
+
+var destructivelyRemoveFirstKitten = function(){
+  return kittens.shift();
+};
+
+var appendKitten = function(name){
+  var newArray = [...kittens, name];
+  return newArray;
+};
+
+var prependKitten = function(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+};
+
+var removeLastKitten = function(){
+  return kittens.slice(0, kittens.length - 1);
+};
+
+var removeFirstKitten = function(){
+  return kittens.slice(1);
+};
