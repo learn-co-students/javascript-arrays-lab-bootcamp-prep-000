ce4241c2161da3b31b81c0830b8a93c1357f0830
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-13 17:16:28 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 var array = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
+<<<<<<< HEAD
   array.push(name)
+=======
+  push("Ralph")
+>>>>>>> e513a3d200d4a821c14fb9e387468eea9f5ced08
   return array
 }
 
