c5107057985701d00c305f4dfaec6e45f7e207ca
timothy.knoll@gmail.com <timothy.knoll@gmail.com>
2018-07-13 03:28:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your functions and code here
+
+
+function kittens (){
+  var kittens = ["Milo", "Otis", "Garfield"] 
+}
+
+function destructivelyAppendKitten (name) {
+  return kittens.push(name)
+  
+}
+
+function destructivelyPrependKitten (name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  // array.splice(-1,1)
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  // array.splice(0,1)
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten (name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  return [name,...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
