4fe06292c81895a1ebed3a084b6ebba3ca11e7b3
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-13 03:50:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ...kittens];
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,-1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
