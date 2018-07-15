ae39545975584a9d502477c9589ab07a7ec9363a
Jess Elledge <jessbooks28@gmail.com>
2018-07-15 01:42:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name) {
+  var newKittens = kittens.slice(0);
+  newKittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0);
+  newKittens.pop();
+  return newKittens;
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(0);
+  newKittens.shift();
+  return newKittens;
+}
