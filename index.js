ea397d0ebe5b321f5636f3a3ed87499356dd4b4e
jvpink200 <jvillarr20@gmail.com>
2018-07-17 04:10:16 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(name){
   kittens.pop(name);
 }
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  var new_array = [name,...kittens];
+  return new_array;
+}
+
+function removeLastKitten(name){
+  var new_array = kittens.slice(0,kittens.length-1);
+  return new_array;
+}
+
+function removeFirstKitten(name){
+  var new_array = kittens.slice(1);
+  return new_array;
+}
