1468717ed12f4cef942e498c3f2939e4a3984cda
Tiffany Choong <t.choong@yahoo.com>
2018-07-17 16:55:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  var kittens2 = kittens.push("Ralph");
+  return kittens2;
+}
+
+function destructivelyPrependKitten(name){
+  var kitten3 = kittens.unshift("Bob");
+  return kitten3;
+}
