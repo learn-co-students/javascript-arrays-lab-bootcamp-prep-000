852a85fe276e4ffb3f0f95a7b55d6451812f6183
Alexandra Aulum <alex.aulum@gmail.com>
2018-07-18 22:35:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  var moreKittens = [...kittens, 'Broom'];
+  return moreKittens;
+}
+
+function prependKitten(name) {
+  var moreKittens = ['Arnold', ...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten() {
+  var fewerKittens = kittens.slice(0, kittens.length - 1);
+  return fewerKittens;
+}
+
+function removeFirstKitten() {
+  var fewerKittens = kittens.slice(1);
+  return fewerKittens;
+}
