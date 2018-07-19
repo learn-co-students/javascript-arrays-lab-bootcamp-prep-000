884a3ad2f2c7153eb337260f962de6560c2c7120
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-19 02:56:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your functions and code here
+function  destructivelyAppendKitten(name) {
+  kittens.push(name)
+} 
+  
+function  destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+} 
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var myKittens=kittens.concat(name)
+  return myKittens
+}
+
+function prependKitten(name) {
+  var myKittens=[name].concat(kittens)
+  return myKittens
+}
+
+
+function removeLastKitten() {
+  var myKittens=kittens.slice(0,kittens.length-1)
+  return myKittens
+}
+
+function removeFirstKitten() {
+   var myKittens=kittens.slice(1,kittens.length)
+  return myKittens
+}
+
+/*
+describe('removeFirstKitten()', function() {
+    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+})
+*/
