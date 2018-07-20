06b0b70fb22d9d9166a4b1cf59ed0ed86bb6bbde
mjcabrera@gmail.com <mjcabrera@gmail.com>
2018-07-20 13:30:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+var kittens = ["Milo", "Otis", "Garfield"]
 // Add your functions and code here
+function destructivelyAppendKitten() {
+  kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten() {
+  kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
