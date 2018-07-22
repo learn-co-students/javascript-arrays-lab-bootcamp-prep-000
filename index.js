34299090d26eb0d3ca4aad543d4c716dd760cbef
min kim <minwookkim2013@u.northwestern.edu>
2018-07-22 20:47:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your functions and code here
+function destructivelyAppendKitten(kitten){
+  return kittens.push(kitten)
+}
+
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
+
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+
+function appendKitten(kitten){
+  return kittens.concat(kitten)
+}
+
+
+function prependKitten(kitten){
+  var li = [kitten]
+  return li.concat(kittens)
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
