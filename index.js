76badaf732556c073acc503f2a2b362cd48a0509
Squall Leonhart <squallpl1983@gmail.com>
2018-07-24 23:18:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(x){
+  kittens.push(x)
+}
+
+function destructivelyPrependKitten(x){
+  kittens.unshift(x)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(x){
+  return [...kittens, x]
+}
+
+function prependKitten(x){
+  return [x, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, (kittens.length - 1))
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
+
+
