523aadb9b34e91b6a7165715221191a8afcfa0ac
Morris Ravis <morrisravis@berkeley.edu>
2018-07-24 19:20:43 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ function destructivelyAppendKitten(name) { kittens.push("Ralph");
 function destructivelyPrependKitten(name) { kittens.unshift("Bob");
 }
 
-function destructivelyRemoveLastKitten() { kitten.pop();
-return kittens
+function destructivelyRemoveLastKitten() {
+kittens.pop();
+return kittens;
+}
+
+function destructivelyRemoveFirstKitten()  {
+kittens.shift();
+return kittens;
+}
+
+function appendKitten(name) {
+var newKittens = [...kittens];
+newKittens.push(name);
+return newKittens;
+}
+
+function prependKitten(name) {
+var newKittens = [...kittens];
+newKittens.unshift(name);
+return newKittens;
+}
+
+function removeLastKitten() {
+var newKittens = [...kittens]
+newKittens.pop()
+return newKittens
+}
+
+function removeFirstKitten() {
+var newKittens = [...kittens];
+newKittens.shift();
+return newKittens;
 }
