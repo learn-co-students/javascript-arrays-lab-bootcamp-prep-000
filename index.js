f97ac833ab285cfd35f322c4f412ec8fb60d95ef
Hanna Strezh <hannastrezh@gmail.com>
2018-07-24 00:46:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
