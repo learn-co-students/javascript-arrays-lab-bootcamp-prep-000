6218766e9d550204fdc6ad0602b123547f60b8ee
Morris Ravis <morrisravis@berkeley.edu>
2018-07-24 18:46:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) { kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) { kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(name) { kitten.pop("Garfield");
+}
