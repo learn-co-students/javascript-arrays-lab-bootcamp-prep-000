97f295e5aaca61e5d46cfa387ff4fd75809f704a
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-25 23:55:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions and code here
+//var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten (name) { kittens.push(name) }
+function destructivelyPrependKitten (name) { kittens.unshift(name) }
+function destructivelyRemoveLastKitten () { kittens.pop() }
+function destructivelyRemoveFirstKitten () { kittens.shift() }
+function appendKitten (name) { return kittens.concat(name) }
+function prependKitten (name) { var newKittens; newKittens = kittens; return [name, ...newKittens] }
+function removeLastKitten () { return kittens.slice(0, kittens.length -1) }
+function removeFirstKitten() { return kittens.slice(1) }
+
+destructivelyAppendKitten ("Ralph");
+destructivelyPrepndKitten ("Bob");
+destructivelyRemoveLastKitten (); 
+destructivelyRemoveFirstKitten ();
+appendKitten ("Broom"); 
+prependKitten ("Arnold"); 
+removeLastKitten ();
+removeFirstKitten ();
