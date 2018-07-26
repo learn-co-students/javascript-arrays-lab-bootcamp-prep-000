1d7d524d9b8b66da755662de0d30682cd6949a2a
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-26 00:01:08 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function destructivelyPrependKitten (name) { kittens.unshift(name) }
 function destructivelyRemoveLastKitten () { kittens.pop() }
 function destructivelyRemoveFirstKitten () { kittens.shift() }
 function appendKitten (name) { return kittens.concat(name) }
-function prependKitten (name) { var newKittens; newKittens = kittens; return [name, ...newKittens] }
+
+function prependKitten (name) { 
+  var newKittens; 
+  newKittens = kittens; 
+  return [name, ...newKittens] 
+}
+
 function removeLastKitten () { return kittens.slice(0, kittens.length -1) }
 function removeFirstKitten() { return kittens.slice(1) }
 
