8ab297a81115b6ea1c547254ea64e04576087623
John Grubbs <johnmlowery@gmail.com>
2018-07-27 17:45:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your functions and code here
+// Add your functions and code her
+function destructivelyAppendKitten(){
+  kittens.push('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(){
+  return [...kittens, "Broom"]
+}
+
+function prependKitten() {
+ return ["Arnold",...kittens] 
+}
+
+function removeLastKitten() {
+  return [...kittens.slice(0,2),...kittens.slice(3)]
+}
+
+function removeFirstKitten() {
+  return [...kittens.slice(1)]
+}
