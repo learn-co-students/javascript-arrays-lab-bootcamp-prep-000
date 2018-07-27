416a0dc65bd39254959a3fc84d7faf33970f5024
Daniel Schoch <d.schoch90@gmail.com>
2018-07-27 05:20:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (name) {
+  kittens.pop(name)
+  return kittens
+}
+
+kittens = ["Milo", "Otis", "Garfield"]
