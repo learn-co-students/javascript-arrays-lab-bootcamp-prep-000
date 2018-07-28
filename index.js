b8e3b56bd48ab109288a1ed906acbea7ee744df2
samantha <sstephens614@yahoo.com>
2018-07-28 02:24:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Grafield"]
+
+function destructivelyAppendKitten (name) {
+kittens.push('Ralph');
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+}
+
+function destructivelyPrependKitten () {
+  kittens.unshift('Bob');
+  }
+  
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  
+}
+
+function appendKitten (name) {
+ console.log (kitten.concat(name));
+}
