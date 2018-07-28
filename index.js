c60cc2f8136790c9fc0aabe911cf701ad19ef432
Wyatt Massey <wyattisboss276@gmail.com>
2018-07-28 06:43:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  return kittens.concat(name);
+}
+function prependKitten(name){
+  /*dad's Example 
+    var foo = [name];
+    foo.concat(kittens);
+  */
+  return [name, ...kittens];
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1);
+}
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
