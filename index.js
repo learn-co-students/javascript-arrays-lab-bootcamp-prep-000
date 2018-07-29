9919ef9618ed60bf2a12331b1a38c66db5aa40dc
lilianasrmartins@gmail.com <lilianasrmartins@gmail.com>
2018-07-29 13:32:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens 
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(array) {
+  kittens.pop()
+  return kittens  
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name,array) {
+  return [...kittens,"Broom"]
+} 
+
+function prependKitten(name,array) {
+  return ["Arnold",...kittens]
+}
+
+function removeLastKitten(array) {
+  return kittens.slice(0,kittens.length -1)
+}
+
+function removeFirstKitten(array) {
+  return kittens.slice(1)
+}
+
+
+
