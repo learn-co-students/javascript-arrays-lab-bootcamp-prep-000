b7144c219c1763ca6137914071b700927898c4a3
Zeshawn Khan <zeshawnk@gmail.com>
2018-07-31 00:00:41 +0000

    Done.

--- index.js
+++ index.js
@@ -13,8 +13,27 @@ function destructivelyPrependKitten(name){
 
 function destructivelyRemoveLastKitten(){
   kittens.pop()
+  return kittens
 }
 
 function destructivelyRemoveFirstKitten(){
   kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,-1)
+  
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
 }
