66a33b7ebc54546ced0f4ce65248e76fece9186e
Brenda  Almonte <breal0811@gmail.com>
2018-07-31 19:17:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+//GOOD ABOVE
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+   return kittens;
+}
+//GOOD ABOVE
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+//GOOD ABOVE
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+//GOOD ABOVE
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+//GOOD ABOVE
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+//GOOD ABOVE
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+//GOOD ABOVE
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1);
+  return newArray;
+}
+
+
+
+
+
+
+
+
