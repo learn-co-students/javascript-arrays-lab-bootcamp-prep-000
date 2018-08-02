522e0f4d6427ab059dc955f55170124ea40cb798
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-02 16:55:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(Name) {
+  kittens.push(Name)
+  return kittens
+}
+function destructivelyPrependKitten(Name) {
+  kittens.unshift(Name)
+  return kittens
+} 
+function destructivelyRemoveLastKitten(Name) {
+  kittens.pop(Name)
+  return kittens
+}
+function destructivelyRemoveFirstKitten(Name) {
+  kittens.shift(Name)
+  return kittens
+}
+function appendKitten(Name) {
+  return [...kittens, Name]
+}
+function prependKitten(Name) {
+  return [Name, ...kittens]
+}
+function removeLastKitten(Name) {
+  return kittens.slice(0, kittens.length - 1)
+}
+function removeFirstKitten(Name) {
+  return kittens.slice(1)
+}
