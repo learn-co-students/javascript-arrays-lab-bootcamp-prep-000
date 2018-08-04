86126feabbca1da9260bf5634fa0382315e782ea
Michael Barbone <michael.barbone@gmail.com>
2018-08-04 00:12:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your functions and code here
+  var kittens = ["Milo", "Otis", "Garfield"];
+  
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newKittens=[name];
+  return (kittens.concat(newKittens));
+}
+
+function prependKitten (name) {
+  return ("name",...kittens);
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0,2);
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(-2);
+}
+
+
+
