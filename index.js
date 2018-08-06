83159ac10762062e2b83057fed56050ae4ed1731
Tyler W Palmer <tylerpalmer7@gmail.com>
2018-08-06 20:31:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+//1 
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return(kittens);
+}
+//2 
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return(kittens);
+}
+//3 
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return(kittens);
+}
+//4 
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return(kittens);
+}
+//5
+function appendKitten(name){
+  return(kittens.concat("Broom"))
+}
+//6 
+function prependKitten(name){
+  var newcat1 = ["Arnold"]
+  return(newcat1.concat(kittens))
+}
+//7
+function removeLastKitten(){
+  return(kittens.slice(0,2))
+}
+//8 
+function removeFirstKitten(){
+  return(kittens.slice(1))
+}
