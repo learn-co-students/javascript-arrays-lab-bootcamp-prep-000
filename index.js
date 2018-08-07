4596614132030c382890661b1950e75fd4cd899c
Jennifer Bishop <jbishop@gmail.com>
2018-08-07 20:53:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+console.log(kittens)
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  var newCat = [name]
+  var moreKittens = kittens.concat(newCat)
+  return moreKittens
+}
+function prependKitten(name){
+  var newCat = [name]
+  var moreKittens = newCat.concat(kittens)
+  return moreKittens
+}
+function removeLastKitten(){
+  var fewerCats = kittens.slice(0, 2)
+  return fewerCats
+}
+function removeFirstKitten(){
+  var fewerCats = kittens.slice(1)
+  return fewerCats
+}
