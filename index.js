49ab60e3e000e167e02d50b08984875abe1a7db4
Roundabout Rider <ntm.email@icloud.com>
2018-08-07 12:13:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your functions and code here
+var kittens = [`Milo` , `Otis` , `Garfield`]
+
+function Arrays() {
+  return kittens
+}
+
+function destructivelyAppendKitten() {
+  kittens.push(`Ralph`)
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift(`Bob`)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten() {
+  kittens.slice(1 , 3)
+  return kittens.concat(`Broom`)
+}
+
+function prependKitten() {
+  return kittens.splice(0 , 0 ,`Arnold`)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0 , kittens.length -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1 , 3)
+}
