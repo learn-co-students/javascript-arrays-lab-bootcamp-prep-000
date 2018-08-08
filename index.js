b8ff01327ee93234235ca735ab8b654a072495cf
Preston A Arnette <arnettepreston@gmail.com>
2018-08-08 17:51:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 // Add your functions and code here
 
 // kitten Array
-var kitten = ['Milo', 'Otis', 'Garfield']
+var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  kitten.push('Ralph');
-  return kitten;
+  return kittens.push('Ralph');
 }
 
+function destructivelyPrependKitten(name) {
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var addKitten = ['Broom'];
+  return kittens.concat(addKitten);
+}
+
+function prependKitten(name) {
+  var addKitten = ['Arnold'];
+  return addKitten.concat(kittens);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, 2);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}  
