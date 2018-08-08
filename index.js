67b8c4143b9a9764b3cf3d30be4a28eaffd8356c
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-08 12:46:26 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function destructivelyPrependKitten(name) {
 function destructivelyRemoveLastKitten() {
   return kittens.pop()
 }
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
