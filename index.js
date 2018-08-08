cf33804301c9dc9bdf992cf7f9160907f311b05c
Ana Mafla <anamafla@hotmail.com>
2018-08-08 15:10:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  let newArray = [...kittens, name]
+  return newArray;
+}
+
+function prependKitten(name){
+  let newArray = [name, ...kittens]
+  return newArray;
+}
+
+function removeLastKitten(){
+  let newArray = kittens.slice(0, kittens.length-1)
+  return newArray;
+}
+
+function removeFirstKitten(){
+  let newArray = kittens.slice(1)
+  return newArray;
+}
