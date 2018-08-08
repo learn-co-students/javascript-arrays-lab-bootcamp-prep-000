8508da77977c3572985843ff4afb681b316b7426
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-08 18:28:43 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,36 @@ function destructivelyAppendKitten(name) {
   return kittens
 }
   
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+   return kittens.shift()
+ 
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, 2)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, 3)
+}
+
+
+  
