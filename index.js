4e97bea2b4c809ebe9605eb5ab8b6d189b71a9ec
Preston A Arnette <arnettepreston@gmail.com>
2018-08-08 17:28:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions and code here
+
+// kitten Array
+var kitten = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kitten.push('Ralph');
+  return kitten;
+}
+
