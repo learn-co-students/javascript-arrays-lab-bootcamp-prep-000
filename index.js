b70a253d4ece28ab195418be3c64e93194a9d377
Mark Shipley <mshipley1992@hotmail.com>
2018-08-09 19:50:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return window.kittens(["Ralph", kittens...])
+}
+function destructivelyPrependKitten(name) {
+  return window.kittens([kittens..., "Bob"])
+}
