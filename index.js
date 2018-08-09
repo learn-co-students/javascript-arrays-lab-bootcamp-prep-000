fefb8ff1e3606e13fcfd21ccd0029fe0ec8d6d4d
Mark Shipley <mshipley1992@hotmail.com>
2018-08-09 22:00:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // Add your functions and code here
 var kittens = ["Milo", "Otis", "Garfield"]
 
+var kitten = ["Arnold"]
+
 function destructivelyAppendKitten(name) {
-  return window.kittens(["Ralph", kittens...])
+  return kittens.push("Ralph")
 }
 function destructivelyPrependKitten(name) {
-  return window.kittens([kittens..., "Bob"])
+  return kittens.unshift("Bob")
+}
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop("Garfield")
+}
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift("Milo")
+}
+function appendKitten(name) {
+  return kittens.concat("Broom")
+}
+function prependKitten(name) {
+  return kitten.concat(kittens)
+}
+function removeLastKitten(name) {
+  return kittens.slice(0,2,"Garfield")
+}
+function removeFirstKitten(name) {
+  return kittens.slice(1,3,"Milo")
 }
