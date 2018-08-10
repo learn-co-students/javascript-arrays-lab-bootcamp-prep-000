90f538ccf6e10832a7341c20d6b0cd33f52b9d57
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-10 20:00:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+
 // Add your functions and code here
