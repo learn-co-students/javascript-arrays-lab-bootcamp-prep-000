09a0a46be999518c9b2e645d365bc22f6f4ba82d
Ashley Tolen <tolenashley7@gmail.com>
2018-08-10 01:40:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your functions and code here
+// Destructively...
+// Append
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+// Prepend
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+// Remove Last
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+// Remove First
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+// Nicely now
+// Append
+function appendKitten(name) {
+  kittens = [...kittens, name];
+  return kittens;
+}
+// Prepend
+function prependKitten(name) {
+  kittens = [name, ...kittens];
+  return kittens;
+}
+// Remove Last
+// Remove First
