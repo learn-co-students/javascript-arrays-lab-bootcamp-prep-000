95ffb311063058e621e4255c3587cf4a81513ba3
Chris Li <cli78822@gmail.com>
2018-08-10 05:07:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var arr = [...kittens];
+  arr.push(name)
+  return arr
+}
+
+function prependKitten(name){
+  var arr = [...kittens];
+  arr.unshift(name)
+  return arr
+}
+
+function removeLastKitten(){
+  var arr = [...kittens];
+  arr.pop()
+  return arr
+}
+
+function removeFirstKitten(){
+  var arr = [...kittens];
+  arr.shift()
+  return arr
+}
