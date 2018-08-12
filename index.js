311277f6ddfddf4bd0117a07cf576cf829c3ae79
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 18:55:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(array) {
+  kittens.pop(array);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift(array);
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
+  
+function removeLastKitten(array) {
+  return kittens.slice(0, kittens.length - 1);
+}
+  
+function removeFirstKitten(array) {
+  return kittens.slice(1);
+}  
+  
+  
+  
