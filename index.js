7ba57c14d65c4b03b14c9d9da8192281c5c62bec
Steven C Eaton <steveeaton5@gmail.com>
2018-08-12 20:02:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+function appendKitten(name){
+  var myKittens = [name]
+var allKittens = kittens.concat(myKittens);
+return allKittens;
+}
+function prependKitten(name) {
+  var myKittens = [name]
+var allKittens = myKittens.concat(kittens);
+return allKittens;
+}
+function removeLastKitten() {
+  var myKittens = kittens.slice(kittens, kittens.length - 1);
+  return myKittens;
+}
+function removeFirstKitten(){
+  var myKittens = kittens.slice(1, kittens.length)
+  return myKittens;
+}
