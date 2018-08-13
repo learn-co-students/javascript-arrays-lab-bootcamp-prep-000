14cc70a36050c6d5aa80affb2277b08c705c8e05
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-13 11:49:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions and code here
+function destructivelyAppendKitten (array, element) {
+  kittens.push('Ralph');
+  return kittens;
+}
+function destructivelyPrependKitten (array, element) {
+  kittens.unshift('Bob');
+  return kittens;
+}
+function destructivelyRemoveLastKitten (array) {
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten (array) {
+  kittens.shift();
+  return kittens;
+}
+function appendKitten (array,element) {
+  return [...kittens, 'Broom'];
+}
+function prependKitten (array, element) {
+  return ['Arnold', ...kittens];
+}
+function removeLastKitten (array) {
+  return kittens.slice(0,2);
+}
+function removeFirstKitten(array) {
+  return kittens.slice(1,3);
+}
