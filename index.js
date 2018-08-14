d5b1f3efaa6f41f279e3e4ca27f7dfb2d2dd6159
Dario Spina <dario_spina01@live.it>
2018-08-14 11:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions and code here
+function destructivelyAppendKitten (name) {
+  kittens.splice(3,0,name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.splice(0,0,name)
+}
+function destructivelyRemoveLastKitten() {
+   kittens.splice(2,1)
+}
+function destructivelyRemoveFirstKitten() {
+   kittens.splice(0,1)
+}
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+function prependKitten(name) {
+  return [name].concat(kittens)
+}
+function removeLastKitten() {
+return kittens.slice(0,kittens.length -1)
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
