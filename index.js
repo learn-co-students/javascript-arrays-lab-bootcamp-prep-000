2ee504d29c1ce0a347c5cee39640e824c5190902
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-15 03:59:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Add your functions and code here
+let kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+} 
+
+
+function prependKitten(name) {
+ return [name, ...kittens]
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function removeLastKitten() {
+  const _kittens = [...kittens]
+  _kittens.pop()
+  return _kittens
+}
+
+function removeFirstKitten() {
+  const _kittens = [...kittens]
+  _kittens.shift()
+  return _kittens
+}
+
