9cdabc46810bf99c9591a8e03c068a057bb3b2c2
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-15 01:54:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // Add your functions and code here
 
-function destructivelyAppendKitten(kitten) {
-  return kittens.concat(kitten)
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  return [name].concat(kittens)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1,3)
 }
