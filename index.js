5b0bb1a26a902b32926e650bdd11f954079d78b8
Justin Chang <justinxchang@hotmail.com>
2018-08-17 05:24:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  return kittens.concat(name);
+}
+
+function prependKitten(name){
+  return [name].concat(kittens);
+}
+
+
+function removeLastKitten(){
+  return kittens.slice(0, 2);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
