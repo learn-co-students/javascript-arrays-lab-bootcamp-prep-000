0ef38482fd8706ceb5a8157fa1ffa46e6a9cd257
Viam Mercer <viam.mercer@hotmail.com>
2018-08-20 14:46:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions and code here
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  const kittens1 = [...kittens, name];
+  return kittens1;
+}
+
+function prependKitten(name) {
+  const kittens1 = [name, ...kittens];
+  return kittens1;
+}
+
+function removeLastKitten() {
+  const kittens1 = kittens.slice(0,2);
+  return kittens1;
+}
+
+function removeFirstKitten() {
+  const kittens1 = kittens.slice(1);
+  return kittens1;
+}
