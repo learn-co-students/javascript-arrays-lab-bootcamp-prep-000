af337eddc222b74f9630cbaa8fe7b5526fa829a5
jonroger3@gmail.com <jonroger3@gmail.com>
2018-08-22 15:42:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your functions and code here
+var kittens = [Milo, Otis, Garfield];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+destructivelyAppendKitten('Ralph');
+
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+destructivelyPrependKitten('Bob');
+
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(-1);
+  return kittens;
+}
+
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(0);
+  return kittens;
+}
+
