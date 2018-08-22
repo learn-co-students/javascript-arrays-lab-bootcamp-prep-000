a0266ea8d8b06707f77a16ba9fcc486069bc22f8
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 18:02:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+function appendKitten(name) {
+  return [...kittens, "Broom"]
+}
+function prependKitten(name) {
+  return ["Arnold", ...kittens]
+}
+function removeLastKitten() {
+  return kittens.slice(0,kittens.length - 1)
+}
+function removeFirstKitten() {
+  return kittens.slice(1,3)
+}
