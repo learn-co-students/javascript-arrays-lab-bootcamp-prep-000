a189e8fa1c2673ff9c66cd6588be8c14846d6cad
Chovin Carlson <chovin@guam.net>
2018-08-23 02:35:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  return [name].concat(kittens)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
