2eaf3df07ec2d0523c1261d3104a750f84610499
Sarah Pai <pai.sarah89@gmail.com>
2018-08-24 07:49:08 +0000

    Done.

--- index.js
+++ index.js
@@ -33,3 +33,47 @@ function removeFirstKitten() {
   kittens.slice(1,3)
   return kittens.slice(1,3)
 }
+
+
+// -----------------------
+//below is first set of coding
+//above is 2nd time coding
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop("Garfield")
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  kittens.concat("Broom")
+  return kittens.concat("Broom")
+}
+
+
+function prependKitten(name) {
+  return [name, ...kittens]
+  
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length - 0)
+}
