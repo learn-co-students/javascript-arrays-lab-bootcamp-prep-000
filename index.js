a1be32b9013b9fb5d7e4f5ca3c7e3c9f36500e05
Caren Rz <anruiz110@gmail.com>
2018-08-24 03:06:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your functions and code here
+// anruiz23
+
+var kittens = ['Milo','Otis','Garfield']
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+  
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
+  return kittens
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  var nkitts = kittens.slice(0)
+  nkitts.push(name)
+  return nkitts
+}
+
+function prependKitten(name){
+  var nkitts = kittens.slice(0)
+  nkitts.unshift(name)
+  return nkitts
+}
+
+function removeLastKitten(){
+  var len = kittens.length
+  var nkitts = kittens.slice(0,len-1)
+  return nkitts
+}
+
+function removeFirstKitten(){
+  var nkitts = kittens.slice(1)
+  return nkitts
+}
