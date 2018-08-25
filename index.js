7957bed988baee0fd843e88abfcac78ffc3bc33a
Harry Bigos <hrbigos@gmail.com>
2018-08-25 18:02:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Add your functions and code here
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function arrays() {
+  return kittens
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(2);
+  return kittens
+}
+
+ 
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(1);
+  return kittens
+}
+ 
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+ 
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+  
+
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
+ 
