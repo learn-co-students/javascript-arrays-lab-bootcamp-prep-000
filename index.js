da3baea6bcaa9cdb464d3d4b2e0e7fca7021ab4a
Gina Fedder <ginafedder@yahoo.com>
2018-08-27 20:30:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions and code here
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"];
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
+  
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  newkittens = [...kittens, "Broom"];
+}
