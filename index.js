8ebcc5fc6a0715c1d164188ae983358bc5c7c056
Allen Rhee <rhee.daehyun@gmail.com>
2018-08-28 00:25:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.slice(0, kittens.length -1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
