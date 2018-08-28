0fae83998212b631d8b5c3e6ce5f06dae08277d3
gr98577 <gr98577@umbc.edu>
2018-08-28 04:43:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  window.kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  window.kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  window.kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  window.kittens.shift();
+}
+
+function appendKitten(name){
+  return window.kittens.concat(name);
+  
+}
+
+function prependKitten(name){
+  var nameArr = [name];
+  return nameArr.concat(window.kittens);
+}
+
+function removeLastKitten(){
+  return window.kittens.slice(0,-1);
+  
+}
+
+function removeFirstKitten(){
+  return window.kittens.slice(1);
+}
