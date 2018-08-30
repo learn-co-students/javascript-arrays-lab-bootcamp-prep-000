995dfc3245159f23cd2e026ba3b6e0bdca322099
M A <m@100cube.com>
2018-08-30 22:43:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+/*
+ Mutable - destructive verions
+ */
+ function destructivelyAppendKitten(name) {
+   kittens.push(name)
+   return kittens
+ }
+
+ function destructivelyRemoveLastKitten() {
+   kittens.pop()
+   return kittens
+ }
+
+ function destructivelyPrependKitten(name) {
+   kittens.unshift(name)
+   return kittens
+ }
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+/*
+NON-Destructive verions
+*/
+function appendKitten(name) {
+  return kittens.concat(name)
+  // OR
+  //return [...kittens, name]
+}
+
+function prependKitten(name) {
+  var tmp = new Array();
+  tmp.push(name);
+  return tmp.concat(kittens);
+/*  // OR
+  return [name, ...kittens]
+*/
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,-1)
+}
