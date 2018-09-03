cd2af479cadb74339cae3a8c45553c7731fdec3b
Taylor Watson <taylorwatsonb@gmail.com>
2018-09-03 19:59:42 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -9,8 +9,8 @@ describe('Arrays', function() {
     })
   })
 
-  describe('destructivelyAppendKitten(name)', function(Ralph) {
-    it('appends a kitten to the end of the kittens array', function(Ralph) {
+  describe('destructivelyAppendKitten(name)', function() {
+    it('appends a kitten to the end of the kittens array', function() {
       destructivelyAppendKitten('Ralph')
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
     })
@@ -73,3 +73,35 @@ describe('Arrays', function() {
   })
 
 })
+
+
+const app = "I don't do much.";
+ function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+ function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+ function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+ function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+ function appendKitten(name){
+  return [...kittens, name];
+}
+ function prependKitten(name) {
+  return [name, ...kittens];
+}
+ function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+ function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
