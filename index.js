c54abe5134cd187c6b1ab402a199b5c6ea8dcec0
ana.volaric95@gmail.com <ana.volaric95@gmail.com>
2018-09-04 13:31:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions and code here
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+  var newKittens = kittens.push(name);
+  return newKittens;
+}
+
+function prependKitten(name){
+  var newKittens = kittens.unshift(name);
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(1);
+  return newKittens;
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(-1);
+  return newKittens;
+}
