b2f1649f3d7ebcec3bd980f399e97211b74ee42b
Kara Isreal <isreal.kara92@gmail.com>
2018-09-05 16:40:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your functions and code here
+function kittens(array){
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens
+}
+
+function destructivelyAppendKitten(array, element){
+kittens.push('Ralph')
+return kittens
+}
+
+function destructivelyPrependKitten(array, element){
+  kittens.unshift('Bob')
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(array){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(array){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(array, element){
+return [...kittens, 'Broom'];
+}
+
+function prependKitten(array, element){
+  return ['Arnold', ...kittens]
+}
+
+function removeLastKitten(array){
+return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten(array){
+return kittens.slice(1)
+}
