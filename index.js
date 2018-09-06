f139d3c25a6b11706f644e027aa90ccd23aa8110
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 16:06:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield']// Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kittens = kittens.concat('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens = ['Bob', ...kittens]
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens = kittens.slice(0, 2)
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens = kittens.splice(1)
+  return kittens
+}
+function appendKitten(name){
+  kittens = kittens.slice(0, 'Broom')
+  return kittens
+}
