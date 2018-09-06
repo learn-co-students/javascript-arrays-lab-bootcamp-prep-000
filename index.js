37bfd8b629c01f2197e04200191385368d6f2853
ca.fernando415@gmail.com <ca.fernando415@gmail.com>
2018-09-06 12:58:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  var tmp = [...kittens];
+  tmp.pop();
+  return tmp;
+}
+
+function removeFirstKitten() {
+  var tmp = [...kittens];
+  tmp.shift();
+  return tmp;
+}
