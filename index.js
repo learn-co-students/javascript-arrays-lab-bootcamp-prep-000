7f55eaa3f8aa973128ba492a462ac86b6479b493
brindab50@gmail.com behal <brindab50@gmail.com>
2018-09-06 19:37:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
-  array.push(name);
-  return kitten
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name);
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+function appendKitten(name){
+return [...kittens, name];
+
+}
+function prependKitten(name) {
+ return [name,...kittens];
+  
+}
+function removeFirstKitten() {
+  return kittens.slice(1);
+ 
+}
+function removeLastKitten() {
+ return kittens.slice(0, kittens.length - 1)
 }
