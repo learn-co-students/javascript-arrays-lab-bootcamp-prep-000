aa800203a59e7a146f50843f261acd52c0c8b896
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-08 03:06:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(name)
+}
+
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name)
+}
+
+
+function appendKitten(name) {
+  var newArray = []
+  newArray = [...kittens, name]
+  return newArray
+}
+
+
+function prependKitten(name) {
+ var newArray = []
+  newArray = [name, ...kittens]
+  return newArray 
+}
+
+
+function removeLastKitten() {
+  var newArray = []
+  newArray = kittens.slice(0, -1)
+  return newArray
+}
+
+
+function removeFirstKitten() {
+  var newArray = []
+  newArray = kittens.slice(1)
+  return newArray
+}
