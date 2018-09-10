7c32a423d16fda6f7b7f82b2c202113f14b4878a
caboky31@hotmail.com Bernard <caboky31@hotmail.com>
2018-09-10 21:56:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions and code here
+
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(Ralph){
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(Bob){
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(Broom){
+return [...kittens, "Broom"]
+}
+
+function prependKitten(Arnold){
+  return ["Arnold", ...kittens]
+}
+
+function removeFirstKitten(){
+  var newKittens = [...kittens]
+ newKittens.shift()
+ return newKittens
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
