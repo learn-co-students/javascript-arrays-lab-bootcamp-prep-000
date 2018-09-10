9d13458bddfcf8bb21fe3d9c46aa08b281313a03
Nicholas Werner <micropacman@gmail.com>
2018-09-10 20:59:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+
+function appendKitten(name) {
+  var appendedKittensArray = [...kittens, name]
+  return appendedKittensArray
+}
+
+function prependKitten(name) {
+  var prependedKittensArray = [name, ...kittens]
+  return prependedKittensArray
+}
+
+function removeLastKitten() {
+  var poppedKittensArray = kittens.slice(0,-1)
+  return poppedKittensArray
+}
+
+function removeFirstKitten() {
+  var shiftedKittensArray = kittens.slice(1)
+  return shiftedKittensArray
+}
