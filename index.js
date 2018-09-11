72871de44e5f94b030629f2c540b32da3a4a1895
BWitz <bhorowitz2014@gmail.com>
2018-09-11 02:36:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your functions and code here
+
+let kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(Milo, Otis, Garfield) {
+  return kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(Milo, Otis, Garfield) {
+  return kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten(Milo, Otis, Garfield) {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(Milo, Otis, Garfield) {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  let kittenAppend = [...kittens, "Broom"];
+  return kittenAppend;
+}
+
+function prependKitten(name) {
+  let kittenPrepend = ["Arnold", ...kittens];
+  return kittenPrepend;
+}
+
+function removeLastKitten() {
+  let lastKittenRemoval = kittens.slice(0, 2);
+  return lastKittenRemoval;
+}
+
+function removeFirstKitten() {
+  let firstKittenRemoval = kittens.slice(-2);
+  return firstKittenRemoval;
+}
