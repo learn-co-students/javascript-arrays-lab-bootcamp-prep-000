5d0e9b7819491a3c840ae5e63bbf178214394bf9
Dorothy Davenport <dorothympdx@gmail.com>
2018-09-13 03:47:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(){
+  return kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(){
+  return kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift(0);
+}
+
+function appendKitten(){
+  return [...kittens, "Broom"];
+}
+
+function prependKitten(){
+  return ["Arnold",...kittens];
+}
+
+function removeLastKitten(){
+  return kittens.splice(2);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
