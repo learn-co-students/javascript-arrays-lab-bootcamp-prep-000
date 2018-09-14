361ca42ad428e4b96486f639a77582bb321c038d
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-09-14 17:15:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  var newKittens = [...kittens]
+  newKittens.unshift(name)
+  return newKittens
+}
+
+function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop(name)
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = [...kittens]
+  newKittens.shift(name)
+  return newKittens
+}
