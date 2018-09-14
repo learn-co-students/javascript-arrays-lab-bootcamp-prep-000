d619401ced74a1644fd542dde739222b51072bb3
Virginia Dooley <vcdooley@gmail.com>
2018-09-14 08:50:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop(name);
+  return kittens
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift(name);
+  return kittens
+}
+
+function appendKitten (name) {
+  return kittens
+}
