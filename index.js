0bd65f298d8a1d0c2ebfbe410f6853550fc4c464
Paul Clark <clark.paul123@gmail.com>
2018-09-14 19:19:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+
+function appendKitten(name) {
+  kittens.splice(3, 0, name)
+  return kittens
+}
+
+function prependKitten(name) {
+  kittens
+  return kittens
+}
