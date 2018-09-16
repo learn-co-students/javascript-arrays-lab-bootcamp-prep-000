e66e7520e78c8669c0983dd162b6def2e37d8d77
demian sims <demiansims@gmail.com>
2018-09-16 01:11:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens
@@ -27,3 +28,43 @@ function removeLastKitten(name) {
  return kittens.slice(1);
    
  }
+
+// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  var newArray = [...kittens, name];
+  return newArray
+}
+
+function prependKitten(name) {
+  var newArray = [name, ...kittens]
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = kittens.length(-1);
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.splice(0, 1)
+  return newArray
+}
+
+
