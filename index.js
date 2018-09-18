f30fed6335bbc366a0bda1b362eb74d1ce39e74d
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-18 19:10:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
