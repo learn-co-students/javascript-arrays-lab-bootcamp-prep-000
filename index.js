f77b33a2cd97979d86bb0fee2cb985d967314c7e
Brian <briandsalemi@gmail.com>
2018-09-18 17:21:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+kittens.push("Ralph");
+
+ function destructivelyAppendKitten(array, element) {
+    kittens.push("Ralph"); {
+      return kittens } 
+  }
+
+function destructivelyPrependKitten(array, element) {
+  kittens.unshift("Bob"); {
+  return kittens }
+}
