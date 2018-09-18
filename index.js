73e0c055637cadf485fbd7880f66b389ea6a4eec
Ana Bebic <anabebic.x@gmail.com>
2018-09-18 10:26:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+const kittens =["Milo","Otis","Garfield"]
+
+
+function destructivelyAppendKitten(name){
+
+  return kittens.push(name)
+  
+}
+
+function destructivelyPrependKitten(name){
+
+  return kittens.shift(name)
+  
+}
