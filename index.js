fd506cd9ccf40d9ed6dc2eff37472bc008e64947
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-18 21:42:29 +0000

    Done.

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ function destructivelyRemoveFirstKitten(){
   kittens.shift()
   return kittens
 }
+
+function appendKitten(name){
+ return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
