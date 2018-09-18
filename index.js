a8c57efaf8b55da029922e9edab3b1abb519a733
Austin Puckett <puckett_austin@yahoo.com>
2018-09-18 06:52:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  const kittens2 = [...kittens, name];
+  return kittens2;
+}
+
+function prependKitten(name) {
+  const kittens2 = [name, ...kittens];
+  return kittens2;
+}
+
+
+function removeLastKitten() {
+  const kittens2 = kittens.slice(0, kittens.length - 1)
+  return kittens2
+}
+
+function removeFirstKitten() {
+  const kittens2 = kittens.slice(1);
+  return kittens2;
+}
+
+
+
+
