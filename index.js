22b9815b61ec613d03ace984370320aa341e2245
Kody Clemens <kody.clemens@gmail.com>
2018-09-19 18:36:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  var newArray = []
+  newArray = kittens.concat(newArray)
+  newArray.push(name)
+  return newArray
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function prependKitten(name) {
+  var newArray = []
+  newArray = kittens.concat(newArray)
+  newArray.unshift(name)
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = []
+  newArray = kittens.concat(newArray)
+  newArray.pop()
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray = []
+  newArray = kittens.concat(newArray)
+  newArray.shift()
+  return newArray
+}
