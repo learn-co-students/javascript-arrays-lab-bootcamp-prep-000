873b5d4cfb81efbc74f3aba2cfa85e31a1c91163
Joshua Chinyama <chinyamajosh@gmail.com>
2018-09-19 11:50:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Add your functions and code here
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+  ];
+  
+  function destructivelyAppendKitten(name) {
+    kittens.push('Ralph');
+    return kittens;
+  }
+  
+  function destructivelyPrependKitten(name) {
+    kittens.unshift('Bob');
+    return kittens;
+  }
+  
+ function destructivelyRemoveLastKitten(name) {
+   kittens.pop();
+   return kittens;
+ }
+  
+  function destructivelyRemoveFirstKitten(name) {
+    kittens.shift();
+    return kittens;
+  }
+  
+  function appendKitten(name) { 
+    return [...kittens, "Broom"];
+  } 
+  
+  function prependKitten(name) { 
+    return ["Arnold", ...kittens];
+  }
+  
+ function removeFirstKitten(name) {
+   return kittens.slice(-2);
+ }
+ 
+  function removeLastKitten(name) {
+    return kittens.slice(0, kittens.length -1);
+  } 
+  
+  
+  
+  
+
+  
+
+  
+  
