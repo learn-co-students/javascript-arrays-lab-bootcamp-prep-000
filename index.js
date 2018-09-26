02bcc7016bd8c38de761f4cf1cff86edcb16eb52
Rosanna Brown <rosannabrown@outlook.com>
2018-09-26 18:42:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 var kittens=["Milo","Otis","Garfield"];
-function destructivelyAppendKitten(kittens){
-  kittens.splice('Ralph');
-  return(kittens);
+var newArray=["Milo","Otis","Garfield", "Broom"];
+var newArray2=["Arnold","Milo","Otis","Garfield"];
+var cutOffLast=kittens.slice(0,2);
+var cutOffFirst=kittens.slice(1,3);
+
+
+function destructivelyAppendKitten(Ralph){
+ kittens.push("Ralph");
+ return kittens;
+}
+function destructivelyPrependKitten(Bob){
+  kittens.unshift("Bob");
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens. pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(Broom){
+ kittens.concat(Broom);
+ return newArray;
 }
 
+function prependKitten(Arnold){
+  kittens.concat(Arnold);
+  return newArray2;
+  
+}
+
+function removeLastKitten(Kittens){
+kittens.slice(0,2);
+return cutOffLast;
+}
+function removeFirstKitten(Kittens){
+kittens.slice(1,3);
+return cutOffFirst;
+}
