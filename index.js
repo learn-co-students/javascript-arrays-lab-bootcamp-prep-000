1099e4bfa475c3e2244093fb6b87290c5afc6836
Cesar Perez <capvkd@msn.com>
2018-09-26 05:20:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name); return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name); return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(); return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(); return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function prependKitten(name) {
+ return Array(name).concat(kittens)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function removeLastKitten() {
+  return kittens.slice(0,-1)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function removeFirstKitten() {
+  return kittens.slice(1)}
