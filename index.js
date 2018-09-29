0ffda778b98b08aac04c8ff8893eade22a276fb4
Spencer D Stone <doomydoomdomm@gmail.com>
2018-09-29 06:01:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your functions and code here
+function kittens(){
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens
+}
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(length, length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
