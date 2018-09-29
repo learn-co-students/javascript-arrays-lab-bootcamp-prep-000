11abd40ff8821f2d7c5ecb4b03b3ce59bcfcbe8d
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-09-29 23:55:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten (name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop(kittens[kittens.length-1])
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  return Array(name).concat(kittens)
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1, kittens.length)
+}
