6fc306427358607cbb274189a6a09b906097d6be
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-03 14:21:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+kittens.push(name);
+return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push(name)
+  return newArray
+}
+
+function prependKitten(name){
+  var newArray = kittens.slice();
+  newArray.unshift(name)
+  return newArray
+}
+
+function removeLastKitten(){
+  var newArray = kittens.slice();
+  newArray.pop()
+  return newArray
+}
+
+function removeFirstKitten(){
+  var newArray = kittens.slice();
+  newArray.shift()
+return newArray
+}
