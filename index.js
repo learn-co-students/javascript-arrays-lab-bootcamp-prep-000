994850f08fa57f3436f1d2f024e6a0dc857212f0
Joshua S Lopez <joshua.lopez.89@gmail.com>
2018-10-03 00:50:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendKitten(kitty){
+  kittens.push(kitty);
+}
+
+ function destructivelyPrependKitten(kitty){
+  kittens.unshift(kitty);
+}
+
+ function destructivelyRemoveLastKitten(kitty){
+  kittens.pop();
+}
+
+ function destructivelyRemoveFirstKitten(kitty){
+  kittens.shift();
+}
+
+ function appendKitten(kitty){
+  var cats = kittens.concat(kitty);
+  return cats;
+}
+
+ function prependKitten(kitty){
+   kitty= [kitty];
+  var cats = kitty.concat(kittens);
+  return cats;
+}
+
+ function removeLastKitten(kitty){
+  var cats = kittens.slice(0, -1);
+  return cats;
+}
+
+ function removeFirstKitten(kitty){
+  var cats= kittens.slice(1);
+  return cats;
+}
+
+
+
+
