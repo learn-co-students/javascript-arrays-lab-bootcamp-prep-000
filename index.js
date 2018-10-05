51073f40a4657017fd0edc572402673a4ed9c180
Jason Wiram <jwiram@gmail.com>
2018-10-05 21:29:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var array = [name]
+  return kittens.concat(array)
+}
+
+function prependKitten(name) {
+  var array = [name]
+  return array.concat(kittens)
+}
+
+function removeLastKitten() {
+  var array = kittens.slice(0, kittens.length - 1)
+  return array
+}
+
+function removeFirstKitten() {
+  var array = kittens.slice(1, kittens.length)
+  return array
+}
