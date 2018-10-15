9ee65bf9d72f232e10568782f5f98d9e87e6c577
elisa stater <elisa_60@msn.com>
2018-10-15 15:13:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function kittens (){
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens;
+}
+  
+function destructivelyAppendKitten() {
+  console.log(kittens.push("Ralph"));
+  return kittens;
+
+}
+function destructivelyPrependKitten () {
+  console.log(kittens.unshift('Bob'));
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  console.log(kittens.pop("Garfield"));
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  console.log(kittens.shift("Milo"));
+  return kittens;
+}
+
+
+function appendKitten() {
+ var newkittens = kittens.concat(["Broom"]);
+ console.log(newkittens);
+ return newkittens;
+}
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function removeLastKitten() {
+   var newkittens = kittens.slice(-0,2);
+ console.log(newkittens);
+ return newkittens;
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function removeFirstKitten() {
+   var newkittens = kittens.slice(-2);
+ console.log(newkittens);
+ return newkittens;
+}
+var kittens = ["Milo", "Otis", "Garfield"];
+function prependKitten() {
+var newkittens = (kittens);
+newkittens.splice(0, 0, 'Arnold');
+
+ console.log(newkittens.slice(1,4));
+ return newkittens;
+}
+
+
