439bc5d200a53fd8625b49dd4f0b2c4031d3f418
Kah Yap <kahyap13@gmail.com>
2018-10-19 04:05:40 +0000

    Done.

--- index.js
+++ index.js
@@ -32,3 +32,7 @@ function prependKitten(name) {
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1)
 }
+
+function removeFirstKitten() {
+  return kittens.slice(1, kittens.length)
+}
