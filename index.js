2d347b1a96aaa5c517ff45c0a3c5c3793220cb00
David Sollesse <davidsollesse@gmail.com>
2018-10-31 09:47:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+// rip [`Cheshire`, `Felix`, `Long`]
 
 // Add your functions and code here
+
+function destructivelyAppendKitten() {
+  kittens.push("Ralph")
+  return
+}
+//verb = function.
+//+1 beginning
+
+function destructivelyPrependKitten() {
+  kittens.unshift("Bob");
+}
+//-1 beginning
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
