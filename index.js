175b7086d0fd3fe05b357e6c7ae1c2946c2bdb68
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-07 06:34:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] 
 
 // Add your functions and code here
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+ 
+ function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  }
+
+function destructivelyRemoveFirstKitten(){
+   kittens.shift();
+  }
+
+  function appendKitten(name){
+  var newArr = kittens.slice(0);
+  newArr.push(name);
+  return newArr;
+  }
+
+ function prependKitten(name){
+    var newArr = kittens.slice(0);
+    newArr.unshift(name);
+    return newArr;
+  }
+
+function removeLastKitten(){
+  var len = kittens.length - 1;
+  var newArr = kittens.slice(0,len);
+  return newArr;
+  }
+
+function removeFirstKitten(){
+  var newArr = kittens.slice(1);
+  return newArr;
+    }
