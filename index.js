58411dcfe5080987636c13d22bbff0c656d280c5
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-11 10:07:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
