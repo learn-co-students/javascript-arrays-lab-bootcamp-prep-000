51ac729549ddf70062996c5ec21e7bf6585a20ff
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-11 10:52:11 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ function destructivelyRemoveFirstKitten(){
   kittens.shift();
   return kittens;
 }
+
+function appendKitten(name){
+  var newKitten = [...kittens, "Broom"];
+  return newKitten;
+}
+
+function prependKitten(name){
+  var newKitten = ["Arnold", ...kittens];
+  return newKitten;
+}
+
+function removeLastKitten(){
+  var newArr = kittens.slice(0, kittens.length - 1);
+  return newArr;
+}
+
+function removeFirstKitten(){
+  var newArr = kittens.slice(1);
+  return newArr;
+}
