48b59ad1c773a305e06958111af4ffe7c82c1068
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 19:40:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var newKittenArray = kittens.concat()
+  newKittenArray.push(name)
+  return newKittenArray
+}
+
+function prependKitten(name) {
+  var newKittenArray = kittens.concat()
+  newKittenArray.unshift(name)
+  return newKittenArray
+}
+
+function removeLastKitten () {
+  var newKittenArray = kittens.concat()
+  newKittenArray.pop()
+  return newKittenArray
+}
+
+function removeFirstKitten () {
+  var newKittenArray = kittens.concat()
+  newKittenArray.shift()
+  return newKittenArray
+}
