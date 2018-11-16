a429e344be7064576a9cff222e1e2f1c6d3f7885
William Robert Simpson <will.simpson85@gmail.com>
2018-11-16 00:49:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(kitten){
+  return kittens.push(kitten)
+
+}
+function destructivelyPrependKitten(kitten){
+  return kittens.unshift(kitten)
+}
+function destructivelyRemoveLastKitten(kitten){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(kitten){
+  return kittens.shift()
+}
+
+function appendKitten(kitten){
+  return kittens.concat(kitten)
+}
+
+function prependKitten(kitten){
+  return [kitten,...kittens];
+}
+function removeLastKitten(kitten){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(kitten){
+  return kittens.slice(1)
+}
