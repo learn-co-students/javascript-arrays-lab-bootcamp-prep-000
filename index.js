83c450fd8f9bfa60c66fb99038c71c458b69f263
evolfo <spampinatodesign@gmail.com>
2018-12-01 22:16:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 // Add your functions and code here
+
+function destructivelyAppendKitten() {
+	kittens.push('Ralph');
+	return kittens;
+}
+
+function destructivelyPrependKitten() {
+	kittens.unshift('Bob');
+	return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+	kittens.pop();
+	return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+	kittens.shift();
+	return kittens;
+}
+
+function appendKitten() {
+	var appKitten = kittens.concat("Broom");
+	return appKitten;
+}
+
+function prependKitten() {
+	var preKitten = ['Arnold'].concat(kittens);
+	return preKitten;
+}
+
+function removeLastKitten() {
+	var removeKitten = kittens.slice(0, 2);
+	return removeKitten;
+}
+
+function removeFirstKitten() {
+	var removeKitten = kittens.slice(1);
+	return removeKitten;
+}
