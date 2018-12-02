7afaeab9aa2107de2b2be54cb3f69a83bf33ec64
Leonardo Peralta <leo.pm15@gmail.com>
2018-12-02 08:27:22 +0000

    Done.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return (kittens.slice(0, 2));
+  return (kittens.slice(0, kittens.length - 1));
 }
 
 function removeFirstKitten() {
