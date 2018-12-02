6be9626af78f57bed958c12cf7d7e15a36e229bf
Leonardo Peralta <leo.pm15@gmail.com>
2018-12-02 08:24:07 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name];
 }
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return (kittens.slice(0, 2));
+}
+
+function removeFirstKitten() {
+  return (kittens.slice(1));
+}
