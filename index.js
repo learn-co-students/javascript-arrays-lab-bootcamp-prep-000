ead4af51005c5a1bde6e199ed767ece46a1208f9
Paris Dolfman <parisdolfman@gmail.com>
2018-12-03 20:30:39 +0000

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name)
   return kittens
 }
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+ return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, -1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1)
+}
