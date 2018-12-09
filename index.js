a2464a41f68ed1a4d8878e1feeee70f8ed250090
Lerena Holloway <holloway.lerena@gmail.com>
2018-12-09 22:06:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
