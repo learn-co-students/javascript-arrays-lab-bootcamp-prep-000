62fe8f7d32e116b28c7a2386ca09b6961c5c445c
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-12-10 20:55:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+var kittens = ["Milo", "Otis", "Garfield"];
  function destructivelyAppendKitten(name){
-    var kittens = ["Milo", "Otis", "Garfield"];
     kittens.push(name);
-    return kittens;
+    
   }
   
   
