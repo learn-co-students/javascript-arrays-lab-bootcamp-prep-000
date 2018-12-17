058ac7b2523375d95a9a807e34d3986572f478b0
Jaclyn Setina <jaclyn.setina@gmail.com>
2018-12-17 02:00:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
