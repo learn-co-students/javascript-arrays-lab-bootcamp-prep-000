e1186f06a1226c80135aa5685106775e98189541
Errol W <ewatsonrei@gmail.com>
2018-12-18 03:24:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+}
 
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  [kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  ["Arnold", kittens];
+}
+
+function removeLastKitten() {
+  kittens.slice(2);
+}
+
+function removeFirstKitten() {
+  kittens.slice(1);
 }
 
 //define your array here
