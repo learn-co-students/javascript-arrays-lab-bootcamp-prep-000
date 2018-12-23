f8f63cbd91c7e966058ae83afa2f50faa77a7cb8
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-23 17:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"]  //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
