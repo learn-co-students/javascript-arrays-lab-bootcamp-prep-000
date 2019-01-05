603a9c459694837ce522e67d0463b39b1ee30e1c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-01-05 04:36:26 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ function destructivelyAppendKitten(name) {
   return kittens;
 }
 
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
 
 /*
  describe('kittens', function() {
