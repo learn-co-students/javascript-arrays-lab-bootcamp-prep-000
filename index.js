07e4effb80145b28d82b7f7ebc89798748d2530c
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-01-15 06:13:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-var name = Broom
+var name = Purrrcy
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
   kittens.push(name);
-  return kittens;
+  return kittens
 }
+
 function destructivelyPrependKitten(name){
   kittens.unshift(name);
-  return kittens;
+  return kittens
 }
+
 function destructivelyRemoveLastKitten(){
-  return kittens.pop();
+  kittens.pop();
+  return kittens
 }
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
 function destructivelyRemoveFirstKitten(){
-  return kittens.shift();
+  kittens.shift();
+  return kittens
 }
-function appendKitten(name){
-  return kittens.push("Broom");
-}
-function appendKitten(name){
-  return kittens.push(name);
-}
-function appendKitten(name){
-    return [...kittens, name];
-}
+
 function prependKitten(name){
-    return [name,...kittens];
+  return [name,...kittens]
 }
-function removeFirstKitten(name){
-    return kittens.slice(1);
-}
+
 function removeLastKitten(){
-    return kittens.slice(0, kittens.length - 1);
+  return kittens.slice(0,kittens.length-1)
 }
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
+
+
+
+
+
