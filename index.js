f80e93e66263f1acbaf1ea73472c50c1f92a85e1
Jonathan Koss <jonathan_koss@yahoo.com>
2019-01-16 16:47:25 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ function destructivelyPrependKitten(name) {
   return name;
 }
 
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
+
 /*
  describe('kittens', function() {
     it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
