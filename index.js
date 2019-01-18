9513619f4007c0440804b6f44b570f92bdb5f613
Peter Choi <jc4883@columbia.edu>
2019-01-18 00:43:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+ var toReturn = new Array()
+ toReturn.push(...kittens)
+ toReturn.push(name)
+ return toReturn
+}
+
+function prependKitten(name) {
+  var toReturn = new Array() 
+  toReturn.push(name)
+  toReturn.push(...kittens)
+  return toReturn
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(name) {
+  return kittens.slice(1, kittens.length)
+}
