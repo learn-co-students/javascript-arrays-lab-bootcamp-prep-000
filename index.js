859985c5e24b4a06c6f499d4d57d9d2226d8ccd9
Fernando Martinez <martinezfer@gmail.com>
2019-01-19 23:40:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; 
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){ 
+kittens.push(name) ;
+return kittens ;
+} 
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyPrependKitten(name){ 
+kittens.unshift(name) ;
+return kittens ;
+} 
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveLastKitten(name){ 
+kittens.pop(name) ;
+return kittens ;
+} 
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyRemoveFirstKitten(name){ 
+kittens.shift(name) ;
+return kittens ;
+} 
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function appendKitten(name){
+  var newArray = [...kittens];
+  newArray.push(name);
+  return newArray;
+}
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function prependKitten(name){
+  var newArray = [...kittens];
+  newArray.unshift(name);
+  return newArray;
+}
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function removeLastKitten(name){
+  var newArray = [...kittens];
+  newArray.pop(name);
+  return newArray;
+}
+
+kittens = ['Milo', 'Otis', 'Garfield'];
+function removeFirstKitten(name){
+  var newArray = [...kittens];
+  newArray.shift(name);
+  return newArray;
+}
+
+
+
+
+
