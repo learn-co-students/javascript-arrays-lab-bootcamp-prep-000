5ab38e0afbda15721c9e968b49dc1b1d249d6aaa
convivial-system-4430 <cdneal091@gmail.com>
2019-01-24 22:45:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+  return(kittens);
+}
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+  return(kittens);
+}
+
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+  return(kittens);
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+  return(kittens);
+}
+
+function appendKitten(name)
+{
+  var safeKitten = [...kittens, name];
+  return(safeKitten);
+}
+
+function prependKitten(name)
+{
+  var safeKitten = [name,...kittens];
+  return(safeKitten);
+}
+
+function removeLastKitten()
+{
+  var safeKitten = kittens.slice(0, kittens.length -1);
+  return(safeKitten);
+}
+
+function removeFirstKitten()
+{
+  var safeKitten = kittens.slice(1);
+  return(safeKitten);
+}
