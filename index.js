c10551285cb5f50e7bb5448c1867700924f6fbee
kendall willard <kendallwillard@hotmail.com>
2019-01-27 21:53:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var result = [...kittens, name]
+  return result
+}
+
+function prependKitten(name) {
+  var result = [name, ...kittens]
+  return result
+}
+
+function removeLastKitten() {
+  var result = kittens
+  return result.slice(0, result.length - 1)
+}
+
+function removeFirstKitten() {
+  var result = kittens
+  return result.slice(1)
+}
