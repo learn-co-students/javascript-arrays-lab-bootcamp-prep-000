c753c87a3bc7eb167c77d73a8bbe176bbb51f3a4
Sherief Farid <sherief@farid.cloud>
2019-02-03 05:14:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+    return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittens2 = kittens
+  return kittens2.concat(name)
+}
+
+function prependKitten(name){
+  var kittens2 = [name]
+  return kittens2.concat(kittens)
+}
+
+function removeLastKitten(){
+  var kittens2 = kittens
+  return kittens2.slice(0,kittens2.length-1)
+
+}
+
+function removeFirstKitten(){
+  var kittens2=kittens
+  return kittens2.slice(1)
+}
