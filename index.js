ab02aa801c367007399b110a3014843a449a639f
L D <tapircode@gmail.com>
2019-02-16 16:29:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function appendKitten(name){
+  let newKittens = kittens.concat(name);
+  return newKittens;
+}
