64521d49f357c17853abb8b85a1d23c9bf19fc5e
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-17 20:22:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
-
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+var kitty = "Ralph"
+var kit = "Snow"
+function destructivelyAppendKitten(kitty) {
+  return kittens.push(kitty);
+}
+function destructivelyPrependKitten(kitty) {
+  return kittens.unshift(kitty);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+function appendKitten(kit) {
+  return [...kittens, kit];
+}
+function prependKitten(kit) {
+  return [kit, ...kittens]
+}
+function removeLastKitten() {
+  return kittens.slice(0,2)
+}
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
