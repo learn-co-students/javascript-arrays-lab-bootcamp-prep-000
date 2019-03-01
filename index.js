1f5b464c9670fa726b56c4fc0e7b4def9e963e52
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:38:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  kittens.push(name)
+  kittens.push(name);
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.unshift(name)
+  kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop()
+  kittens.pop();
 }
 
 function destructivelyRemoveFirstKitten() {
-  kittens.shift()
+  kittens.shift();
 }
 
 function appendKitten(name) {
-  return [...kittens, name]
+  return [...kittens, name];
 }
 
 function prependKitten(name) {
-  return [name, ...kittens]
+  return [name, ...kittens];
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens.slice(1);
 }
