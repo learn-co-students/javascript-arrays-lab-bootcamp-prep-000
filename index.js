f475b0e11c0fbcd69962f93b2d18883ce30f37ae
rational-dispatch-0384 <baja903@gmail.com>
2019-04-21 16:30:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name){
+kittens.unshift("Bob");  
+}
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+kittens.shift();
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name]
+  return newArray;
+}
+
+function prependKitten(name){
+  var newArray = [name, ...kittens]
+  return newArray;
+}
+
+function removeLastKitten(){
+  var newArray = kittens.slice(0, 2);
+  return newArray;
+}
+
+function removeFirstKitten(){
+  var newArray = kittens.slice(1, 3);
+  return newArray;
+}
