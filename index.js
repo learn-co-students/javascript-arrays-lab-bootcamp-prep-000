7227941eff4c312e81c211ea11515288448821a0
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-28 00:52:09 +0000

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,38 @@ kittens.push(name)
 return kittens 
 }
 
-  
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+return kittens
+}
 
+function destructivelyRemoveLastKitten(name){
+kittens.pop(name)
+return kittens
+}
 
+function destructivelyRemoveFirstKitten(name){
+kittens.shift(name)
+return kittens
+}
 
-// Add your functions and code here
+function appendKitten(name){
+return [...kittens,"Broom"]
+}
+
+function prependKitten(name){
+return [name,...kittens]
+}
+
+function removeLastKitten(name){
+kittens.slice(-1,kittens.length-1)
+return kittens
+}
+
+function removeLastKitten(name){
+ return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(name){
+return kittens.slice(1)
+}
