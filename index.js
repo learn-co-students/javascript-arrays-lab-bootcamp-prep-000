40886ca297c99de934b36ee00b7d668be5f8c49e
Marco Frisina <marco@frisina.xyz>
2019-05-03 12:52:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var newKitten = [...kittens, name];
+  return newKitten;
+
+}
+
+function prependKitten(name) {
+  var newKitten = [name, ...kittens];
+  return newKitten;
+}
+
+function removeLastKitten(name) {
+  var newKitten = kittens.slice(0, kittens.length - 1);
+  return newKitten;
+}
+
+function removeFirstKitten(name) {
+  var newKitten = kittens.slice(1);
+  return newKitten;
+}
