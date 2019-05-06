d44bb7640ea220961a468a856d5d0e34f2fca689
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-06 02:29:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis", "Garfield"] 
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+   kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+ var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens1 = [name, ...kittens]
+  return kittens1
+}
+
+function removeLastKitten() {
+  var kittens3 = kittens.slice(0, kittens.length - 1)
+  return kittens3
+}
+
+function removeFirstKitten() {
+  var kittens4 = kittens.slice(1)
+  return kittens4
+}
+
