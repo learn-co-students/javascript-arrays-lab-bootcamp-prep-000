13b9e4e5cb9595f2d568ce3e691dd99f6b27fa33
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-22 17:15:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
