7772542d2f10d42d7f0769f56b8d0b43d93a51bf
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-30 12:54:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name) {
+ return[...kittens, name]
+}
+
+function prependKitten(name){
+  return[name, ...kittens]
+}
