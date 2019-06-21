40ed6954618800523cdf7f3715f5e17e2e64ca07
Tish Chungoora <tishal82@gmail.com>
2019-06-21 21:39:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+//define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; 
 
 // Add your functions and code here
 
 function destructivelyAppendKitten(kittenElement) {
-  kittens.push(kittenElement)
-  return kittens
+  kittens.push(kittenElement);
+  return kittens;
 }
 
 function destructivelyPrependKitten(kittenElement) {
-  kittens.unshift(kittenElement)
-  return kittens
+  kittens.unshift(kittenElement);
+  return kittens;
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop()
-  return kittens
+  kittens.pop();
+  return kittens;
 }
 
 function destructivelyRemoveFirstKitten() {
-  kittens.shift()
-  return kittens
+  kittens.shift();
+  return kittens;
 }
 
 function appendKitten(kittenElement) {
-  return [...kittens, kittenElement]
+  return [...kittens, kittenElement];
 }
 
 function prependKitten(kittenElement) {
-  return [kittenElement, ...kittens]
+  return [kittenElement, ...kittens];
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens.slice(1);
 }
