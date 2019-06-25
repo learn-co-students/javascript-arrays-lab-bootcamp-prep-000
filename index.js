761aef52913f0b213023dc8a4ba4500601d977dd
Ping Ping <ping.ping.lu@hotmail.com>
2019-06-25 22:27:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] 
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  return kittens.concat("Broom")
+}
+
+function prependKitten(name) {
+  return ["Arnold",...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,2)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
