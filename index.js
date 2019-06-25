87c34f3691b9a976d9f37d643fa22fce8676134d
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-25 13:45:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name){
+  let kitten2 = [name];
+  var allKittens = kittens.concat(kitten2);
+  return allKittens
+}
+
+function prependKitten(name){
+  let moreKittens = [name];
+  var evenMoreKittens = moreKittens.concat(kittens);
+  return evenMoreKittens;
+}
+
+function removeLastKitten(){
+  var noLastKitten = kittens.slice(0, 2);
+  return noLastKitten;
+}
+
+function removeFirstKitten(){
+  var noFirstKitten = kittens.slice(1);
+  return noFirstKitten;
+}
