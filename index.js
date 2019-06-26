5f1013e72daa44402617497fdda29ecf218b1ddb
Richard Carpino <carpino82@gmail.com>
2019-06-26 11:54:12 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   return kittens.pop()
 }
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+function appendKitten(name){
+  return [...kittens, name]
+}
+function prependKitten(name){
+  return [name, ...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length - 1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+
+}
