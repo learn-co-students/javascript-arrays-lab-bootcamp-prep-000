20140d5e660da56d179d09f7b734fb360bce5004
Seeta Rajpara <seeta_rajpara@alumni.brown.edu>
2019-07-02 02:25:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph")
+}
 
-// Add your functions and code here
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(kittens[3])
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift(kittens[0])
+}
+
+var newKitten = "Broom"
+
+function appendKitten() {
+  return kittens.concat(newKitten)
+}
+
+var prependKittenName = ["Arnold", ]
+
+function prependKitten() {
+  return prependKittenName.concat(kittens)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0 , -1)
+}
