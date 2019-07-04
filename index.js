ed1dddc1d63454bf73cdf0f42d302eb8406646a1
Bello Osagie <osagienoah04@gmail.com>
2019-07-04 21:12:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var kittens = new Array("Milo", "Otis", "Garfield") //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
-
 function destructivelyAppendKitten(name) {
   kittens.push(name)
   console.log(kittens)
