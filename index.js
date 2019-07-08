dd69d5e7018e6738a21a82edea102a1752026b46
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-08 16:32:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+var destructivelyAppendKitten = (el) => kittens.push(el)
+var destructivelyPrependKitten = el => kittens.unshift(el)
+var destructivelyRemoveLastKitten = () => kittens.pop()
+var destructivelyRemoveFirstKitten = () => kittens.shift()
+var appendKitten = (el) => kittens.concat(el)
+var prependKitten = (el) => [el, ...kittens]
+var removeLastKitten = () => kittens.slice(0, -1)
+var removeFirstKitten = () => kittens.slice(1)
