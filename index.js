cb1d09b249014938482d493c2409e9c8dbccbdcb
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-21 23:37:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
-var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
+//Step One: Define kittens as an array
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
+//Step Two:Arrays destructivelyAppendKitten(name) appends a kitten to the end of the kittens array:
 function destructivelyAppendKitten(name){
-return kittens.push(name);
- 
- 
+  kittens.push(name);
+  return kittens
 }
 
-function destructivelyPrependKitten(name) {
- return  kittens.unshift(name);
-  
+//Step Three: Arrays destructivelyPrependKitten(name) prepends a kitten to the beginningof the kittens array
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens
 }
 
-function destructivelyRemoveLastKitten() {
- return kittens.pop(name)
-  
+//Step Four: Arrays destructivelyRemoveLastKitten() removes the last kitten from the kittens array:
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
 }
 
-function destructivelyRemoveFirstKitten(){
-  return kittens.shift(name);
+//Step Five: Arrays destructivelyRemoveFirstKitten() removes the First kitten from the kittens array:
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
 }
 
+//Step Six: Arrays appendKitten(name) appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
 function appendKitten(name){
- return  [...kittens, name];
+return [...kittens, name];
   
 }
 
+//Step Seven:Arrays prependKitten(name) prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
 function prependKitten(name){
-  return [name,...kittens];
+ return [name, ...kittens];
 }
 
+//Step Eight: Arrays removeLastKitten() removes the last kitten in the kittens array andreturns a new array, leaving the kittens array unchanged:
 function removeLastKitten(){
-  return kittens.slice(0, kittens.length - 1);
-  
+  return kittens.slice(0, kittens.length -1);
 }
 
+//Step Nine: Arrays removeFirstKitten() removes the first kitten from the kittens arrayand returns a new array, leaving the kittens array unchanged:
 function removeFirstKitten(){
- return kittens.slice(1);
+  return kittens.slice(1);
   
 }
