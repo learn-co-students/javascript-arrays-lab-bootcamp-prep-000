5fa3d3bc405119aa3bd642edfc55d1a55232d9ac
Chris Lyons <chris@lyonsgroup.family>
2019-07-22 16:23:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-var kittens = [] //define your array here
+var kittens = ["Milo","Otis","Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  window.kittens= [...window.kittens, name];
+}
+
+function destructivelyPrependKitten(name) {
+  window.kittens= [name, ...window.kittens];
+}
+
+function destructivelyRemoveLastKitten() {
+  window.kittens.pop();
+  return window.kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  window.kittens.shift();
+  return window.kittens;
+}
+
+function appendKitten(name) {
+  var a=window.kittens.slice(0);
+  a.push(name);
+  return a;
+}
+
+function prependKitten(name) {
+  return [name, ...window.kittens];
+}
+
+function removeLastKitten() {
+  var a=window.kittens.slice(0);
+  a.pop();
+  return a;
+}
+
+function removeFirstKitten() {
+  var a=window.kittens.slice(0);
+  a.shift();
+  return a;
+}
