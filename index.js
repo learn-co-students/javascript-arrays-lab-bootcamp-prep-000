758d7934f4b160418969aa5747558d029b44a8c0
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-22 23:21:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+//define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  [...kittens,name];
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  [name, ...kittens];
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+ kittens.slice(0, kittens.length -1);
+  return kittens.slice(0, kittens.length -1);
+}
+
+function removeFirstKitten() {
+  kittens.slice(1)
+  return kittens.slice(1);
+}
