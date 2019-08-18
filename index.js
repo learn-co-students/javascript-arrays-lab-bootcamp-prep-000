8a6acf5f3cba33d1180466a58dc8533c1c99b8dc
Vanessa Corlette <nessacor16@gmail.com>
2019-08-18 04:50:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = [`Milo`, `Otis`, `Garfield`]
 
-function destructivelyAppendKitten('Ralph') {
+function destructivelyAppendKitten(`Ralph`) {
   kittens.push(Ralph)
   return kittens;
 }
