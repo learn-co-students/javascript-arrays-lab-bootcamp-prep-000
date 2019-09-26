18e9964917fb05512d086335270f29d12fcc9252
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-26 03:59:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return kittens.push("Ralph")
+  return kittens.push("Ralph");
 }
 
 function destructivelyPrependKitten(name) {
-  return kittens.unshift("Bob")
+  return kittens.unshift("Bob");
 }
 
 function destructivelyRemoveLastKitten() {
-  return kittens.pop()
+  return kittens.pop();
 }
 
 function destructivelyRemoveFirstKitten() {
-  return kittens.shift()
+  return kittens.shift();
 }
 
 function appendKitten(name) {
-  return kittens.concat("Broom")
+  return kittens.concat("Broom");
 }
 
 function prependKitten(name) {
-  return kittens.unshift("Arnold")
+  var kitty = ["Arnold"];
+ 
+  return kitty.concat(kittens);
 }
 
 function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens.slice(1);
 }
 
 function removeLastKitten() {
-  return kittens.pop()
+  return kittens.slice(0, kittens.length - 1);
 }
