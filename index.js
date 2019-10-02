5b2eb16ef3c54d0817938294f4a1e79c463761d4
remi.odufuye <remi.odufuye@gmail.com>
2019-10-02 15:09:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] ;
+
+
+//define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  
+  kittens.push(name) ;
+  return kittens ; 
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+  kittens.unshift(name);
+  return kittens ; 
+}
+
+function destructivelyRemoveLastKitten() {
+  
+  
+ kittens = kittens.slice(0, kittens.length - 1) ;
+ return kittens ; 
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+  kittens.shift() ;
+  return kittens ; 
+}
+
+function appendKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"] ;
+  var kittens = kittens ; 
+  kittens = [...kittens , name]
+  return kittens ; 
+}
+
+function prependKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"] ;
+  var kittens = kittens ; 
+  kittens = [name , ...kittens]
+  return kittens ; 
+}
+
+
+function removeLastKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"] ;
+  kittens = kittens ; 
+  kittens = kittens.slice(0, kittens.length - 1) ;
+  return kittens ; 
+}
+
+
+ 
+function removeFirstKitten() {
+  var kittens = ["Milo", "Otis", "Garfield"] ;
+  kittens = kittens ; 
+  kittens = kittens.slice(1);
+  return kittens ;
+  
+}
+
+
+
+
+
+
+
+
+
