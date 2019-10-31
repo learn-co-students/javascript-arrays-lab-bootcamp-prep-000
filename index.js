f31bcf0368f21aa2c5f229c38c8984aeeb90a986
olusola olagoroye <solaolagoroye@gmail.com>
2019-10-31 18:59:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph");
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob");
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+function appendKitten(name){
+  
+  return [...kittens,"Broom"];
+}
+function prependKitten(name){
+  kittens = ["Arnold", ...kittens];
+ return kittens;
+}
