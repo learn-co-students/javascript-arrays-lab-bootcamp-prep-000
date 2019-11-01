3e7d1acf9b6430fce2d7a97642299bbfd1b9a914
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-01 01:27:30 +0000

    Done.

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function appendKitten(name){
   return [...kittens,"Broom"];
 }
 function prependKitten(name){
-  kittens = ["Arnold", ...kittens];
- return kittens;
+  
+ return ["Arnold", ...kittens];
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length -1);
+}
+function removeFirstKitten(){
+  return kittens.slice(1);
 }
