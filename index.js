9a0d8c0086b082ccd4fb5aa9abffd257942bd840
April Escobar <aprilfaith.e@gmail.com>
2019-11-29 05:33:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
@@ -26,7 +28,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, length - 1)
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten() {
