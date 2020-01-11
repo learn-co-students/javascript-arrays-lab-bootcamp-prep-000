82447334a01d2486ae26fabc28617dda833c366d
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 06:01:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+
+kittens = [ "Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push('Ralph');
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift('Bob');
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens
+}
+
+function appendKitten(name){
+  return kittens.concat("Broom")
+}
+
+function prependKitten(name){
+  var sKitten = ["Arnold"]
+  return sKitten.concat(kittens)
+  
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+  
+}
+
+function removeFirstKitten(){
+  return kittens.slice(-2)
+}
