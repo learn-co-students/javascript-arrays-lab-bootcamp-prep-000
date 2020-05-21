adeb336dbe40d2e736c91a0563e2d28eb126036e
SamDump <samokoder@yandex.ru>
2020-05-21 20:00:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+
+function removeLastKitten() {
+  return kittens.slice(0, -1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
