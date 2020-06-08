950fb327ef117a08f0cff2ba3960972883f25e7d
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-08 02:20:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+ return kittens.appendKitten()
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function removeLastKitten() {
+  return kittens.pop(...kittens, "Garfield")
+}
