8a0dd6727a54688d1b937348f048cdf08155d783
Tracy Ngo <tngo1206@gmail.com>
2020-06-22 16:53:13 -0700

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = [] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
