2be4f7028f6cd341902acef85f20160b9819ba43
Tracy Ngo <tngo1206@gmail.com>
2020-06-23 09:13:51 -0700

    Done.

--- index.js
+++ index.js
@@ -24,3 +24,11 @@ function appendKitten(name){
 function prependKitten(name){
   return [name, ...kittens]
 }
+
+function removeLastKitten(){
+  return kittens.slice(-1)
+}
+
+function removeFirstKitten(){
+
+}
