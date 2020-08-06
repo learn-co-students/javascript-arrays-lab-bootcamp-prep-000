8a7e933b62b1a4a06465dc22919bfadeb5ba35c0
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 20:04:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){kittens.push(name); return kittens}
+function destructivelyPrependKitten(name){kittens.unshift(name); return kittens}
+function destructivelyRemoveLastKitten(){return kittens.pop()}
+function destructivelyRemoveFirstKitten(){return kittens.shift()}
+function appendKitten(name){return kittens.concat(name)}
+function prependKitten(name){return [name, ...kittens]}
+function removeLastKitten(){return kittens.slice(0, kittens.length -1)}
+function removeFirstKitten(){return kittens.slice(1)}
