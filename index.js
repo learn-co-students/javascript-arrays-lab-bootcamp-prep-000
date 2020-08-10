d4d66cbb0c6bc6e2f87c0fc9d948ac52752f6abd
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-10 21:11:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
-appendKitten()
+appendKitten();
 
-function destructivelyAppendKitten() {
-  appendKitten
+function destructivelyAppendKitten(name) {
+kittens.push("Ralph");
 }
-// Add your functions and code here
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
