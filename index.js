39bb2d62bea5464448402f238f89668ac6ea0826
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-28 23:31:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return(kittens);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return(kittens);
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop();
+  return(kittens);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift();
+  return(kittens);
+}
+
+function appendKitten(name) {
+  return[...kittens, "Broom"];
+}
+
+function prependKitten(name) {
+  return["Arnold", ...kittens];
+}
+
+function removeLastKitten() {
+  return(kittens.slice(0, length -1));
+}
+
+function removeFirstKitten() {
+  return(kittens.slice(1));
+}  
+
+
+
+
+
+
