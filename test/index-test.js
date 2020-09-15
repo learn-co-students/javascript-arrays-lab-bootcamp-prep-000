862614d7e9d2d36245d80a8a7f82fcf9224fd27a
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-15 12:28:05 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-describe('Arrays', function() {
+ describe('Arrays', function() {
   describe('kittens', function() {
     it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
