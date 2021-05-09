6e3f612c908ffa7ae4de25f97209a98caeedd372
daring-demo-aacc <azizrocke@gmail.com>
2021-05-09 17:09:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  return [...kittens,name]
+  
+}
+function prependKitten(name){
+  return [name,...kittens]
+}  
+function removeLastKitten(){
+  var new_kittens  = kittens.slice(0,-1)
+  return new_kittens
+}
+function removeFirstKitten(){
+  var new_kittens = kittens.slice(1)
+  return new_kittens
+}
